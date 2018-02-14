e74547b2f4aae886ceb4c93628e1be8bdf618f39
Dominik Gabi <dominik@fb.com>
2018-02-14 14:30:43 -0800

    forgot one `.stdout`
    Summary: shamecube
    
    Reviewed By: sinancepel, shannonzhu
    
    Differential Revision: D6992924
    
    fbshipit-source-id: 8ec33c97c408fe01b531f6a8b35c77c73efe3f29

--- scripts/commands.py
+++ scripts/commands.py
@@ -446,7 +446,7 @@ class Incremental(ErrorHandling):
     def _read_stderr(self, _stream, source_directory):
         stderr_file = os.path.join(
             source_directory,
-            '.pyre/server/server.stdout')
+            '.pyre/server/server.stderr')
         with subprocess.Popen(
                 ['tail', '-f', stderr_file],
                 stdout=subprocess.PIPE,
