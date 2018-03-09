3a52bfca8840a5572153dcc45a53c48f1f86788c
Marco Leogrande <mleogrande@fb.com>
2018-03-08 21:47:43 -0800

    Add packaging script for OSS builds
    Summary: Add a new packaging script in Pyre. This script allows the creation of sdist and bdist_wheel artifacts.
    
    Reviewed By: dkgi
    
    Differential Revision: D7171208
    
    fbshipit-source-id: 9d5d40016cb530b04957b9a6930111adab80de83

---
+++ scripts/build-pypi-package.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Copyright (c) 2018-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -e
+
+# global constants
+PACKAGE_NAME="pyre-check"
+PACKAGE_VERSION="0.0.1"
+AUTHOR='Facebook'
+AUTHOR_EMAIL='mleogrande@fb.com'
+MAINTAINER='Facebook'
+MAINTAINER_EMAIL='mleogrande@fb.com'
+
+# helpers
+die() {
+  printf '%s: %s, exiting.\n' "$(basename "$0")" "$*" >&2
+  exit 1
+}
+error_trap () {
+  die "Command '${BASH_COMMAND}' failed at ${BASH_SOURCE[0]}:${BASH_LINENO[0]}"
+}
+
+trap error_trap ERR
+
+# create build tree
+SCRIPTS_DIRECTORY="$(dirname "$(readlink -f "$0")")"
+cd "${SCRIPTS_DIRECTORY}/"
+BUILD_ROOT="${SCRIPTS_DIRECTORY}/buildroot"
+rm -rf "${BUILD_ROOT}"
+mkdir "${BUILD_ROOT}"
+cd "${BUILD_ROOT}"
+
+# copy source files
+mkdir "${PACKAGE_NAME}"
+cp "${SCRIPTS_DIRECTORY}"/*.py "${BUILD_ROOT}/${PACKAGE_NAME}"
+
+# copy binary files
+BINARY_FILE="${SCRIPTS_DIRECTORY}/../_build/all/main.native"
+if [[ ! -f "${BINARY_FILE}" ]]; then
+  echo "The binary file ${BINARY_FILE} does not exist."
+  echo "Have you run 'make' in the toplevel directory?"
+  exit 1
+fi
+mkdir -p "${BUILD_ROOT}/bin"
+cp "${BINARY_FILE}" "${BUILD_ROOT}/bin"
+
+# create setup.py file
+cat > "${BUILD_ROOT}/setup.py" <<HEREDOC
+from setuptools import setup, find_packages
+
+setup(
+    name='${PACKAGE_NAME}',
+    version='${PACKAGE_VERSION}',
+    description='A performant type checker for Python',
+
+    url='https://github.com/facebookexperimental/pyre-check',
+    author='${AUTHOR}',
+    author_email='${AUTHOR_EMAIL}',
+    maintainer='${MAINTAINER}',
+    maintainer_email='${MAINTAINER_EMAIL}',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='typechecker development',
+
+    packages=find_packages(exclude=['tests']),
+    data_files=[('bin', ['bin/main.native'])],
+    python_requires='>=3',
+)
+HEREDOC
+
+# build
+python setup.py sdist bdist_wheel
+
+# copy artifacts outside of the the build directory
+mkdir -p "${SCRIPTS_DIRECTORY}/dist"
+cp "${BUILD_ROOT}"/dist/* "${SCRIPTS_DIRECTORY}/dist/"
+
+# cleanup
+cd "${SCRIPTS_DIRECTORY}"
+rm -rf "${BUILD_ROOT}"
+
+printf '\nAll done. Build artifacts are available in: %s/dist/\n' "${SCRIPTS_DIRECTORY}"
+exit 0
