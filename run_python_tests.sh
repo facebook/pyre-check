3aaa3d3783becef1eb65f3f2720367511fe07580
Marco Leogrande <mleogrande@fb.com>
2018-05-13 05:51:11 -0700

    Add script and makefile target to run Python tests
    Summary: There was no easy way to run our Python tests. Now there is one.
    
    Reviewed By: dkgi
    
    Differential Revision: D7983096
    
    fbshipit-source-id: 01601a341cfbcca70f0582864f261d27c3d0aea2

---
+++ run_python_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright (c) 2018-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -e
+
+if ! which python3 &>/dev/null; then
+   echo 'Could not find a python3 interpreter to run tests'
+   exit 2
+fi
+
+echo "  Using interpreter at $(which python3) with version: $(python3 --version)"
+
+echo '  Enumerating test files:'
+files=$(find scripts -name '*_test.py')
+echo "${files}"
+if [[ -z "${files}" ]]; then
+  echo 'No test files found, exiting.'
+  exit 2
+fi
+
+echo '  Running all tests:'
+echo "${files}" | sed 's/.py$//' | sed 's:/:.:g' | xargs python3 -m unittest -v
