39fbefc7301400321b1b9ff29722434ebd6d540c
Rijnard Van Tonder <rvt@fb.com>
2018-06-25 19:40:56 -0700

    simplify abstract domain tests
    Reviewed By: dkgi
    
    Differential Revision: D8629634
    
    fbshipit-source-id: ac8dd77c0acd1c1a942500e0fd8963dc9f46c835

--- analysis/test/abstractDomainTest.ml
+++ analysis/test/abstractDomainTest.ml
@@ -11,99 +11,111 @@ open Analysis
 
 module type AbstractDomainUnderTest = sig
   include AbstractDomain.S
-  val bottom : t
+
+  val bottom: t
+
   (* A non-comparable list of values. *)
-  val unrelated : t list
+  val unrelated: t list
+
   (* More values that may be comparable with each other and unrelated *)
-  val values : t list
+  val values: t list
 end
 
-let build_cartesian_prod l1 l2 =
-  List.fold_right
-    ~f:(fun v1 acc ->
-        List.fold_right
-          ~f:(fun v2 acc -> (v1, v2)::acc)
-          ~init:acc l2)
-    ~init:[] l1
 
 (* General functor that tests abstract domains. *)
-module TestAbstractDomain(D : AbstractDomainUnderTest) = struct
+module TestAbstractDomain(Domain: AbstractDomainUnderTest) = struct
 
-  let create_test v ~f =
-    let v_s = D.show v in
-    v_s >:: (f v)
+  let create_test value ~f =
+    (Domain.show value) >:: (f value)
 
   let values =
-    (D.bottom :: D.unrelated)
-    |> List.rev_append D.values
+    (Domain.bottom :: Domain.unrelated)
+    |> List.rev_append Domain.values
 
-  let test_basic v _ =
-    assert_bool "Bottom not <=" (D.less_or_equal ~left:D.bottom ~right:v);
-    assert_bool "non symmetric" (D.less_or_equal ~left:v ~right:v)
+  let test_basic value _ =
+    assert_bool "Bottom not <=" (Domain.less_or_equal ~left:Domain.bottom ~right:value);
+    assert_bool "non symmetric" (Domain.less_or_equal ~left:value ~right:value)
 
-  let test_bot_top _ =
-    assert_bool "Bottom not bottom" (D.is_bottom D.bottom)
+  let test_bottom_top _ =
+    assert_bool "Bottom not bottom" (Domain.is_bottom Domain.bottom)
 
   let test_basic_values =
     List.map ~f:(create_test ~f:test_basic) values
 
   let test_cartesian ~title ~f values =
+    let test_values index (v1, v2) =
+      let name =
+        Format.sprintf
+          "%s@%d: %s with %s"
+          title
+          index
+          (Domain.show v1)
+          (Domain.show v2)
+      in
+      (name >:: (fun _ -> f v1 v2))
+    in
     List.mapi
-      ~f:(fun index (v1, v2) ->
-          let v1_s = D.show v1 in
-          let name = Printf.sprintf "%s@%d: %s with %s" title index
-                       v1_s
-                       (D.show v2)
-          in
-          (name >:: (fun _ -> f v1 v2)))
-      (build_cartesian_prod values values)
+      (List.cartesian_product values values)
+      ~f:test_values
 
-  (* Checks that the provided example D.unrelated are unrelated. *)
+  (* Checks that the provided example Domain.unrelated are unrelated. *)
   let test_diff_unrelated =
-    test_cartesian ~title:"unrelated"
-      ~f:(fun v1 v2 ->
-          if not (phys_equal v1 v2) then begin
-            assert_bool "v1 <= v2" (not (D.less_or_equal ~left:v1 ~right:v2));
-            assert_bool "v2 <= v1" (not (D.less_or_equal ~left:v2 ~right:v1))
-          end)
-      D.unrelated
+    let test_values v1 v2 =
+      if not (phys_equal v1 v2) then
+        begin
+          assert_bool "v1 <= v2" (not (Domain.less_or_equal ~left:v1 ~right:v2));
+          assert_bool "v2 <= v1" (not (Domain.less_or_equal ~left:v2 ~right:v1))
+        end
+    in
+    test_cartesian ~title:"unrelated" ~f:test_values Domain.unrelated
 
   let test_join_conformance v1 v2 =
-    let join = D.join v1 v2 in
-    let join_s = D.show join in
-    assert_bool "join >= v1" (D.less_or_equal ~left:v1 ~right:join);
-    assert_bool "join >= v2" (D.less_or_equal ~left:v2 ~right:join);
-    assert_bool (Printf.sprintf "join reflexivity: %s" join_s) (D.less_or_equal ~left:join ~right:join);
-    let r_join = D.join v2 v1 in
-    let r_join_s = D.show r_join in
-    assert_bool (Printf.sprintf "join symmetry: %s, %s" join_s r_join_s)
-      (D.less_or_equal ~left:join ~right:r_join && D.less_or_equal ~left:r_join ~right:join)
+    let join = Domain.join v1 v2 in
+    let join_s = Domain.show join in
+    assert_bool "join >= v1" (Domain.less_or_equal ~left:v1 ~right:join);
+    assert_bool "join >= v2" (Domain.less_or_equal ~left:v2 ~right:join);
+    assert_bool
+      (Format.sprintf "join reflexivity: %s" join_s)
+      (Domain.less_or_equal ~left:join ~right:join);
+    let right_join = Domain.join v2 v1 in
+    let right_join_s = Domain.show right_join in
+    assert_bool
+      (Format.sprintf "join symmetry: %s, %s" join_s right_join_s)
+      (Domain.less_or_equal ~left:join ~right:right_join
+       && Domain.less_or_equal ~left:right_join ~right:join)
 
   let test_widen_conformance ~iteration v1 v2 =
-    let join = D.join v1 v2 in
-    let join_s = D.show join in
-    let widen = D.widen ~iteration ~previous:v1 ~next:v2 in
-    let widen_s = D.show widen in
-    let msg = Printf.sprintf "iteration=%d:" iteration in
-    assert_bool (Printf.sprintf "%s: widen >= v1" msg) (D.less_or_equal ~left:v1 ~right:widen);
-    assert_bool (Printf.sprintf "%s: widen >= v2" msg) (D.less_or_equal ~left:v2 ~right:widen);
-    assert_bool (Printf.sprintf "%s: widen symmetry: %s" msg widen_s) (D.less_or_equal ~left:widen ~right:widen);
-    assert_bool (Printf.sprintf "%s: join <= widen, %s <= %s" msg join_s widen_s) (D.less_or_equal ~left:join ~right:widen)
+    let join = Domain.join v1 v2 in
+    let join_s = Domain.show join in
+    let widen = Domain.widen ~iteration ~previous:v1 ~next:v2 in
+    let widen_s = Domain.show widen in
+    let message = Format.sprintf "iteration=%d:" iteration in
+    assert_bool
+      (Format.sprintf "%s: widen >= v1" message)
+      (Domain.less_or_equal ~left:v1 ~right:widen);
+    assert_bool
+      (Format.sprintf "%s: widen >= v2" message)
+      (Domain.less_or_equal ~left:v2 ~right:widen);
+    assert_bool
+      (Format.sprintf "%s: widen symmetry: %s" message widen_s)
+      (Domain.less_or_equal ~left:widen ~right:widen);
+    assert_bool
+      (Format.sprintf "%s: join <= widen, %s <= %s" message join_s widen_s)
+      (Domain.less_or_equal ~left:join ~right:widen)
 
   let test_joins =
-    test_cartesian ~title:"join"
-      ~f:test_join_conformance
-      values
+    test_cartesian ~title:"join" ~f:test_join_conformance values
 
   let test_widens ~iteration =
-    test_cartesian ~title:(Printf.sprintf "widen(%d)" iteration)
+    test_cartesian
+      ~title:(Format.sprintf "widen(%d)" iteration)
       ~f:(test_widen_conformance ~iteration)
       values
 
   (* The test suite created by this functor. *)
   let suite =
     test_basic_values
-    |> List.rev_append [ "test_bot_top" >:: test_bot_top]
+    |> List.rev_append [ "test_bottom_top" >:: test_bottom_top]
     |> List.rev_append test_diff_unrelated
     |> List.rev_append test_joins
     |> List.rev_append (test_widens ~iteration:0)
@@ -114,16 +126,22 @@ module TestAbstractDomain(D : AbstractDomainUnderTest) = struct
 end
 
 (* Build up abstract domains to test. *)
-module StringSet : AbstractDomainUnderTest = struct
+module StringSet: AbstractDomainUnderTest = struct
   include AbstractSetDomain.Make(String)
-  let top = None
-  let unrelated = List.map ~f:singleton ["a"; "b"; "c"]
+
+  let top =
+    None
+
+  let unrelated =
+    List.map ~f:singleton ["a"; "b"; "c"]
+
   let values =
-    List.map ~f:(fun (a, b) -> union a b)
-      (build_cartesian_prod unrelated unrelated)
+    List.cartesian_product unrelated unrelated
+    |> List.map ~f:(Tuple2.uncurry union)
+
   let values =
-    List.map ~f:(fun (a, b) -> union a b)
-      (build_cartesian_prod values values)
+    List.cartesian_product values values
+    |> List.map ~f:(Tuple2.uncurry union)
     |> List.dedup_and_sort ~compare
 end
 
@@ -132,9 +150,12 @@ module TestStringSet = TestAbstractDomain(StringSet)
 module IntToStringSet = struct
   include AbstractMapDomain.Make(Int)(StringSet)
 
-  let top = None
+  let top =
+    None
+
   (* Test that maps are strict in bottom elements. *)
-  let bottom = set empty ~key:1 ~data:StringSet.bottom
+  let bottom =
+    set empty ~key:1 ~data:StringSet.bottom
 
   (* Builds maps from i -> string sets, where all unrelated string elements are in the range
      except for one, and keys are from [0,n), n being the number of unrelated string elements.
@@ -144,26 +165,31 @@ module IntToStringSet = struct
      [ 2 -> { "a" }, 0 -> { "b" } ]
   *)
   let rotation =
-    let len = List.length TestStringSet.values in
+    let length = List.length TestStringSet.values in
     let populate offset =
       let result = ref empty in
-      List.iteri ~f:(fun i v ->
-          if i = offset then () (* skip *)
-          else
-            result := set (!result) ~key:((i + offset) mod len) ~data:v)
-        StringSet.unrelated;
+      let build_set index value =
+        if index = offset then
+          () (* skip *)
+        else
+          result := set !result ~key:((index + offset) mod length) ~data:value
+      in
+      List.iteri StringSet.unrelated ~f:build_set;
       !result
     in
-    let rec build i acc =
-      if i < len then
-        populate i :: build (i + 1) acc
+    let rec build index accumulator =
+      if index < length then
+        populate index :: build (index + 1) accumulator
       else
-        acc
+        accumulator
     in
     build 0 []
 
-  let unrelated = rotation
-  let values = []
+  let unrelated =
+    rotation
+
+  let values =
+    []
 end
 
 module TestIntToStringSet = TestAbstractDomain(IntToStringSet)
@@ -172,14 +198,16 @@ module PairStringMapIntToString = struct
   include AbstractPairDomain.Make(StringSet)(IntToStringSet)
 
   let unrelated =
-    List.fold_left
-      ~f:(fun l v1 ->
-          List.fold_left ~f:(fun l v2 -> make v1 v2 :: l) ~init:l
-            IntToStringSet.unrelated
-        )
-      ~init:[] StringSet.unrelated
+    let fold_sets accumulator v1 =
+      List.fold
+        IntToStringSet.unrelated
+        ~init:accumulator
+        ~f:(fun accumulator v2 -> make v1 v2 :: accumulator)
+    in
+    List.fold StringSet.unrelated ~init:[] ~f:fold_sets
 
-  let values = []
+  let values =
+    []
 end
 
 module TestPair = TestAbstractDomain(PairStringMapIntToString)
@@ -187,22 +215,32 @@ module TestPair = TestAbstractDomain(PairStringMapIntToString)
 module IntSet = struct
   include AbstractSetDomain.Make(Int)
 
-  let top = None
+  let top =
+    None
 
-  let unrelated = [singleton 1; singleton 2; singleton 3]
+  let unrelated =
+    [singleton 1; singleton 2; singleton 3]
+
   let values =
-    List.map ~f:(fun (a, b) -> union a b)
-      (build_cartesian_prod unrelated unrelated)
+    List.map
+      ~f:(Tuple2.uncurry union)
+      (List.cartesian_product unrelated unrelated)
+
   let values =
-    List.map ~f:(fun (a, b) -> union a b)
-      (build_cartesian_prod values values)
+    List.map
+      ~f:(Tuple2.uncurry union)
+      (List.cartesian_product values values)
     |> List.dedup_and_sort ~compare
 end
 
 module TestIntSet = TestAbstractDomain(IntSet)
 
-(* Do the testing. *)
-let _ = run_test_tt_main ("flat string" >::: TestStringSet.suite)
-let _ = run_test_tt_main ("int -> flat string" >::: TestIntToStringSet.suite)
-let _ = run_test_tt_main ("string x (int -> flat string)" >::: TestPair.suite)
-let _ = run_test_tt_main ("int set" >::: TestIntSet.suite)
+
+let () =
+  "abstractDomainTest">:::[
+    "flat_string">:::TestStringSet.suite;
+    "map_int_to_flat_string">:::TestIntToStringSet.suite;
+    "string_x_maps_int_to_flat_string">:::TestPair.suite;
+    "int_set">:::TestIntSet.suite;
+  ]
+  |> run_test_tt_main
