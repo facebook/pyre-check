809cec62ae2be5d7c32118992115498aa721cd73
Rijnard Van Tonder <rvt@fb.com>
2018-07-12 13:42:01 -0700

    forward/backwards taint module pass
    Reviewed By: dkgi
    
    Differential Revision: D8813722
    
    fbshipit-source-id: d3ee3ed8acacad633480c0dbe11ecf5113d0c0dc

--- taint/taintBackwardAnalysis.ml
+++ taint/taintBackwardAnalysis.ml
@@ -7,6 +7,7 @@ open Core
 
 open Analysis
 open Ast
+open Expression
 open Statement
 open TaintDomains
 open TaintAccessPath
@@ -40,6 +41,7 @@ module rec FixpointState : FixpointState = struct
   }
   [@@deriving show]
 
+
   let initial_taint =
     let result_taint = BackwardTaint.add BackwardTaint.empty TaintSinks.LocalReturn in
     BackwardState.assign
@@ -48,13 +50,16 @@ module rec FixpointState : FixpointState = struct
       (BackwardState.make_leaf result_taint)
       BackwardState.empty
 
+
   let create () = {
     taint = BackwardState.empty;
     models = [];
   }
 
+
   let less_or_equal ~left:{ taint = left; _ } ~right:{ taint = right; _ } =
     BackwardState.less_or_equal ~left ~right
+
 
   let join { taint = left; models } { taint = right; _ } =
     let taint = BackwardState.join left right in
@@ -63,6 +68,7 @@ module rec FixpointState : FixpointState = struct
       models;  (* There should be no joining at class/file level. *)
     }
 
+
   let widen ~previous:{ taint = previous; _ } ~next:{ taint = next; models; } ~iteration =
     let taint = BackwardState.widen ~iteration ~previous ~next in
     {
@@ -70,54 +76,60 @@ module rec FixpointState : FixpointState = struct
       models;  (* There should be no joining at class/file level. *)
     }
 
+
   let extract_taint_in_taint_out_model parameters entry_taint =
     let filter_to_local_return taint =
       BackwardTaint.filter ~f:((=) TaintSinks.LocalReturn) taint
     in
-    let extract_taint_in_taint_out position model { Node.value = parameter } =
+    let extract_taint_in_taint_out position model { Node.value = { Parameter.name; _ } } =
       let taint_in_taint_out_taint =
-        BackwardState.read (Root.Variable parameter.Parameter.name) entry_taint
+        BackwardState.read (Root.Variable name) entry_taint
         |> BackwardState.filter_map_tree ~f:filter_to_local_return
       in
       if not (BackwardState.is_empty_tree taint_in_taint_out_taint) then
-        let parameter = Root.(Parameter { name = parameter.Parameter.name; position; }) in
+        let parameter = Root.(Parameter { name; position }) in
         BackwardState.assign ~root:parameter ~path:[] taint_in_taint_out_taint model
       else
         model
     in
     List.foldi parameters ~f:extract_taint_in_taint_out ~init:BackwardState.empty
 
-  let get_taint ap state =
-    match ap with
-    | None -> BackwardState.empty_tree
-    | Some { root; path; } ->
-        BackwardState.read_access_path ~root ~path state.taint
 
-  let store_weak_taint ~root ~path taint state =
-    { state with taint = BackwardState.assign_weak ~root ~path taint state.taint; }
+  let get_taint access_path { taint; _ } =
+    match access_path with
+    | None ->
+        BackwardState.empty_tree
+    | Some { root; path } ->
+        BackwardState.read_access_path ~root ~path taint
 
-  let store_weak_taint_opt ap taint state =
-    match ap with
+
+  let store_weak_taint ~root ~path taint ({ taint = state_taint } as state) =
+    { state with taint = BackwardState.assign_weak ~root ~path taint state_taint }
+
+
+  let store_weak_taint_option access_path taint state =
+    match access_path with
     | None -> state
     | Some (root, path) -> store_weak_taint ~root ~path taint state
 
-  let rec analyze_argument taint arg state =
-    analyze_expression taint arg.Argument.value state
 
-  and analyze_normalized_expression state taint e =
-    match e with
+  let rec analyze_argument taint { Argument.value = argument } state =
+    analyze_expression taint argument state
+
+  and analyze_normalized_expression state taint expression =
+    match expression with
     | Access { expression; member } ->
         let field = TaintAccessPathTree.Label.Field member in
         let taint =
           BackwardState.assign_tree_path [field] ~tree:BackwardState.empty_tree ~subtree:taint
         in
-        analyze_normalized_expression state taint e
+        analyze_normalized_expression state taint expression
     | Call { callee = Access { expression = receiver; member = method_name}; arguments; } ->
         (* TODO: figure out the BW and TAINT_IN_TAINT_OUT model for whatever is called here. *)
         (* Member access. Don't propagate the taint to the member, skip to the receiver. *)
         let state = List.fold_right ~f:(analyze_argument taint) arguments ~init:state in
         analyze_normalized_expression state taint receiver
-    | Call { callee; arguments; } ->
+    | Call { callee; arguments } ->
         (* TODO: figure out the BW and TAINT_IN_TAINT_OUT model for whatever is called here. *)
         (* For now, we propagate taint to all args implicitly, and to the function. *)
         let state = List.fold_right ~f:(analyze_argument taint) arguments ~init:state in
@@ -127,45 +139,51 @@ module rec FixpointState : FixpointState = struct
     | Identifier name ->
         store_weak_taint ~root:(Root.Variable name) ~path:[] taint state
 
-  and analyze_expression taint (expr : Expression.t) state =
-    match expr.Node.value with
-    | Expression.Access a ->
-        normalize_access a
+  and analyze_expression taint { Node.value = expression; _ } state =
+    match expression with
+    | Access access ->
+        normalize_access access
         |> analyze_normalized_expression state taint
-    | Expression.Await _
-    | Expression.BooleanOperator _
-    | Expression.Bytes _
-    | Expression.ComparisonOperator _
-    | Expression.Complex _
-    | Expression.Dictionary _
-    | Expression.DictionaryComprehension _
-    | Expression.False
-    | Expression.Float _
-    | Expression.FormatString _
-    | Expression.Generator _
-    | Expression.Integer _
-    | Expression.Lambda _
-    | Expression.List _
-    | Expression.ListComprehension _
-    | Expression.Set _
-    | Expression.SetComprehension _
-    | Expression.Starred _
-    | Expression.String _
-    | Expression.Ternary _
-    | Expression.True
-    | Expression.Tuple _
-    | Expression.UnaryOperator _
-    | Expression.Yield _ -> state
+    | Await _
+    | BooleanOperator _
+    | Bytes _
+    | ComparisonOperator _
+    | Complex _
+    | Dictionary _
+    | DictionaryComprehension _
+    | False
+    | Float _
+    | FormatString _
+    | Generator _
+    | Integer _
+    | Lambda _
+    | List _
+    | ListComprehension _
+    | Set _
+    | SetComprehension _
+    | Starred _
+    | String _
+    | Ternary _
+    | True
+    | Tuple _
+    | UnaryOperator _
+    | Yield _ ->
+        state
 
-  let analyze_expression_opt taint e state =
-    match e with
+
+  let analyze_expression_option taint expression state =
+    match expression with
     | None -> state
-    | Some e -> analyze_expression taint e state
+    | Some expression -> analyze_expression taint expression state
 
-  let analyze_definition ({ Define.name; _ } as define) state =
+
+  let analyze_definition ~define:({ Define.name; parameters } as define) ({ models; _ } as state) =
     let cfg = Cfg.create define in
-    let initial = { taint = initial_taint; models = []; } in
-    let result = Analyzer.backward ~cfg ~initial |> Analyzer.entry in
+    let initial = { taint = initial_taint; models = [] } in
+    let result =
+      Analyzer.backward ~cfg ~initial
+      |> Analyzer.entry
+    in
     match result with
     | None ->
         Log.log
@@ -173,25 +191,25 @@ module rec FixpointState : FixpointState = struct
           "Definition %s did not produce result for entry node."
           (Log.Color.yellow (Access.show name));
         state
-    | Some result ->
-        let taint_in_taint_out_model =
-          extract_taint_in_taint_out_model define.Record.Define.parameters result.taint in
-        let model = { define_name=define.name; taint_in_taint_out=taint_in_taint_out_model; } in
-        { state with models = model :: List.rev_append result.models state.models }
+    | Some { taint; models = new_models } ->
+        let taint_in_taint_out_model = extract_taint_in_taint_out_model parameters taint in
+        let model = { define_name = name; taint_in_taint_out = taint_in_taint_out_model; } in
+        { state with models = model :: List.rev_append new_models models }
+
 
   let analyze_statement state statement =
     Log.log ~section:`Taint "Backward state: %s" (Log.Color.yellow (show state));
     match statement with
-    | Assign { target; annotation; value; parent } ->
+    | Assign { target; value; _ } ->
         let access_path = of_expression target in
-        analyze_expression_opt (get_taint access_path state) value state
+        analyze_expression_option (get_taint access_path state) value state
     | Assert _
     | Break
     | Class _
     | Continue ->
         state
     | Define define ->
-        analyze_definition define state
+        analyze_definition ~define state
     | Delete _
     | Expression _
     | For _
@@ -201,11 +219,11 @@ module rec FixpointState : FixpointState = struct
     | Nonlocal _
     | Pass
     | Raise _ -> state
-    | Return { expression=Some expression; _ } ->
-        let ap = { root = Root.LocalResult; path = []; } in
-        let return_taint = get_taint (Some ap) state in
+    | Return { expression = Some expression; _ } ->
+        let access_path = { root = Root.LocalResult; path = [] } in
+        let return_taint = get_taint (Some access_path) state in
         analyze_expression return_taint expression state
-    | Return { expression=None; _ }
+    | Return { expression = None; _ }
     | Stub _
     | Try _
     | With _
@@ -214,18 +232,20 @@ module rec FixpointState : FixpointState = struct
     | YieldFrom _ ->
         state
 
+
   let backward state ~statement:({ Node.value = statement; _ }) =
     analyze_statement state statement
 
+
   let forward ?key:_ state ~statement =
     failwith "Don't call me"
+
 
   let show_models = function
     | None ->
         "no result."
     | Some result ->
         String.concat ~sep:"\n" (List.map ~f:show_model result.FixpointState.models)
-
 end
 
 
--- taint/taintForwardAnalysis.ml
+++ taint/taintForwardAnalysis.ml
@@ -7,6 +7,7 @@ open Core
 
 open Analysis
 open Ast
+open Expression
 open Statement
 open TaintDomains
 open TaintAccessPath
@@ -71,15 +72,19 @@ module rec FixpointState : FixpointState = struct
   }
   [@@deriving show]
 
+
   let initial_taint = ForwardState.empty
+
 
   let create ?(models = []) () = {
     taint = ForwardState.empty;
     models;
   }
 
+
   let less_or_equal ~left:{ taint = left } ~right:{ taint = right } =
     ForwardState.less_or_equal ~left ~right
+
 
   let join { taint = left; models; } { taint = right; _ } =
     let taint = ForwardState.join left right in
@@ -88,6 +93,7 @@ module rec FixpointState : FixpointState = struct
       models;  (* There should be no joining at class/file level. *)
     }
 
+
   let widen ~previous:{ taint = previous; _ } ~next:{ taint = next; models; } ~iteration =
     let taint = ForwardState.widen ~iteration ~previous ~next in
     {
@@ -95,29 +101,35 @@ module rec FixpointState : FixpointState = struct
       models;  (* There should be no joining at class/file level. *)
     }
 
+
   let extract_source_model _parameters exit_taint =
     let return_taint = ForwardState.read Root.LocalResult exit_taint in
     ForwardState.assign ~root:Root.LocalResult ~path:[] return_taint ForwardState.empty
 
-  let get_taint_opt ap state =
-    match ap with
-    | None -> ForwardState.empty_tree
+
+  let get_taint_option access_path state =
+    match access_path with
+    | None ->
+        ForwardState.empty_tree
     | Some (root, path) ->
         ForwardState.read_access_path ~root ~path state.taint
 
-  let store_taint ~root ~path taint state =
-    { state with taint = ForwardState.assign ~root ~path taint state.taint; }
 
-  let store_taint_opt ap taint state =
-    match ap with
-    | Some { root; path} -> store_taint ~root ~path taint state
+  let store_taint ~root ~path taint ({ taint = state_taint } as state) =
+    { state with taint = ForwardState.assign ~root ~path taint state_taint }
+
+
+  let store_taint_option access_path taint state =
+    match access_path with
+    | Some { root; path } -> store_taint ~root ~path taint state
     | None -> state
 
-  let rec analyze_argument state taint_accumulator arg =
-    analyze_expression arg.Argument.value state
+
+  let rec analyze_argument state taint_accumulator { Argument.value = argument; _ } =
+    analyze_expression argument state
     |> ForwardState.join_trees taint_accumulator
 
-  and analyze_call state callee arguments =
+  and analyze_call ~callee arguments state =
     match callee with
     | Identifier identifier ->
         let existing_model =
@@ -165,7 +177,7 @@ module rec FixpointState : FixpointState = struct
         in
         taint
     | Call { callee; arguments; } ->
-        analyze_call state callee arguments
+        analyze_call ~callee arguments state
     | Expression expression ->
         analyze_expression expression state
     | Identifier identifier ->
@@ -173,45 +185,48 @@ module rec FixpointState : FixpointState = struct
 
   and analyze_expression expression state =
     match expression.Node.value with
-    | Expression.Access a ->
-        normalize_access a
+    | Access access ->
+        normalize_access access
         |> analyze_normalized_expression state
-    | Expression.Await _
-    | Expression.BooleanOperator _
-    | Expression.Bytes _
-    | Expression.ComparisonOperator _
-    | Expression.Complex _
-    | Expression.Dictionary _
-    | Expression.DictionaryComprehension _
-    | Expression.False
-    | Expression.Float _
-    | Expression.FormatString _
-    | Expression.Generator _
-    | Expression.Integer _
-    | Expression.Lambda _
-    | Expression.List _
-    | Expression.ListComprehension _
-    | Expression.Set _
-    | Expression.SetComprehension _
-    | Expression.Starred _
-    | Expression.String _
-    | Expression.Ternary _
-    | Expression.True
-    | Expression.Tuple _
-    | Expression.UnaryOperator _
-    | Expression.Yield _ ->
+    | Await _
+    | BooleanOperator _
+    | Bytes _
+    | ComparisonOperator _
+    | Complex _
+    | Dictionary _
+    | DictionaryComprehension _
+    | False
+    | Float _
+    | FormatString _
+    | Generator _
+    | Integer _
+    | Lambda _
+    | List _
+    | ListComprehension _
+    | Set _
+    | SetComprehension _
+    | Starred _
+    | String _
+    | Ternary _
+    | True
+    | Tuple _
+    | UnaryOperator _
+    | Yield _ ->
         ForwardState.empty_tree
 
 
-  let analyze_expression_opt expression state =
+  let analyze_expression_option expression state =
     match expression with
     | None -> ForwardState.empty_tree
     | Some expression -> analyze_expression expression state
 
 
-  let analyze_definition ({ Define.name; _  } as define) state =
+  let analyze_definition ~define:({ Define.name; parameters  } as define) state =
     let cfg = Cfg.create define in
-    let result = Analyzer.forward ~cfg ~initial:state |> Analyzer.exit in
+    let result =
+      Analyzer.forward ~cfg ~initial:state
+      |> Analyzer.exit
+    in
     match result with
     | None ->
         Log.log
@@ -219,26 +234,26 @@ module rec FixpointState : FixpointState = struct
           "Definition %s did not produce result for entry node."
           (Log.Color.cyan (Access.show name));
         state
-    | Some result ->
-        let source_model = extract_source_model define.Record.Define.parameters result.taint in
-        let model = { Model.define_name = define.name; source_taint = source_model; } in
-        { state with models = model :: List.rev_append result.models state.models }
+    | Some { taint; models } ->
+        let source_model = extract_source_model parameters taint in
+        let model = { Model.define_name = name; source_taint = source_model } in
+        { state with models = model :: List.rev_append models state.models }
 
 
   let forward ?key:_ state ~statement:({ Node.value = statement; _ }) =
     Log.log ~section:`Taint "Forward state: %s" (Log.Color.cyan (show state));
     match statement with
     | Assign { target; annotation; value; parent } ->
-        let taint = analyze_expression_opt value state in
+        let taint = analyze_expression_option value state in
         let access_path = of_expression target in
-        store_taint_opt access_path taint state
+        store_taint_option access_path taint state
     | Assert _
     | Break
     | Class _
     | Continue ->
         state
     | Define define ->
-        analyze_definition define state
+        analyze_definition ~define state
     | Delete _
     | Expression _
     | For _
@@ -260,14 +275,15 @@ module rec FixpointState : FixpointState = struct
     | YieldFrom _ ->
         state
 
+
   let backward state ~statement =
     failwith "Don't call me"
+
 
   let show_models = function
     | None -> "no result."
     | Some result ->
         String.concat ~sep:"\n" (List.map ~f:Model.show result.FixpointState.models)
-
 end
 
 
