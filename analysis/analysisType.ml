0719a0cb5ec1e63d055d62342d318fac79a737b9
Sinan Cepel <sinancepel@fb.com>
2018-08-07 17:52:57 -0700

    sort lines in analysisType.ml
    Summary:
    Even though we now separate some related items now, I think it's a larger scale benefit to
    be able to look alphabetically for the substitution rather than having to search randomly. It's also consistent
    with the parametric substitution map now.
    
    Reviewed By: rvantonder
    
    Differential Revision: D9201347
    
    fbshipit-source-id: 07fde43f6d7622b794d5b62c4ec8660074a8140c

--- analysis/analysisType.ml
+++ analysis/analysisType.ml
@@ -533,12 +533,15 @@ let primitive_substitution_map =
     Parametric { name = Identifier.create name; parameters = (parameters [] number_of_anys) }
   in
   [
-    "object", Object;
-    "typing.Any", Object;
-    "None", none;
     "$bottom", Bottom;
-    "$unknown", Top;
     "$deleted", Deleted;
+    "$unknown", Top;
+    "None", none;
+    "dict", parametric_anys "dict" 2;
+    "list", list Object;
+    "object", Object;
+    "type", parametric_anys "type" 1;
+    "typing.Any", Object;
     "typing.AsyncGenerator", parametric_anys "typing.AsyncGenerator" 2;
     "typing.AsyncIterable", parametric_anys "typing.AsyncIterable" 1;
     "typing.AsyncIterator", parametric_anys "typing.AsyncIterator" 1;
@@ -546,19 +549,16 @@ let primitive_substitution_map =
     "typing.ContextManager", parametric_anys "typing.ContextManager" 1;
     "typing.Coroutine", parametric_anys "typing.Coroutine" 3;
     "typing.DefaultDict", parametric_anys "collections.defaultdict" 2;
-    "dict", parametric_anys "dict" 2;
     "typing.Dict", parametric_anys "dict" 2;
     "typing.Generator", parametric_anys "typing.Generator" 3;
     "typing.Iterable", parametric_anys "typing.Iterable" 1;
     "typing.Iterator", parametric_anys "typing.Iterator" 1;
-    "list", list Object;
     "typing.List", list Object;
     "typing.Mapping", parametric_anys "typing.Mapping" 2;
     "typing.Sequence", parametric_anys "typing.Sequence" 1;
     "typing.Set", parametric_anys "typing.Set" 1;
     "typing.Tuple", Tuple (Unbounded Object);
     "typing.Type", parametric_anys "type" 1;
-    "type", parametric_anys "type" 1;
   ]
   |> List.map
     ~f:(fun (original, substitute) -> Identifier.create original, substitute)
