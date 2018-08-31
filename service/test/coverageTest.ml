74b65536c0400d33c76a69d2514465b45ef6531c
Sinan Cepel <sinancepel@fb.com>
2018-08-30 18:25:59 -0700

    add copyright to coverageTest
    Reviewed By: dkgi
    
    Differential Revision: D9522072
    
    fbshipit-source-id: abe4e85d0c444450753a41d4946fb0bef5c51be8

--- service/test/coverageTest.ml
+++ service/test/coverageTest.ml
@@ -1,4 +1,11 @@
+(** Copyright (c) 2016-present, Facebook, Inc.
+
+    This source code is licensed under the MIT license found in the
+    LICENSE file in the root directory of this source tree. *)
+
+
 open Core
+
 open OUnit2
 
 open Pyre
