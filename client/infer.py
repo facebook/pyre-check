de504aa83049158dc711e29d2b5f72b655880d7d
Marco Leogrande <mleogrande@fb.com>
2018-10-25 15:38:15 -0700

    Fix infer
    Reviewed By: shannonzhu
    
    Differential Revision: D10859288
    
    fbshipit-source-id: a50c5fbb9c9818dae5f5965e71b56dea129ef585

--- client/infer.py
+++ client/infer.py
@@ -434,6 +434,9 @@ def main():
     parser.add_argument("--debug", action="store_true", help=argparse.SUPPRESS)
     parser.add_argument("--sequential", action="store_true", help=argparse.SUPPRESS)
     parser.add_argument("--strict", action="store_true", help=argparse.SUPPRESS)
+    parser.add_argument(
+        "--run-additional-checks", action="store_true", help=argparse.SUPPRESS
+    )
 
     parser.add_argument("--verbose", action="store_true", help="Enable verbose logging")
     parser.add_argument(
