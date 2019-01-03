b451d7ee5490baab54522246c002ed785a47aadd
Sinan Cepel <sinancepel@fb.com>
2019-01-02 16:05:36 -0800

    split reveal_type tests
    Reviewed By: shannonzhu
    
    Differential Revision: D13569666
    
    fbshipit-source-id: 9abfb13594570125968099b09124e4add71fb774

---
+++ analysis/test/integration/revealTypeTest.ml
@@ -0,0 +1,94 @@
+(** Copyright (c) 2016-present, Facebook, Inc.
+
+    This source code is licensed under the MIT license found in the
+    LICENSE file in the root directory of this source tree. *)
+
+open OUnit2
+open IntegrationTest
+
+
+let test_reveal_type _ =
+  assert_type_errors
+    {|
+      def foo(x: str) -> None:
+        reveal_type(x)
+    |}
+    [
+      "Revealed type [-1]: Revealed type for `x` is `str`.";
+    ];
+
+  assert_type_errors
+    ~debug:false
+    {|
+      def foo(x) -> None:
+        reveal_type(x)
+    |}
+    ["Revealed type [-1]: Revealed type for `x` is `undefined`."];
+  assert_type_errors
+    {|
+      def foo(x: int, y: int) -> None:
+        reveal_type(x + y)
+    |}
+    ["Revealed type [-1]: Revealed type for `x.__add__.(...)` is `int`."];
+  assert_type_errors
+    {|
+      def foo(x: int) -> None:
+
+        reveal_type(int_to_str(x))
+    |}
+    ["Revealed type [-1]: Revealed type for `int_to_str.(...)` is `str`."];
+
+  assert_type_errors
+    {|
+      def foo() -> int:
+        bar, baz = list(range(2))
+        reveal_type(bar)
+        return bar
+    |}
+    ["Revealed type [-1]: Revealed type for `bar` is `int`."];
+
+  assert_type_errors
+    {|
+      def foo(s: typing.Sequence[float]) -> list[float]:
+        l = list(s)
+        bar, baz = l
+        reveal_type(bar)
+        return l
+    |}
+    ["Revealed type [-1]: Revealed type for `bar` is `float`."];
+
+  assert_type_errors
+    {|
+      def foo() -> dict[str, int]:
+        d = dict(a = 1, b = 2)
+        bar = d['a']
+        reveal_type(bar)
+        return d
+    |}
+    ["Revealed type [-1]: Revealed type for `bar` is `int`."];
+
+  assert_type_errors
+    {|
+      def foo(map: typing.Mapping[str, int]) -> dict[str, int]:
+        d = dict(map)
+        bar = d['a']
+        reveal_type(bar)
+        return d
+    |}
+    ["Revealed type [-1]: Revealed type for `bar` is `int`."];
+
+  assert_type_errors
+    {|
+      def foo(t: typing.Iterable[typing.Tuple[str, int]]) -> dict[str, int]:
+        d = dict(t)
+        bar = d['a']
+        reveal_type(bar)
+        return d
+    |}
+    ["Revealed type [-1]: Revealed type for `bar` is `int`."]
+
+let () =
+  "revealType">:::[
+    "reveal_type">::test_reveal_type;
+  ]
+  |> Test.run
--- analysis/test/typeCheckTest.ml
+++ analysis/test/typeCheckTest.ml
@@ -1212,87 +1212,6 @@ let test_coverage _ =
     { Coverage.full = 1; partial = 0; untyped = 1; ignore = 0; crashes = 0 }
 
 
-let test_reveal_type _ =
-  assert_type_errors
-    {|
-      def foo(x: str) -> None:
-        reveal_type(x)
-    |}
-    [
-      "Revealed type [-1]: Revealed type for `x` is `str`.";
-    ];
-
-  assert_type_errors
-    ~debug:false
-    {|
-      def foo(x) -> None:
-        reveal_type(x)
-    |}
-    ["Revealed type [-1]: Revealed type for `x` is `undefined`."];
-  assert_type_errors
-    {|
-      def foo(x: int, y: int) -> None:
-        reveal_type(x + y)
-    |}
-    ["Revealed type [-1]: Revealed type for `x.__add__.(...)` is `int`."];
-  assert_type_errors
-    {|
-      def foo(x: int) -> None:
-
-        reveal_type(int_to_str(x))
-    |}
-    ["Revealed type [-1]: Revealed type for `int_to_str.(...)` is `str`."];
-
-  assert_type_errors
-    {|
-      def foo() -> int:
-        bar, baz = list(range(2))
-        reveal_type(bar)
-        return bar
-    |}
-    ["Revealed type [-1]: Revealed type for `bar` is `int`."];
-
-  assert_type_errors
-    {|
-      def foo(s: typing.Sequence[float]) -> list[float]:
-        l = list(s)
-        bar, baz = l
-        reveal_type(bar)
-        return l
-    |}
-    ["Revealed type [-1]: Revealed type for `bar` is `float`."];
-
-  assert_type_errors
-    {|
-      def foo() -> dict[str, int]:
-        d = dict(a = 1, b = 2)
-        bar = d['a']
-        reveal_type(bar)
-        return d
-    |}
-    ["Revealed type [-1]: Revealed type for `bar` is `int`."];
-
-  assert_type_errors
-    {|
-      def foo(map: typing.Mapping[str, int]) -> dict[str, int]:
-        d = dict(map)
-        bar = d['a']
-        reveal_type(bar)
-        return d
-    |}
-    ["Revealed type [-1]: Revealed type for `bar` is `int`."];
-
-  assert_type_errors
-    {|
-      def foo(t: typing.Iterable[typing.Tuple[str, int]]) -> dict[str, int]:
-        d = dict(t)
-        bar = d['a']
-        reveal_type(bar)
-        return d
-    |}
-    ["Revealed type [-1]: Revealed type for `bar` is `int`."]
-
-
 let test_check _ =
   assert_type_errors
     {|
@@ -1675,7 +1594,6 @@ let () =
     "forward_expression">::test_forward_expression;
     "forward_statement">::test_forward_statement;
     "forward">::test_forward;
-    "reveal_type">::test_reveal_type;
     "check_error_traces">::test_show_error_traces;
     "coverage">::test_coverage;
     "check">::test_check;
