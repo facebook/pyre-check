c28b65a7d7abb2e18e1a4c9e9886f5704de8cc1e
Sinan Cepel <sinancepel@fb.com>
2019-01-02 16:56:32 -0800

    split off error traces test
    Reviewed By: shannonzhu
    
    Differential Revision: D13569953
    
    fbshipit-source-id: 78986d8baf4bf3d2670cd425ace9d60f4f1908c8

---
+++ analysis/test/integration/errorTracesTest.ml
@@ -0,0 +1,214 @@
+(** Copyright (c) 2016-present, Facebook, Inc.
+
+    This source code is licensed under the MIT license found in the
+    LICENSE file in the root directory of this source tree. *)
+
+open OUnit2
+open IntegrationTest
+
+let test_show_error_traces _ =
+  assert_type_errors ~show_error_traces:true
+    "def foo() -> int: return 1.0"
+    [
+      "Incompatible return type [7]: Expected `int` but got `float`. Type `int` expected on line " ^
+      "1, specified on line 1."
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    "def foo() -> str: return"
+    [
+      "Incompatible return type [7]: Expected `str` but got `None`. " ^
+      "Type `str` expected on line 1, specified on line 1."
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    "def foo() -> typing.List[str]: return 1"
+    [
+      "Incompatible return type [7]: Expected `typing.List[str]` but got `int`. Type " ^
+      "`typing.List[str]` expected on line 1, specified on line 1.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      def f() -> dict: return {}
+      def foo() -> typing.Dict[typing.Any, typing.Any]: return f()
+    |}
+    [];
+
+  assert_type_errors ~show_error_traces:true
+    "def foo(): pass"
+    [
+      "Missing return annotation [3]: Returning `None` but no return type is specified. " ^
+      "Type `None` was returned on line 1, return type should be specified on line 1."
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      def foo():
+        return None
+    |}
+    [
+      "Missing return annotation [3]: Returning `None` but no return type is specified. " ^
+      "Type `None` was returned on line 3, return type should be specified on line 2.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      class Foo:
+        attribute: int
+        def __init__(self) -> None:
+          self.attribute = ""
+    |}
+    [
+      "Incompatible attribute type [8]: Attribute `attribute` declared in class `Foo` has type " ^
+      "`int` but is used as type `str`. Attribute `attribute` declared on line 3, incorrectly " ^
+      "used on line 5.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      constant: int
+      def foo() -> None:
+        global constant
+        constant = "hi"
+    |}
+    [
+      "Incompatible variable type [9]: constant is declared to have type `int` but is used as " ^
+      "type `str`. Redeclare `constant` on line 5 if you wish to override the previously " ^
+      "declared type.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      def foo() -> None:
+        a = 1
+        b = 2
+        reveal_type(a + b)
+    |}
+    [
+      "Revealed type [-1]: Revealed type for `a.__add__.(...)` is `int`.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      class Foo:
+        attribute: int
+        def __init__(self) -> None:
+          attribute = 0
+    |}
+    [
+      "Uninitialized attribute [13]: Attribute `attribute` is declared in class `Foo` to have " ^
+      "non-optional type `int` but is never initialized. Attribute `attribute` is declared on " ^
+      "line 3, never initialized and therefore must be `typing.Optional[int]`.";
+    ];
+  assert_type_errors ~show_error_traces:true
+    {|
+      class Foo:
+        attribute = x
+      class Bar:
+        def bar(self) -> str:
+          foo = Foo()
+          foo.attribute = 'string'
+          return foo.attribute
+    |}
+    [
+      "Missing attribute annotation [4]: Attribute `attribute` of class `Foo` has type `str` " ^
+      "but no type is specified. Attribute `attribute` declared on line 3, type `str` deduced " ^
+      "from test.py:7:4.";
+      "Undefined name [18]: Global name `x` is undefined.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      constant = x
+      def foo() -> None:
+        global constant
+        constant = 1
+    |}
+    [
+      "Missing global annotation [5]: Globally accessible variable `constant` has type `int` but " ^
+      "no type is specified. Global variable `constant` declared on line 2, type `int` deduced " ^
+      "from test.py:5:2.";
+      "Undefined name [18]: Global name `x` is undefined.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      constant = x
+      def foo() -> None:
+        global constant
+        constant = "hi"
+        constant = 1
+    |}
+    [
+      "Missing global annotation [5]: Globally accessible variable `constant` has type " ^
+      "`typing.Union[int, str]` but no type is specified. Global variable `constant` declared " ^
+      "on line 2, type `typing.Union[int, str]` deduced from test.py:5:2, test.py:6:2.";
+      "Undefined name [18]: Global name `x` is undefined.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      class Other():
+        attribute = x
+        def foo(self) -> None:
+          self.attribute = 1
+    |}
+    [
+      "Missing attribute annotation [4]: Attribute `attribute` of class `Other` has type " ^
+      "`int` but no type is specified. Attribute `attribute` declared on line 3, " ^
+      "type `int` deduced from test.py:5:4.";
+      "Undefined name [18]: Global name `x` is undefined.";
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      def foo() -> None:
+        global x
+        x = 5
+      def bar() -> None:
+        global x
+        x = "str"
+    |}
+    [
+      "Missing global annotation [5]: Globally accessible variable `x` has type " ^
+      "`typing.Union[int, str]` but no type is specified. Global variable `x` " ^
+      "declared on line 4, type `typing.Union[int, str]` deduced from test.py:4:2, " ^
+      "test.py:7:2.";
+      "Missing global annotation [5]: Globally accessible variable `x` has type " ^
+      "`typing.Union[int, str]` but no type is specified. Global variable `x` " ^
+      "declared on line 7, type `typing.Union[int, str]` deduced from test.py:4:2, " ^
+      "test.py:7:2."
+    ];
+
+  assert_type_errors ~show_error_traces:true
+    {|
+      a: typing.List[float] = [1]
+      b: typing.List[int] = [2]
+      a = b
+    |}
+    [
+      "Incompatible variable type [9]: a is declared to have type `typing.List[float]` but is \
+       used as type `typing.List[int]`. Redeclare `a` on line 4 if you wish to override the \
+       previously declared type.  See https://pyre-check.org/docs/error-types.html#list-and-\
+       dictionary-mismatches-with-subclassing for mutable container errors.";
+    ];
+  assert_type_errors ~show_error_traces:true
+    {|
+      def foo() -> typing.List[float]:
+        l = [1]
+        return l
+    |}
+    [
+      "Incompatible return type [7]: Expected `typing.List[float]` but got `typing.List[int]`. \
+       Type `typing.List[float]` expected on line 4, specified on line 2.  \
+       See https://pyre-check.org/docs/error-types.html#list-and-dictionary-mismatches-with-\
+       subclassing for mutable container errors.";
+    ]
+
+
+let () =
+  "errorTraces">:::[
+    "check_error_traces">::test_show_error_traces;
+  ]
+  |> Test.run
