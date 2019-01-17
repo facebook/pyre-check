b35c3284fc5c6e65f4f2b14549f808fd36a3c42a
Xing Qiao Liu <jerryliu55@fb.com>
2019-01-17 10:32:18 -0800

    Move pysa_taint_parser to pyre/sapp
    Summary:
    This parser massages the pysa static analysis JSON output into a more manageable
    form.
    
    Reviewed By: zlandau
    
    Differential Revision: D13676869
    
    fbshipit-source-id: 6c1c3c07ebaaf4ad305f6dac51e4b1f65d4970d9

---
+++ sapp/lib/pysa_taint_parser.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+"""Parse Pysa/Taint output for Zoncolan processing"""
+
+import logging
+from typing import Any, Dict, Iterable, List, Tuple
+
+import ujson as json
+from tools.sapp import errors
+from tools.sapp.analysis_output import AnalysisOutput
+from tools.sapp.base_parser import (
+    BaseParser,
+    ParseType,
+    log_trace_keyerror_in_generator,
+)
+
+
+log = logging.getLogger()
+
+
+class Parser(BaseParser):
+    """The parser takes a json file as input, and provides a simplified output
+    for the Processor.
+    """
+
+    def __init__(self, repo_dir=None, extractor=None):
+        super(Parser, self).__init__(repo_dir, extractor)
+
+    def parse_raw(self, input):
+        return self._parse(input)
+
+    def parse(self, input: AnalysisOutput) -> Iterable[Dict[str, Any]]:
+        for handle in input.file_handles():
+            for entry in self.parse_handle(handle):
+                yield entry
+
+    def parse_handle(self, handle) -> Iterable[Dict[str, Any]]:
+        data = json.load(handle)
+        config = data["config"]
+        self.repo_dir = config["repo"]
+        results = data["results"]
+        for entry in results:
+            yield from self._parse_by_type(entry)
+
+    def _parse_by_type(self, entry):
+        if entry["kind"] == "model":
+            yield from self._parse_model(entry["data"])
+        elif entry["kind"] == "issue":
+            yield from self._parse_issue(entry["data"])
+
+    @log_trace_keyerror_in_generator
+    def _parse_model(self, json):
+        callable = json["callable"]
+        yield from self._parse_model_sources(callable, json["sources"])
+        yield from self._parse_model_sinks(callable, json["sinks"])
+
+    def _parse_model_sources(self, callable, source_traces):
+        for source_trace in source_traces:
+            port = source_trace["port"]
+            for fragment in self._parse_trace_fragments(
+                "source", source_trace["taint"]
+            ):
+                yield {
+                    "type": ParseType.POSTCONDITION,
+                    "caller": callable,
+                    "callee": fragment["callee"],
+                    "callee_location": fragment["location"],
+                    "filename": fragment["location"]["filename"],
+                    "sources": list(fragment["leaves"]),
+                    "caller_port": port,
+                    "callee_port": fragment["port"],
+                    "type_interval": {},
+                    "features": [],
+                }
+
+    def _parse_model_sinks(self, callable, sink_traces):
+        for sink_trace in sink_traces:
+            port = sink_trace["port"]
+            for fragment in self._parse_trace_fragments("sink", sink_trace["taint"]):
+                yield {
+                    "type": ParseType.PRECONDITION,
+                    "caller": callable,
+                    "callee": fragment["callee"],
+                    "callee_location": fragment["location"],
+                    "filename": fragment["location"]["filename"],
+                    "titos": fragment["titos"],
+                    "sinks": list(fragment["leaves"]),
+                    "caller_port": port,
+                    "callee_port": fragment["port"],
+                    "type_interval": {},
+                    "features": [],
+                }
+
+    @log_trace_keyerror_in_generator
+    def _parse_issue(self, json):
+        issue = {}
+
+        issue["type"] = ParseType.ISSUE
+        issue["code"] = json["code"]
+        issue["line"] = json["line"]
+        issue["callable_line"] = json["callable_line"]
+        issue["start"] = json["start"]
+        issue["end"] = json["end"]
+        issue["callable"] = json["callable"]
+        issue["handle"] = self._generate_issue_master_handle(issue)
+        issue["message"] = json["message"]
+        issue["filename"] = self._extract_filename(json["filename"])
+
+        issue["preconditions"], issue[
+            "final_sinks"
+        ], bw_features = self._parse_issue_traces(json["traces"], "backward", "sink")
+        issue["postconditions"], issue[
+            "initial_sources"
+        ], fw_features = self._parse_issue_traces(json["traces"], "forward", "source")
+        issue["taint_locations"] = []
+        issue["features"] = bw_features + fw_features
+        yield issue
+
+    def _generate_issue_master_handle(self, issue):
+        line = issue["line"] - issue["callable_line"]
+        return self.compute_master_handle(
+            callable=issue["callable"],
+            line=line,
+            start=issue["start"],
+            end=issue["end"],
+            code=issue["code"],
+        )
+
+    def _extract_filename(self, complete_filename):
+        repo_dir = self.repo_dir
+        if repo_dir is None:
+            return complete_filename
+        if not complete_filename.startswith("/"):
+            # already relative
+            return complete_filename
+        if not complete_filename.startswith(repo_dir):
+            raise errors.AIException(
+                "Expected filename ({}) to start with repo_dir ({}). "
+                "Check the --repo-dir option.".format(complete_filename, repo_dir)
+            )
+        repo_dir = repo_dir.rstrip("/")
+        if repo_dir == "":
+            return complete_filename
+        return complete_filename[len(repo_dir) + 1 :]
+
+    def _parse_issue_traces(self, traces, name, leaf_port):
+        for trace in traces:
+            if trace["name"] == name:
+                return self._parse_issue_trace_fragments(leaf_port, trace["roots"])
+        return ([], set(), set())
+
+    def _get_position_or_default(self, json):
+        return json.get("position", {"line": 0, "start": 0, "end": 0})
+
+    def _parse_issue_trace_fragments(self, leaf_port, traces):
+        fragments = []
+        leaf_distances = set()
+        all_features = []
+
+        for trace in traces:
+            for fragment in self._parse_trace_fragment(leaf_port, trace):
+                fragments.append(fragment)
+                leaf_distances.update(fragment["leaves"])
+                all_features.extend(fragment["features"])
+
+        return (fragments, leaf_distances, all_features)
+
+    def _parse_trace_fragments(self, leaf_port, traces):
+        for trace in traces:
+            yield from self._parse_trace_fragment(leaf_port, trace)
+
+    def _parse_trace_fragment(self, leaf_port, trace):
+        # For now we don't have leaf distances.
+        leaves = self._parse_leaves(trace.get("leaves", []))
+        if "root" in trace:
+            yield {
+                "callee": "leaf",
+                "port": leaf_port,
+                "location": trace["root"],
+                "leaves": leaves,
+                "titos": trace.get("tito", []),
+                "features": trace.get("features", []),
+                "type_interval": {},
+            }
+        elif "call" in trace:
+            call = trace["call"]
+            location = call["position"]
+            port = call["port"]
+            resolves_to = call.get("resolves_to", [])
+            length = call.get("length", 0)
+            leaves = [(leaf, length) for (leaf, _) in leaves]
+
+            for resolved in resolves_to:
+                yield {
+                    "callee": resolved,
+                    "port": port,
+                    "location": location,
+                    "leaves": leaves,
+                    "titos": trace.get("tito", []),
+                    "features": trace.get("features", []),
+                    "type_interval": {},
+                }
+
+    def _leaf_name(self, leaf) -> str:
+        return leaf.get("name", leaf["kind"])
+
+    def _parse_leaves(self, leaves) -> List[Tuple[str, int]]:
+        """Returns a list of pairs (leaf_name, distance)"""
+        return [(self._leaf_name(leaf), 0) for leaf in leaves]
---
+++ sapp/lib/tests/pysa_taint_parser_tests.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2016-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+from tools.sapp.analysis_output import AnalysisOutput
+from tools.sapp.base_parser import ParseType
+from tools.sapp.pysa_taint_parser import Parser
+
+
+class TestParser(unittest.TestCase):
+    def setUp(self) -> None:
+        self.parser = Parser()
+        self.json_file = "tools/pyre/facebook/pysa/integration_test/results.json"
+        self.results = None
+
+    def test_basic(self):
+        entry = self._get_issue("hotpot.admin_views.query")
+        self.assertRegex(entry.get("filename"), "hotpot/admin_views.py")
+        self.assertEqual(entry.get("line"), 41)
+        self.assertEqual(entry.get("start"), 44)
+        self.assertEqual(entry.get("end"), 70)
+        self.assertEqual(entry.get("code"), 5001)
+        self.assertEqual(entry.get("initial_sources"), {("UserControlled", 0)})
+        self.assertEqual(entry.get("final_sinks"), {("RemoteCodeExecution", 0)})
+        self.assertEqual(entry.get("postconditions")[0]["callee"], "leaf")
+        self.assertEqual(
+            entry.get("preconditions")[0]["callee"],
+            "hotpot.helper.try_execute_hotpot_query",
+        )
+
+    def _load_results(self):
+        if not self.results:
+            self.results = self.parser.parse(AnalysisOutput.from_file(self.json_file))
+        return self.results
+
+    def _get_issue(self, name):
+        entries = self._load_results()
+        for entry in entries:
+            if entry.get("type") == ParseType.ISSUE:
+                return entry
+        self.assertFalse("No issue for callable with name {} found".format(name))
