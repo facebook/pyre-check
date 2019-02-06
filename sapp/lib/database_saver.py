4ecdf103787703bab429f3164d3beb427cf08788
Xing Qiao Liu <jerryliu55@fb.com>
2019-02-05 16:31:36 -0800

    Add database saver pipeline step
    Summary: The database saver pipeline step uses the `DB` and `BulkSaver` objects to store issue models using very few database sessions.
    
    Reviewed By: zlandau
    
    Differential Revision: D13954093
    
    fbshipit-source-id: 1a9ddb3b289abe777ef246c28de8e0f31228626f

---
+++ sapp/lib/database_saver.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+import logging
+from typing import Optional, Tuple
+
+from tools.sapp.bulk_saver import BulkSaver
+from tools.sapp.db import DB
+from tools.sapp.decorators import disable_gc, log_time
+from tools.sapp.models import (
+    Issue,
+    Postcondition,
+    Precondition,
+    PrimaryKeyGenerator,
+    Run,
+    RunStatus,
+    RunSummary,
+    TraceFrame,
+    TraceFrameAnnotation,
+)
+from tools.sapp.pipeline import PipelineStep, Summary
+from tools.sapp.trace_graph import TraceGraph
+
+
+log = logging.getLogger()
+
+
+class DatabaseSaver(PipelineStep[TraceGraph, RunSummary]):
+    RUN_MODEL = Run
+
+    def __init__(
+        self,
+        database: DB,
+        use_lock: bool = False,
+        primary_key_generator: Optional[PrimaryKeyGenerator] = None,
+    ):
+        self.use_lock = use_lock
+        self.dbname = database.dbname
+        self.database = database
+        self.primary_key_generator = primary_key_generator or PrimaryKeyGenerator()
+        self.bulk_saver = BulkSaver(self.primary_key_generator)
+
+    @disable_gc
+    @log_time
+    def run(
+        self, trace_graph: TraceGraph, summary: Summary
+    ) -> Tuple[RunSummary, Summary]:
+        self.graph = trace_graph
+        self.summary = summary
+
+        self._prep_save()
+        return self._save(), self.summary
+
+    def _prep_save(self):
+        """ Prepares the bulk saver to load the trace graph info into the
+        database.
+        """
+        log.info("Preparing bulk save.")
+        self.graph.update_bulk_saver(self.bulk_saver)
+
+        log.info(
+            "Dropped %d unused preconditions, %d are missing",
+            sum(len(v) for v in self.summary["precondition_entries"].values()),
+            len(self.summary["missing_preconditions"]),
+        )
+
+        log.info(
+            "Dropped %d unused postconditions, %d are missing",
+            sum(len(v) for v in self.summary["postcondition_entries"].values()),
+            len(self.summary["missing_postconditions"]),
+        )
+        del self.summary["postcondition_entries"]
+
+    def _save(self) -> RunSummary:
+        """ Saves bulk saver's info into the databases in bulk.
+        """
+        assert self.summary["run"] is not None, "Must have called process before"
+
+        log.info(
+            "Saving %d issues, %d preconditions, %d postconditions, "
+            "%d trace frames, %d trace annotations",
+            len(self.bulk_saver.get_items_to_add(Issue)),
+            len(self.bulk_saver.get_items_to_add(Precondition)),
+            len(self.bulk_saver.get_items_to_add(Postcondition)),
+            len(self.bulk_saver.get_items_to_add(TraceFrame)),
+            len(self.bulk_saver.get_items_to_add(TraceFrameAnnotation)),
+        )
+
+        with self.database.make_session() as session:
+            pk_gen = self.primary_key_generator.reserve(
+                session, [Run], use_lock=self.use_lock
+            )
+            self.summary["run"].id.resolve(id=pk_gen.get(Run), is_new=True)
+            session.add(self.summary["run"])
+            session.commit()
+
+            run_id = self.summary["run"].id.resolved()
+            self.summary["run"] = None  # Invalidate it
+
+        self.bulk_saver.save_all(self.database, self.use_lock)
+
+        # Now that the run is finished, fetch it from the DB again and set its
+        # status to FINISHED.
+        with self.database.make_session() as session:
+            run = session.query(self.RUN_MODEL).filter_by(id=run_id).one()
+            run.status = RunStatus.FINISHED
+            session.add(run)
+            session.commit()
+            run_summary = run.get_summary()
+
+        run_summary.num_invisible_issues = 0
+        run_summary.num_missing_preconditions = len(
+            self.summary["missing_preconditions"]
+        )
+        run_summary.num_missing_postconditions = len(
+            self.summary["missing_postconditions"]
+        )
+
+        return run_summary
