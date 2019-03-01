21002f7b7cd0742401c40fe6ef0b3b11a41f35d1
Sinan Cepel <sinancepel@fb.com>
2019-02-28 16:52:50 -0800

    make signatures in ast shared memory consistent
    Summary: Ran into this while working on something else.
    
    Reviewed By: dkgi
    
    Differential Revision: D14249849
    
    fbshipit-source-id: 3c8045ad0a3dafbf78c4a6c892f8beecd0b70fac

--- ast/sharedMemory.mli
+++ ast/sharedMemory.mli
@@ -6,32 +6,39 @@
 
 module SymlinksToPaths: sig
   val get: string -> PyrePath.t option
+
   val add: string -> PyrePath.t -> unit
+
   val remove: targets: string list -> unit
 end
 
 module Sources: sig
   val get: File.Handle.t -> Source.t option
+
   val get_for_qualifier: Expression.Access.t -> Source.t option
+
   val add: File.Handle.t -> Source.t -> unit
+
   val remove: handles: File.Handle.t list -> unit
 end
 
 module HandleKeys: sig
   val get: unit -> File.Handle.Set.Tree.t
+
+  val add: handles: File.Handle.Set.Tree.t -> unit
+
   (* Can only be called from the master process. *)
   val clear: unit -> unit
-  val add: handles: File.Handle.Set.Tree.t -> unit
 end
 
 module Modules: sig
-  val add: qualifier: Expression.Access.t -> ast_module: Module.t -> unit
-
-  val remove: qualifiers: Expression.Access.t list -> unit
-
   val get: qualifier: Expression.Access.t -> Module.t option
 
   val get_exports: qualifier: Expression.Access.t -> (Expression.Access.t list) option
+
+  val add: qualifier: Expression.Access.t -> ast_module: Module.t -> unit
+
+  val remove: qualifiers: Expression.Access.t list -> unit
 
   val exists: qualifier: Expression.Access.t -> bool
 end
