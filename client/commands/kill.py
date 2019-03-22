eb5cde2814440ca6ed3a9fd5fd1eb7a0ff5e0ec8
Sinan Cepel <sinancepel@fb.com>
2019-03-22 15:17:16 -0700

    try to remove server sockets when stopping if possible
    Summary: We've had users enter a bad state where they had broken socket symlinks, and were unable to call `pyre restart`. Ensure we clean up the socket files on a kill/restart so that no one tries to connect to an old socket file.
    
    Reviewed By: DSouzaM
    
    Differential Revision: D14584308
    
    fbshipit-source-id: f0a513177c9824b0cfdfc3fe7684a6c7e1c9b50f

--- client/commands/kill.py
+++ client/commands/kill.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
+import os
 import subprocess
 
 from .. import BINARY_NAME
@@ -23,3 +24,20 @@ class Kill(Command):
         # Kills all processes that have the same binary as the one specified
         # in the configuration.
         subprocess.run(["pkill", "{}".format(BINARY_NAME)])
+        server_root = os.path.join(
+            os.path.realpath(self._analysis_directory.get_root()), ".pyre", "server"
+        )
+
+        def _delete_linked_path(link_path: str) -> None:
+            try:
+                actual_path = os.readlink(link_path)
+                os.remove(actual_path)
+            except OSError:
+                pass
+            try:
+                os.unlink(link_path)
+            except OSError:
+                pass
+
+        _delete_linked_path(os.path.join(server_root, "server.sock"))
+        _delete_linked_path(os.path.join(server_root, "json_server.sock"))
--- client/commands/tests/kill_test.py
+++ client/commands/tests/kill_test.py
@@ -4,18 +4,29 @@
 # LICENSE file in the root directory of this source tree.
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, call, patch
 
 from ... import commands  # noqa
 from .command_test import mock_arguments, mock_configuration
 
 
 class KillTest(unittest.TestCase):
+    @patch("os.remove")
+    @patch("os.unlink")
+    @patch("os.readlink", side_effect=["/tmp/actual_socket", "/tmp/json_socket"])
     @patch("os.path.realpath")
     @patch("subprocess.run")
-    def test_kill(self, run, realpath) -> None:
-        realpath.side_effect = ["/test-binary"]
+    def test_kill(self, run, realpath, readlink, unlink, remove) -> None:
+        realpath.return_value = "/test-binary"
         arguments = mock_arguments()
         configuration = mock_configuration()
-        commands.Kill(arguments, configuration, analysis_directory=None).run()
+        analysis_directory = MagicMock()
+        commands.Kill(arguments, configuration, analysis_directory).run()
         run.assert_called_with(["pkill", "pyre.bin"])
+        remove.assert_has_calls([call("/tmp/actual_socket"), call("/tmp/json_socket")])
+        unlink.assert_has_calls(
+            [
+                call("/test-binary/.pyre/server/server.sock"),
+                call("/test-binary/.pyre/server/json_server.sock"),
+            ]
+        )
