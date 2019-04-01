ff314c85c1875848474db8740c3bda6e430b68a4
Jia Chen <grievejia@fb.com>
2019-04-01 11:19:49 -0700

    Add a very simple script to turn profiling event logs into TraceEvent format
    Summary: The output format can be consumed by the [Chrome/Chromium browser](https://www.chromium.org/developers/how-tos/trace-event-profiling-tool) for nicer visualization
    
    Reviewed By: sinancepel
    
    Differential Revision: D14634423
    
    fbshipit-source-id: e9fe16eccf367d67b44ed70e543e716b4b3a9307

---
+++ scripts/trace_event.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2019-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+import json
+import logging
+import sys
+from typing import Any, Dict
+
+
+LOG: logging.Logger = logging.getLogger(__name__)
+
+
+def _translate(event: Dict[str, Any]) -> Dict[str, Any]:
+    name = event["name"]
+    pid = event["pid"]
+    event_type = event["event_type"]
+    timestamp_ms = event["timestamp"]
+    if event_type[0] == "Duration":
+        duration_ms = event_type[1]
+        start_time_ms = timestamp_ms - duration_ms
+        trace_event_type = "X"
+        return {
+            "pid": pid,
+            "ts": start_time_ms * 1000,
+            "ph": trace_event_type,
+            "name": name,
+            "dur": duration_ms * 1000,
+        }
+    else:
+        raise ValueError(f"{event_type[0]}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )
+
+    try:
+        events = [json.loads(line) for line in sys.stdin]
+        translated_events = []
+        for index, event in enumerate(events):
+            try:
+                translated_event = _translate(event)
+                translated_events.append(translated_event)
+            except (KeyError, IndexError) as e:
+                LOG.error(f"Malformed log entry detected on line {index+1}")
+                LOG.exception(f"Unexpected format: {e}")
+            except ValueError as e:
+                LOG.error(f"Malformed log entry on line {index+1}")
+                LOG.exception(f"Unrecognized event type: {e}")
+        final_output = {"traceEvents": translated_events}
+        print(json.dumps(final_output))
+    except json.decoder.JSONDecodeError as e:
+        LOG.error(f"JSON parsing error: {e}")
+        sys.exit(1)
