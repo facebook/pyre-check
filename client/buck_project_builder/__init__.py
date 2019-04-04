cd88e172bd0425e43a6fde614c41cc886a61c0ef
Matt D'Souza <mattdsouza@fb.com>
2019-04-03 17:45:57 -0700

    Support file- and directory-wildcard targets in buck project builder
    Summary: Buck targets can be of the form `//xyz:` (corresponding to all targets in xyz's build file) and `//xyz/...` (corresponding to all targets in all build files within xyz), so these patterns need to be supported within the buck project builder.
    
    Reviewed By: dkgi
    
    Differential Revision: D14757748
    
    fbshipit-source-id: 452cdd3fc7d40e00cae7e923de6c9a6b9a149c25

--- client/buck_project_builder/__init__.py
+++ client/buck_project_builder/__init__.py
@@ -5,8 +5,10 @@
 
 # pyre-strict
 
+import glob
+import os
 from collections import deque
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Optional
 
 from . import parser
 from .parser import BuildTarget
@@ -15,15 +17,10 @@ from .parser import BuildTarget
 Target = NamedTuple("Target", [("build_file_directory", str), ("name", str)])
 
 
-def parse_target(target: str) -> Target:
-    # TODO(T38892701): Handle 'a/b/c/...'- and 'a/b/c:'-style targets
-    split = target.lstrip("//").split(":")
-    return Target(split[0], split[1])
-
-
 class Builder(object):
     def __init__(self, buck_root: str, build_file_name: str = "TARGETS") -> None:
         self.buck_root = buck_root
+        self.build_file_name = build_file_name
         self.parser = parser.Parser(buck_root, build_file_name)
 
     def compute_targets_to_build(self, target: str) -> List[BuildTarget]:
@@ -31,14 +28,15 @@ class Builder(object):
             Compute the set of targets to build for the given target using a
             breadth-first traversal.
         """
-        targets_to_parse = deque([target])
-        targets_seen = {target}
+        normalized_targets = self._normalize_target(target)
+        targets_to_parse = deque(normalized_targets)
+        targets_seen = set(normalized_targets)
         targets_not_found = []
         build_targets = []
 
         while targets_to_parse:
             next_target_string = targets_to_parse.popleft()
-            next_target = parse_target(next_target_string)
+            next_target = self._parse_target(next_target_string)
             build_file = self.parser.parse_file(next_target.build_file_directory)
             if next_target.name not in build_file.targets:
                 targets_not_found.append(next_target_string)
@@ -59,3 +57,39 @@ class Builder(object):
                 "Target(s) not found: {}".format(", ".join(targets_not_found))
             )
         return build_targets
+
+    def _parse_target(self, target: str) -> Target:
+        if target.endswith("...") or target.endswith(":"):
+            raise ValueError("Target {} is not an absolute target.".format(target))
+        split = _strip(target, left="//").split(":")
+        return Target(split[0], split[1])
+
+    def _normalize_target(self, target: str) -> List[str]:
+        if target.endswith("..."):
+            # Find all build files and recursively add all rules from each file.
+            targets = []
+            directory = os.path.join(
+                self.buck_root, _strip(target, left="//", right="...")
+            )
+            for build_file in glob.iglob(
+                os.path.join(directory, "**", self.build_file_name), recursive=True
+            ):
+                relative_path = os.path.relpath(build_file, self.buck_root)
+                build_file_directory = os.path.dirname(relative_path)
+                wildcard_target = "//{}:".format(build_file_directory)
+                targets.extend(self._normalize_target(wildcard_target))
+            return targets
+        elif target.endswith(":"):
+            # Normalize to every target in the specified file.
+            build_file = self.parser.parse_file(_strip(target, left="//", right=":"))
+            return [build_target.target for build_target in build_file.targets.values()]
+        else:
+            return [target]
+
+
+def _strip(string: str, left: Optional[str] = None, right: Optional[str] = None) -> str:
+    if left and string.startswith(left):
+        string = string[len(left) :]
+    if right and string.endswith(right):
+        string = string[: -len(right)]
+    return string
--- client/buck_project_builder/tests/builder_test.py
+++ client/buck_project_builder/tests/builder_test.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import glob
 import unittest
 from typing import List
 from unittest.mock import MagicMock, patch
@@ -12,12 +13,15 @@ from ..parser.build_target import BuildTarget, PythonBinary, PythonLibrary
 
 
 class BuilderTest(unittest.TestCase):
+    def assert_target_names_equal(self, list_a: List[str], list_b: List[str]) -> None:
+        self.assertListEqual(sorted(list_a), sorted(list_b))
+
     def assert_targets_equal(
         self, targets: List[BuildTarget], target_names: List[str]
     ) -> None:
-        computed_target_names = sorted(target.target for target in targets)
-        expected_target_names = sorted(target_names)
-        self.assertListEqual(computed_target_names, expected_target_names)
+        self.assert_target_names_equal(
+            [target.target for target in targets], target_names
+        )
 
     def test_compute_targets_to_build_simple(self):
         # Dependency graph:
@@ -114,3 +118,233 @@ class BuilderTest(unittest.TestCase):
             self.assertRaises(
                 ValueError, builder.compute_targets_to_build, "//project1:f"
             )
+
+    def test_targets_to_build_file_wildcard(self):
+        # Dependency graph:
+        #  a -> c -> d <- e   b
+        build_file_1 = MagicMock()
+        build_file_1.targets = {
+            "a": PythonBinary("project1", "a", ["//project2:c"]),
+            "b": PythonLibrary("project1", "b", [], []),
+        }
+        build_file_2 = MagicMock()
+        build_file_2.targets = {
+            "c": PythonLibrary("project2", "c", ["//project2:d"], []),
+            "d": PythonLibrary("project2", "d", [], []),
+            "e": PythonLibrary("project2", "e", ["//project2:d"], []),
+        }
+
+        build_file_mapping = {"project1": build_file_1, "project2": build_file_2}
+        with patch.object(
+            parser.Parser, "parse_file", side_effect=build_file_mapping.get
+        ):
+            builder = Builder("/ROOT")
+
+            targets = builder.compute_targets_to_build("//project1:")
+            self.assert_targets_equal(
+                targets,
+                ["//project1:a", "//project1:b", "//project2:c", "//project2:d"],
+            )
+
+            targets = builder.compute_targets_to_build("//project2:")
+            self.assert_targets_equal(
+                targets, ["//project2:c", "//project2:d", "//project2:e"]
+            )
+
+    def test_targets_to_build_directory_wildcard(self):
+        # Dependency graph:
+        # a -> c   d   b <- e
+        build_file_1 = MagicMock()
+        build_file_1.targets = {
+            "a": PythonBinary("project1", "a", ["//project1/subproject:c"]),
+            "b": PythonLibrary("project1", "b", [], []),
+        }
+
+        build_file_2 = MagicMock()
+        build_file_2.targets = {
+            "c": PythonLibrary("project1/subproject", "c", [], []),
+            "d": PythonLibrary("project1/subproject", "d", [], []),
+        }
+
+        build_file_3 = MagicMock()
+        build_file_3.targets = {
+            "e": PythonLibrary("project2", "e", ["//project1:b"], [])
+        }
+
+        build_file_mapping = {
+            "project1": build_file_1,
+            "project1/subproject": build_file_2,
+            "project2": build_file_3,
+        }
+        with patch.object(
+            parser.Parser, "parse_file", side_effect=build_file_mapping.get
+        ):
+            builder = Builder("/ROOT")
+
+            with patch.object(
+                glob,
+                "iglob",
+                return_value=[
+                    "/ROOT/project1/TARGETS",
+                    "/ROOT/project1/subproject/TARGETS",
+                    "/ROOT/project2/TARGETS",
+                ],
+            ):
+                targets = builder.compute_targets_to_build("//...")
+                self.assert_targets_equal(
+                    targets,
+                    [
+                        "//project1:a",
+                        "//project1:b",
+                        "//project1/subproject:c",
+                        "//project1/subproject:d",
+                        "//project2:e",
+                    ],
+                )
+
+            with patch.object(
+                glob,
+                "iglob",
+                return_value=[
+                    "/ROOT/project1/TARGETS",
+                    "/ROOT/project1/subproject/TARGETS",
+                ],
+            ):
+                targets = builder.compute_targets_to_build("//project1/...")
+                self.assert_targets_equal(
+                    targets,
+                    [
+                        "//project1:a",
+                        "//project1:b",
+                        "//project1/subproject:c",
+                        "//project1/subproject:d",
+                    ],
+                )
+
+            with patch.object(
+                glob, "iglob", return_value=["/ROOT/project1/subproject/TARGETS"]
+            ):
+                targets = builder.compute_targets_to_build("//project1/subproject/...")
+                self.assert_targets_equal(
+                    targets, ["//project1/subproject:c", "//project1/subproject:d"]
+                )
+
+            with patch.object(glob, "iglob", return_value=["/ROOT/project2/TARGETS"]):
+                targets = builder.compute_targets_to_build("//project2/...")
+                self.assert_targets_equal(targets, ["//project2:e", "//project1:b"])
+
+    def test_normalize_targets(self):
+        build_file_1 = MagicMock()
+        build_file_1.targets = {
+            "a": PythonLibrary("project1", "a", [], []),
+            "b": PythonLibrary("project1", "b", [], []),
+            "c": PythonLibrary("project1", "c", [], []),
+        }
+
+        build_file_2 = MagicMock()
+        build_file_2.targets = {
+            "d": PythonLibrary("project1/subproject", "d", [], []),
+            "e": PythonLibrary("project1/subproject", "e", [], []),
+        }
+
+        build_file_3 = MagicMock()
+        build_file_3.targets = {"f": PythonLibrary("project2", "f", [], [])}
+
+        build_file_mapping = {
+            "project1": build_file_1,
+            "project1/subproject": build_file_2,
+            "project2": build_file_3,
+        }
+        with patch.object(
+            parser.Parser, "parse_file", side_effect=build_file_mapping.get
+        ):
+            builder = Builder("/ROOT")
+
+            # Regular targets
+            normalized_targets = builder._normalize_target("//project1:a")
+            self.assert_target_names_equal(normalized_targets, ["//project1:a"])
+
+            # File wildcard targets
+            normalized_targets = builder._normalize_target("//project1:")
+            self.assert_target_names_equal(
+                normalized_targets, ["//project1:a", "//project1:b", "//project1:c"]
+            )
+
+            normalized_targets = builder._normalize_target("//project1/subproject:")
+            self.assert_target_names_equal(
+                normalized_targets,
+                ["//project1/subproject:d", "//project1/subproject:e"],
+            )
+
+            normalized_targets = builder._normalize_target("//project2:")
+            self.assert_target_names_equal(normalized_targets, ["//project2:f"])
+
+            # Directory wildcard targets
+            with patch.object(
+                glob,
+                "iglob",
+                return_value=[
+                    "/ROOT/project1/TARGETS",
+                    "/ROOT/project1/subproject/TARGETS",
+                    "/ROOT/project2/TARGETS",
+                ],
+            ) as fake_iglob:
+                normalized_targets = builder._normalize_target("//...")
+                self.assert_target_names_equal(
+                    normalized_targets,
+                    [
+                        "//project1:a",
+                        "//project1:b",
+                        "//project1:c",
+                        "//project1/subproject:d",
+                        "//project1/subproject:e",
+                        "//project2:f",
+                    ],
+                )
+                fake_iglob.assert_called_once_with("/ROOT/**/TARGETS", recursive=True)
+
+            with patch.object(
+                glob,
+                "iglob",
+                return_value=[
+                    "/ROOT/project1/TARGETS",
+                    "/ROOT/project1/subproject/TARGETS",
+                ],
+            ) as fake_iglob:
+                normalized_targets = builder._normalize_target("//project1/...")
+                self.assert_target_names_equal(
+                    normalized_targets,
+                    [
+                        "//project1:a",
+                        "//project1:b",
+                        "//project1:c",
+                        "//project1/subproject:d",
+                        "//project1/subproject:e",
+                    ],
+                )
+                fake_iglob.assert_called_once_with(
+                    "/ROOT/project1/**/TARGETS", recursive=True
+                )
+
+            with patch.object(
+                glob, "iglob", return_value=["/ROOT/project1/subproject/TARGETS"]
+            ) as fake_iglob:
+                normalized_targets = builder._normalize_target(
+                    "//project1/subproject/..."
+                )
+                self.assert_target_names_equal(
+                    normalized_targets,
+                    ["//project1/subproject:d", "//project1/subproject:e"],
+                )
+                fake_iglob.assert_called_once_with(
+                    "/ROOT/project1/subproject/**/TARGETS", recursive=True
+                )
+
+            with patch.object(
+                glob, "iglob", return_value=["/ROOT/project2/TARGETS"]
+            ) as fake_iglob:
+                normalized_targets = builder._normalize_target("//project2/...")
+                self.assert_target_names_equal(normalized_targets, ["//project2:f"])
+                fake_iglob.assert_called_once_with(
+                    "/ROOT/project2/**/TARGETS", recursive=True
+                )
