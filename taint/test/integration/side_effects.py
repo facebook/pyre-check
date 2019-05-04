c09a37c602fbb8d3918315861b921e7a81488ffa
Manuel Fahndrich <fahndrich@fb.com>
2019-05-03 21:41:52 -0700

    reformat test file with black
    Reviewed By: dkgi
    
    Differential Revision: D15211990
    
    fbshipit-source-id: 71690128b3b187363cb5a39eb2dce797c64fddd2

--- taint/test/integration/side_effects.py
+++ taint/test/integration/side_effects.py
@@ -1,35 +1,43 @@
 # @nolint
 
+
 def test_from_1_to_0():
-  x = 0
-  change_arg0(x, __test_source())
-  return x
+    x = 0
+    change_arg0(x, __test_source())
+    return x
+
 
 def test_from_0_to_1():
-  y = 0
-  change_arg1(__test_source(), y)
-  return y
+    y = 0
+    change_arg1(__test_source(), y)
+    return y
+
 
 def test_from_1_to_0_nested():
-  x = {}
-  change_arg0(x.foo, __test_source())
-  return x.foo
+    x = {}
+    change_arg0(x.foo, __test_source())
+    return x.foo
+
 
 def test_from_1_to_0_nested_distinct():
-  x = {}
-  change_arg0(x.foo, __test_source())
-  return x.bar
+    x = {}
+    change_arg0(x.foo, __test_source())
+    return x.bar
+
 
 def test_list_append():
-  l = MyList()
-  l.append(__test_source())
-  return l
+    l = MyList()
+    l.append(__test_source())
+    return l
+
 
 def wrapper_from_1_to_0(x, y):
     change_arg0(x, y)
 
+
 def wrapper_from_0_to_1(x, y):
     change_arg1(x, y)
+
 
 def to_sink_via_side_effect(y):
     x = {}
@@ -39,9 +47,15 @@ def to_sink_via_side_effect(y):
 
 # Mocks that have models
 
-def change_arg0(x, y): ...
-def change_arg1(x, y): ...
+
+def change_arg0(x, y):
+    ...
+
+
+def change_arg1(x, y):
+    ...
+
 
 class MyList:
-  def append(self, data):
-    pass
+    def append(self, data):
+        pass
