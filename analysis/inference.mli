46e84f46c14af9b22588063b44fe90834ed70460
Dominik Gabi <dominik@fb.com>
2019-05-17 12:23:28 -0700

    minimize `Inference` interface
    Reviewed By: grievejia
    
    Differential Revision: D15396576
    
    fbshipit-source-id: 3550f61ce1d8c94e163c0237c6ef7cd60cb44592

--- analysis/inference.mli
+++ analysis/inference.mli
@@ -23,43 +23,24 @@ module State : sig
     -> ?resolution_fixpoint: ResolutionSharedMemory.annotation_map Int.Map.Tree.t
     -> unit
     -> t
-
-  val errors: t -> Error.t list
-
   val initial
     :  ?configuration: Configuration.Analysis.t
     -> resolution: Resolution.t
     -> Define.t Node.t
     -> t
 
-  val resolve_exports: resolution: Resolution.t -> Reference.t -> Reference.t
-
-  (* Visible for testing. *)
-  type resolved = {
-    state: t;
-    resolved: Type.t;
-  }
-
-  val parse_and_check_annotation: ?bind_variables:bool ->  state: t -> Expression.t -> t * Type.t
-  val forward_expression: state: t -> expression: Expression.t -> resolved
-  val forward_statement: state: t -> statement: Statement.t -> t
-
-  include Fixpoint.State with type t := t
-
   val initial_forward
     :  configuration: Configuration.Analysis.t
     -> resolution: Resolution.t
     -> Statement.Define.t Node.t
     -> t
-
   val initial_backward
     :  ?configuration: Configuration.Analysis.t
     -> Statement.Define.t Node.t
     -> forward: t
     -> t
 
-  val update_only_existing_annotations: t -> t -> t
-  val check_entry: t -> t
+  include Fixpoint.State with type t := t
 end
 
 module Fixpoint : Fixpoint.Fixpoint with type state := State.t
