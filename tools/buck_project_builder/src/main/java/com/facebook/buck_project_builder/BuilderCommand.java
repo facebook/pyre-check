fdabf22c255dfcee77f6cabc983bd923ebc45ea8
Sam Zhou <samzhou19815@fb.com>
2019-06-07 11:00:30 -0700

    Parse command line arguments that can be passed by pyre client
    Summary: Added support for parsing command line arguments to extract `buckRoot`, `outputDirectory` and a list of `targets` to be used for building the entire buck project.
    
    Reviewed By: sinancepel
    
    Differential Revision: D15698988
    
    fbshipit-source-id: 795c7a7f5699dd42d4d62c165023c5b904dcc835

---
+++ tools/buck_project_builder/src/main/java/com/facebook/buck_project_builder/BuilderCommand.java
@@ -0,0 +1,84 @@
+package com.facebook.buck_project_builder;
+
+import com.google.common.collect.ImmutableList;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Objects;
+
+final class BuilderCommand {
+
+  private final String buckRoot;
+  private final @Nullable String outputDirectory;
+  private final ImmutableList<String> targets;
+
+  BuilderCommand(String buckRoot, String outputDirectory, ImmutableList<String> targets) {
+    this.buckRoot = buckRoot;
+    this.outputDirectory = outputDirectory;
+    this.targets = targets;
+  }
+
+  static BuilderCommand fromCommandLineArguments(String[] arguments) throws BuilderException {
+    CommandLineParser parser = new DefaultParser();
+    Options parsingOptions = new Options();
+    parsingOptions.addOption(Option.builder().hasArg().longOpt("buck_root").build());
+    parsingOptions.addOption(Option.builder().hasArg().longOpt("output_directory").build());
+    try {
+      CommandLine parsedArguments = parser.parse(parsingOptions, arguments);
+      String buckRoot = parsedArguments.getOptionValue("buck_root");
+      if (buckRoot == null) {
+        throw new BuilderException("buck_root is a required command line argument.");
+      }
+      String outputDirectory = parsedArguments.getOptionValue("output_directory");
+      List<String> targets = parsedArguments.getArgList();
+      return new BuilderCommand(buckRoot, outputDirectory, ImmutableList.copyOf(targets));
+    } catch (ParseException exception) {
+      throw new BuilderException(
+          "Unexpected command line arguments. Detail: " + exception.getMessage());
+    }
+  }
+
+  public String getBuckRoot() {
+    return buckRoot;
+  }
+
+  @Nullable
+  public String getOutputDirectory() {
+    return outputDirectory;
+  }
+
+  public ImmutableList<String> getTargets() {
+    return targets;
+  }
+
+  @Override
+  public boolean equals(@Nullable Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+    BuilderCommand builderCommand = (BuilderCommand) other;
+    return buckRoot.equals(builderCommand.buckRoot)
+        && Objects.equals(outputDirectory, builderCommand.outputDirectory)
+        && targets.equals(builderCommand.targets);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(buckRoot, outputDirectory, targets);
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "{buckRoot=%s, outputDirectory=%s, targets=%s}", buckRoot, outputDirectory, targets);
+  }
+}
---
+++ tools/buck_project_builder/src/test/java/com/facebook/buck_project_builder/BuilderCommandTest.java
@@ -0,0 +1,56 @@
+package com.facebook.buck_project_builder;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BuilderCommandTest {
+
+  private static void assertParsedTo(BuilderCommand expected, String arguments)
+      throws BuilderException {
+    assertEquals(expected, BuilderCommand.fromCommandLineArguments(arguments.split(" ")));
+  }
+
+  @Test
+  public void goodArgumentsDoParse() throws BuilderException {
+    // All optional arguments do appear. Multiple build targets.
+    assertParsedTo(
+        new BuilderCommand("ROOT", "OUT", ImmutableList.of("foo", "bar")),
+        "--buck_root ROOT --output_directory OUT foo bar");
+
+    // Empty targets are allowed.
+    assertParsedTo(
+        new BuilderCommand("ROOT", "OUT", ImmutableList.of()),
+        "--buck_root ROOT --output_directory OUT");
+
+    // Output directory is indeed optional.
+    assertParsedTo(
+        new BuilderCommand("ROOT", null, ImmutableList.of("foo", "bar")),
+        "--buck_root ROOT foo bar");
+
+    // All optional args are omitted.
+    assertParsedTo(new BuilderCommand("ROOT", null, ImmutableList.of()), "--buck_root ROOT");
+  }
+
+  @Test(expected = BuilderException.class)
+  public void randomArgumentsDoNotParse() throws BuilderException {
+    BuilderCommand.fromCommandLineArguments(new String[] {"hello"});
+  }
+
+  @Test(expected = BuilderException.class)
+  public void emptyArgumentsDoNotParse() throws BuilderException {
+    BuilderCommand.fromCommandLineArguments(new String[0]);
+  }
+
+  @Test(expected = BuilderException.class)
+  public void missingBuckRootDoNotParse() throws BuilderException {
+    BuilderCommand.fromCommandLineArguments(new String[] {"target_1"});
+  }
+
+  @Test(expected = BuilderException.class)
+  public void missingOutputDirectoryArgumentDoNotParse() throws BuilderException {
+    BuilderCommand.fromCommandLineArguments(
+        new String[] {"--buck_root", "ROOT", "--output_directory"});
+  }
+}
