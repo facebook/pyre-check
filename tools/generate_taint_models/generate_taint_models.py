3d6f46dcbef0e036686d3fe54408bf720ba975d4
Sinan Cepel <sinancepel@fb.com>
2019-06-20 13:22:54 -0700

    sort models before generating them in model generation script
    Reviewed By: grievejia
    
    Differential Revision: D15909818
    
    fbshipit-source-id: 829d709810be4a9f6a5a7dd3d4f065a8367a4ed2

--- tools/generate_taint_models/generate_taint_models.py
+++ tools/generate_taint_models/generate_taint_models.py
@@ -416,4 +416,4 @@ if __name__ == "__main__":
     for mode in arguments.mode:
         models = models.union(MODES[mode](arguments))
 
-    print("\n".join(models))
+    print("\n".join(sorted(models)))
