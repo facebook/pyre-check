19df28eb3512391e4ce164a29050b6d22d823ca8
Sinan Cepel <sinancepel@fb.com>
2019-06-25 18:10:43 -0700

    add common `__getitem__` and `__setitem__` models for containers
    Summary: These common operations on containers are cases where, even if explicitly called, our static analysis should preserve taint. There's a bit of additional complexity due to Python's type system.
    
    Reviewed By: dkgi
    
    Differential Revision: D15923917
    
    fbshipit-source-id: 989e986cf90552be749346daf2e988d0d4abcf5b

---
+++ stubs/integration_test/containers.py
@@ -0,0 +1,48 @@
+# @nolint
+
+from django.http import HttpRequest
+
+def dictionary_update(request: HttpRequest):
+    result = {}
+    source = {"source": request.GET["bad"]}
+    result.update(source)
+    eval(result)
+
+def list_append(request: HttpRequest):
+    l = []
+    l.append(request.GET["bad"])
+    eval(l[0])
+
+
+def list_update(request: HttpRequest):
+    l = []
+    tainted_list = [request.GET["bad"]]
+    l.extend(tainted_list)
+    eval(l[0])
+
+
+def list_insert(request: HttpRequest):
+    l = [1] * 10
+    l.insert(5, request.GET["bad"])
+    eval(l[5])
+
+
+def set_add(request: HttpRequest):
+    s = {1}
+    s.add(request.GET["bad"])
+    for element in s:
+        eval(element)
+
+
+def set_intersection_update(request: HttpRequest):
+    s = {1}
+    s.intersection_update({request.GET["bad"]})
+    for element in s:
+        eval(element)
+
+
+def set_update(request: HttpRequest):
+    s = {1}
+    s.update({request.GET["bad"]})
+    for element in s:
+        eval(element)
