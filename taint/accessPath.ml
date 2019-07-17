d86ed8fa6e1e8b929c1088ff265a1eec3ffeed9e
Sinan Cepel <sinancepel@fb.com>
2019-07-17 15:45:53 -0700

    add support to track enumeration fields and constants
    Summary:
    The `first-index` feature is often used to filter enums. Our taint analysis was losing the name of the enum being accessed.
    
    Go with the heuristic of only keeping the last element of all upper case simple names, as this will also automatically handle enums & constants that are more complicated than strings or integers.
    
    Reviewed By: fahndrich
    
    Differential Revision: D16347719
    
    fbshipit-source-id: 7df701e133994dd558164a4576b71df0a5b2db21

--- taint/accessPath.ml
+++ taint/accessPath.ml
@@ -183,6 +183,15 @@ let get_index { Node.value = expression; _ } =
   match expression with
   | String literal -> AbstractTreeDomain.Label.Field literal.value
   | Integer i -> AbstractTreeDomain.Label.Field (string_of_int i)
+  | Name name -> (
+      let name = Reference.from_name name >>| Reference.delocalize >>| Reference.last in
+      match name with
+      (* Heuristic: All uppercase names tend to be enums, so only taint the field in those cases. *)
+      | Some name
+        when String.for_all name ~f:(fun character ->
+                 (not (Char.is_alpha character)) || Char.is_uppercase character) ->
+          AbstractTreeDomain.Label.Field name
+      | _ -> AbstractTreeDomain.Label.Any )
   | _ -> AbstractTreeDomain.Label.Any
 
 
---
+++ taint/test/integration/constant_fields.py
@@ -0,0 +1,43 @@
+# @nolint
+
+from enum import Enum
+
+class CustomEnum(Enum):
+    TRACKED_FIELD = "A"
+    UNTRACKED_field = "B"
+    untracked_field = "C"
+
+
+def tracked_index():
+    d = {}
+    d[CustomEnum.TRACKED_FIELD] = __test_source()
+    return d[CustomEnum.TRACKED_FIELD]
+
+def untracked_index_a():
+    d = {}
+    d[CustomEnum.untracked_field] = __test_source()
+    return d[CustomEnum.untracked_field]
+
+def untracked_index_b():
+    d = {}
+    d[CustomEnum.UNTRACKED_field] = __test_source()
+    return d[CustomEnum.UNTRACKED_field]
+
+CONSTANT_A = "A"
+CONSTANT_B = {"a": "b"}
+untracked_constant = "1"
+
+def tracked_constant_A():
+    d = {}
+    d[CONSTANT_A] = __test_source()
+    return d[CONSTANT_A]
+
+def tracked_constant_B():
+    d = {}
+    d[CONSTANT_B] = __test_source()
+    return d[CONSTANT_B]
+
+def untracked_constant():
+    d = {}
+    d[untracked_constant] = __test_source()
+    return d[untracked_constant]
