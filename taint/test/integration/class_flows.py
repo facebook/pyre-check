7a770ca91a4d05cd4f281dc89834929af73eabc9
Jia Chen <grievejia@fb.com>
2019-07-18 17:07:33 -0700

    Blackify taint integration tests
    Summary: These are causing travis CI failures
    
    Reviewed By: shannonzhu
    
    Differential Revision: D16364637
    
    fbshipit-source-id: b36d2d6e2e8bbe1f6f78ae83ab026a01defb52ba

--- taint/test/integration/class_flows.py
+++ taint/test/integration/class_flows.py
@@ -2,21 +2,27 @@
 
 from typing import Type
 
+
 class C:
     tainted_attribute: List[int] = []
     not_tainted = 2
 
+
 class D(C):
     pass
+
 
 def tainted_attribute_flow(c: C) -> None:
     c.tainted_attribute = __test_source()
 
+
 def untainted_flow(c: C) -> None:
     c.not_tainted = __test_source()
 
+
 def tainted_attribute_for_class(c: Type[C]) -> None:
     c.tainted_attribute = __test_source()
+
 
 def tainted_attribute_through_inheritance(d: D) -> None:
     # TODO(T47337940): Support this.
--- taint/test/integration/constant_fields.py
+++ taint/test/integration/constant_fields.py
@@ -2,6 +2,7 @@
 
 from enum import Enum
 
+
 class CustomEnum(Enum):
     TRACKED_FIELD = "A"
     UNTRACKED_field = "B"
@@ -13,29 +14,35 @@ def tracked_index():
     d[CustomEnum.TRACKED_FIELD] = __test_source()
     return d[CustomEnum.TRACKED_FIELD]
 
+
 def untracked_index_a():
     d = {}
     d[CustomEnum.untracked_field] = __test_source()
     return d[CustomEnum.untracked_field]
+
 
 def untracked_index_b():
     d = {}
     d[CustomEnum.UNTRACKED_field] = __test_source()
     return d[CustomEnum.UNTRACKED_field]
 
+
 CONSTANT_A = "A"
 CONSTANT_B = {"a": "b"}
 untracked_constant = "1"
+
 
 def tracked_constant_A():
     d = {}
     d[CONSTANT_A] = __test_source()
     return d[CONSTANT_A]
 
+
 def tracked_constant_B():
     d = {}
     d[CONSTANT_B] = __test_source()
     return d[CONSTANT_B]
+
 
 def untracked_constant():
     d = {}
