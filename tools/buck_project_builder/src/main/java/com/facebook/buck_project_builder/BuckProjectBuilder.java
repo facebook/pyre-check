49c1eb7e6674234908fef59737bae959529b68e5
Sam Zhou <samzhou19815@fb.com>
2019-07-20 16:47:03 -0700

    Invalidating all cache if build failed
    Summary: We should invalidate all caches if build fails, otherwise during the next build we might read corrupted cache.
    
    Reviewed By: grievejia
    
    Differential Revision: D16359899
    
    fbshipit-source-id: 58ff72acc5113fdb7955ea3716548dfb4bd9151f

--- tools/buck_project_builder/src/main/java/com/facebook/buck_project_builder/BuckProjectBuilder.java
+++ tools/buck_project_builder/src/main/java/com/facebook/buck_project_builder/BuckProjectBuilder.java
@@ -2,10 +2,14 @@
 
 package com.facebook.buck_project_builder;
 
+import com.facebook.buck_project_builder.cache.BuilderCache;
 import com.facebook.buck_project_builder.targets.BuildTargetsBuilder;
 import com.facebook.buck_project_builder.targets.BuildTargetsCollector;
 import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
 
 public final class BuckProjectBuilder {
 
@@ -16,13 +20,20 @@ public final class BuckProjectBuilder {
    * in standard error.
    */
   public static void main(String[] arguments) {
+    long start = System.currentTimeMillis();
+    BuilderCommand command;
     try {
-      long start = System.currentTimeMillis();
-      BuilderCommand command = BuilderCommand.fromCommandLineArguments(arguments);
-      String buckRoot = command.getBuckRoot();
-      ImmutableList<String> targets = command.getTargets();
-      BuildTargetsBuilder builder =
-          new BuildTargetsBuilder(start, buckRoot, command.getOutputDirectory(), targets);
+      command = BuilderCommand.fromCommandLineArguments(arguments);
+    } catch (BuilderException exception) {
+      SimpleLogger.error(exception.getMessage());
+      System.exit(1);
+      return;
+    }
+    String buckRoot = command.getBuckRoot();
+    ImmutableList<String> targets = command.getTargets();
+    BuildTargetsBuilder builder =
+        new BuildTargetsBuilder(start, buckRoot, command.getOutputDirectory(), targets);
+    try {
       BuildTargetsCollector.collectBuckTargets(buckRoot, targets)
           .forEach(target -> target.addToBuilder(builder));
       DebugOutput debugOutput = builder.buildTargets();
@@ -32,6 +43,8 @@ public final class BuckProjectBuilder {
       BuildTimeLogger.logBuildTime(start, System.currentTimeMillis(), targets);
     } catch (BuilderException exception) {
       SimpleLogger.error(exception.getMessage());
+      SimpleLogger.error("Build failure. Invalidating all build cache.");
+      FileUtils.deleteQuietly(new File(BuilderCache.getCachePath(targets)));
       System.exit(1);
     }
   }
