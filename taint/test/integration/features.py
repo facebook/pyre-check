90261c51ea2a2078b47d6291db27ee317e9f359a
Jia Chen <grievejia@fb.com>
2019-07-22 18:32:00 -0700

    Blackify `taint/test/integration/features.py`
    Summary: Our travis CI is breaking again
    
    Reviewed By: shannonzhu
    
    Differential Revision: D16433634
    
    fbshipit-source-id: dc7a7f464d6473bb0b1fe60748acd88553ced1d0

--- taint/test/integration/features.py
+++ taint/test/integration/features.py
@@ -2,18 +2,30 @@
 
 from typing import Any
 
-def len(o: Any) -> int: ...
-def min(x: int, y: str): ...
-def named(*, named_parameter: int, **kw): ...
+
+def len(o: Any) -> int:
+    ...
+
+
+def min(x: int, y: str):
+    ...
+
+
+def named(*, named_parameter: int, **kw):
+    ...
+
 
 def tito_via_len(o: Any):
     return len(o)
 
+
 def tito_via_min_left(o: Any):
-    return min(o, '')
+    return min(o, "")
+
 
 def tito_via_min_right(o: Any):
     return min(5, o)
 
+
 def tito_via_named(o: Any):
-    return named(named_parameter = o)
+    return named(named_parameter=o)
