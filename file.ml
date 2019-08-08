527749ac3abaf6716ff1b30890c647a968032d55
Jia Chen <grievejia@fb.com>
2019-08-08 15:48:38 -0700

    Purge `File.Handle` from codebase
    Summary: All usages have been migrated. Time to kill it.
    
    Reviewed By: dkgi
    
    Differential Revision: D16715177
    
    fbshipit-source-id: 6446692904b8dd6fbc4091a33abd05c762d1c8a9

--- file.ml
+++ file.ml
@@ -55,75 +55,6 @@ let append ~lines path =
   Out_channel.with_file ~append:true ~fail_if_exists:false ~f:append_lines (Path.absolute path)
 
 
-module Handle = struct
-  type t = string [@@deriving compare, eq, show, sexp, hash]
-
-  let _ = show (* shadowed below *)
-
-  let show path = path
-
-  let create_for_testing path = path
-
-  let is_stub path = String.is_suffix ~suffix:".pyi" path
-
-  let is_init path =
-    String.is_suffix ~suffix:"__init__.py" path || String.is_suffix ~suffix:"__init__.pyi" path
-
-
-  let to_path ~configuration handle =
-    let construct_relative_to_root root =
-      let should_be_considered =
-        match root with
-        | SearchPath.Root _ -> true
-        | SearchPath.Subdirectory { subdirectory; _ } ->
-            (* Ensure that we don't reconstruct paths that live outside the subdirectory. *)
-            String.is_prefix ~prefix:subdirectory handle
-      in
-      let root = SearchPath.get_root root in
-      let path = Path.create_relative ~root ~relative:handle in
-      if should_be_considered && Path.file_exists path then
-        Some path
-      else
-        None
-    in
-    List.find_map (Configuration.Analysis.search_path configuration) ~f:construct_relative_to_root
-
-
-  include Hashable.Make (struct
-    type nonrec t = t
-
-    let compare = compare
-
-    let hash = Hashtbl.hash
-
-    let hash_fold_t = hash_fold_t
-
-    let sexp_of_t = sexp_of_t
-
-    let t_of_sexp = t_of_sexp
-  end)
-
-  module Map = Map.Make (struct
-    type nonrec t = t
-
-    let compare = compare
-
-    let sexp_of_t = sexp_of_t
-
-    let t_of_sexp = t_of_sexp
-  end)
-
-  module Set = Set.Make (struct
-    type nonrec t = t
-
-    let compare = compare
-
-    let sexp_of_t = sexp_of_t
-
-    let t_of_sexp = t_of_sexp
-  end)
-end
-
 module Set = Set.Make (struct
   type nonrec t = t
 
@@ -133,20 +64,3 @@ module Set = Set.Make (struct
 
   let t_of_sexp = t_of_sexp
 end)
-
-exception NonexistentHandle of string
-
-let is_stub { path; _ } = Path.absolute path |> Handle.create_for_testing |> Handle.is_stub
-
-let handle ~configuration { path; _ } =
-  let search_path = Configuration.Analysis.search_path configuration in
-  match SearchPath.search_for_path ~search_path path with
-  | Some SearchPath.{ relative_path; _ } -> Path.RelativePath.relative relative_path
-  | None ->
-      let message =
-        Format.sprintf
-          "Unable to construct handle for %s. Possible roots: %s"
-          (Path.absolute path)
-          (search_path |> List.map ~f:SearchPath.to_path |> List.to_string ~f:Path.absolute)
-      in
-      raise (NonexistentHandle message)
--- file.mli
+++ file.mli
@@ -22,30 +22,4 @@ val write : t -> unit
 
 val append : lines:string list -> Path.t -> unit
 
-exception NonexistentHandle of string
-
-module Handle : sig
-  type t [@@deriving compare, eq, show, sexp, hash]
-
-  (* Should not be used in production code; it only provides a conveniences for tests. You should
-     use File.handle. *)
-  val create_for_testing : string -> t
-
-  val is_stub : t -> bool
-
-  val is_init : t -> bool
-
-  val to_path : configuration:Configuration.Analysis.t -> t -> Path.t option
-
-  include Hashable with type t := t
-
-  module Map : Map.S with type Key.t = t
-
-  module Set : Set.S with type Elt.t = t
-end
-
 module Set : Set.S with type Elt.t = t
-
-val is_stub : t -> bool
-
-val handle : configuration:Configuration.Analysis.t -> t -> Handle.t
