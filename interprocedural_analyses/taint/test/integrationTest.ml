be3c9ae4688dbd3275b93f873eaf799b808a33fc
Shannon Zhu <szhu@fb.com>
2019-08-12 08:21:07 -0700

    Add directory for interprocedural analyses kinds
    Summary: Move tools/pyre/taint into a directory that will include other kinds of interprocedural analyses besides taint analysis. Provides a place to start writing other kinds of analyses without polluting the toplevel directory.
    
    Reviewed By: fahndrich
    
    Differential Revision: D16678360
    
    fbshipit-source-id: 1914be3697e7288df1797cebbba100c628a62850

--- interprocedural_analyses/taint/test/integrationTest.ml
+++ interprocedural_analyses/taint/test/integrationTest.ml
@@ -15,9 +15,10 @@ let test_integration context =
     (* Shameful things happen here... *)
     Path.current_working_directory ()
     |> Path.show
-    |> String.chop_suffix_exn ~suffix:"_build/default/taint/test"
+    |> String.chop_suffix_exn ~suffix:"_build/default/interprocedural_analyses/taint/test"
     |> (fun root -> Path.create_absolute root)
-    |> (fun root -> Path.create_relative ~root ~relative:"taint/test/integration/")
+    |> (fun root ->
+         Path.create_relative ~root ~relative:"interprocedural_analysis/taint/test/integration/")
     |> fun root -> Path.list ~file_filter:(String.is_suffix ~suffix:".py") ~root ()
   in
   let run_test path =
