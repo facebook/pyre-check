534b303720b3fe7637bf880b87d808c059277ce3
Sinan Cepel <sinancepel@fb.com>
2019-09-20 16:09:20 -0700

    triage broken test on master
    Reviewed By: grievejia
    
    Differential Revision: D17510611
    
    fbshipit-source-id: 1be0c29eb2f60c072f071c352d6279b428e80021

--- analysis/test/integration/decoratorTest.ml
+++ analysis/test/integration/decoratorTest.ml
@@ -422,14 +422,16 @@ let test_check_user_decorators context =
           pass
 
       class D(C):
-        # Since the type system disallows calling f with `x=1`, this is ok.
         def f(self, y: int) -> None:
           pass
       reveal_type(C.f)
       reveal_type(D.f)
     |}
     [
-      "Revealed type [-1]: Revealed type for `test.C.f` is `typing.Callable(C.f)[[C, int], None]`.";
+      "Inconsistent override [14]: `test.D.f` overrides method defined in `C` inconsistently. \
+       Could not find parameter `x` in overriding signature.";
+      "Revealed type [-1]: Revealed type for `test.C.f` is `typing.Callable(C.f)[[Named(self, \
+       unknown), Named(x, int)], None]`.";
       "Revealed type [-1]: Revealed type for `test.D.f` is `typing.Callable(D.f)[[Named(self, \
        unknown), Named(y, int)], None]`.";
     ];
@@ -478,8 +480,7 @@ let test_check_callable_class_decorators context =
       "Missing parameter annotation [2]: Parameter `coroutine` must have a type that does not \
        contain `Any`.";
       "Revealed type [-1]: Revealed type for `test.am_i_async` is \
-       `typing.Callable(am_i_async)[[Named(x, int)], typing.Coroutine[typing.Any, typing.Any, \
-       str]]`.";
+       `typing.Callable(am_i_async)[[Named(x, int)], str]`.";
     ];
 
   (* We don't support overloaded callable classes. *)
@@ -507,8 +508,7 @@ let test_check_callable_class_decorators context =
       "Missing return annotation [3]: Return type must be specified as type other than `Any`.";
       "Missing parameter annotation [2]: Parameter `coroutine` must have a type other than `Any`.";
       "Revealed type [-1]: Revealed type for `test.am_i_async` is \
-       `typing.Callable(am_i_async)[[Named(x, int)], typing.Coroutine[typing.Any, typing.Any, \
-       str]]`.";
+       `typing.Callable(am_i_async)[[Named(x, int)], str]`.";
     ]
 
 
