fbbba2416c71a36cd862ae5c4fe53a21fb2ab999
Maggie Moss <maggiemoss@fb.com>
2019-09-26 13:03:52 -0700

    Add fixme counts
    Summary: Add support for counting fix mes + small refactor to move all relevant code into the same file.
    
    Reviewed By: grievejia
    
    Differential Revision: D17528734
    
    fbshipit-source-id: 729c9c1a92a010667838dab539c7ea576c581ed5

--- client/commands/statistics.py
+++ client/commands/statistics.py
@@ -6,19 +6,126 @@
 # pyre-strict
 
 import argparse
+import json
+import re
+from collections import defaultdict
 from pathlib import Path
-from typing import List
+from typing import Dict, List, Sequence
 
+import libcst as cst
+
+from .. import log
 from ..configuration import Configuration
-from ..count_annotations import TypeCollector, run
 from ..filesystem import AnalysisDirectory
 from .command import Command
 
 
-def _run_count_annotations(paths: List[Path]) -> TypeCollector:
-    collector = TypeCollector()
+class StatisticsCollector(cst.CSTVisitor):
+    def build_json(self) -> Dict[str, int]:
+        return {}
+
+
+class AnnotationCountCollector(StatisticsCollector):
+    def __init__(
+        self,
+        return_count: int = 0,
+        annotated_return_count: int = 0,
+        globals_count: int = 0,
+        annotated_globals_count: int = 0,
+        parameter_count: int = 0,
+        annotated_parameter_count: int = 0,
+    ) -> None:
+        self.return_count = return_count
+        self.annotated_return_count = annotated_return_count
+        self.globals_count = globals_count
+        self.annotated_globals_count = annotated_globals_count
+        self.parameter_count = parameter_count
+        self.annotated_parameter_count = annotated_parameter_count
+
+    def build_json(self) -> Dict[str, int]:
+        return {
+            "return_count": self.return_count,
+            "annotated_return_count": self.annotated_return_count,
+            "globals_count": self.globals_count,
+            "annotated_globals_count": self.annotated_globals_count,
+            "parameter_count": self.parameter_count,
+            "annotated_parameter_count": self.annotated_parameter_count,
+        }
+
+    def _check_parameter_annotations(self, parameters: Sequence[cst.Param]) -> None:
+        for parameter in list(parameters):
+            self.parameter_count += 1
+            annotation = parameter.annotation
+            if annotation is not None:
+                self.annotated_parameter_count += 1
+
+    def visit_FunctionDef(self, node: cst.FunctionDef) -> None:
+        self.return_count += 1
+        if node.returns is not None:
+            self.annotated_return_count += 1
+
+        self._check_parameter_annotations(node.params.default_params)
+        self._check_parameter_annotations(node.params.params)
+
+    def visit_Assign(self, node: cst.Assign) -> None:
+        self.globals_count += 1
+
+    def visit_AnnAssign(self, node: cst.AnnAssign) -> None:
+        self.globals_count += 1
+        self.annotated_globals_count += 1
+
+
+class FixmeCountCollector(StatisticsCollector):
+    def __init__(self, fixme_counts: Dict[str, int] = defaultdict(int)) -> None:  # noqa
+        self.fixme_counts = fixme_counts
+        self.fixme_regex = "# pyre-fixme\[(\d*)\]:"  # noqa
+
+    def visit_Comment(self, node: cst.Comment) -> None:
+        match = re.match(self.fixme_regex, node.value)
+        if match:
+            self.fixme_counts[match.group(1)] += 1
+
+    def build_json(self) -> Dict[str, int]:
+        return dict(self.fixme_counts)
+
+
+def _get_paths(target_directory: Path) -> List[Path]:
+    return [
+        path
+        for path in target_directory.glob("**/*.py")
+        if not path.name.startswith("__") and not path.name.startswith(".")
+    ]
+
+
+def _parse_directory(directory: Path) -> List[cst.Module]:
+    files = _get_paths(directory)
+    new_files = []
+    for file in files:
+        new_files.append(_parse_file(file))
+    return new_files
+
+
+def _parse_file(path: Path) -> cst.Module:
+    return cst.parse_module(path.read_text())
+
+
+def _parse_paths(paths: List[Path]) -> List[cst.Module]:
+    parsed_paths = []
     for path in paths:
-        run(path, collector)
+        if path.is_dir():
+            parsed_directory_paths = _parse_directory(path)
+            for path in parsed_directory_paths:
+                parsed_paths.append(path)
+        else:
+            parsed_paths.append(_parse_file(path))
+    return parsed_paths
+
+
+def _count(
+    paths: List[cst.Module], collector: StatisticsCollector
+) -> StatisticsCollector:
+    for path in paths:
+        path.visit(collector)
     return collector
 
 
@@ -50,7 +157,12 @@ class Statistics(Command):
 
     def _run(self) -> None:
         self._analysis_directory.prepare()
-        annotations = _run_count_annotations(
-            _find_paths(self._local_configuration, self._filter_paths)
-        )
-        annotations.print_results()
+        paths = _find_paths(self._local_configuration, self._filter_paths)
+        parsed_paths = _parse_paths(paths)
+        annotations = _count(parsed_paths, AnnotationCountCollector())
+        fixmes = _count(parsed_paths, FixmeCountCollector())
+        as_json = {
+            "annotations": annotations.build_json(),
+            "fixmes": fixmes.build_json(),
+        }
+        log.stdout.write(json.dumps(as_json))
--- client/commands/tests/statistics_test.py
+++ client/commands/tests/statistics_test.py
@@ -5,11 +5,16 @@
 
 # pyre-strict
 
+import textwrap
 import unittest
+from collections import defaultdict
 from pathlib import Path
+from typing import Dict
+
+from libcst import Module, parse_module
 
 from ... import commands  # noqa
-from ..statistics import _find_paths
+from ..statistics import AnnotationCountCollector, FixmeCountCollector, _find_paths
 from .command_test import mock_arguments
 
 
@@ -33,4 +38,85 @@ class StatisticsTest(unittest.TestCase):
         self.assertEqual(
             _find_paths(arguments.local_configuration, arguments.filter_paths),
             [Path("example/path/client/a.py"), Path("example/path/client/b.py")],
+        )
+
+
+class AnnotationCountCollectorTest(unittest.TestCase):
+    @staticmethod
+    def format_files(source: str) -> Module:
+        return parse_module(textwrap.dedent(source.rstrip()))
+
+    def assert_counts(self, source: str, expected: Dict[str, int]) -> None:
+        source_module = self.format_files(source)
+        collector = AnnotationCountCollector()
+        source_module.visit(collector)
+        self.assertEqual(collector.build_json(), expected)
+
+    def test_annotate_functions(self) -> None:
+        self.assert_counts(
+            """
+            def foo(x) -> int:
+                pass
+            """,
+            {
+                "annotated_return_count": 1,
+                "annotated_globals_count": 0,
+                "annotated_parameter_count": 0,
+                "return_count": 1,
+                "globals_count": 0,
+                "parameter_count": 1,
+            },
+        )
+
+        self.assert_counts(
+            """
+            def bar(x: int, y):
+                pass
+            """,
+            {
+                "annotated_return_count": 0,
+                "annotated_globals_count": 0,
+                "annotated_parameter_count": 1,
+                "return_count": 1,
+                "globals_count": 0,
+                "parameter_count": 2,
+            },
+        )
+
+        self.assert_counts(
+            """
+            a = foo()
+            b: int = bar()
+            """,
+            {
+                "annotated_return_count": 0,
+                "annotated_globals_count": 1,
+                "annotated_parameter_count": 0,
+                "return_count": 0,
+                "globals_count": 2,
+                "parameter_count": 0,
+            },
+        )
+
+
+class FixmeCountCollectorTest(unittest.TestCase):
+    @staticmethod
+    def format_files(source: str) -> Module:
+        return parse_module(textwrap.dedent(source.rstrip()))
+
+    def assert_counts(self, source: str, expected: Dict[str, int]) -> None:
+        source_module = self.format_files(source)
+        collector = FixmeCountCollector(defaultdict(int))
+        source_module.visit(collector)
+        self.assertEqual(collector.build_json(), expected)
+
+    def test_annotate_functions(self) -> None:
+        self.assert_counts("# pyre-fixme[2]: Example Error Message", {"2": 1})
+        self.assert_counts(
+            "# pyre-fixme[3]: Example Error Message \n\n\n # pyre-fixme[34]: Example",
+            {"3": 1, "34": 1},
+        )
+        self.assert_counts(
+            "# pyre-fixme[2]: Example Error Message\n\n\n# pyre-fixme[2]: message",
+            {"2": 2},
         )
