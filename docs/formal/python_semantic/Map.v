c3d3c6719274dacba0efa26d506771d75330908f
Vincent Siles <vsiles@fb.com>
2019-10-29 09:05:40 -0700

    Adding some helpers lemma to Map.v
    Summary: minor helpers lemma to deal with sequence of `set`/`get` in maps.
    
    Reviewed By: grievejia
    
    Differential Revision: D18169184
    
    fbshipit-source-id: a56b005742a049c7e47d7b510b572a7d16846473

--- docs/formal/python_semantic/Map.v
+++ docs/formal/python_semantic/Map.v
@@ -22,6 +22,13 @@ Definition set_map (map: Map) id (data: A) : Map :=
 Definition unset_map (map: Map) id : Map :=
     fun x => if String.eqb id x then None else get_map map x.
 
+Lemma get_set_map: forall map x y a,
+  get_map (set_map map x a) y =
+  if  String.eqb x y then Some a else get_map map y.
+Proof.
+now  unfold set_map, get_map; simpl.
+Qed.
+
 (** Intermediate "state", which will be used to track types or values
 associated with identifiers *)
 Record RawState0: Set := mkRawState0 {
@@ -34,6 +41,17 @@ Definition get0 s k := get_map (local s) k.
 
 Definition set0 st k (v: A) :=
     mkRawState0 (set_map (local st) k v).
+
+Definition unset0 st k :=
+    mkRawState0 (unset_map (local st) k).
+
+Lemma get0_set0: forall s x y a,
+  get0 (set0 s x a) y =
+  if  String.eqb x y then Some a else get0 s y.
+Proof.
+intros s x y a; unfold set0, get0; simpl.
+now rewrite get_set_map.
+Qed.
 
 End Map.
 
@@ -53,6 +71,16 @@ Definition Empty (info: Map B) : RawState := mkRawState (Empty0 _) info.
 Definition get (s: RawState) id := get0 _ (state s) id.
 Definition set (s: RawState) id v :=
     mkRawState (set0 _ (state s) id v) (info s).
+Definition unset (s: RawState) id :=
+    mkRawState (unset0 _ (state s) id) (info s).
 
 Definition get_info (s: RawState) f := get_map _ (info s) f.
+
+Lemma get_set: forall s x y a,
+  get (set s x a) y =
+  if  String.eqb x y then Some a else get s y.
+intros [s ?] x y a; simpl.
+unfold set, get, set0, get0; simpl.
+now rewrite get_set_map.
+Qed.
 End RawState.
