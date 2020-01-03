46d09d9655b456eb586056ed39291e669af8fa84
Graham Bleaney <gbleaney@fb.com>
2020-01-03 13:13:22 -0800

    Add third exercise of Pysa tutorial
    Summary: This exercise introduces the concept of sanitizers.
    
    Reviewed By: sinancepel
    
    Differential Revision: D19237366
    
    fbshipit-source-id: 1d34ab40c8d35802862ad6f08d7998e6816b25e5

---
+++ pysa_tutorial/exercise3/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpRequest, HttpResponse
+
+
+def example_sanitizer():
+    ...
+
+
+def get_operator_safe(request: HttpRequest) -> str:
+    operator = request.POST["operator"]
+
+    assert operator in {"+", "-", "*", "/"}
+
+    return operator
+
+
+def operate_on_twos(request: HttpRequest) -> HttpResponse:
+    operator = get_operator_safe(request)
+
+    result = eval(f"2 {operator} 2")  # noqa: P204
+
+    return result
+
+
+def operate_on_threes(request: HttpRequest) -> HttpResponse:
+    operator = request.GET["operator"]
+
+    assert operator in {"+", "-", "*", "/"}
+
+    exec(f"result = 3 {operator} 3")
+
+    return result  # noqa: F821
