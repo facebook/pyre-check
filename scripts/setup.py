c62a1eb417785330a446b82d84d82592910f6922
Sinan Cepel <sinancepel@fb.com>
2020-01-08 12:55:20 -0800

    refactor find_typeshed_root in setup.py
    Summary:
    The context here is that in a later diff, we'll want to publish more stubs with the pyre-check package.
    
    This refactor makes accomplishing that easier.
    
    Reviewed By: MaggieMoss
    
    Differential Revision: D19314944
    
    fbshipit-source-id: 0f1fa288f66ccde9f29bed16b2348f47124bac30

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,6 +2,7 @@ import glob
 import json
 import os
 import sys
+from pathlib import Path
 from typing import List, Tuple
 
 from setuptools import find_packages, setup
@@ -11,24 +12,22 @@ if sys.version_info < (3, 5):
     sys.exit("Error: pyre-check only runs on Python 3.5 and above.")
 
 
-def get_data_files(base, directory, extension_glob) -> Tuple[str, List[str]]:
+def get_data_files(directory: str, extension_glob: str) -> Tuple[str, List[str]]:
     # We need to relativize data_files, see https://github.com/pypa/wheel/issues/92.
-    relative_directory = os.path.relpath(os.path.abspath(directory), base)
+    relative_directory = os.path.relpath(os.path.abspath(directory), os.getcwd())
     return (
         relative_directory,
         glob.glob(os.path.join(relative_directory, extension_glob)),
     )
 
 
-def find_typeshed_files():
-    base = os.getcwd()
-    typeshed_root = os.path.join(base, "typeshed")
-    if not os.path.isdir(typeshed_root):
+def get_all_stubs(root: str) -> List[Tuple[str, List[str]]]:
+    if not os.path.isdir(root):
         return []
     result = []
-    for absolute_directory, _, _ in os.walk(typeshed_root):
+    for absolute_directory, _, _ in os.walk(root):
         relative_directory, files = get_data_files(
-            base=base, directory=absolute_directory, extension_glob="*.pyi"
+            directory=absolute_directory, extension_glob="*.pyi"
         )
         if not files:
             continue
@@ -38,12 +37,11 @@ def find_typeshed_files():
 
 
 def find_taint_stubs():
-    base = os.getcwd()
     _, taint_stubs = get_data_files(
-        base=base, directory=os.path.join(base, "taint"), extension_glob="*"
+        directory=os.path.join(os.getcwd(), "taint"), extension_glob="*"
     )
     _, third_party_taint_stubs = get_data_files(
-        base=base, directory=os.path.join(base, "third_party_taint"), extension_glob="*"
+        directory=os.path.join(os.getcwd(), "third_party_taint"), extension_glob="*"
     )
     taint_stubs += third_party_taint_stubs
     if not taint_stubs:
@@ -91,7 +89,9 @@ setup(
     ],
     keywords="typechecker development",
     packages=find_packages(exclude=["tests", "pyre-check"]),
-    data_files=[("bin", ["bin/pyre.bin"])] + find_typeshed_files() + find_taint_stubs(),
+    data_files=[("bin", ["bin/pyre.bin"])]
+    + get_all_stubs(root=Path.cwd() / "typeshed")
+    + find_taint_stubs(),
     python_requires=">=3.5",
     install_requires=["pywatchman", "psutil", "libcst", "pyre_extensions"],
     extras_require=extra_requirements,
