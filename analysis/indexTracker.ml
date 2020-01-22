4e3cfafd48228ac0be0a06756da54049b7669907
Sinan Cepel <sinancepel@fb.com>
2020-01-22 14:12:11 -0800

    fill out missing license headers
    Reviewed By: grievejia
    
    Differential Revision: D19518883
    
    fbshipit-source-id: d4ef5954216600f255050bfbe360901212434289

--- analysis/indexTracker.ml
+++ analysis/indexTracker.ml
@@ -1,3 +1,8 @@
+(* Copyright (c) 2019-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. *)
+
 open Core
 
 type t = int [@@deriving eq, compare, show, sexp, hash]
