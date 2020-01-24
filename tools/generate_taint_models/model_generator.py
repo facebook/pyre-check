ba4aa0900a39e39bd30557b2e8e8650b430223cb
Sinan Cepel <sinancepel@fb.com>
2020-01-23 17:00:57 -0800

    remove Configuration calss
    Reviewed By: gbleaney
    
    Differential Revision: D19520382
    
    fbshipit-source-id: c35030f33045df827c5ce213f758487b9ee81f88

--- tools/generate_taint_models/model_generator.py
+++ tools/generate_taint_models/model_generator.py
@@ -55,26 +55,3 @@ class ModelGenerator(ABC):
 
     def generate_models(self) -> Set[Model]:
         return set(self.compute_models(self.gather_functions_to_model()))
-
-
-class Configuration:
-    # Arguments that can be supplied through argparse and callers - these are globals to
-    # allow registering generators at the definition point and not having to repeat the
-    # arguments for similar generators.
-    urls_module: ClassVar[Optional[str]] = None
-    # pyre-ignore[4]: Too dynamic.
-    url_pattern_type: ClassVar[Type[Any]] = object
-    # pyre-ignore[4]: Too dynamic.
-    url_resolver_type: ClassVar[Type[Any]] = object
-    # pyre-ignore[4]: Too dynamic.
-    graphql_object_type: ClassVar[Type[Any]] = object
-    whitelisted_classes: ClassVar[List[str]] = []
-    whitelisted_views: ClassVar[List[str]] = []
-    root: ClassVar[str] = ""
-    stub_root: ClassVar[Optional[str]] = ""
-    graphql_module: ClassVar[Union[List[str], str]] = ""
-    blacklisted_globals: ClassVar[Set[str]] = set()
-    blacklisted_global_directories: ClassVar[Set[str]] = set()
-    annotation_specifications: ClassVar[List[DecoratorAnnotationSpecification]] = []
-    logger: ClassVar[Optional[str]] = None
-    classes_to_taint: ClassVar[Optional[List[str]]] = []
