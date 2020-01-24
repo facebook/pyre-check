1de7b4cd3faf6d2758310e52a71df960c455c746
Maggie Moss <maggiemoss@fb.com>
2020-01-24 14:10:32 -0800

    Add helpers and basic setup functions (2/n)
    Summary:
    Add helpers for locating the `pyre_check` and the `scripts` directories.
    
    Add functionality to create the temporary build_root directory and add the necessary directories and __init__ files.
    
    Added appropriate tests.
    
    Reviewed By: sinancepel
    
    Differential Revision: D19547099
    
    fbshipit-source-id: b205df9c04db3ab3cc6684228bf171571071f644

--- scripts/build_pypi_package.py
+++ scripts/build_pypi_package.py
@@ -8,7 +8,17 @@
 import argparse
 import platform
 import re
+import tempfile
 from pathlib import Path
+
+
+MODULE_NAME = "pyre_check"
+
+
+PYRE_CHECK_DIRECTORY: Path = Path(__file__).parent.parent
+STUBS_DIRECTORY: Path = PYRE_CHECK_DIRECTORY / "stubs"
+CLIENT_DIRECTORY: Path = PYRE_CHECK_DIRECTORY / "client"
+UPGRADE_DIRECTORY: Path = PYRE_CHECK_DIRECTORY / "tools/upgrade"
 
 
 def is_linux() -> bool:
@@ -32,12 +42,37 @@ def valid_version(version: str) -> str:
     raise ValueError("Invalid version format.")
 
 
+# sapp directory is either beside or inside pyre-check directory
+def sapp_directory() -> Path:
+    path = PYRE_CHECK_DIRECTORY / "tools/sapp"
+    if not path.is_dir():
+        path = PYRE_CHECK_DIRECTORY.parent / "sapp"
+    return path
+
+
+def mkdir_and_init(module_path: Path) -> None:
+    module_path.mkdir()
+    (module_path / "__init__.py").touch()
+
+
+def add_init_files(build_root: str) -> None:
+    # setup.py sdist will refuse to work for directories without a `__init__.py`.
+    build_path = Path(build_root)
+    module_path = build_path / MODULE_NAME
+    mkdir_and_init(module_path)
+    mkdir_and_init(module_path / "tools")
+    mkdir_and_init(module_path / "tools/upgrade")
+    (module_path / "client").mkdir()
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(description="Build a PyPi Package.")
     parser.add_argument("--typeshed-path", type=valid_typeshed, required=True)
     parser.add_argument("--version", type=valid_version, required=True)
 
     _ = parser.parse_args()
+    with tempfile.TemporaryDirectory() as build_root:
+        add_init_files(build_root)
 
 
 if __name__ == "__main__":
--- scripts/tests/build_pypi_package_test.py
+++ scripts/tests/build_pypi_package_test.py
@@ -5,9 +5,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
+import tempfile
 import unittest
+from pathlib import Path
 
-from ..build_pypi_package import valid_version
+from ..build_pypi_package import add_init_files, valid_version
 
 
 class TestArgumentValidationMethods(unittest.TestCase):
@@ -15,3 +18,19 @@ class TestArgumentValidationMethods(unittest.TestCase):
         self.assertEqual(valid_version("0.0.01"), "0.0.01")
         with self.assertRaises(ValueError):
             valid_version("x0.0.01")
+
+
+class TestCreatingWheel(unittest.TestCase):
+    def test_setup_skeleton(self) -> None:
+        with tempfile.TemporaryDirectory() as build_root:
+            add_init_files(build_root)
+            # Assert the expected __init__ files are present
+            path = Path(build_root)
+            self.assertEqual(
+                [str(path) for path in path.glob("**/*.py")],
+                [
+                    build_root + "/pyre_check/__init__.py",
+                    build_root + "/pyre_check/tools/__init__.py",
+                    build_root + "/pyre_check/tools/upgrade/__init__.py",
+                ],
+            )
