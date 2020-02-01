ea03f10d8972c247949f0eb91b410db9973132c7
Mark Mendoza <markamendoza@fb.com>
2020-01-31 17:18:42 -0800

    Simplifying AnnotatedClass.overrides
    Summary:
    These two forms are actually equivalent due to the special casing on constraints when the instantiated type is Primitive parent.
    
    Doing it all in one shot clarifies what we actually mean here, which is to find the attribute in a parent, with generics propagated.
    
    Note that this creates a small regression in full check time, due to cache misses from that instantiated type.  That impact is removed by late instantiation.
    
    Reviewed By: grievejia
    
    Differential Revision: D19668655
    
    fbshipit-source-id: 4b63c44c1dac5100845a8574027fa1821f4f8c94

--- analysis/annotatedClass.ml
+++ analysis/annotatedClass.ml
@@ -173,27 +173,14 @@ let has_explicit_constructor class_name ~resolution =
 
 let overrides definition ~resolution ~name =
   let find_override parent =
-    let attribute =
-      GlobalResolution.attribute_from_class_name
-        ~transitive:false
-        ~class_attributes:true
-        ~name
-        parent
-        ~resolution
-        ~instantiated:(Type.Primitive parent)
-    in
-    match attribute with
-    | Some attribute when AnnotatedAttribute.defined attribute ->
-        annotation definition
-        |> (fun instantiated ->
-             GlobalResolution.constraints ~target:parent ~resolution ~instantiated ())
-        |> (fun solution ->
-             AnnotatedAttribute.instantiate
-               ~constraints:(fun annotation ->
-                 Some (TypeConstraints.Solution.instantiate solution annotation))
-               attribute)
-        |> Option.some
-    | _ -> None
+    GlobalResolution.attribute_from_class_name
+      ~transitive:false
+      ~class_attributes:true
+      ~name
+      parent
+      ~resolution
+      ~instantiated:(annotation definition)
+    >>= fun attribute -> Option.some_if (AnnotatedAttribute.defined attribute) attribute
   in
   GlobalResolution.successors definition ~resolution |> List.find_map ~f:find_override
 
