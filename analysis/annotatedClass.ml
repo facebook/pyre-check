3621458f2672c9c6849583ed3b098fdb83980ee9
Mark Mendoza <markamendoza@fb.com>
2020-02-12 10:16:55 -0800

    Make fallback attribute avoid unnecessary class summary lookup
    Reviewed By: grievejia
    
    Differential Revision: D19824145
    
    fbshipit-source-id: beb8a929facfc1b54f1817128bd4967616122c51

--- analysis/annotatedClass.ml
+++ analysis/annotatedClass.ml
@@ -35,110 +35,104 @@ let implicit_attributes { Node.value = { ClassSummary.attribute_components; _ };
 
 
 let fallback_attribute ~(resolution : Resolution.t) ~name class_name =
-  match
-    GlobalResolution.class_definition
-      (Resolution.global_resolution resolution)
-      (Primitive class_name)
-  with
-  | Some { Node.value = { ClassSummary.name = class_name_reference; _ }; _ } -> (
-      let compound_backup =
-        let name =
-          match name with
-          | "__iadd__" -> Some "__add__"
-          | "__isub__" -> Some "__sub__"
-          | "__imul__" -> Some "__mul__"
-          | "__imatmul__" -> Some "__matmul__"
-          | "__itruediv__" -> Some "__truediv__"
-          | "__ifloordiv__" -> Some "__floordiv__"
-          | "__imod__" -> Some "__mod__"
-          | "__idivmod__" -> Some "__divmod__"
-          | "__ipow__" -> Some "__pow__"
-          | "__ilshift__" -> Some "__lshift__"
-          | "__irshift__" -> Some "__rshift__"
-          | "__iand__" -> Some "__and__"
-          | "__ixor__" -> Some "__xor__"
-          | "__ior__" -> Some "__or__"
-          | _ -> None
-        in
-        match name with
-        | Some name ->
-            GlobalResolution.attribute_from_class_name
-              ~resolution:(Resolution.global_resolution resolution)
-              class_name
-              ~class_attributes:false
-              ~transitive:true
-              ~instantiated:(Type.Primitive class_name)
-              ~name
-        | _ -> None
-      in
-      let getitem_backup () =
-        let fallback =
-          GlobalResolution.attribute_from_class_name
-            class_name
-            ~class_attributes:true
-            ~transitive:true
-            ~resolution:(Resolution.global_resolution resolution)
-            ~name:"__getattr__"
-            ~instantiated:(Type.Primitive class_name)
-        in
-        match fallback with
-        | Some fallback when AnnotatedAttribute.defined fallback -> (
-            let annotation = fallback |> AnnotatedAttribute.annotation |> Annotation.annotation in
-            match annotation with
-            | Type.Callable ({ implementation; _ } as callable) ->
-                let arguments =
-                  let self_argument =
+  let class_name_reference = Reference.create class_name in
+  let compound_backup =
+    let name =
+      match name with
+      | "__iadd__" -> Some "__add__"
+      | "__isub__" -> Some "__sub__"
+      | "__imul__" -> Some "__mul__"
+      | "__imatmul__" -> Some "__matmul__"
+      | "__itruediv__" -> Some "__truediv__"
+      | "__ifloordiv__" -> Some "__floordiv__"
+      | "__imod__" -> Some "__mod__"
+      | "__idivmod__" -> Some "__divmod__"
+      | "__ipow__" -> Some "__pow__"
+      | "__ilshift__" -> Some "__lshift__"
+      | "__irshift__" -> Some "__rshift__"
+      | "__iand__" -> Some "__and__"
+      | "__ixor__" -> Some "__xor__"
+      | "__ior__" -> Some "__or__"
+      | _ -> None
+    in
+    match name with
+    | Some name ->
+        GlobalResolution.attribute_from_class_name
+          ~resolution:(Resolution.global_resolution resolution)
+          class_name
+          ~class_attributes:false
+          ~transitive:true
+          ~instantiated:(Type.Primitive class_name)
+          ~name
+    | _ -> None
+  in
+  let getitem_backup () =
+    let fallback =
+      GlobalResolution.attribute_from_class_name
+        class_name
+        ~class_attributes:true
+        ~transitive:true
+        ~resolution:(Resolution.global_resolution resolution)
+        ~name:"__getattr__"
+        ~instantiated:(Type.Primitive class_name)
+    in
+    match fallback with
+    | Some fallback when AnnotatedAttribute.defined fallback -> (
+        let annotation = fallback |> AnnotatedAttribute.annotation |> Annotation.annotation in
+        match annotation with
+        | Type.Callable ({ implementation; _ } as callable) ->
+            let arguments =
+              let self_argument =
+                {
+                  Call.Argument.name = None;
+                  value = from_reference ~location:Location.any class_name_reference;
+                }
+              in
+              let name_argument =
+                {
+                  Call.Argument.name = None;
+                  value =
                     {
-                      Call.Argument.name = None;
-                      value = from_reference ~location:Location.any class_name_reference;
-                    }
-                  in
-                  let name_argument =
-                    {
-                      Call.Argument.name = None;
-                      value =
-                        {
-                          Node.location = Location.any;
-                          value = Expression.String (StringLiteral.create name);
-                        };
-                    }
-                  in
-                  [self_argument; name_argument]
-                in
-                let implementation =
-                  match
-                    GlobalResolution.signature_select
-                      ~global_resolution:(Resolution.global_resolution resolution)
-                      ~resolve:(Resolution.resolve resolution)
-                      ~arguments
-                      ~callable
-                  with
-                  | AttributeResolution.Found { Type.Callable.implementation; _ } -> implementation
-                  | AttributeResolution.NotFound _ -> implementation
-                in
-                let return_annotation = Type.Callable.Overload.return_annotation implementation in
-                Some
-                  (AnnotatedAttribute.create
-                     ~annotation:return_annotation
-                     ~original_annotation:return_annotation
-                     ~abstract:false
-                     ~async:false
-                     ~class_attribute:false
-                     ~defined:true
-                     ~initialized:NotInitialized
-                     ~name
-                     ~parent:(Reference.show class_name_reference)
-                     ~visibility:ReadWrite
-                     ~property:false
-                     ~static:false
-                     ~has_ellipsis_value:true)
-            | _ -> None )
-        | _ -> None
-      in
-      match compound_backup with
-      | Some backup when AnnotatedAttribute.defined backup -> Some backup
-      | _ -> getitem_backup () )
-  | None -> None
+                      Node.location = Location.any;
+                      value = Expression.String (StringLiteral.create name);
+                    };
+                }
+              in
+              [self_argument; name_argument]
+            in
+            let implementation =
+              match
+                GlobalResolution.signature_select
+                  ~global_resolution:(Resolution.global_resolution resolution)
+                  ~resolve:(Resolution.resolve resolution)
+                  ~arguments
+                  ~callable
+              with
+              | AttributeResolution.Found { Type.Callable.implementation; _ } -> implementation
+              | AttributeResolution.NotFound _ -> implementation
+            in
+            let return_annotation = Type.Callable.Overload.return_annotation implementation in
+            Some
+              (AnnotatedAttribute.create
+                 ~annotation:return_annotation
+                 ~original_annotation:return_annotation
+                 ~abstract:false
+                 ~async:false
+                 ~class_attribute:false
+                 ~defined:true
+                 ~initialized:NotInitialized
+                 ~name
+                 ~parent:(Reference.show class_name_reference)
+                 ~visibility:ReadWrite
+                 ~property:false
+                 ~static:false
+                 ~has_ellipsis_value:true)
+        | _ -> None )
+    | _ -> None
+  in
+  match compound_backup with
+  | Some backup when AnnotatedAttribute.defined backup -> Some backup
+  | _ -> getitem_backup ()
 
 
 let has_explicit_constructor class_name ~resolution =
