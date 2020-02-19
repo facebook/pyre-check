bd7e651a497cc264d92a14bb44e65e54360f49b0
David Caro <dcaro@fb.com>
2020-02-19 02:15:34 -0800

    Add missing params to the lxml.etree tostring stub
    Summary:
    Added all the parameters as defined here:
    https://lxml.de/api/lxml.etree-module.html#tostring
    
    Reviewed By: grievejia
    
    Differential Revision: D19940653
    
    fbshipit-source-id: b9a85b33da78f2f0c4efc7f5bd94b69dcb7c83d4

--- stubs/lxml/etree.pyi
+++ stubs/lxml/etree.pyi
@@ -1,7 +1,20 @@
-from typing import Any
+from typing import Any, List, Optional
 
 # Lazy hack to not have to stub those out.
 from lxml.unstubbed import Element, SubElement, _Element, parse
 
 def fromstring(input: str) -> Any: ...
-def tostring(input: Any) -> str: ...
+def tostring(
+    element_or_tree: Any,
+    encoding: Optional[str] = ...,
+    method: str = ...,
+    xml_declaration: Optional[bool] = ...,
+    pretty_print: bool = ...,
+    with_tail: bool = ...,
+    standalone: Optional[bool] = ...,
+    doctype: Optional[str] = ...,
+    exclusive: bool = ...,
+    inclusive_ns_prefixes: Optional[List[str]] = ...,
+    with_comments: bool = ...,
+    strip_text: bool = ...,
+) -> bytes: ...
