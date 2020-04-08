92c4fc8f582c492dff76bdc095f25c1bfd647746
Maggie Moss <maggiemoss@fb.com>
2020-04-08 12:44:39 -0700

    Add helper method for determining a null server
    Reviewed By: dkgi
    
    Differential Revision: D20524880
    
    fbshipit-source-id: a92560c9ff891a19e4fa8850c402280a099b5723

--- lsp_adapter/main.py
+++ lsp_adapter/main.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import argparse
 import asyncio
 import json
 import re
@@ -13,22 +14,39 @@ from typing import Optional, Union
 from ..client.json_rpc import Response
 
 
+def _should_run_null_server(null_server_flag: bool) -> bool:
+    # TODO[T58989824]: We also need to check if the project can be run here.
+    # Needs updating to mimic the current implementation (i.e. catch the buck errors)
+    return null_server_flag
+
+
 def _null_initialize_response(request_id: Optional[Union[str, int]]) -> None:
     response = Response(id=request_id, result={"capabilities": {}})
     response.write(sys.stdout.buffer)
 
 
-class AdapterProtocol(asyncio.Protocol):
+class NullServerAdapterProtocol(asyncio.Protocol):
     def data_received(self, data: bytes) -> None:
         body = re.sub(r"Content-Length:.+\d*\r\n\r\n", "", data.decode("utf-8"))
         json_body = json.loads(body)
         _null_initialize_response(json_body["id"])
 
 
-def main() -> None:
+class AdapterProtocol(asyncio.Protocol):
+    def data_received(self, data: bytes) -> None:
+        # TODO[T58989824]: Send request to running pyre server
+        pass
+
+
+def main(arguments: argparse.Namespace) -> None:
     loop: AbstractEventLoop = asyncio.get_event_loop()
     try:
-        stdin_pipe_reader = loop.connect_read_pipe(AdapterProtocol, sys.stdin)
+        if _should_run_null_server(arguments.null_server):
+            stdin_pipe_reader = loop.connect_read_pipe(
+                NullServerAdapterProtocol, sys.stdin
+            )
+        else:
+            stdin_pipe_reader = loop.connect_read_pipe(AdapterProtocol, sys.stdin)
         loop.run_until_complete(stdin_pipe_reader)
         loop.run_forever()
     finally:
@@ -36,4 +54,7 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="LSP adapter for Pyre.")
+    parser.add_argument("--null-server", default=False, action="store_true")
+    arguments: argparse.Namespace = parser.parse_args()
+    main(arguments)
--- lsp_adapter/tests/main_test.py
+++ lsp_adapter/tests/main_test.py
@@ -7,14 +7,14 @@ import sys
 import unittest
 from unittest.mock import MagicMock, patch
 
-from ..main import AdapterProtocol
+from ..main import NullServerAdapterProtocol
 
 
 class AdapterProtocolTest(unittest.TestCase):
     @patch.object(sys.stdout.buffer, "write")
     def test_run_null_server(self, stdout_write: MagicMock) -> None:
         example_request = b'Content-Length: 2756\r\n\r\n{"jsonrpc":"2.0","id":0,"method":"initialize","params":{"processId":null}}'  # noqa
-        adapter = AdapterProtocol()
+        adapter = NullServerAdapterProtocol()
         adapter.data_received(example_request)
         stdout_write.assert_called_once_with(
             b'Content-Length: 59\r\n\r\n{"jsonrpc": "2.0", "id": 0, "result": {"capabilities": {}}}'  # noqa
