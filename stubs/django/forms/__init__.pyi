536cf7773c246b45ab8ef348642472af32ce9be6
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-05-10 00:24:25 -0400

    Add initial stubs for Django forms
    There's quite a bit more than can be annotated for `django.forms`, but
    this is a jumping off point. It adds a handful of input types and a
    couple of fields, along with `Form` and `ModelForm`.

---
+++ stubs/django/forms/__init__.pyi
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+from django.forms.fields import BooleanField, EmailField
+from django.forms.forms import Form
+from django.forms.models import ModelForm
+from django.forms.widgets import (
+    CheckboxInput,
+    EmailInput,
+    HiddenInput,
+    NumberInput,
+    TextInput,
+    URLInput,
+)
