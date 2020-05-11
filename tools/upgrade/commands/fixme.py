e12355488d8f054738a63512dd4d9f0d04d318ef
Dominik Gabi <dominik@fb.com>
2020-05-11 10:32:55 -0700

    explicit constructor for `Fixme`
    Reviewed By: shannonzhu
    
    Differential Revision: D21488635
    
    fbshipit-source-id: e2c759905b17e42419246f54a18017eee104b6c6

--- tools/upgrade/commands/fixme.py
+++ tools/upgrade/commands/fixme.py
@@ -3,12 +3,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import argparse
 import json
 import logging
 from typing import Optional
 
 from ..configuration import Configuration
 from ..errors import Errors
+from ..repository import Repository
 from .command import ErrorSuppressingCommand
 
 
@@ -26,15 +28,21 @@ def _errors_from_run(only_fix_error_code: Optional[int] = None) -> Errors:
 
 
 class Fixme(ErrorSuppressingCommand):
+    def __init__(self, arguments: argparse.Namespace, repository: Repository) -> None:
+        super().__init__(arguments, repository)
+        self._lint: bool = arguments.lint
+        self._error_source: str = arguments.error_source
+        self._only_fix_error_code: Optional[int] = arguments.only_fix_error_code
+
     def run(self) -> None:
         # Suppress errors in project with no local configurations.
-        if self._arguments.error_source == "generate":
-            errors = _errors_from_run(self._arguments.only_fix_error_code)
+        if self._error_source == "generate":
+            errors = _errors_from_run(self._only_fix_error_code)
             self._suppress_errors(errors)
 
-            if self._arguments.lint:
+            if self._lint:
                 if self._repository.format():
-                    errors = _errors_from_run(self._arguments.only_fix_error_code)
+                    errors = _errors_from_run(self._only_fix_error_code)
                     self._suppress_errors(errors)
         else:
             errors = Errors.from_stdin(self._arguments.only_fix_error_code)
