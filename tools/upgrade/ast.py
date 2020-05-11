d96c54b49e27677c40a2a2c65be3047d3bc24f2e
Dominik Gabi <dominik@fb.com>
2020-05-11 10:32:55 -0700

    stable AST verification on strings instead of files
    Summary: Not yet used.
    
    Reviewed By: shannonzhu
    
    Differential Revision: D21485980
    
    fbshipit-source-id: af0883cf8fd535094ee6a77811b6a554b455218f

--- tools/upgrade/ast.py
+++ tools/upgrade/ast.py
@@ -11,6 +11,7 @@ from logging import Logger
 from typing import Callable
 
 from pyre_extensions import ListVariadic
+from pyre_extensions.type_variable_operators import Concatenate
 
 
 Ts = ListVariadic("Ts")
@@ -21,6 +22,27 @@ LOG: Logger = logging.getLogger(__name__)
 
 class UnstableAST(Exception):
     pass
+
+
+def check_stable(input: str, transformed: str) -> None:
+    parsed_original = ast.parse(input)
+    try:
+        parsed_transformed = ast.parse(transformed)
+        if ast.dump(parsed_original) != ast.dump(parsed_transformed):
+            raise UnstableAST("ASTs differ")
+    except SyntaxError:
+        raise UnstableAST("Could not parse transformed AST")
+
+
+def check_stable_transformation(
+    transform: "Callable[Concatenate[str, Ts], str]",
+) -> "Callable[Concatenate[str, Ts], str]":
+    def wrapper(input: str, *args: Ts) -> str:
+        transformed = transform(input, *args)
+        check_stable(input, transformed)
+        return transformed
+
+    return wrapper
 
 
 # pyre-fixme[11]: Annotation `Ts` is not defined as a type.
