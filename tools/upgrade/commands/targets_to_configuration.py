c2bdf10dc8bc4e1b88b397bcef4ef46a3b44d68a
Shannon Zhu <szhu@fb.com>
2020-05-11 20:06:31 -0700

    Move targets to configurations to its own module
    Summary: justmovingthingsaround
    
    Reviewed By: arthaud
    
    Differential Revision: D21506878
    
    fbshipit-source-id: 0a656ab8e5c216f63b5a20b8ebcb923390e19250

---
+++ tools/upgrade/commands/targets_to_configuration.py
@@ -0,0 +1,189 @@
+# Copyright (c) 2016-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import json
+import logging
+import subprocess
+from pathlib import Path
+from typing import List
+
+from ..configuration import Configuration
+from ..errors import Errors
+from ..filesystem import (
+    FilesystemException,
+    LocalMode,
+    add_local_mode,
+    find_files,
+    find_targets,
+    get_filesystem,
+    path_exists,
+    remove_non_pyre_ignores,
+)
+from ..repository import Repository
+from .command import ErrorSuppressingCommand
+
+
+LOG: logging.Logger = logging.getLogger(__name__)
+
+
+class TargetsToConfiguration(ErrorSuppressingCommand):
+    def __init__(self, arguments: argparse.Namespace, repository: Repository) -> None:
+        super().__init__(arguments, repository)
+        self._subdirectory: str = arguments.subdirectory
+        self._glob: int = arguments.glob
+        self._lint: bool = arguments.lint
+        self._fixme_threshold: int = arguments.fixme_threshold
+        self._no_commit: bool = arguments.no_commit
+        self._submit: bool = arguments.submit
+
+    def remove_target_typing_fields(self, files: List[str]) -> None:
+        LOG.info("Removing typing options from %s targets files", len(files))
+        typing_options_regex = [
+            r"typing \?=.*",
+            r"check_types \?=.*",
+            r"check_types_options \?=.*",
+            r"typing_options \?=.*",
+        ]
+        remove_typing_fields_command = [
+            "sed",
+            "-i",
+            "/" + r"\|".join(typing_options_regex) + "/d",
+        ] + files
+        subprocess.run(remove_typing_fields_command)
+
+    def convert_directory(self, directory: Path) -> None:
+        all_targets = find_targets(directory)
+        if not all_targets:
+            LOG.warning("No configuration created because no targets found.")
+            return
+        targets_files = [
+            str(directory / path)
+            for path in get_filesystem().list(str(directory), patterns=[r"**/TARGETS"])
+        ]
+        if self._glob:
+            new_targets = ["//" + str(directory) + "/..."]
+        else:
+            new_targets = []
+            for path, target_names in all_targets.items():
+                new_targets += ["//" + path + ":" + name for name in target_names]
+
+        configuration_path = directory / ".pyre_configuration.local"
+        if path_exists(str(configuration_path)):
+            LOG.warning(
+                "Pyre project already exists at %s.\n\
+                Amending targets to existing configuration.",
+                configuration_path,
+            )
+            with open(configuration_path) as configuration_file:
+                configuration = Configuration(
+                    configuration_path, json.load(configuration_file)
+                )
+                configuration.add_targets(new_targets)
+                configuration.deduplicate_targets()
+                configuration.write()
+        else:
+            LOG.info("Creating local configuration at %s.", configuration_path)
+            configuration_contents = {"targets": new_targets, "strict": True}
+            # Heuristic: if all targets with type checked targets are setting
+            # a target to be strictly checked, let's turn on default strict.
+            for targets_file in targets_files:
+                regex_patterns = [
+                    r"check_types_options \?=.*strict.*",
+                    r"typing_options \?=.*strict.*",
+                ]
+                result = subprocess.run(
+                    ["grep", "-x", r"\|".join(regex_patterns), targets_file]
+                )
+                if result.returncode != 0:
+                    configuration_contents["strict"] = False
+                    break
+            configuration = Configuration(configuration_path, configuration_contents)
+            configuration.write()
+
+            # Add newly created configuration files to version control
+            self._repository.add_paths([configuration_path])
+
+        # Remove all type-related target settings
+        self.remove_target_typing_fields(targets_files)
+        remove_non_pyre_ignores(directory)
+
+        all_errors = configuration.get_errors()
+        error_threshold = self._fixme_threshold
+        glob_threshold = self._glob
+
+        for path, errors in all_errors:
+            errors = list(errors)
+            error_count = len(errors)
+            if glob_threshold and error_count > glob_threshold:
+                # Fall back to non-glob codemod.
+                LOG.info(
+                    "Exceeding error threshold of %d; falling back to listing "
+                    "individual targets.",
+                    glob_threshold,
+                )
+                self._repository.revert_all(remove_untracked=True)
+                self._glob = None
+                return self.run()
+            if error_threshold and error_count > error_threshold:
+                LOG.info(
+                    "%d errors found in `%s`. Adding file-level ignore.",
+                    error_count,
+                    path,
+                )
+                add_local_mode(path, LocalMode.IGNORE)
+            else:
+                self._suppress_errors(Errors(errors))
+
+        # Lint and re-run pyre once to resolve most formatting issues
+        if self._lint:
+            if self._repository.format():
+                errors = configuration.get_errors(should_clean=False)
+                self._suppress_errors(errors)
+
+    def run(self) -> None:
+        # TODO(T62926437): Basic integration testing.
+        subdirectory = self._subdirectory
+        subdirectory = Path(subdirectory) if subdirectory else Path.cwd()
+        LOG.info(
+            "Converting typecheck targets to pyre configurations in `%s`", subdirectory
+        )
+
+        configurations = find_files(subdirectory, ".pyre_configuration.local")
+        configuration_directories = sorted(
+            Path(configuration.replace("/.pyre_configuration.local", ""))
+            for configuration in configurations
+        )
+        if len(configuration_directories) == 0:
+            configuration_directories = [subdirectory]
+        converted = []
+        for directory in configuration_directories:
+            if all(
+                str(directory).startswith(str(converted_directory)) is False
+                for converted_directory in converted
+            ):
+                self.convert_directory(directory)
+                converted.append(directory)
+
+        try:
+            summary = self._repository.MIGRATION_SUMMARY
+            glob = self._glob
+            if glob:
+                summary += (
+                    f"\n\nConfiguration target automatically expanded to include "
+                    f"all subtargets, expanding type coverage while introducing "
+                    f"no more than {glob} fixmes per file."
+                )
+            title = f"Convert type check targets in {subdirectory} to use configuration"
+            self._repository.submit_changes(
+                commit=(not self._no_commit),
+                submit=self._submit,
+                title=title,
+                summary=summary,
+                set_dependencies=False,
+            )
+        except subprocess.CalledProcessError:
+            action = "submit" if self._submit else "commit"
+            raise FilesystemException(f"Error while attempting to {action} changes.")
---
+++ tools/upgrade/commands/tests/targets_to_configuration_test.py
@@ -0,0 +1,236 @@
+# Copyright (c) 2016-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-unsafe
+
+import json
+import unittest
+from pathlib import Path
+from unittest.mock import MagicMock, call, mock_open, patch
+
+from ... import errors, upgrade
+from ...repository import Repository
+from .. import targets_to_configuration
+from ..targets_to_configuration import TargetsToConfiguration
+
+
+repository = Repository()
+
+
+class TargetsToConfigurationTest(unittest.TestCase):
+    @patch("builtins.open")
+    @patch(f"{targets_to_configuration.__name__}.Repository.revert_all")
+    @patch(f"{targets_to_configuration.__name__}.Repository.add_paths")
+    @patch(f"{targets_to_configuration.__name__}.find_targets")
+    @patch(f"{targets_to_configuration.__name__}.get_filesystem")
+    @patch(f"{targets_to_configuration.__name__}.path_exists")
+    @patch(f"{targets_to_configuration.__name__}.remove_non_pyre_ignores")
+    @patch(f"{targets_to_configuration.__name__}.Configuration.get_errors")
+    @patch(f"{targets_to_configuration.__name__}.add_local_mode")
+    @patch.object(upgrade.ErrorSuppressingCommand, "_suppress_errors")
+    @patch(f"{targets_to_configuration.__name__}.Repository.format")
+    @patch(
+        f"{targets_to_configuration.__name__}.TargetsToConfiguration.remove_target_typing_fields"
+    )
+    def test_convert_directory(
+        self,
+        remove_target_typing_fields,
+        repository_format,
+        suppress_errors,
+        add_local_mode,
+        get_errors,
+        remove_non_pyre_ignores,
+        path_exists,
+        get_filesystem,
+        find_targets,
+        add_paths,
+        revert_all,
+        open_mock,
+    ) -> None:
+        arguments = MagicMock()
+        arguments.subdirectory = "subdirectory"
+        arguments.lint = True
+        arguments.glob = None
+        arguments.fixme_threshold = None
+        arguments.no_commit = False
+        find_targets.return_value = {
+            "subdirectory/a": ["target_one"],
+            "subdirectory/b/c": ["target_three", "target_two"],
+        }
+        filesystem_list = MagicMock()
+        filesystem_list.return_value = []
+        get_filesystem.list = filesystem_list
+        path_exists.return_value = False
+        pyre_errors = [
+            {
+                "line": 2,
+                "column": 4,
+                "path": "local.py",
+                "code": 7,
+                "name": "Kind",
+                "concise_description": "Error",
+                "inference": {},
+                "ignore_error": False,
+                "external_to_global_root": False,
+            }
+        ]
+
+        # Create local project configuration
+        get_errors.side_effect = [
+            errors.Errors(pyre_errors),
+            errors.Errors(pyre_errors),
+        ]
+        with patch("json.dump") as dump_mock:
+            mocks = [mock_open(read_data="{}").return_value]
+            open_mock.side_effect = mocks
+            TargetsToConfiguration(arguments, repository).convert_directory(
+                Path("subdirectory")
+            )
+            expected_configuration_contents = {
+                "targets": [
+                    "//subdirectory/a:target_one",
+                    "//subdirectory/b/c:target_three",
+                    "//subdirectory/b/c:target_two",
+                ],
+                "strict": True,
+            }
+            open_mock.assert_has_calls(
+                [call(Path("subdirectory/.pyre_configuration.local"), "w")]
+            )
+            dump_mock.assert_called_once_with(
+                expected_configuration_contents, mocks[0], indent=2, sort_keys=True
+            )
+            suppress_errors.assert_has_calls([call(errors.Errors(pyre_errors))])
+            add_local_mode.assert_not_called()
+            add_paths.assert_called_once_with(
+                [Path("subdirectory/.pyre_configuration.local")]
+            )
+            remove_target_typing_fields.assert_called_once()
+
+        # Add to existing local project configuration
+        suppress_errors.reset_mock()
+        open_mock.reset_mock()
+        dump_mock.reset_mock()
+        remove_target_typing_fields.reset_mock()
+        path_exists.return_value = True
+        get_errors.side_effect = [
+            errors.Errors(pyre_errors),
+            errors.Errors(pyre_errors),
+        ]
+        configuration_contents = json.dumps({"targets": ["//existing:target"]})
+        with patch("json.dump") as dump_mock:
+            mocks = [
+                mock_open(read_data=configuration_contents).return_value,
+                mock_open(read_data="{}").return_value,
+            ]
+            open_mock.side_effect = mocks
+            TargetsToConfiguration(arguments, repository).convert_directory(
+                Path("subdirectory")
+            )
+            expected_configuration_contents = {
+                "targets": [
+                    "//existing:target",
+                    "//subdirectory/a:target_one",
+                    "//subdirectory/b/c:target_three",
+                    "//subdirectory/b/c:target_two",
+                ]
+            }
+            open_mock.assert_has_calls(
+                [
+                    call(Path("subdirectory/.pyre_configuration.local")),
+                    call(Path("subdirectory/.pyre_configuration.local"), "w"),
+                ]
+            )
+            dump_mock.assert_called_once_with(
+                expected_configuration_contents, mocks[1], indent=2, sort_keys=True
+            )
+        suppress_errors.assert_has_calls([call(errors.Errors(pyre_errors))])
+        add_local_mode.assert_not_called()
+        remove_target_typing_fields.assert_called_once()
+
+    @patch(f"{targets_to_configuration.__name__}.find_files")
+    @patch(f"{targets_to_configuration.__name__}.Repository.submit_changes")
+    @patch(
+        f"{targets_to_configuration.__name__}.TargetsToConfiguration.convert_directory"
+    )
+    def test_run_targets_to_configuration(
+        self, convert_directory, submit_changes, find_files
+    ) -> None:
+        arguments = MagicMock()
+        arguments.subdirectory = "subdirectory"
+        arguments.lint = True
+        arguments.glob = None
+        arguments.fixme_threshold = None
+        arguments.no_commit = False
+
+        find_files.return_value = ["subdirectory/.pyre_configuration.local"]
+        TargetsToConfiguration(arguments, repository).run()
+        convert_directory.assert_called_once_with(Path("subdirectory"))
+        submit_changes.assert_called_once()
+
+        convert_directory.reset_mock()
+        find_files.return_value = [
+            "subdirectory/a/.pyre_configuration.local",
+            "subdirectory/b/.pyre_configuration.local",
+        ]
+        TargetsToConfiguration(arguments, repository).run()
+        convert_directory.assert_has_calls(
+            [call(Path("subdirectory/a")), call(Path("subdirectory/b"))]
+        )
+
+        convert_directory.reset_mock()
+        find_files.return_value = [
+            "subdirectory/a/.pyre_configuration.local",
+            "subdirectory/a/nested/.pyre_configuration.local",
+        ]
+        TargetsToConfiguration(arguments, repository).run()
+        convert_directory.assert_called_once_with(Path("subdirectory/a"))
+
+    @patch("subprocess.check_output")
+    def test_deduplicate_targets(self, mock_check_output) -> None:
+        configuration = upgrade.Configuration(Path("test"), {"targets": ["//a:a"]})
+        configuration.deduplicate_targets()
+        expected_targets = ["//a:a"]
+        self.assertEqual(expected_targets, configuration.targets)
+
+        mock_check_output.side_effect = [b"a", b"b"]
+        configuration = upgrade.Configuration(
+            Path("test"), {"targets": ["//a/...", "//b/..."]}
+        )
+        configuration.deduplicate_targets()
+        expected_targets = ["//a/...", "//b/..."]
+        self.assertEqual(expected_targets, configuration.targets)
+
+        mock_check_output.side_effect = [b"a", b"a"]
+        configuration = upgrade.Configuration(
+            Path("test"), {"targets": ["//a/...", "//b/..."]}
+        )
+        configuration.deduplicate_targets()
+        expected_targets = ["//a/..."]
+        self.assertEqual(expected_targets, configuration.targets)
+
+        mock_check_output.side_effect = [b"a", b"a\nb"]
+        configuration = upgrade.Configuration(
+            Path("test"), {"targets": ["//a/...", "//b/..."]}
+        )
+        configuration.deduplicate_targets()
+        expected_targets = ["//a/...", "//b/..."]
+        self.assertEqual(expected_targets, configuration.targets)
+
+        mock_check_output.side_effect = [b"a", b"//c:c"]
+        configuration = upgrade.Configuration(
+            Path("test"), {"targets": ["//a/...", "//b/...", "//c:c"]}
+        )
+        configuration.deduplicate_targets()
+        expected_targets = ["//a/...", "//b/..."]
+        self.assertEqual(expected_targets, configuration.targets)
+
+        mock_check_output.side_effect = [b"//a/b:x\n//a/b:y"]
+        configuration = upgrade.Configuration(
+            Path("test"), {"targets": ["//a/b:", "//a/b:x"]}
+        )
+        configuration.deduplicate_targets()
+        expected_targets = ["//a/b:"]
+        self.assertEqual(expected_targets, configuration.targets)
