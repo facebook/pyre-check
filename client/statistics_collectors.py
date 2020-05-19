4d8f36d58b8445895cbcaaa951fdba2545bc38c2
Maggie Moss <maggiemoss@fb.com>
2020-05-18 18:22:32 -0700

    Update strict rule to not be entire file
    Reviewed By: dkgi
    
    Differential Revision: D21629485
    
    fbshipit-source-id: ab715859fe06a4e682db903dd5a2d979392a2025

--- client/statistics_collectors.py
+++ client/statistics_collectors.py
@@ -237,7 +237,8 @@ class StrictIssueCollector(StrictCountCollector):
     issues: List[CodeQualityIssue] = []
 
     def _create_issue(self, node: cst.Module) -> None:
-        code_range = self.get_metadata(PositionProvider, node)
+        file_range = self.get_metadata(PositionProvider, node)
+        code_range = CodeRange(start=file_range.start, end=file_range.start)
         issue = CodeQualityIssue(
             code_range, self.path, "PYRE_STRICT", "Unsafe Pyre file."
         )
