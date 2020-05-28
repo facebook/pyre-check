feda2c9a25fa40f82ba64dc16f6e9e58ad686d26
Graham Bleaney <gbleaney@fb.com>
2020-05-28 09:04:26 -0700

    Add generator that can model Django's class-based views
    Summary: This introduces a new generator that can taint arguments to Django's class-based views. These views are the newer way of specifying entrypoints in Django.
    
    Reviewed By: sinancepel
    
    Differential Revision: D21694718
    
    fbshipit-source-id: 59fc5892720637327312c3c1113288115329a226

--- tools/generate_taint_models/__init__.py
+++ tools/generate_taint_models/__init__.py
@@ -21,6 +21,7 @@ from .get_annotated_free_functions_with_decorator import (  # noqa
     AnnotatedFreeFunctionWithDecoratorGenerator,
 )
 from .get_class_sources import ClassSourceGenerator  # noqa
+from .get_django_class_based_view_models import DjangoClassBasedViewModels  # noqa
 from .get_exit_nodes import ExitNodeGenerator  # noqa
 from .get_filtered_sources import FilteredSourceGenerator  # noqa
 from .get_globals import GlobalModelGenerator  # noqa
