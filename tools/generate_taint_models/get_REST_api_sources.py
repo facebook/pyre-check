939f4d6a435ca98d86528fdb01d1d9d0a4aacc29
Graham Bleaney <gbleaney@fb.com>
2020-06-12 07:16:22 -0700

    Support AnnotationSpecification and WhitelistSpecification in RESTApiSourceGenerator
    Summary: Add support for the newer `AnnotationSpecification` and `WhitelistSpecification` in `RESTApiSourceGenerator`.
    
    Reviewed By: sinancepel
    
    Differential Revision: D22007123
    
    fbshipit-source-id: 0d46a1ff155a026ad33cfdc221bb47b23cc559ed

--- tools/generate_taint_models/get_REST_api_sources.py
+++ tools/generate_taint_models/get_REST_api_sources.py
@@ -9,23 +9,51 @@
 from typing import Callable, Iterable, List, Optional
 
 from .function_tainter import taint_callable_functions
+from .generator_specifications import (
+    AnnotationSpecification,
+    WhitelistSpecification,
+    default_entrypoint_taint,
+)
 from .model import CallableModel
 from .model_generator import ModelGenerator
 from .view_generator import DjangoUrls, get_all_views
 
 
 class RESTApiSourceGenerator(ModelGenerator[CallableModel]):
+    annotations: AnnotationSpecification
+    whitelisted_parameters: WhitelistSpecification
+
     def __init__(
         self,
         django_urls: DjangoUrls,
         whitelisted_classes: Optional[List[str]] = None,
         whitelisted_views: Optional[List[str]] = None,
         taint_annotation: str = "TaintSource[UserControlled]",
+        annotations: Optional[AnnotationSpecification] = None,
+        whitelisted_parameters: Optional[WhitelistSpecification] = None,
     ) -> None:
         self.django_urls: DjangoUrls = django_urls
-        self.whitelisted_classes: List[str] = whitelisted_classes or []
         self.whitelisted_views: List[str] = whitelisted_views or []
-        self.taint_annotation = taint_annotation
+
+        if annotations:
+            self.annotations = annotations
+        elif taint_annotation != "TaintSource[UserControlled]":
+            self.annotations = AnnotationSpecification(
+                arg=taint_annotation, vararg=taint_annotation, kwarg=taint_annotation
+            )
+        else:
+            self.annotations = default_entrypoint_taint
+
+        if whitelisted_parameters:
+            self.whitelisted_parameters = whitelisted_parameters
+        elif whitelisted_classes:
+            self.whitelisted_parameters = WhitelistSpecification(
+                parameter_type=set(whitelisted_classes)
+            )
+        else:
+            self.whitelisted_parameters = WhitelistSpecification(
+                parameter_name={"self"}, parameter_type={"HttpRequest"}
+            )
 
     def gather_functions_to_model(self) -> Iterable[Callable[..., object]]:
         return get_all_views(self.django_urls)
@@ -35,7 +63,7 @@ class RESTApiSourceGenerator(ModelGenerator[CallableModel]):
     ) -> Iterable[CallableModel]:
         return taint_callable_functions(
             functions_to_model,
-            whitelisted_classes=self.whitelisted_classes,
             whitelisted_views=self.whitelisted_views,
-            taint_annotation=self.taint_annotation,
+            annotations=self.annotations,
+            whitelist=self.whitelisted_parameters,
         )
--- tools/generate_taint_models/tests/get_REST_api_sources_test.py
+++ tools/generate_taint_models/tests/get_REST_api_sources_test.py
@@ -8,28 +8,27 @@
 import unittest
 from unittest.mock import MagicMock
 
+from ..generator_specifications import AnnotationSpecification, WhitelistSpecification
 from ..get_REST_api_sources import RESTApiSourceGenerator
 from .test_functions import __name__ as qualifier, all_functions
 
 
 class GetRESTApiSourcesTest(unittest.TestCase):
     def test_compute_models(self) -> None:
-        source = "TaintSource[UserControlled]"
+        # Test with default arguments
+        source = "Taint"
         self.assertEqual(
             [
                 *map(
                     str,
                     RESTApiSourceGenerator(
-                        django_urls=MagicMock(),
-                        whitelisted_classes=[],
-                        whitelisted_views=[],
+                        django_urls=MagicMock(), taint_annotation=source
                     ).compute_models(all_functions),
                 )
             ],
             [
-                f"def {qualifier}.TestClass.methodA(self: {source}, x: {source}): ...",
-                f"def {qualifier}.TestClass.methodB(self: {source}, *args: {source})"
-                ": ...",
+                f"def {qualifier}.TestClass.methodA(self, x: {source}): ...",
+                f"def {qualifier}.TestClass.methodB(self, *args: {source})" ": ...",
                 f"def {qualifier}.testA(): ...",
                 f"def {qualifier}.testB(x: {source}): ...",
                 f"def {qualifier}.testC(x: {source}): ...",
@@ -37,6 +36,8 @@ class GetRESTApiSourcesTest(unittest.TestCase):
                 f"def {qualifier}.testE(x: {source}, **kwargs: {source}): ...",
             ],
         )
+
+        # Test with old-style whitelisting
         self.assertEqual(
             [
                 *map(
@@ -45,6 +46,7 @@ class GetRESTApiSourcesTest(unittest.TestCase):
                         django_urls=MagicMock(),
                         whitelisted_classes=["int"],
                         whitelisted_views=[f"{qualifier}.testA"],
+                        taint_annotation=source,
                     ).compute_models(all_functions),
                 )
             ],
@@ -52,6 +54,56 @@ class GetRESTApiSourcesTest(unittest.TestCase):
                 f"def {qualifier}.TestClass.methodA(self: {source}, x): ...",
                 f"def {qualifier}.TestClass.methodB(self: {source}, *args: {source})"
                 ": ...",
+                f"def {qualifier}.testB(x: {source}): ...",
+                f"def {qualifier}.testC(x): ...",
+                f"def {qualifier}.testD(x, *args): ...",
+                f"def {qualifier}.testE(x, **kwargs: {source}): ...",
+            ],
+        )
+
+        # Test with AnnotationSpecification
+        self.assertEqual(
+            [
+                *map(
+                    str,
+                    RESTApiSourceGenerator(
+                        django_urls=MagicMock(),
+                        annotations=AnnotationSpecification(
+                            arg="Arg", vararg="VarArg", kwarg="KWArg", returns="Returns"
+                        ),
+                    ).compute_models(all_functions),
+                )
+            ],
+            [
+                f"def {qualifier}.TestClass.methodA(self, x: Arg) -> Returns: ...",
+                f"def {qualifier}.TestClass.methodB(self, *args: VarArg)"
+                " -> Returns: ...",
+                f"def {qualifier}.testA() -> Returns: ...",
+                f"def {qualifier}.testB(x: Arg) -> Returns: ...",
+                f"def {qualifier}.testC(x: Arg) -> Returns: ...",
+                f"def {qualifier}.testD(x: Arg, *args: VarArg) -> Returns: ...",
+                f"def {qualifier}.testE(x: Arg, **kwargs: KWArg) -> Returns: ...",
+            ],
+        )
+
+        # Test with WhitelistSpecification
+        self.assertEqual(
+            [
+                *map(
+                    str,
+                    RESTApiSourceGenerator(
+                        django_urls=MagicMock(),
+                        whitelisted_parameters=WhitelistSpecification(
+                            parameter_name={"self"}, parameter_type={"int"}
+                        ),
+                        taint_annotation=source,
+                    ).compute_models(all_functions),
+                )
+            ],
+            [
+                f"def {qualifier}.TestClass.methodA(self, x): ...",
+                f"def {qualifier}.TestClass.methodB(self, *args: {source})" ": ...",
+                f"def {qualifier}.testA(): ...",
                 f"def {qualifier}.testB(x: {source}): ...",
                 f"def {qualifier}.testC(x): ...",
                 f"def {qualifier}.testD(x, *args): ...",
