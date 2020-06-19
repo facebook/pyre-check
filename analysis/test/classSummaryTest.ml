6ff11e5d22ebf8459675c16b4badb38747b46c90
Pradeep Kumar Srinivasan <pradeepkumars@fb.com>
2020-06-19 15:18:25 -0700

    Accept `typing_extensions.final`.
    Summary: Problem: We don't currently accept `typing_extensions.final` when computing `ClassSummary.is_final`.
    
    Reviewed By: MaggieMoss
    
    Differential Revision: D22143458
    
    fbshipit-source-id: b2d6eab81d21cdc6c637a0aed5837fc814000717

---
+++ analysis/test/classSummaryTest.ml
@@ -0,0 +1,34 @@
+(* Copyright (c) 2019-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. *)
+
+open Core
+open OUnit2
+open Analysis
+open Test
+open ClassSummary
+open Ast
+open Statement
+
+let test_is_final _ =
+  let decorator ~name =
+    { Decorator.name = Node.create_with_default_location (Reference.create name); arguments = None }
+  in
+  let class_summary ~decorators =
+    {
+      name = Reference.create "foo";
+      qualifier = Reference.create "bar";
+      bases = [];
+      decorators;
+      attribute_components = Class.AttributeComponents.empty ();
+    }
+  in
+  assert_false (ClassSummary.is_final (class_summary ~decorators:[]));
+  assert_true (ClassSummary.is_final (class_summary ~decorators:[decorator ~name:"typing.final"]));
+  assert_true
+    (ClassSummary.is_final (class_summary ~decorators:[decorator ~name:"typing_extensions.final"]));
+  ()
+
+
+let () = "classSummary" >::: ["is_final" >:: test_is_final] |> Test.run
