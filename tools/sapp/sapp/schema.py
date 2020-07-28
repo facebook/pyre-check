dd04685f5dd2f0f258e6c3b109514dfe76d7e898
Nolan Alexander Jimenez <nolanjimenez@fb.com>
2020-07-28 00:08:23 -0700

    switched issue model to use connection attribute
    Summary: using issue connection for resolve. changed so relay features can be utilized for react frontend
    
    Reviewed By: sinancepel
    
    Differential Revision: D22768414
    
    fbshipit-source-id: dbb000504dc8e75ca5e9783ef59d548812fd59b9

--- tools/sapp/sapp/schema.py
+++ tools/sapp/sapp/schema.py
@@ -1,20 +1,35 @@
 # (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
+from typing import TYPE_CHECKING, List
+
 import graphene
+from graphene import relay
 
 from .models import IssueInstance, SharedText
+
+
+if TYPE_CHECKING:
+    from graphql.execution.base import ResolveInfo
 
 
 SharedTextType = SharedText.generateSQLAlchemyObject()
 IssueInstanceType = IssueInstance.generateSQLAlchemyObject()
 
 
-class Query(graphene.ObjectType):
-    issue_instances = graphene.List(IssueInstanceType)
+class IssueConnection(relay.Connection):
+    class Meta:
+        node = IssueInstanceType
 
-    def resolve_issue_instances(self, info):
-        query = IssueInstanceType.get_query(info)
-        return query.all()
+
+class Query(graphene.ObjectType):
+    node = relay.Node.Field()
+    issue_instances = relay.ConnectionField(IssueConnection)
+
+    def resolve_issue_instances(
+        self, info: "ResolveInfo", **args
+    ) -> List[IssueInstance]:
+        issue_instances_query = IssueInstanceType.get_query(info)
+        return issue_instances_query.all()
 
 
 schema = graphene.Schema(query=Query)
