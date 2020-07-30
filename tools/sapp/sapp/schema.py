135a361b8b8eea980c8070b44b398757daf6acd0
Nolan Alexander Jimenez <nolanjimenez@fb.com>
2020-07-30 15:49:11 -0700

    Added filtering of issues as part of schema
    Summary: updated to filter by query filters which take in a list of strings or list of integers. Filters which are currently excluded are those which require a range and those which filter leaves.
    
    Reviewed By: zlandau
    
    Differential Revision: D22808863
    
    fbshipit-source-id: fada11a12fbdd950a70a40bf651798968777abfb

--- tools/sapp/sapp/schema.py
+++ tools/sapp/sapp/schema.py
@@ -20,16 +20,39 @@ class IssueConnection(relay.Connection):
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    issues = relay.ConnectionField(IssueConnection)
+    issues = relay.ConnectionField(
+        IssueConnection,
+        codes=graphene.List(graphene.Int, default_value=[]),
+        callables=graphene.List(graphene.String, default_value=[]),
+        file_names=graphene.List(graphene.String, default_value=[]),
+        trace_length_to_sinks=graphene.List(graphene.String, default_value=[]),
+        trace_length_to_sources=graphene.List(graphene.String, default_value=[]),
+    )
 
-    def resolve_issues(self, info: ResolveInfo, **args) -> List[IssueQueryResult]:
+    def resolve_issues(
+        self,
+        info: ResolveInfo,
+        codes: List[int],
+        callables: List[str],
+        file_names: List[str],
+        trace_length_to_sinks: List[str],
+        trace_length_to_sources: List[str],
+        **args
+    ) -> List[IssueQueryResult]:
         session = get_session(info.context)
         latest_run_id = (
             session.query(func.max(Run.id))
             .filter(Run.status == RunStatus.FINISHED)
             .scalar()
         )
-        builder = IssueQueryBuilder(latest_run_id).with_session(session)
+
+        builder = (
+            IssueQueryBuilder(latest_run_id)
+            .with_session(session)
+            .where_codes_is_any_of(codes)
+            .where_callables_is_any_of(callables)
+            .where_file_names_is_any_of(file_names)
+        )
 
         return builder.get()
 
