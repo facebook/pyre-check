1eb0b4428922e2e0b554df6da5593272bed21fc8
Nolan Alexander Jimenez <nolanjimenez@fb.com>
2020-07-30 15:49:11 -0700

    modified query to make new graphql schema
    Summary: query in front-end modified to match updated graphene model. IssueInstance component also modified to match new query values
    
    Reviewed By: sinancepel
    
    Differential Revision: D22820379
    
    fbshipit-source-id: 15cbe774e3859ee76840c88e5df58466a97ec92f

--- tools/sapp/sapp/frontend/src/App.js
+++ tools/sapp/sapp/frontend/src/App.js
@@ -15,27 +15,17 @@ const client = new ApolloClient({
 
 const ISSUE_QUERY = gql`
     query IssueInstances($after: String) {
-      issue_instances(first: 1 after: $after){
+      issues(first: 1 after: $after){
         edges {
           node {
-            location
-            filename_id
-            filename {
-              contents
-            }
-            callable_id
-            is_new_issue
-            run_id
             issue_id
-            message_id
+            filename
+            location
+            code
+            callable
+            message
             min_trace_length_to_sources
             min_trace_length_to_sinks
-            rank
-            callable_count
-            min_trace_length_to_entrypoints
-            issue {
-              code
-            }
           }
         }
         pageInfo {
@@ -54,32 +44,26 @@ function IssueInstances() {
   return (
     <>
       <ul>
-        {data.issue_instances.edges.map(({node}) => (
+        {data.issues.edges.map(({node}) => (
           <IssueInstance
-            location={node.location}
-            filename_id={node.filename_id}
-            filename={node.filename.contents}
-            code={node.issue.code}
-            callable_id={node.callable_id}
-            is_new_issue={node.is_new_issue}
-            run_id={node.run_id}
             issue_id={node.issue_id}
-            message_id={node.message_id}
+            filename={node.filename}
+            location={node.location}
+            code={node.code}
+            callable={node.callable}
+            message={node.message}
             min_trace_length_to_sources={node.min_trace_length_to_sources}
             min_trace_length_to_sinks={node.min_trace_length_to_sinks}
-            rank={node.rank}
-            callable_count={node.callable_count}
-            min_trace_length_to_entrypoints={node.min_trace_length_to_entrypoints}
           />))}
       </ul>
       <button onClick={() => {
-        const endCursor = data.issue_instances.pageInfo.endCursor;
+        const endCursor = data.issues.pageInfo.endCursor;
         fetchMore({
           variables: {after: endCursor},
           updateQuery: (prevResult, {fetchMoreResult}) => {
-            fetchMoreResult.issue_instances.edges = [
-              ...prevResult.issue_instances.edges,
-              ...fetchMoreResult.issue_instances.edges,
+            fetchMoreResult.issues.edges = [
+              ...prevResult.issues.edges,
+              ...fetchMoreResult.issues.edges,
             ];
             return fetchMoreResult
           }
--- tools/sapp/sapp/frontend/src/IssueInstance.js
+++ tools/sapp/sapp/frontend/src/IssueInstance.js
@@ -3,22 +3,15 @@ import React from 'react';
 class IssueInstance extends React.Component {
   render() {
       return <div className="issue_instance">
-        <h3> Issue {this.props.issue_id}</h3>
-        <p>Location: {this.props.location}</p>
-        <p>File: {this.props.filename}</p>
-        <p>Filename Id: {this.props.filename_id}</p>
+        <h3>Issue {this.props.issue_id}</h3>
         <p>Code: {this.props.code}</p>
-        <p>Callable Id: {this.props.callable_id}</p>
-        <p>Callable Count: {this.props.callable_count}</p>
-        <p>isNewIssue: {this.props.is_new_issue}</p>
-        <p>Run Id: {this.props.run_id}</p>
-        <p>Message Id: {this.props.message_id}</p>
-        <p>Rank: {this.props.rank}</p>
+        <p>Message: {this.props.message}</p>
+        <p>Callable: {this.props.callable}</p>
+        <p>Location: {this.props.filename}:{this.props.location}</p>
         <div id="trace_lengths">
           <strong>Min Trace Lengths</strong>
           <p>Sources: {this.props.min_trace_length_to_sources}</p>
           <p>Sinks: {this.props.min_trace_length_to_sinks}</p>
-          <p>Entrypoints: {this.props.min_trace_length_to_entrypoints}</p>
         </div>
         <br/>
       </div>
