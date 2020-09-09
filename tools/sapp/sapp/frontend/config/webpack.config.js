7e56f419ee1ccba23482ac0c3816922f380240fd
Dominik Gabi <dominik@fb.com>
2020-09-09 10:44:45 -0700

    turn off some minification
    Summary:
    Ideally we should have a distinction between development and production build. We don't right now.
    Making my life a bit easier...
    
    Reviewed By: arthaud
    
    Differential Revision: D23591329
    
    fbshipit-source-id: e93353024a5177bb2db2b8c4082a477d27a21a9a

--- tools/sapp/sapp/frontend/config/webpack.config.js
+++ tools/sapp/sapp/frontend/config/webpack.config.js
@@ -336,7 +336,7 @@ module.exports = function(webpackEnv) {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
-                
+
               },
               loader: require.resolve('eslint-loader'),
             },
@@ -369,7 +369,7 @@ module.exports = function(webpackEnv) {
                 customize: require.resolve(
                   'babel-preset-react-app/webpack-overrides'
                 ),
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -411,7 +411,7 @@ module.exports = function(webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.
@@ -519,8 +519,8 @@ module.exports = function(webpackEnv) {
           isEnvProduction
             ? {
                 minify: {
-                  removeComments: true,
-                  collapseWhitespace: true,
+                  removeComments: false,
+                  collapseWhitespace: false,
                   removeRedundantAttributes: true,
                   useShortDoctype: true,
                   removeEmptyAttributes: true,
