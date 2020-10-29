d14f92688944ce638dd65bf02b15e3e50883212b
Sinan Cepel <sinancepel@fb.com>
2020-10-28 18:00:46 -0700

    morally remove callResolution.ml
    Summary: There should be no more trying to resolve targets in the analysis directly - everything should go through the call graph interface :)
    
    Reviewed By: grievejia
    
    Differential Revision: D24518465
    
    fbshipit-source-id: b2eaa5ad3a2b4159cd5e88d9ad52f9aedd87e339

--- interprocedural/callResolution.mli
+++ interprocedural/callResolution.mli
@@ -5,50 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  *)
 
-open Ast
-open Analysis
-open Expression
-
-type target = Callable.t * Type.t option [@@deriving eq, show]
-
-val is_local : Identifier.t -> bool
+val is_local : Ast.Identifier.t -> bool
 
 (* Evaluates to the representation of literal strings, integers and enums. *)
-val extract_constant_name : Expression.t -> string option
-
-val resolve_target : resolution:Resolution.t -> ?receiver_type:Type.t -> Expression.t -> target list
-
-(* Evaluates to the list of indirect targets and the implicit self that needs to be passed in, if
-   any. *)
-val get_indirect_targets
-  :  resolution:Resolution.t ->
-  receiver:Expression.t ->
-  method_name:Identifier.t ->
-  target list * Call.Argument.t option
-
-(* Given an attribute self.x, returns the underlying callable if x is a @property. *)
-val resolve_property_targets
-  :  resolution:Resolution.t ->
-  base:Expression.t ->
-  attribute:string ->
-  setter:bool ->
-  target list option
-
-type constructor_targets = {
-  new_targets: target list;
-  init_targets: target list;
-}
-
-val get_constructor_targets
-  :  resolution:Resolution.t ->
-  receiver:Expression.t ->
-  constructor_targets
-
-type global_targets =
-  | ConstructorTargets of {
-      constructor_targets: constructor_targets;
-      callee: Expression.t;
-    }
-  | GlobalTargets of target list
-
-val get_global_targets : resolution:Resolution.t -> Reference.t -> global_targets
+val extract_constant_name : Ast.Expression.t -> string option
