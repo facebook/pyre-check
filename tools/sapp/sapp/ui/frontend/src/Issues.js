36a6783b0a2f2d67f0e0d3bbdb02cbb5719ba0e5
Dominik Gabi <dominik@fb.com>
2020-11-04 11:58:19 -0800

    move `IssuesList` into `Issues`
    Summary: moving together since one is essentially an implementation detail of the other
    
    Reviewed By: arthaud
    
    Differential Revision: D24652303
    
    fbshipit-source-id: 37527bb9253964e3b14e4076207001b82b03481f

--- tools/sapp/sapp/ui/frontend/src/Issues.js
+++ tools/sapp/sapp/ui/frontend/src/Issues.js
@@ -11,10 +11,118 @@
 import React, {useEffect, useState} from 'react';
 import {useQuery, gql} from '@apollo/client';
 import {Modal, Breadcrumb} from 'antd';
-import IssuesList from './IssuesList';
 import Filter from './Filter';
+import {Issue, IssueSkeleton} from './Issue.js';
 
 import './Issues.css';
+
+type Props = $ReadOnly<{|
+  data: any,
+  fetchMore: any,
+  loading: boolean,
+|}>;
+
+type State = $ReadOnly<{|
+  recently_started_loading: boolean,
+|}>;
+
+class IssuesList extends React.Component<Props, State> {
+  state: State = {
+    recently_started_loading: false,
+  };
+
+  constructor(props: Props) {
+    super(props);
+    // $FlowFixMe
+    this._fetchIssues = this._fetchIssues.bind(this);
+    // $FlowFixMe
+    this._handleScroll = this._handleScroll.bind(this);
+  }
+
+  componentDidMount(): void {
+    window.addEventListener('scroll', this._handleScroll);
+  }
+
+  componentWillUnount(): void {
+    window.removeEventListener('scroll', this._handleScroll);
+  }
+
+  render() {
+    return (
+      <>
+        {this._renderIssues()}
+        {this._renderLoadingSkeleton()}
+      </>
+    );
+  }
+
+  _fetchIssues() {
+    const endCursor = this.props.data.issues.pageInfo.endCursor;
+    this.props.fetchMore({
+      variables: {after: endCursor},
+      updateQuery: (prevResult, {fetchMoreResult}) => {
+        fetchMoreResult.issues.edges = [
+          ...prevResult.issues.edges,
+          ...fetchMoreResult.issues.edges,
+        ];
+        return fetchMoreResult;
+      },
+    });
+  }
+
+  _handleScroll(event: any): void {
+    let body = document.body;
+    if (body === null) {
+      return;
+    }
+    if (
+      !this.props.loading &&
+      !this.state.recently_started_loading &&
+      window.pageYOffset + body.clientHeight >= body.scrollHeight - 100
+    ) {
+      this.setState({recently_started_loading: true});
+      const captured = this;
+      setTimeout(function() {
+        // Avoid clobbering the server with too many requests.
+        captured.setState({recently_started_loading: false});
+      }, 1000);
+      this._fetchIssues();
+    }
+  }
+
+  _renderIssues() {
+    if (this.props.loading) {
+      return null;
+    }
+
+    return (
+      <>
+        {this.props.data.issues.edges.map(({node}) => (
+          <>
+            <Issue issue={node} />
+            <br />
+          </>
+        ))}
+      </>
+    );
+  }
+
+  _renderLoadingSkeleton() {
+    if (!this.props.loading && !this.state.recently_started_loading) {
+      return null;
+    }
+    return (
+      <>
+        {[...Array(5).keys()].map(() => (
+          <>
+            <IssueSkeleton />
+            <br />
+          </>
+        ))}
+      </>
+    );
+  }
+}
 
 const IssueQuery = gql`
   query Issue(
