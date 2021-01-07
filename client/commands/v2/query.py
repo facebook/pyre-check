90f88e37c128b0e1e90117fd8dbf98d6d0dc2b45
Jia Chen <grievejia@fb.com>
2021-01-07 10:07:24 -0800

    Query command (2/n) -- response parsing
    Reviewed By: dkgi
    
    Differential Revision: D25809869
    
    fbshipit-source-id: e749046d014401f343db68ff080a0a39022ae2b3

--- client/commands/v2/query.py
+++ client/commands/v2/query.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import dataclasses
+import json
 import logging
 from pathlib import Path
 
@@ -11,6 +13,34 @@ from . import server_connection
 
 
 LOG: logging.Logger = logging.getLogger(__name__)
+
+
+class InvalidQueryResponse(Exception):
+    pass
+
+
+@dataclasses.dataclass(frozen=True)
+class Response:
+    payload: object
+
+
+def parse_query_response_json(response_json: object) -> Response:
+    if (
+        isinstance(response_json, list)
+        and len(response_json) > 1
+        and response_json[0] == "Query"
+    ):
+        return Response(response_json[1])
+    raise InvalidQueryResponse(f"Unexpected JSON response from server: {response_json}")
+
+
+def parse_query_response(text: str) -> Response:
+    try:
+        response_json = json.loads(text)
+        return parse_query_response_json(response_json)
+    except json.JSONDecodeError as decode_error:
+        message = f"Cannot parse response as JSON: {decode_error}"
+        raise InvalidQueryResponse(message) from decode_error
 
 
 def run(
