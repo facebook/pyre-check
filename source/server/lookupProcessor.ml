85ee35f3039b092366200e5300ac2d17f2941936
Jia Chen <grievejia@fb.com>
2021-01-14 14:28:27 -0800

    Remove outdated lookup logging
    Reviewed By: sinancepel
    
    Differential Revision: D25914625
    
    fbshipit-source-id: c58ab37e92c48fee13be3cf8249b221f46118c46

--- source/server/lookupProcessor.ml
+++ source/server/lookupProcessor.ml
@@ -21,17 +21,16 @@ type types_by_path = {
 
 type lookup = {
   path: PyrePath.t;
-  source_path: SourcePath.t option;
   lookup: (Lookup.t, error_reason) Result.t;
 }
 
 let get_lookups ~configuration ~environment paths =
-  let generate_lookup_for_existent_path (path, ({ SourcePath.qualifier; _ } as source_path)) =
+  let generate_lookup_for_existent_path (path, { SourcePath.qualifier; _ }) =
     let lookup = Lookup.create_of_module (TypeEnvironment.read_only environment) qualifier in
-    { path; source_path = Some source_path; lookup = Result.Ok lookup }
+    { path; lookup = Result.Ok lookup }
   in
   let generate_lookup_for_nonexistent_path (path, error_reason) =
-    { path; source_path = None; lookup = Result.Error error_reason }
+    { path; lookup = Result.Error error_reason }
   in
   let generate_lookup_for_path path =
     let module_tracker = TypeEnvironment.module_tracker environment in
@@ -45,36 +44,9 @@ let get_lookups ~configuration ~environment paths =
   List.map paths ~f:generate_lookup_for_path
 
 
-let log_lookup ~handle ~position ~timer ~name ?(integers = []) ?(normals = []) () =
-  let normals =
-    let base_normals = ["handle", handle; "position", Location.show_position position] in
-    base_normals @ normals
-  in
-  Statistics.performance
-    ~section:`Event
-    ~category:"perfpipe_pyre_ide_integration"
-    ~name
-    ~timer
-    ~integers
-    ~normals
-    ()
-
-
 let find_annotation ~environment ~configuration ~path ~position =
-  let timer = Timer.start () in
-  let { lookup; source_path; _ } = get_lookups ~configuration ~environment [path] |> List.hd_exn in
+  let { lookup; _ } = get_lookups ~configuration ~environment [path] |> List.hd_exn in
   let annotation = Result.ok lookup >>= Lookup.get_annotation ~position in
-  let _ =
-    match source_path with
-    | Some { SourcePath.relative = handle; _ } ->
-        let normals =
-          annotation
-          >>| fun (location, annotation) ->
-          ["resolved location", Location.show location; "resolved annotation", Type.show annotation]
-        in
-        log_lookup ~handle ~position ~timer ~name:"find annotation" ?normals ()
-    | _ -> ()
-  in
   annotation
 
 
@@ -91,16 +63,6 @@ let find_all_annotations_batch ~environment ~configuration ~paths =
 
 
 let find_definition ~environment ~configuration path position =
-  let timer = Timer.start () in
-  let { lookup; source_path; _ } = get_lookups ~configuration ~environment [path] |> List.hd_exn in
+  let { lookup; _ } = get_lookups ~configuration ~environment [path] |> List.hd_exn in
   let definition = Result.ok lookup >>= Lookup.get_definition ~position in
-  let _ =
-    match source_path with
-    | Some { SourcePath.relative = handle; _ } ->
-        let normals =
-          definition >>| fun location -> ["resolved location", Location.show location]
-        in
-        log_lookup ~handle ~position ~timer ~name:"find definition" ?normals ()
-    | _ -> ()
-  in
   definition
