7d569e7e708cce4eb59dbc873be919f02f37747e
Maxime Arthaud <arthaud@fb.com>
2021-02-12 17:42:40 -0800

    allow running a specific integration test
    Summary:
    I want to run a specific integration test to prevent global state pollution.
    This patch treats each integration test file as a different test that can be run with:
    `dune exec source/interprocedural_analyses/taint/test/integrationTest.exe -- -only-test taint:60:applies_to_index.py`
    
    Reviewed By: sinancepel
    
    Differential Revision: D26431601
    
    fbshipit-source-id: 558b377b4924b62d66b23c49ea905a9f2c0545d3

--- source/interprocedural_analyses/taint/test/integrationTest.ml
+++ source/interprocedural_analyses/taint/test/integrationTest.ml
@@ -19,151 +19,127 @@ type mismatch_file = {
   actual: string;
 }
 
-let test_integration context =
+let test_integration path context =
   TaintIntegrationTest.Files.dummy_dependency |> ignore;
-  let test_paths =
-    let file_filter name =
-      String.is_suffix ~suffix:".py" name
-      && (not (String.contains name '#'))
-      && not (String.contains name '~')
+  let create_expected_and_actual_files ~suffix actual =
+    let output_filename ~suffix ~initial =
+      if initial then
+        Path.with_suffix path ~suffix
+      else
+        Path.with_suffix path ~suffix:(suffix ^ ".actual")
     in
-    (* Shameful things happen here... *)
-    Path.current_working_directory ()
-    |> Path.show
-    |> (fun path ->
-         String.chop_suffix ~suffix:"_build/default/interprocedural_analyses/taint/test" path
-         |> Option.value ~default:(path ^ "/source"))
-    |> (fun root -> Path.create_absolute root)
-    |> (fun root ->
-         Path.create_relative ~root ~relative:"interprocedural_analyses/taint/test/integration/")
-    |> fun root -> Path.list ~file_filter ~root ()
+    let write_output ~suffix ?(initial = false) content =
+      try output_filename ~suffix ~initial |> File.create ~content |> File.write with
+      | Unix.Unix_error _ ->
+          failwith (Format.asprintf "Could not write `%s` file for %a" suffix Path.pp path)
+    in
+    let remove_old_output ~suffix =
+      try output_filename ~suffix ~initial:false |> Path.show |> Sys.remove with
+      | Sys_error _ ->
+          (* be silent *)
+          ()
+    in
+    let get_expected ~suffix =
+      try Path.with_suffix path ~suffix |> File.create |> File.content with
+      | Unix.Unix_error _ -> None
+    in
+    match get_expected ~suffix with
+    | None ->
+        (* expected file does not exist, create it *)
+        write_output ~suffix actual ~initial:true;
+        None
+    | Some expected ->
+        if String.equal expected actual then (
+          remove_old_output ~suffix;
+          None )
+        else (
+          write_output ~suffix actual;
+          Some { path; suffix; expected; actual } )
   in
-  let run_test path =
-    let create_expected_and_actual_files ~suffix actual =
-      let output_filename ~suffix ~initial =
-        if initial then
-          Path.with_suffix path ~suffix
-        else
-          Path.with_suffix path ~suffix:(suffix ^ ".actual")
-      in
-      let write_output ~suffix ?(initial = false) content =
-        try output_filename ~suffix ~initial |> File.create ~content |> File.write with
-        | Unix.Unix_error _ ->
-            failwith (Format.asprintf "Could not write `%s` file for %a" suffix Path.pp path)
-      in
-      let remove_old_output ~suffix =
-        try output_filename ~suffix ~initial:false |> Path.show |> Sys.remove with
-        | Sys_error _ ->
-            (* be silent *)
-            ()
-      in
-      let get_expected ~suffix =
-        try Path.with_suffix path ~suffix |> File.create |> File.content with
-        | Unix.Unix_error _ -> None
-      in
-      match get_expected ~suffix with
-      | None ->
-          (* expected file does not exist, create it *)
-          write_output ~suffix actual ~initial:true;
-          None
-      | Some expected ->
-          if String.equal expected actual then (
-            remove_old_output ~suffix;
-            None )
-          else (
-            write_output ~suffix actual;
-            Some { path; suffix; expected; actual } )
+  let error_on_actual_files { path; suffix; expected; actual } =
+    Printf.printf
+      "%s"
+      (Format.asprintf
+         "Expectations differ for %s %s\n%a"
+         suffix
+         (Path.show path)
+         (Test.diff ~print:String.pp)
+         (expected, actual))
+  in
+  let divergent_files, serialized_models =
+    let source = File.create path |> File.content |> fun content -> Option.value_exn content in
+    let model_source =
+      try
+        let model_path = Path.with_suffix path ~suffix:".pysa" in
+        File.create model_path |> File.content
+      with
+      | Unix.Unix_error _ -> None
     in
-    let error_on_actual_files { path; suffix; expected; actual } =
-      Printf.printf
-        "%s"
-        (Format.asprintf
-           "Expectations differ for %s %s\n%a"
-           suffix
-           (Path.show path)
-           (Test.diff ~print:String.pp)
-           (expected, actual))
+    let taint_configuration =
+      try
+        let path = Path.with_suffix path ~suffix:".config" in
+        File.create path
+        |> File.content
+        |> Option.map ~f:(fun content ->
+               Taint.TaintConfiguration.parse [Yojson.Safe.from_string content])
+        |> Option.value ~default:Taint.TaintConfiguration.default
+      with
+      | Unix.Unix_error _ -> Taint.TaintConfiguration.default
     in
-    let divergent_files, serialized_models =
-      let source = File.create path |> File.content |> fun content -> Option.value_exn content in
-      let model_source =
-        try
-          let model_path = Path.with_suffix path ~suffix:".pysa" in
-          File.create model_path |> File.content
-        with
-        | Unix.Unix_error _ -> None
+    let handle = Path.show path |> String.split ~on:'/' |> List.last_exn in
+    let create_call_graph_files call_graph =
+      let dependencies = DependencyGraph.from_callgraph call_graph in
+      let actual =
+        Format.asprintf "@%s\nCall dependencies\n%a" "generated" DependencyGraph.pp dependencies
       in
-      let taint_configuration =
-        try
-          let path = Path.with_suffix path ~suffix:".config" in
-          File.create path
-          |> File.content
-          |> Option.map ~f:(fun content ->
-                 Taint.TaintConfiguration.parse [Yojson.Safe.from_string content])
-          |> Option.value ~default:Taint.TaintConfiguration.default
-        with
-        | Unix.Unix_error _ -> Taint.TaintConfiguration.default
-      in
-      let handle = Path.show path |> String.split ~on:'/' |> List.last_exn in
-      let create_call_graph_files call_graph =
-        let dependencies = DependencyGraph.from_callgraph call_graph in
-        let actual =
-          Format.asprintf "@%s\nCall dependencies\n%a" "generated" DependencyGraph.pp dependencies
+      create_expected_and_actual_files ~suffix:".cg" actual
+    in
+    let create_overrides_files overrides =
+      let actual = Format.asprintf "@%s\nOverrides\n%a" "generated" DependencyGraph.pp overrides in
+      create_expected_and_actual_files ~suffix:".overrides" actual
+    in
+    let { callgraph; all_callables; environment; overrides } =
+      initialize ~handle ?models:model_source ~taint_configuration ~context source
+    in
+    let dependencies =
+      DependencyGraph.from_callgraph callgraph
+      |> DependencyGraph.union overrides
+      |> DependencyGraph.reverse
+    in
+    Analysis.compute_fixpoint
+      ~scheduler:(Test.mock_scheduler ())
+      ~environment
+      ~analyses:[TaintAnalysis.abstract_kind]
+      ~dependencies
+      ~filtered_callables:Callable.Set.empty
+      ~all_callables
+      Fixpoint.Epoch.initial
+    |> ignore;
+    let serialized_model callable : string =
+      let externalization =
+        let filename_lookup =
+          TypeEnvironment.ReadOnly.ast_environment environment
+          |> AstEnvironment.ReadOnly.get_relative
         in
-        create_expected_and_actual_files ~suffix:".cg" actual
+        Interprocedural.Analysis.externalize ~filename_lookup TaintAnalysis.abstract_kind callable
+        |> List.map ~f:(fun json -> Yojson.Safe.pretty_to_string ~std:true json ^ "\n")
+        |> String.concat ~sep:""
       in
-      let create_overrides_files overrides =
-        let actual =
-          Format.asprintf "@%s\nOverrides\n%a" "generated" DependencyGraph.pp overrides
-        in
-        create_expected_and_actual_files ~suffix:".overrides" actual
-      in
-      let { callgraph; all_callables; environment; overrides } =
-        initialize ~handle ?models:model_source ~taint_configuration ~context source
-      in
-      let dependencies =
-        DependencyGraph.from_callgraph callgraph
-        |> DependencyGraph.union overrides
-        |> DependencyGraph.reverse
-      in
-      Analysis.compute_fixpoint
-        ~scheduler:(Test.mock_scheduler ())
-        ~environment
-        ~analyses:[TaintAnalysis.abstract_kind]
-        ~dependencies
-        ~filtered_callables:Callable.Set.empty
-        ~all_callables
-        Fixpoint.Epoch.initial
-      |> ignore;
-      let serialized_model callable : string =
-        let externalization =
-          let filename_lookup =
-            TypeEnvironment.ReadOnly.ast_environment environment
-            |> AstEnvironment.ReadOnly.get_relative
-          in
-          Interprocedural.Analysis.externalize ~filename_lookup TaintAnalysis.abstract_kind callable
-          |> List.map ~f:(fun json -> Yojson.Safe.pretty_to_string ~std:true json ^ "\n")
-          |> String.concat ~sep:""
-        in
-        externalization
-      in
+      externalization
+    in
 
-      let divergent_files = [create_call_graph_files callgraph; create_overrides_files overrides] in
-      ( divergent_files,
-        List.map all_callables ~f:serialized_model
-        |> List.sort ~compare:String.compare
-        |> String.concat ~sep:"" )
-    in
-    let divergent_files =
-      create_expected_and_actual_files ~suffix:".models" ("@" ^ "generated\n" ^ serialized_models)
-      :: divergent_files
-      |> List.filter_opt
-    in
-    List.iter divergent_files ~f:error_on_actual_files;
-    divergent_files
+    let divergent_files = [create_call_graph_files callgraph; create_overrides_files overrides] in
+    ( divergent_files,
+      List.map all_callables ~f:serialized_model
+      |> List.sort ~compare:String.compare
+      |> String.concat ~sep:"" )
   in
-  assert_bool "No test paths to check." (not (List.is_empty test_paths));
-  let divergent_files = List.concat_map test_paths ~f:run_test in
+  let divergent_files =
+    create_expected_and_actual_files ~suffix:".models" ("@" ^ "generated\n" ^ serialized_models)
+    :: divergent_files
+    |> List.filter_opt
+  in
+  List.iter divergent_files ~f:error_on_actual_files;
   if not (List.is_empty divergent_files) then
     let message =
       List.map divergent_files ~f:(fun { path; suffix; _ } ->
@@ -174,7 +150,31 @@ let test_integration context =
     assert_bool message false
 
 
+let test_paths =
+  let file_filter name =
+    String.is_suffix ~suffix:".py" name
+    && (not (String.contains name '#'))
+    && not (String.contains name '~')
+  in
+  (* Shameful things happen here... *)
+  Path.current_working_directory ()
+  |> Path.show
+  |> (fun path ->
+       String.chop_suffix ~suffix:"_build/default/interprocedural_analyses/taint/test" path
+       |> Option.value ~default:(path ^ "/source"))
+  |> (fun root -> Path.create_absolute root)
+  |> (fun root ->
+       Path.create_relative ~root ~relative:"interprocedural_analyses/taint/test/integration/")
+  |> fun root -> Path.list ~file_filter ~root ()
+
+
+let test_paths_found _ =
+  if List.is_empty test_paths then
+    assert_bool "No test paths to check." false
+
+
 let () =
-  "taint"
-  >:: (fun _ -> TestHelper.run_with_taint_models ["integration", test_integration] ~name:"taint")
-  |> Test.run
+  test_paths
+  |> List.map ~f:(fun path -> Path.last path, test_integration path)
+  |> List.cons ("paths_found", test_paths_found)
+  |> TestHelper.run_with_taint_models ~name:"taint"
