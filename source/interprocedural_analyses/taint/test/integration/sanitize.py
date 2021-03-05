700d8d090e83251ab4b2eda724eb108b019eff0c
Maxime Arthaud <arthaud@fb.com>
2021-03-04 17:34:35 -0800

    remove a TODO
    Summary: This is done, let's remove it.
    
    Reviewed By: pradeep90
    
    Differential Revision: D26834406
    
    fbshipit-source-id: 627d9425340e4cbc47bb8c6d1c5f97b44fdec1f7

--- source/interprocedural_analyses/taint/test/integration/sanitize.py
+++ source/interprocedural_analyses/taint/test/integration/sanitize.py
@@ -31,7 +31,6 @@ def test2(tainted_other):
 
 
 def test3(colliding_name):
-    # TODO(T59731231): There shouldn't be a false positive here either.
     colliding_name = return_taint_sanitize(colliding_name)
     __test_sink(colliding_name.id)
 
