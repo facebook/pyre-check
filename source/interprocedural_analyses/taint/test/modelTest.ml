10ca4eff87274240aeaf4876713103789350eeb0
Maxime Arthaud <arthaud@fb.com>
2021-04-09 13:41:02 -0700

    fix a broken test
    Summary: Oops
    
    Reviewed By: fahndrich
    
    Differential Revision: D27685873
    
    fbshipit-source-id: 8854964fea98df094d03e32fa44600c110e2b0f2

--- source/interprocedural_analyses/taint/test/modelTest.ml
+++ source/interprocedural_analyses/taint/test/modelTest.ml
@@ -1732,9 +1732,8 @@ let test_invalid_models context =
       def test.foo(x): ...
     |}
     ~expect:
-      {|Invalid model for `test.foo`: `ModelParser.T.Source {source = (Sources.T.NamedSource "A");
-  breadcrumbs = [(SimpleVia "featureA")]; path = ; leaf_names = [];
-  leaf_name_provided = false}` is not a supported taint annotation for sanitizers.|}
+      {|Invalid model for `test.foo`: `ModelParser.T.Source {source = A; breadcrumbs = [(SimpleVia "featureA")];
+  path = ; leaf_names = []; leaf_name_provided = false}` is not a supported taint annotation for sanitizers.|}
     ();
   assert_invalid_model
     ~model_source:
