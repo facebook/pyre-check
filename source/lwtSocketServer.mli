7890f6628412b22ccad4f805c399d29f9f863a48
Jia Chen <grievejia@fb.com>
2021-04-20 13:24:59 -0700

    Finer-grained server establishment logic
    Reviewed By: dkgi
    
    Differential Revision: D27836334
    
    fbshipit-source-id: 1ca580e22c44100e94de0f587681168c3194bdab

---
+++ source/lwtSocketServer.mli
@@ -0,0 +1,60 @@
+(*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
+(** This module provides high-level APIs for async server creation and shutdown.
+
+    These APIs are very similar to what {!Lwt_io.establish_server_with_client_address} and
+    {!Lwt_io.shutdown_server} do. But instead of doing socket [bind]+[listen]+[accept] all at once,
+    we splits [bind]+[listen] from [accept] as a separate step here. This allows the Pyre server to
+    bind the socket first, initialize its internal state (which can be quite expensive), and finally
+    starting to take client requests. We do not want the state to be initialized before calling
+    [bind] because we want to fail fast if another Pyre server is already running. And we do not
+    want the state to be initialized after calling [accept] because it will lead to awkward
+    programming interfaces where the request handlers have to worry about whether the server state
+    has been initialized or not. *)
+
+(** A `[PreparedSocket]` is a socket on which [bind] and [listen] are invoked, but not [accept]. See
+    {!Lwt_unix.bind}, {!Lwt_unix.listen}, and {!Lwt_unix.accept}. *)
+module PreparedSocket : sig
+  type t
+  (** Type of prepared socket. *)
+
+  val create_from_address : Unix.sockaddr -> t Lwt.t
+  (** Create a prepared socket from a given socket address. *)
+
+  val create_from_path : PyrePath.t -> t Lwt.t
+  (** Create a prepared Unix-domain socket with the given path as the location of the socket on the
+      file system. This function may attempt to remove defunct socket at the given location, if
+      there is any. *)
+end
+
+type t
+(** Type of a server *)
+
+val establish
+  :  f:(Unix.sockaddr -> Lwt_io.input Lwt_io.channel * Lwt_io.output Lwt_io.channel -> unit Lwt.t) ->
+  PreparedSocket.t ->
+  t Lwt.t
+(** [establish ~f prepared_socket] creates a server which accepts incoming connections from
+    [prepared_socket]. When a client makes a new connection, the server will create two I/O channel
+    from the connection and invoke [f client_address (input_channel, output_channel)].
+
+    The server does not block waiting for f to complete: it concurrently tries to accept more client
+    connections while f is handling the client.
+
+    When the promise returned by [f] completes, both the connection and the associated I/O channels
+    will be automatically closed. [f] should never attempt to close the I/O channels by itself.
+
+    If [f] raises an exception (or the promise it returns fails with an exception), [establish] can
+    do nothing with that exception, except pass it to [Lwt.async_exception_hook].
+
+    The returned promise (a [t Lwt.t]) resolves right before the first internal call to [accept]. *)
+
+val shutdown : t -> unit Lwt.t
+(** Closes the given server's listening socket. The returned promise resolves when the [close]
+    system call completes. This function does not affect the sockets of connections that have
+    already been accepted, i.e. passed to [f] by [establish]. *)
--- source/new_server/start.ml
+++ source/new_server/start.ml
@@ -46,38 +46,6 @@ let socket_path_of log_path =
     ~relative:(Format.sprintf "pyre_server_%s.sock" log_path_digest)
 
 
-(* One of the biggest pain point of using a Unix-domain socket is that the socket file will not get
-   auto-deleted if the server process is force-killed or crashes with fatal signals. If that
-   happens, the now-defunct socket file on disk will block all future attempts at starting a new
-   server process, until the file gets manually removed.
-
-   It may seem that such an issue can be addressed by always removing the pre-existing socket file
-   on disk whenever a new server is started. But blind file removal will incur a race condition
-   among concurrent server starts: if the pre-existing socket file is not defunct (i.e. another
-   living server is still listening on the socket), removing it will render another living server
-   unreachable, leaking system resources unless the dangling server gets manually killed.
-
-   To determine whether it is safe to remove the pre-existing socket file, we resort to file lock
-   here. One nice property about file lock is that the lock is guaranteed to be automatically
-   released from a process even if it gets forced-killed or crashes unexpectedly (ideally we want
-   the same property to hold for the socket file itself -- but unfortunately Unix-domain sockets do
-   not work this way due to historical reasons). This means that if we cannot grab a lock, we know
-   that another living process may still own the socket file, in which case it's probably not a good
-   idea to remove the file.
-
-   Note that file locks are advisory, meaning that they cannot protect the socket file from being
-   removed by another server process that does not follow the same locking protocol. Besides, file
-   locks are not guaranteed to be functional on all OSes and all filesystems. Hence this function
-   may not be able to remove defunct sockets in some cases even though it is safe to do so. *)
-let remove_existing_socket_file_if_needed socket_path =
-  let lock_path = Path.with_suffix socket_path ~suffix:".lock" in
-  match
-    Lock_file_blocking.create ~close_on_exec:true ~unlink_on_exit:true (Path.absolute lock_path)
-  with
-  | true -> Path.remove_if_exists socket_path
-  | false -> ()
-
-
 let with_performance_logging ?(normals = []) ~name f =
   let open Lwt.Infix in
   let timer = Timer.start () in
@@ -533,7 +501,6 @@ let with_server
   =
   let open Lwt in
   let socket_path = socket_path_of log_path in
-  remove_existing_socket_file_if_needed socket_path;
   (* Watchman connection needs to be up before server can start -- otherwise we risk missing
      filesystem updates during server establishment. *)
   get_watchman_subscriber ?watchman server_configuration
@@ -544,9 +511,9 @@ let with_server
     lazy
       (initialize_server_state ?watchman_subscriber ?build_system_initializer server_configuration)
   in
-  Lwt_io.establish_server_with_client_address
-    (Lwt_unix.ADDR_UNIX (Path.absolute socket_path))
-    (handle_connection ~server_state)
+  LwtSocketServer.PreparedSocket.create_from_path socket_path
+  >>= fun prepared_socket ->
+  LwtSocketServer.establish prepared_socket ~f:(handle_connection ~server_state)
   >>= fun server ->
   let server_waiter () = f (socket_path, server_state) in
   let server_destructor () =
@@ -560,7 +527,7 @@ let with_server
       else
         Lwt.return_unit
     in
-    build_system_cleanup () >>= fun () -> Lwt_io.shutdown_server server
+    build_system_cleanup () >>= fun () -> LwtSocketServer.shutdown server
   in
   finalize
     (fun () ->
