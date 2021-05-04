f9dab7a694d630c25c49aa1d1eb5d7564dcc36d3
Steven Troxler <stroxler@fb.com>
2021-05-04 11:03:51 -0700

    Move attributes to classSummary.ml [0] prep the module
    Summary:
    After talking with Jia I'm working on a refactor to move
    attribute-finding logic out of Ast.Statement and into
    Analysis.ClassSummary, which is where the only use case lives.
    
    This diff is just prepping for the change - which will involve
    dumping several new modules into classSummary.ml - by moving
    that actual ClassSummary top-level logic into a ClassSummary
    module which we then include in the top-level.
    
    Why? My impression is that having an eminymous submodule like this
    is a pretty standard way (within pyre) of having clear delimiters around
    the "main" module of a large multi-module file; we use this pattern in
    most of the environment files as well as in type.ml, it makes navigation
    by code-folding easier in big files
    
    I isolated this easy commit because I want to make the next commit -
    which moves about 1500 lines of code between files - as easy as I can
    to look at.
    
    Reviewed By: grievejia
    
    Differential Revision: D28166358
    
    fbshipit-source-id: 154e914cc68db134121dfe70d23056d38cb5f154

--- source/analysis/classSummary.ml
+++ source/analysis/classSummary.ml
@@ -9,128 +9,137 @@ open Core
 open Ast
 open Statement
 
-type t = {
-  name: Reference.t;
-  qualifier: Reference.t;
-  bases: Expression.Call.Argument.t list;
-  decorators: Decorator.t list;
-  attribute_components: Class.AttributeComponents.t;
-}
-[@@deriving compare, eq, sexp, show, hash]
-
-let create
-    ~qualifier
-    ({ Ast.Statement.Class.name = { Node.value = name; _ }; bases; decorators; _ } as definition)
-  =
-  {
-    name;
-    qualifier;
-    bases;
-    decorators;
-    attribute_components = Class.AttributeComponents.create definition;
+module ClassSummary = struct
+  type t = {
+    name: Reference.t;
+    qualifier: Reference.t;
+    bases: Expression.Call.Argument.t list;
+    decorators: Decorator.t list;
+    attribute_components: Class.AttributeComponents.t;
   }
+  [@@deriving compare, eq, sexp, show, hash]
+
+  let create
+      ~qualifier
+      ({ Ast.Statement.Class.name = { Node.value = name; _ }; bases; decorators; _ } as definition)
+    =
+    {
+      name;
+      qualifier;
+      bases;
+      decorators;
+      attribute_components = Class.AttributeComponents.create definition;
+    }
 
 
-let is_protocol { bases; _ } =
-  let is_protocol { Expression.Call.Argument.name; value = { Node.value; _ } } =
-    let open Expression in
-    match name, value with
-    | ( None,
-        Expression.Call
-          {
-            callee =
-              {
-                Node.value =
-                  Name
-                    (Attribute
-                      {
-                        base =
-                          {
-                            Node.value =
-                              Name
-                                (Attribute
-                                  {
-                                    base = { Node.value = Name (Identifier typing); _ };
-                                    attribute = "Protocol";
-                                    _;
-                                  });
-                            _;
-                          };
-                        attribute = "__getitem__";
-                        _;
-                      });
-                _;
-              };
-            _;
-          } )
-    | ( None,
-        Name
-          (Attribute
-            { base = { Node.value = Name (Identifier typing); _ }; attribute = "Protocol"; _ }) )
-      when String.equal typing "typing" || String.equal typing "typing_extensions" ->
-        true
-    | _ -> false
-  in
-  List.exists ~f:is_protocol bases
+  let is_protocol { bases; _ } =
+    let is_protocol { Expression.Call.Argument.name; value = { Node.value; _ } } =
+      let open Expression in
+      match name, value with
+      | ( None,
+          Expression.Call
+            {
+              callee =
+                {
+                  Node.value =
+                    Name
+                      (Attribute
+                        {
+                          base =
+                            {
+                              Node.value =
+                                Name
+                                  (Attribute
+                                    {
+                                      base = { Node.value = Name (Identifier typing); _ };
+                                      attribute = "Protocol";
+                                      _;
+                                    });
+                              _;
+                            };
+                          attribute = "__getitem__";
+                          _;
+                        });
+                  _;
+                };
+              _;
+            } )
+      | ( None,
+          Name
+            (Attribute
+              { base = { Node.value = Name (Identifier typing); _ }; attribute = "Protocol"; _ }) )
+        when String.equal typing "typing" || String.equal typing "typing_extensions" ->
+          true
+      | _ -> false
+    in
+    List.exists ~f:is_protocol bases
 
 
-let has_decorator { decorators; _ } decorator =
-  let decorators = List.map decorators ~f:Decorator.to_expression in
-  Expression.exists_in_list ~expression_list:decorators decorator
+  let has_decorator { decorators; _ } decorator =
+    let decorators = List.map decorators ~f:Decorator.to_expression in
+    Expression.exists_in_list ~expression_list:decorators decorator
 
 
-let is_final definition =
-  has_decorator definition "typing.final" || has_decorator definition "typing_extensions.final"
+  let is_final definition =
+    has_decorator definition "typing.final" || has_decorator definition "typing_extensions.final"
 
 
-let is_abstract { bases; _ } =
-  let abstract_metaclass { Expression.Call.Argument.value; _ } =
-    let open Expression in
-    match value with
-    | {
-     Node.value =
-       Expression.Name
-         (Attribute
-           { base = { Node.value = Name (Identifier "abc"); _ }; attribute = "ABCMeta" | "ABC"; _ });
-     _;
-    } ->
-        true
-    | _ -> false
-  in
-  List.exists bases ~f:abstract_metaclass
-
-
-let fields_tuple_value { attribute_components; _ } =
-  let attributes =
-    Ast.Statement.Class.attributes
-      ~include_generated_attributes:false
-      ~in_test:false
-      attribute_components
-  in
-  match Identifier.SerializableMap.find_opt "_fields" attributes with
-  | Some
-      {
-        Node.value =
-          {
-            kind =
-              Simple
-                { values = [{ origin = Explicit; value = { Node.value = Tuple fields; _ } }]; _ };
-            _;
-          };
-        _;
+  let is_abstract { bases; _ } =
+    let abstract_metaclass { Expression.Call.Argument.value; _ } =
+      let open Expression in
+      match value with
+      | {
+       Node.value =
+         Expression.Name
+           (Attribute
+             {
+               base = { Node.value = Name (Identifier "abc"); _ };
+               attribute = "ABCMeta" | "ABC";
+               _;
+             });
+       _;
       } ->
-      let name = function
-        | {
-            Node.value = Ast.Expression.Expression.String { Ast.Expression.StringLiteral.value; _ };
-            _;
-          } ->
-            Some value
-        | _ -> None
-      in
-      Some (List.filter_map fields ~f:name)
-  | _ -> None
+          true
+      | _ -> false
+    in
+    List.exists bases ~f:abstract_metaclass
 
 
-let name { name; _ } = name
+  let fields_tuple_value { attribute_components; _ } =
+    let attributes =
+      Ast.Statement.Class.attributes
+        ~include_generated_attributes:false
+        ~in_test:false
+        attribute_components
+    in
+    match Identifier.SerializableMap.find_opt "_fields" attributes with
+    | Some
+        {
+          Node.value =
+            {
+              kind =
+                Simple
+                  { values = [{ origin = Explicit; value = { Node.value = Tuple fields; _ } }]; _ };
+              _;
+            };
+          _;
+        } ->
+        let name = function
+          | {
+              Node.value =
+                Ast.Expression.Expression.String { Ast.Expression.StringLiteral.value; _ };
+              _;
+            } ->
+              Some value
+          | _ -> None
+        in
+        Some (List.filter_map fields ~f:name)
+    | _ -> None
 
-let bases { bases; _ } = bases
+
+  let name { name; _ } = name
+
+  let bases { bases; _ } = bases
+end
+
+include ClassSummary
