be467b495901b4f3e651efde5a6c0b6124d4c015
Lorenzo Fontana <fons@fb.com>
2021-05-07 08:31:09 -0700

    Added documentation for 6312 category
    Summary: Added documentation for 6312 category
    
    Reviewed By: gbleaney, dkgi
    
    Differential Revision: D28255519
    
    fbshipit-source-id: a71784359cf00a4c1187f69eb49a1192885b9b99

--- documentation/website/sidebars.js
+++ documentation/website/sidebars.js
@@ -114,6 +114,7 @@ module.exports = {
           'fb/warning_codes/code-6107',
           'fb/warning_codes/code-6308',
           'fb/warning_codes/code-6310',
+          'fb/warning_codes/code-6312',
           'fb/warning_codes/code-6445',
           'fb/warning_codes/code-6446',
           'fb/warning_codes/code-6449',
