ce8c1550233a4fc0da62126a057c7e6d825201a3
Graham Bleaney <gbleaney@fb.com>
2021-05-20 16:43:24 -0700

    Add testing for class level taint models
    Summary: When debugging T91109375, I realized that we didn't have any coverage of TaintSinks configured through `class Class(TaintSink[])` style models. This diff adds a test for those style models to make sure we don't regress
    
    Reviewed By: arthaud
    
    Differential Revision: D28585613
    
    fbshipit-source-id: 3c1cc60722f97e3deddb2599793314f8bf8e7b1b

---
+++ source/interprocedural_analyses/taint/test/integration/class_level_taint.py
@@ -0,0 +1,18 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+class ClassSink:
+    async def sink(self, argument):
+        pass
+
+
+class ClassSource:
+    async def source(self):
+        pass
+
+
+def test(class_sink: ClassSink, class_source: ClassSource):
+    class_sink.sink(class_source.source())
