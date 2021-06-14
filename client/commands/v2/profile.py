2e0dd01d03d83bde1252f81e2c6ba01164947fc2
Jia Chen <grievejia@fb.com>
2021-06-14 09:40:14 -0700

    Implement profile command (2/3)
    Reviewed By: dkgi
    
    Differential Revision: D29081847
    
    fbshipit-source-id: 3daa5c9f060ce55d71940ea7d11fc4a031c5a190

--- client/commands/v2/profile.py
+++ client/commands/v2/profile.py
@@ -3,7 +3,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import json
 import logging
+import subprocess
+from collections import defaultdict
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+from typing_extensions import Final
 
 from ... import commands, configuration as configuration_module
 from . import remote_logging
@@ -12,10 +19,164 @@ from . import remote_logging
 LOG: logging.Logger = logging.getLogger(__name__)
 
 
+class StatisticsOverTime:
+    _data: List[Tuple[str, int]] = []
+
+    def add(self, line: str) -> None:
+        dividers = [
+            " MEMORY Shared memory size (size: ",
+            " MEMORY Shared memory size post-typecheck (size: ",
+        ]
+        for divider in dividers:
+            if divider in line:
+                time, size_component = line.split(divider)
+                size_in_megabytes = int(size_component[:-2])
+                size_in_bytes = size_in_megabytes * (10 ** 6)
+                self._data.append((time, size_in_bytes))
+
+    def graph_total_shared_memory_size_over_time(self) -> None:
+        try:
+            gnuplot = subprocess.Popen(["gnuplot"], stdin=subprocess.PIPE)
+            # pyre-fixme[16]: `Optional` has no attribute `write`.
+            gnuplot.stdin.write(b"set term dumb 140 25\n")
+            gnuplot.stdin.write(b"plot '-' using 1:2 title '' with linespoints \n")
+            for (i, (_time, size)) in enumerate(self._data):
+                # This is graphing size against # of updates, not time
+                gnuplot.stdin.write(b"%f %f\n" % (i, size))
+            gnuplot.stdin.write(b"e\n")
+            # pyre-fixme[16]: `Optional` has no attribute `flush`.
+            gnuplot.stdin.flush()
+        except FileNotFoundError:
+            LOG.error("gnuplot is not installed")
+
+    def to_json(self) -> str:
+        return json.dumps(self._data)
+
+
+class TableStatistics:
+    # category -> aggregation -> table name -> value
+    # pyre-ignore: T62493941
+    _data: Dict[str, Dict[str, Dict[str, str]]] = defaultdict(lambda: defaultdict(dict))
+    _shared_heap_category: Final = "bytes serialized into shared heap"
+
+    @staticmethod
+    def sort_by_value(items: List[Tuple[str, str]]) -> None:
+        def parse(number: str) -> float:
+            if number[-1] == "G":
+                return float(number[:-1]) * (10 ** 9)
+            if number[-1] == "M":
+                return float(number[:-1]) * (10 ** 6)
+            if number[-1] == "K":
+                return float(number[:-1]) * (10 ** 3)
+            return float(number)
+
+        items.sort(key=lambda x: parse(x[1]), reverse=True)
+
+    def add(self, line: str) -> None:
+        divider = "stats -- "
+        if divider in line:
+            header, data = line.split(divider)
+            cells = data[:-2].split(", ")
+            collected = [cell.split(": ") for cell in cells]
+            tag_and_category = header[:-2].split(" (")
+            if len(tag_and_category) == 2:
+                tag, category = tag_and_category
+            elif header[:3] == "ALL":
+                tag = "ALL"
+                category = header[4:-1]
+            elif header[:4] == "(ALL":
+                tag = "ALL"
+                category = header[5:-2]
+            else:
+                return
+            if len(tag) > 0:
+                for key, value in collected:
+                    self._data[category][key][tag] = value
+
+    def is_empty(self) -> bool:
+        return len(self._data) == 0
+
+    def get_totals(self) -> List[Tuple[str, str]]:
+        totals = list(self._data[self._shared_heap_category]["total"].items())
+        TableStatistics.sort_by_value(totals)
+        return totals
+
+    def get_counts(self) -> List[Tuple[str, str]]:
+        counts = list(self._data[self._shared_heap_category]["samples"].items())
+        TableStatistics.sort_by_value(counts)
+        return counts
+
+
+def _get_server_log(log_directory: Path) -> Path:
+    server_stderr_path = log_directory / "new_server" / "server.stderr"
+    if not server_stderr_path.is_file():
+        raise RuntimeError(f"Cannot find server output at `{server_stderr_path}`.")
+    return server_stderr_path
+
+
+def _collect_memory_statistics_over_time(log_directory: Path) -> StatisticsOverTime:
+    server_log = _get_server_log(log_directory)
+    extracted = StatisticsOverTime()
+    with open(server_log) as server_log_file:
+        for line in server_log_file.readlines():
+            extracted.add(line)
+    return extracted
+
+
+def print_individual_table_sizes(
+    configuration: configuration_module.Configuration,
+) -> None:
+    server_log = _get_server_log(Path(configuration.log_directory))
+    extracted = TableStatistics()
+    with open(str(server_log)) as server_log_file:
+        for line in server_log_file.readlines():
+            extracted.add(line)
+    if extracted.is_empty():
+        raise RuntimeError(
+            "Cannot find table size data in "
+            + f"`{server_log.as_posix()}`. "
+            + "Please run Pyre with `--debug` option first."
+        )
+    sizes = json.dumps(extracted.get_totals())
+    counts = json.dumps(extracted.get_counts())
+    # I manually put together this json in order to be
+    # simultaneously machine and human readable
+    combined = (
+        "{\n"
+        + f'  "total_table_sizes": {sizes},\n'
+        + f'  "table_key_counts": {counts}\n'
+        + "}"
+    )
+    print(combined)
+
+
+def print_total_shared_memory_size_over_time(
+    configuration: configuration_module.Configuration,
+) -> None:
+    memory_over_time = _collect_memory_statistics_over_time(
+        Path(configuration.log_directory)
+    ).to_json()
+    print(memory_over_time)
+
+
+def print_total_shared_memory_size_over_time_graph(
+    configuration: configuration_module.Configuration,
+) -> None:
+    statistics = _collect_memory_statistics_over_time(Path(configuration.log_directory))
+    statistics.graph_total_shared_memory_size_over_time()
+
+
 def run_profile(
     configuration: configuration_module.Configuration, output: commands.ProfileOutput
 ) -> commands.ExitCode:
-    LOG.info("Coming soon...")
+    if output == commands.ProfileOutput.INDIVIDUAL_TABLE_SIZES:
+        print_individual_table_sizes(configuration)
+    elif output == commands.ProfileOutput.TOTAL_SHARED_MEMORY_SIZE_OVER_TIME:
+        print_total_shared_memory_size_over_time(configuration)
+    elif output == commands.ProfileOutput.TOTAL_SHARED_MEMORY_SIZE_OVER_TIME_GRAPH:
+        print_total_shared_memory_size_over_time_graph(configuration)
+    else:
+        LOG.info("Coming soon...")
     return commands.ExitCode.SUCCESS
 
 
---
+++ client/commands/v2/tests/profile_test.py
@@ -0,0 +1,132 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import testslide
+
+from ..profile import (
+    StatisticsOverTime,
+    TableStatistics,
+)
+
+
+class ProfileTest(testslide.TestCase):
+    def test_table_statistics(self) -> None:
+        statistics = TableStatistics()
+        lines = [
+            "(ALL cache hit rate) stats -- samples: 183.378K, total: 143.256K, "
+            "avg: 0.781206, stddev: 0.413429, max: 1, min: 0)",
+            "ALL bytes deserialized from shared heap stats -- samples: 80.124K, "
+            "total: 3.501M, avg: 43.692277, stddev: 338.921118, max: 22.434K, min: 0)",
+            "ALL bytes saved in shared heap due to compression stats -- samples: "
+            "51.672K, total: 6.504M, avg: 125.869813, stddev: 1.211K, "
+            "max: 78.471K, min: 0)",
+            "ALL bytes serialized into shared heap stats -- samples: 51.672K, "
+            "total: 11.721M, avg: 226.833449, stddev: 1.331K, max: 89.739K, "
+            "min: 3)",
+            "ALL bytes shared heap compression ratio stats -- samples: 51.672K, "
+            "total: 46.811K, avg: 0.905918, stddev: 0.132285, max: 1, min: 0.200000)",
+            "AST (bytes deserialized from shared heap) stats -- samples: "
+            "1.690K, total: 1.635M, avg: 967.172781, stddev: 1.721K, max: "
+            "22.434K, min: 18)",
+            "AST (bytes saved in shared heap due to compression) stats -- "
+            "samples: 845, total: 2.571M, avg: 3.043K, stddev: 6.361K, max: "
+            "78.471K, min: 0)",
+            "AST (bytes serialized into shared heap) stats -- samples: 845, "
+            "total: 3.270M, avg: 3.870K, stddev: 6.883K, max: 89.739K, "
+            "min: 73)",
+            "AST (shared heap compression ratio) stats -- samples: 845, "
+            "total: 562.823112, avg: 0.666063, stddev: 0.124657, max: 1, "
+            "min: 0.337358)",
+            "Alias (bytes deserialized from shared heap) stats -- samples: "
+            "46.897K, total: 255.028K, avg: 5.438045, stddev: 1.665136, "
+            "max: 18, min: 5)",
+            "Alias (bytes saved in shared heap due to compression) stats -- "
+            "samples: 1.158K, total: 85.000000, avg: 0.073402, stddev: "
+            "1.005495, max: 21, min: 0)",
+            "Alias (bytes serialized into shared heap) stats -- samples: "
+            "1.158K, total: 27.826K, avg: 24.029361, stddev: 8.903096, "
+            "max: 73, min: 21)",
+            "Alias (shared heap compression ratio) stats -- samples: 1.158K, "
+            "total: 1.157K, avg: 0.999019, stddev: 0.012015, max: 1, min: "
+            "0.769231)",
+            "Class (bytes deserialized from shared heap) stats -- samples: "
+            "3.430K, total: 677.011K, avg: 197.379300, stddev: 343.207031, "
+            "max: 5.507K, min: 12)",
+            "Class (bytes saved in shared heap due to compression) stats -- "
+            "samples: 3.305K, total: 1.300M, avg: 393.340091, stddev: 1.101K, "
+            "max: 19.893K, min: 0)",
+            "Class (bytes serialized into shared heap) stats -- samples: "
+            "3.305K, total: 2.556M, avg: 773.294100, stddev: 1.331K, max: "
+            "22.030K, min: 51)",
+            "Class (cache hit rate) stats -- samples: 19.701K, total: 16.100K, "
+            "avg: 0.817217, stddev: 0.386488, max: 1, min: 0)",
+            "Class (shared heap compression ratio) stats -- samples: 3.305K, "
+            "total: 2.679K, avg: 0.810528, stddev: 0.129101, max: 1, min: "
+            "0.437840)",
+        ]
+        for line in lines:
+            statistics.add(line + "\n")
+        self.assertEqual(
+            statistics.get_totals(),
+            [
+                ("ALL", "11.721M"),
+                ("AST", "3.270M"),
+                ("Class", "2.556M"),
+                ("Alias", "27.826K"),
+            ],
+        )
+        self.assertEqual(
+            statistics.get_counts(),
+            [
+                ("ALL", "51.672K"),
+                ("Class", "3.305K"),
+                ("Alias", "1.158K"),
+                ("AST", "845"),
+            ],
+        )
+
+    def test_statistics_over_time(self) -> None:
+        statistics = StatisticsOverTime()
+        lines = [
+            "2020-04-27 20:08:35 MEMORY Shared memory size post-typecheck (size: 42)",
+            "2020-02-19 10:35:57 PERFORMANCE Check_TypeCheck: 1.767435s",
+            "2020-02-19 10:35:57 PROGRESS Postprocessing 51 sources...",
+            "2020-02-19 10:35:57 PROGRESS Postprocessed 51 of 51 sources",
+            "2020-02-19 10:35:57 MEMORY Shared memory size (size: 2105)",
+            "2020-02-19 10:35:57 INFO Number of new errors = 0",
+            "2020-02-19 10:35:57 PERFORMANCE Incremental check: 2.456214s",
+            "2020-02-19 10:35:57 PERFORMANCE Server request: 2.456249s",
+            "2020-02-19 10:35:57 PERFORMANCE Server request: 2.456372s",
+            "2020-02-19 10:36:06 PERFORMANCE Module tracker updated: 0.000838s",
+            "2020-02-19 10:36:06 INFO Parsing 9 updated modules...",
+            "2020-02-19 10:36:07 INFO Repopulating the environment for 9 " "modules.",
+            "2020-02-19 10:36:07 INFO Updating is from empty stub result "
+            "Environment",
+            "2020-02-19 10:36:07 INFO Updating Alias Environment",
+            "2020-02-19 10:36:07 INFO Updating Edges Environment",
+            "2020-02-19 10:36:07 INFO Updating Undecorated functions " "Environment",
+            "2020-02-19 10:36:07 INFO Updating Class metadata Environment",
+            "2020-02-19 10:36:07 INFO Updating parse annotation Environment",
+            "2020-02-19 10:36:07 INFO Updating attributes Environment",
+            "2020-02-19 10:36:07 INFO Updating Global Environment",
+            "2020-02-19 10:36:07 INFO Updating Global Locations Environment",
+            "2020-02-19 10:36:07 INFO Checking 295 functions...",
+            "2020-02-19 10:36:09 PROGRESS Processed 295 of 295 functions",
+            "2020-02-19 10:36:09 PERFORMANCE Check_TypeCheck: 2.156352s",
+            "2020-02-19 10:36:09 PROGRESS Postprocessing 23 sources...",
+            "2020-02-19 10:36:09 PROGRESS Postprocessed 23 of 23 sources",
+            "2020-02-19 10:36:09 MEMORY Shared memory size (size: 2106)",
+            "2020-02-19 10:36:09 INFO Number of new errors = 0",
+        ]
+        for line in lines:
+            statistics.add(line + "\n")
+        self.assertEqual(
+            statistics._data,
+            [
+                ("2020-04-27 20:08:35", 42000000),
+                ("2020-02-19 10:35:57", 2105000000),
+                ("2020-02-19 10:36:09", 2106000000),
+            ],
+        )
