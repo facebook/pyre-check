d254714461f6250674c9873ef31345b3f741fb85
Brandon Wu <bjwu@fb.com>
2021-06-18 09:38:59 -0700

    Added Pyre connection functionality to `shape_type_coverage`
    Summary:
    + Problem: We need to be able to open a connection to a Pyre server to query, so that we can get the coverage statistics that we need for `shape_type_coverage`.
    + Background: This is one of a series of diffs on implementing tensor shape type coverage for Pyre. Coverage statistics help users know to what degree they are currently utilizing precise shape types, which is desirable. Precise tensor shape types are a good thing because they help catch static shape mismatches, rather than leaving them for runtime. Imprecise shape types, however, give basically no information, and result in programs that have shape mismatches being able to type-check. To prevent these errors, clients should want to achieve good coverage statistics for precise types. This change is to allow clients to ascertain what their coverage metrics are.
    + Solution: This diff adds in ability to call the script from a Pyre project, and then start and stop the server. Nothing fancy yet.
    
    Reviewed By: pradeep90
    
    Differential Revision: D29135225
    
    fbshipit-source-id: ed73cf8b383660ca7e63e9f1c4f318e2610c8b75

--- scripts/shape_type_coverage.py
+++ scripts/shape_type_coverage.py
@@ -4,16 +4,42 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
+import sys
+from pathlib import Path
 
-from libfb.py.log import set_simple_logging
+from ..api.connection import PyreConnection
+from ..client.find_directories import find_global_and_local_root, FoundRoot
 
 LOG: logging.Logger = logging.getLogger(__name__)
 
 
-def main() -> None:
-    set_simple_logging(logging.INFO)
+def _create_pyre_connection(
+    roots: FoundRoot,
+) -> PyreConnection:
 
-    LOG.info("This is a script!")
+    if roots.local_root is not None:
+        return PyreConnection(roots.local_root)
+    return PyreConnection(roots.global_root)
+
+
+def main() -> None:
+    logging.basicConfig(
+        format="[%(asctime)s][%(levelname)s]: %(message)s", level=logging.INFO
+    )
+
+    roots = find_global_and_local_root(Path("."))
+    if roots is None:
+        LOG.error(f"Failed to find global Pyre configuration for {Path().absolute()}.")
+        sys.exit(1)
+
+    try:
+        with _create_pyre_connection(roots) as pyre_connection:
+            LOG.info(
+                f"Server is up: {pyre_connection.server_initialized} at "
+                f"{pyre_connection.pyre_directory}"
+            )
+    except Exception as exception:
+        LOG.error(f"Pyre server raised an exception: {exception}")
 
 
 if __name__ == "__main__":
