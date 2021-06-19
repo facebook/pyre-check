76caafa4aa17a72b7fa6636d15f6d18391a18349
Brandon Wu <bjwu@fb.com>
2021-06-18 18:04:36 -0700

    Added coverage statistics to `shape_type_coverage`
    Summary:
    + Problem: We would like for users to be able to judge the coverage of imprecise tensor shape types within their Python projects, using Pyre.
    + Background: Tensor shape types are an important addition to Pyre, allowing shape mismatches during execution to be caught statically, preventing time being wasted. Precise tensor shape types are those that encode the specific dimensions of their tensors, and mismatches between those dimensions are where we get shape mismatches. Imprecise tensor shape types are those that do not encode specific dimensions, which results in code that potentially causes shape mismatches passing Pyre. As such, it is desirable to minimize the extent to which code has imprecise shape types. This is one of a series of diffs to implement a script which will let users check what degree of precision their code has.
    + Solution: This diff implements the actual coverage statistics, which are printed out on a per-file basis when the file is run.
    
    Reviewed By: pradeep90
    
    Differential Revision: D29173854
    
    fbshipit-source-id: 9f70f23e8b433bd1909cb4916bd6bf213cb97e4b

--- scripts/shape_type_coverage.py
+++ scripts/shape_type_coverage.py
@@ -7,9 +7,10 @@ import logging
 import sys
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Dict, Iterable
 
 from ..api.connection import PyreConnection
+from ..api.query import Annotation, get_types
 from ..client.find_directories import find_global_and_local_root, FoundRoot
 
 LOG: logging.Logger = logging.getLogger(__name__)
@@ -19,6 +20,12 @@ LOG: logging.Logger = logging.getLogger(__name__)
 class ParametricType:
     name: str
     parameters: List[str]
+
+
+@dataclass(frozen=True)
+class ShapeAnnotations:
+    precise_annotations: List[Annotation]
+    imprecise_annotations: List[Annotation]
 
 
 def _create_pyre_connection(
@@ -68,7 +75,44 @@ def _is_precise_tensor(parametric: ParametricType) -> bool:
     )
 
 
-def main() -> None:
+def _collect_shape_types(
+    mapping: Dict[str, List[Annotation]]
+) -> Dict[str, ShapeAnnotations]:
+    final_dictionary = {}
+    for filename, annotations in mapping.items():
+        precise_annotations = []
+        imprecise_annotations = []
+        for annotation in annotations:
+            parametric = _parametric_type(annotation.type_name)
+            if parametric is None or not _is_tensor(parametric):
+                continue
+            if _is_precise_tensor(parametric):
+                precise_annotations.append(annotation)
+            else:
+                imprecise_annotations.append(annotation)
+        final_dictionary[filename] = ShapeAnnotations(
+            precise_annotations, imprecise_annotations
+        )
+
+    return final_dictionary
+
+
+def _report_percentages(mapping: Dict[str, ShapeAnnotations]) -> None:
+    for filename, shape_annotations in mapping.items():
+        precise_count = len(shape_annotations.precise_annotations)
+        imprecise_count = len(shape_annotations.imprecise_annotations)
+        if (precise_count + imprecise_count) == 0:
+            LOG.info(f"File {filename} has no expressions with tensor shape type.")
+            continue
+        percentage = round(precise_count / (imprecise_count + precise_count) * 100.0, 2)
+        LOG.info(
+            f"File {filename} has {percentage}% precise tensor "
+            f"shape types ({precise_count} out of "
+            f"{imprecise_count + precise_count} expressions)"
+        )
+
+
+def main(filenames: Iterable[str]) -> None:
     logging.basicConfig(
         format="[%(asctime)s][%(levelname)s]: %(message)s", level=logging.INFO
     )
@@ -81,12 +125,15 @@ def main() -> None:
     try:
         with _create_pyre_connection(roots) as pyre_connection:
             LOG.info(
-                f"Server is up: {pyre_connection.server_initialized} at "
-                f"{pyre_connection.pyre_directory}"
+                f"Server is up: {pyre_connection.server_initialized} "
+                f"at {pyre_connection.pyre_directory}"
             )
+            typing_summary = get_types(pyre_connection, *filenames)
+            shape_mapping = _collect_shape_types(typing_summary)
+            _report_percentages(shape_mapping)
     except Exception as exception:
         LOG.error(f"Pyre server raised an exception: {exception}")
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
--- scripts/tests/shape_type_coverage_test.py
+++ scripts/tests/shape_type_coverage_test.py
@@ -4,12 +4,16 @@
 # LICENSE file in the root directory of this source tree.
 
 import unittest
+from typing import Dict, List
 
+from ...api.query import Annotation, Position
 from ..shape_type_coverage import (
     _is_tensor,
     _is_precise_tensor,
     ParametricType,
     _parametric_type,
+    ShapeAnnotations,
+    _collect_shape_types,
 )
 
 
@@ -105,4 +109,49 @@ class ParametricTypeTest(unittest.TestCase):
     def test_is_not_parametric(self) -> None:
         self.assert_is_not_parametric("int")
         self.assert_is_not_parametric("")
-        self.assert_is_not_parametric("torch.Tensor")
+
+
+class CollectShapeTypesTest(unittest.TestCase):
+    def assert_collects_as(
+        self,
+        given: Dict[str, List[Annotation]],
+        expected: Dict[str, ShapeAnnotations],
+    ) -> None:
+        self.assertEqual(expected, _collect_shape_types(given))
+
+    def test_collects_as(self) -> None:
+        precise = Annotation(
+            "torch.Tensor[torch.float32, typing_extensions.Literal[5], "
+            "typing_extensions.Literal[2]]",
+            Position(1, 0),
+            Position(2, 3),
+        )
+
+        imprecise = Annotation(
+            "torch.Tensor[torch.float32, int, typing_extensions.Literal[2]]",
+            Position(2, 5),
+            Position(0, 1),
+        )
+
+        non_tensor = Annotation(
+            "typing_extensions.Literal[5]",
+            Position(0, 0),
+            Position(0, 0),
+        )
+        self.assert_collects_as(given={}, expected={})
+
+        self.assert_collects_as(
+            given={"filename": [precise, imprecise, non_tensor]},
+            expected={"filename": ShapeAnnotations([precise], [imprecise])},
+        )
+
+        self.assert_collects_as(
+            given={
+                "first_file": [precise],
+                "second_file": [imprecise],
+            },
+            expected={
+                "first_file": ShapeAnnotations([precise], []),
+                "second_file": ShapeAnnotations([], [imprecise]),
+            },
+        )
