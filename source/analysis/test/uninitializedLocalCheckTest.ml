41f751f177604eb7309cbaeb8b704d9fd080ab05
Kshitij Bansal <kshitijbansal@fb.com>
2021-06-28 15:24:40 -0700

    Uninitialized local: cleanup some test comments
    Reviewed By: pradeep90
    
    Differential Revision: D29379095
    
    fbshipit-source-id: 6b98cc196e8789610061b4df916fd2d50635874b

--- source/analysis/test/uninitializedLocalCheckTest.ml
+++ source/analysis/test/uninitializedLocalCheckTest.ml
@@ -147,13 +147,11 @@ let test_simple context =
     |}
     [];
 
-  (* should be: no error *)
   assert_uninitialized_errors {|
       def f(x: str) -> None:
         assert True, x
     |} [];
 
-  (* should be: no error *)
   assert_uninitialized_errors
     {|
       from media import something
