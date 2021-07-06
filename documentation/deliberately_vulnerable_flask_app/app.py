0ceb52bd41943fc5e66703dd3e51b6973d5cf1c8
Pulkit Mishra <pulkitmishra007@gmail.com>
2021-07-06 17:28:16 +0530

    adds vulnerable functions to flask app

--- documentation/deliberately_vulnerable_flask_app/app.py
+++ documentation/deliberately_vulnerable_flask_app/app.py
@@ -4,9 +4,10 @@
 # LICENSE file in the root directory of this source tree.
 
 import subprocess
-
+import sqlite3
 from flask import Flask
-
+import request
+from lxml import etree
 
 app = Flask(__name__)
 
@@ -24,3 +25,32 @@ def potential_rce_1(payload: str) -> None:
 @app.route("/rce/<int:payload>")
 def potential_rce_2(payload: int) -> None:
     subprocess.run(f"echo {payload}", shell=True)
+
+
+@app.route("/pt/<string:payload>")
+def definite_pt(payload: str) -> str:
+    f = open(payload, "r")
+    text = f.read()
+    return text
+
+
+@app.route("/xss/<string:payload>")
+def definite_xss(payload: str) -> None:
+    flask.Markup(payload)
+
+
+@app.route("/sql/<string:payload>")
+def definite_sql(payload: str) -> None:
+    con = sqlite3.connect()
+    cur = con.cursor()
+    cur.execute(payload)
+
+
+@app.route("/ssrf/<string:payload>")
+def definite_ssrf(payload: str) -> None:
+    requests.get(payload)
+
+
+@app.route("/xxe/<string:payload>")
+def definite_xxe(payload: str) -> None:
+    etree.fromstring(payload)
