3a2def2488fec2622c447a0307fda6b6c4c30d23
Kshitij Bansal <kshitijbansal@fb.com>
2021-07-30 14:17:59 -0700

    Generate raw pyre coverage output
    Summary:
    The raw pyre coverage format is as follows:
    ```
    {
      "filepath": <filepath>
      "covered_lines": <list of covered lines>
      "uncovered_lines": <list of uncovered lines>
    }
    ```
    
    Reviewed By: pradeep90
    
    Differential Revision: D29921123
    
    fbshipit-source-id: 0c0dd2928be51e0024052641fe76bd9551827435

--- client/statistics_collectors.py
+++ client/statistics_collectors.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 
+import dataclasses
 from collections import defaultdict
 from re import compile
 from typing import Any, Dict, List, Pattern, Sequence
@@ -104,7 +105,6 @@ class AnnotationCountCollector(StatisticsCollector):
         return_is_annotated = node.returns is not None
         if return_is_annotated:
             self.annotated_return_count += 1
-
         annotated_parameters = self._check_parameter_annotations(node.params.params)
 
         if return_is_annotated and (annotated_parameters == len(node.params.params)):
@@ -270,6 +270,46 @@ class FunctionsCollector(cst.CSTVisitor):
                 Bodies of unannotated functions are not typechecked by Pyre.",
             )
             self.issues.append(issue)
+
+
+class CoverageCollector(cst.CSTVisitor):
+    METADATA_DEPENDENCIES = (PositionProvider,)
+    path: str = ""
+
+    @dataclasses.dataclass(frozen=True)
+    class FunctionInfo:
+        name: cst.Name
+        code_range: CodeRange
+
+    def __init__(self) -> None:
+        self.covered_functions: List[CoverageCollector.FunctionInfo] = []
+        self.uncovered_functions: List[CoverageCollector.FunctionInfo] = []
+
+    def _code_ranges_to_lines(self, code_ranges: Sequence[CodeRange]) -> List[int]:
+        lines = set()
+        for code_range in code_ranges:
+            lines |= set(range(code_range.start.line, code_range.end.line + 1))
+        return list(lines)
+
+    def visit_FunctionDef(self, node: cst.FunctionDef) -> None:
+        function_info = CoverageCollector.FunctionInfo(
+            name=node.name,
+            code_range=self.get_metadata(PositionProvider, node),
+        )
+        if node.returns is not None:
+            self.covered_functions.append(function_info)
+        else:
+            self.uncovered_functions.append(function_info)
+
+    @property
+    def covered_lines(self) -> List[int]:
+        covered_ranges = [info.code_range for info in self.covered_functions]
+        return self._code_ranges_to_lines(covered_ranges)
+
+    @property
+    def uncovered_lines(self) -> List[int]:
+        uncovered_ranges = [info.code_range for info in self.uncovered_functions]
+        return self._code_ranges_to_lines(uncovered_ranges)
 
 
 class StrictIssueCollector(StrictCountCollector):
