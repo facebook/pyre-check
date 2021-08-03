64054a12745d0685a5313f2a3bc39b2464120503
Jia Chen <grievejia@fb.com>
2021-08-03 10:53:52 -0700

    Implement `pyre infer` (2/n) -- pre-run sanity checks
    Reviewed By: dkgi
    
    Differential Revision: D30060416
    
    fbshipit-source-id: 812a27f47eb229f328f51cdd322801dfc35101e6

--- client/commands/v2/infer.py
+++ client/commands/v2/infer.py
@@ -4,6 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
+from pathlib import Path
+from typing import List, Optional
 
 from ... import commands, command_arguments, configuration as configuration_module
 from . import remote_logging
@@ -11,10 +13,49 @@ from . import remote_logging
 LOG: logging.Logger = logging.getLogger(__name__)
 
 
+def _check_arguments(infer_arguments: command_arguments.InferArguments) -> None:
+    if (
+        infer_arguments.annotate_from_existing_stubs
+        and infer_arguments.paths_to_modify is None
+    ):
+        raise ValueError(
+            "`--annotate-from-existing-stubs` cannot be used without the"
+            " `--in-place` flag"
+        )
+
+
+def _check_working_directory(
+    working_directory: Path, global_root: Path, relative_local_root: Optional[str]
+) -> None:
+    candidate_locations: List[str] = []
+    if working_directory == global_root:
+        return
+    candidate_locations.append(f"`{global_root}` with `--local-configuration` set")
+
+    if relative_local_root is not None:
+        local_root = global_root / relative_local_root
+        if working_directory == local_root:
+            return
+        candidate_locations.append(f"`{local_root}`")
+
+    valid_locations = " or from ".join(candidate_locations)
+    raise ValueError(
+        f"Infer must run from {valid_locations}. "
+        f"Cannot run from current working directory `{working_directory}`."
+    )
+
+
 def run_infer(
     configuration: configuration_module.Configuration,
     infer_arguments: command_arguments.InferArguments,
 ) -> commands.ExitCode:
+    working_directory = Path.cwd()
+    _check_arguments(infer_arguments)
+    _check_working_directory(
+        working_directory=working_directory,
+        global_root=Path(configuration.project_root),
+        relative_local_root=configuration.relative_local_root,
+    )
     LOG.warning("Coming soon...")
     return commands.ExitCode.SUCCESS
 
