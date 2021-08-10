917f9315b3d9664f41cd443b5b6b804ec799b055
Sarthak Khattar <sarthakoct@gmail.com>
2021-08-10 15:29:18 +0530

    import changes

--- client/commands/v2/__init__.py
+++ client/commands/v2/__init__.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from . import analyze  # noqa F401
+from ..v2 import pysa_server  # noqa F401
 from . import backend_arguments  # noqa F401
 from . import check  # noqa F401
 from . import coverage  # noqa F401
@@ -13,7 +13,6 @@ from . import initialize  # noqa F401
 from . import kill  # noqa F401
 from . import persistent  # noqa F401
 from . import profile  # noqa F401
-from . import pysa_server  # noqa F401
 from . import query  # noqa F401
 from . import rage  # noqa F401
 from . import restart  # noqa F401
