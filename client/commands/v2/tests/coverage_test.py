44a9707d4a43ec98ea1833ce42e36998e41bca68
Jia Chen <grievejia@fb.com>
2021-08-19 18:52:06 -0700

    Implement `pyre coverage` (3/3) -- collect&print
    Reviewed By: kbansal
    
    Differential Revision: D30436430
    
    fbshipit-source-id: b3f338c9bd2b604b568a0b176f30a2a2c15061c3

--- client/commands/v2/tests/coverage_test.py
+++ client/commands/v2/tests/coverage_test.py
@@ -3,12 +3,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import tempfile
 from pathlib import Path
+from typing import List
 
 import testslide
 
 from .... import configuration
-from ..coverage import find_root
+from ....tests import setup
+from ..coverage import find_root, collect_coverage_for_paths, FileCoverage
 
 
 class CoverageTest(testslide.TestCase):
@@ -33,3 +36,25 @@ class CoverageTest(testslide.TestCase):
             ),
             Path("/working/dir"),
         )
+
+    def test_collect_coverage(self) -> None:
+        with tempfile.TemporaryDirectory() as root:
+            root_path: Path = Path(root)
+            setup.ensure_files_exist(root_path, ["foo.py", "bar.py"])
+            foo_path = root_path / "foo.py"
+            bar_path = root_path / "bar.py"
+            baz_path = root_path / "baz.py"
+
+            data: List[FileCoverage] = collect_coverage_for_paths(
+                [foo_path, bar_path, baz_path], working_directory=root
+            )
+
+            def is_collected(path: Path) -> bool:
+                return any(
+                    str(path.relative_to(root_path)) == coverage.filepath
+                    for coverage in data
+                )
+
+            self.assertTrue(is_collected(foo_path))
+            self.assertTrue(is_collected(bar_path))
+            self.assertFalse(is_collected(baz_path))
