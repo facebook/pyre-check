574f6d230e11d0cdc2257e268936bd393476f6d0
Steven Troxler <stroxler@fb.com>
2021-09-07 09:22:17 -0700

    Make the python test script more useful
    Summary:
    We want the open-source python test script to be as useful as
    possible. This commit improves it in two ways:
    - make it invariant to the project directory name (we should
      not be assuming the directory is always named `pyre-check`)
    - use testslide instead of unittest to drive tests. It's a drop-in
      replacement, but makes reading terminal output *much* easier.
    
    Reviewed By: shannonzhu
    
    Differential Revision: D30744833
    
    fbshipit-source-id: dc634cf9f9046c6ba14298cea78382c603e5b434

--- scripts/run-python-tests.sh
+++ scripts/run-python-tests.sh
@@ -7,15 +7,26 @@
 set -e
 
 SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${SCRIPTS_DIRECTORY}/../.."
 
-echo '  Enumerating test files:'
-files=$(find 'pyre-check/client' -name '*_test.py' ! -name 'watchman_test.py')
-echo "${files}"
+# For open-source tests to work, we have to run them from one directory
+# higher than the project root, so that the project root is treated as a
+# valid package for relative imports.
+#
+# We also want to be sure the tests work regardless of the project directory name
+# (particularly because both `pyre-check` and `pyre` are sometimes used).
+cd "${SCRIPTS_DIRECTORY}/.."
+ROOT_DIRECTORY="$(pwd)"
+ROOT_DIRECTORY_BASE="$(basename "${ROOT_DIRECTORY}")"
+cd ..
+
+files=$(find "${ROOT_DIRECTORY_BASE}/client" -name '*_test.py' ! -name 'watchman_test.py')
+echo "Found these test files:
+${files}
+---"
 if [[ -z "${files}" ]]; then
   echo 'No test files found, exiting.'
   exit 2
 fi
 
-echo '  Running all tests:'
-echo "${files}" | sed 's/.py$//' | sed 's:/:.:g' | xargs python -m unittest -v
+echo ' Running all tests:'
+echo "${files}" | xargs testslide
