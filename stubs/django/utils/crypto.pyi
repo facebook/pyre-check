65af29b842de2fe1c91317287150b3011538ac68
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-08 14:34:01 +0800

    Delete stubs directory

--- stubs/django/utils/crypto.pyi
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-def constant_time_compare(val1: str, val2: str) -> bool: ...
-def get_random_string(length: int) -> str: ...
--- stubs/django/utils/datastructures.pyi
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-import typing
-
-class MultiValueDict(typing.Dict[typing.Any, typing.Any]):
-    def copy(self) -> MultiValueDict: ...
