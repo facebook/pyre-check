54243f7c6f00d34ee09143ce69632f4b44cf3169
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-08 14:33:32 +0800

    Delete source directory

--- source/hack_parallel/hack_parallel/utils/collections/myMap.mli
+++
@@ -1,9 +0,0 @@
-(*
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *)
-
-module type S = MyMap_sig.S
-module Make (Ord : Map.OrderedType) : S with type key = Ord.t
