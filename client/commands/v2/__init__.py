516a32c8adedb86f23d80b1c441c49310f48ef9b
Sarthak Khattar <sarthakoct@gmail.com>
2021-09-09 14:44:30 +0530

    fix change due to rebase

--- client/commands/v2/__init__.py
+++ client/commands/v2/__init__.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from . import analyze  # noqa F401
 from . import backend_arguments  # noqa F401
 from . import check  # noqa F401
 from . import coverage  # noqa F401
