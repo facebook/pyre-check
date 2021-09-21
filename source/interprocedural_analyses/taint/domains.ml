5e440fc01b2a96f76e0aa2a7bfc51f758ff40e49
Maxime Arthaud <arthaud@fb.com>
2021-09-20 17:30:36 -0700

    Back out "Optimize Tree domain"
    Summary:
    D28135107 (https://github.com/facebook/pyre-check/commit/a11d4a0b05ec8e608db31cd00d0a7d8e30ece439) seems to introduce a bug in pysa, where we get a ton of "transform_tree not minimal".
    See the pyre-pysa-analysis-analyze-diff job failure on that diff.
    Let's revert it for now.
    
    Original commit changeset: 661ebcd245f8
    
    Reviewed By: fahndrich
    
    Differential Revision: D31064673
    
    fbshipit-source-id: 1c5ab305289f892ceeb6a08aa913d2b2344d18aa

--- source/interprocedural_analyses/taint/domains.ml
+++ source/interprocedural_analyses/taint/domains.ml
@@ -543,15 +543,15 @@ module MakeTaintTree (Taint : TAINT_DOMAIN) () = struct
       ()
 
   let apply_call location ~callees ~port taint_tree =
-    let transform_path (path, _) tip =
+    let transform_path (path, tip) =
       let tip =
         tip
         |> Taint.transform Taint.kind Filter ~f:(fun kind -> not (Taint.ignore_kind_at_call kind))
         |> fun taint -> Taint.apply_call location ~callees ~port ~path ~element:taint
       in
-      tip
+      path, tip
     in
-    transform Taint.Self (Context (Path, Map)) ~f:transform_path taint_tree
+    transform Path Map ~f:transform_path taint_tree
 
 
   let empty = bottom
