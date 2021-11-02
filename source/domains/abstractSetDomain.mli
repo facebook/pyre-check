f5ed266b23937455f8d579cae8212dfa86ebbeb6
Maxime Arthaud <arthaud@fb.com>
2021-11-02 13:06:56 -0700

    Support building an abstract set domain from a set
    Summary: This will be used in a follow-up to implement leaf names in pysa with patricia trees.
    
    Reviewed By: dkgi
    
    Differential Revision: D32084345
    
    fbshipit-source-id: 1ca94122123c27ce0bc83e47f73c7ff271178684

--- source/domains/abstractSetDomain.mli
+++ source/domains/abstractSetDomain.mli
@@ -13,6 +13,48 @@ module type ELEMENT = sig
   val compare : t -> t -> int
 end
 
+module type SET = sig
+  type t
+
+  type element
+
+  val empty : t
+
+  val is_empty : t -> bool
+
+  val singleton : element -> t
+
+  val add : element -> t -> t
+
+  val remove : element -> t -> t
+
+  val mem : element -> t -> bool
+
+  val union : t -> t -> t
+
+  val inter : t -> t -> t
+
+  val subset : t -> t -> bool
+
+  val diff : t -> t -> t
+
+  val map : (element -> element) -> t -> t
+
+  val filter : (element -> bool) -> t -> t
+
+  val fold : (element -> 'a -> 'a) -> t -> 'a -> 'a
+
+  val exists : (element -> bool) -> t -> bool
+
+  val of_list : element list -> t
+
+  val elements : t -> element list
+
+  val show_element : element -> string
+
+  val element_name : string
+end
+
 module type S = sig
   include AbstractDomainCore.S
 
@@ -32,5 +74,7 @@ module type S = sig
 
   val of_list : element list -> t
 end
+
+module MakeWithSet (Set : SET) : S with type element = Set.element
 
 module Make (Element : ELEMENT) : S with type element = Element.t
