fa8da8139fbfa108f655c9a189e9d204db9ebf92
Jia Chen <grievejia@fb.com>
2021-11-10 09:46:58 -0800

    Remove legacy `Reporting` command
    Reviewed By: shannonzhu
    
    Differential Revision: D32303203
    
    fbshipit-source-id: 4f7162ec26c4f6e90909d59ab7c4da9f1fc6f993

--- client/commands/__init__.py
+++ client/commands/__init__.py
@@ -3,16 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List, Type
-
 from .command import (  # noqa; noqa; noqa
     ClientException as ClientException,
     Command as Command,
     CommandParser as CommandParser,
     ExitCode as ExitCode,
 )
-from .reporting import Reporting as Reporting
-
-COMMANDS: List[Type[CommandParser]] = [
-    Reporting,
-]
