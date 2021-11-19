bff77c8f4288cef6520b785752fe61723db4822d
Maxime Arthaud <arthaud@fb.com>
2021-11-19 12:24:12 -0800

    Add tests for sources and sinks in format strings
    Summary: Now that we properly resolve calls in format strings, we can also catch all these cases.
    
    Reviewed By: dkgi
    
    Differential Revision: D32551318
    
    fbshipit-source-id: ca4f1ca3bbb190f663f20367a17a2c0abd8049e4

--- source/interprocedural_analyses/taint/test/integration/format.py
+++ source/interprocedural_analyses/taint/test/integration/format.py
@@ -11,9 +11,22 @@ def source_via_format():
     return f"{taint} is bad"
 
 
+def source_in_format():
+    return f"{_test_source()} is bad"
+
+
 def tito_via_format(arg1, arg2, arg3):
     return f"{arg1} and {arg2} but not arg3"
 
 
 def sink_via_format(arg):
     _test_sink(f"{arg}")
+
+
+def sink_in_format(arg):
+    f"{_test_sink(arg)} is bad"
+
+
+def issue_in_format():
+    taint = _test_source()
+    return f"{_test_sink(taint)} is bad"
