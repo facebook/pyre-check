e7defe2b8986ec11c7bf18fcb470da2b8de64aad
Steven Troxler <stroxler@fb.com>
2022-01-11 06:29:29 -0800

    Fix generated copyright
    Summary:
    We updated all our file license headers, but missed this
    one in generated code.
    
    Reviewed By: arthaud
    
    Differential Revision: D33513724
    
    fbshipit-source-id: 891f2071329fcb5fa22bf8ff516183462773d9c7

--- scripts/pypi/build_pypi_package.py
+++ scripts/pypi/build_pypi_package.py
@@ -55,7 +55,7 @@ def _mkdir_and_init(module_path: Path, version: Optional[str] = None) -> None:
     else:
         init_path.write_text(
             f"""\
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
