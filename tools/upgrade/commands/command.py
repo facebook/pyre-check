63fd16ffe3a1fa1eee31c44e5ed4e0db255e68c3
Shannon Zhu <szhu@fb.com>
2022-02-08 23:59:27 -0800

    Refactor support for fixme threshold into general error suppression logic
    Summary:
    Found some bugs in our error suppression handling on the sandcastle side that duplicates the work generating errors.
    
    This is a nfc lead-up to the fixes, where we make the error suppression common method support a fixme threshold that is used in many upgrade commands. The logic is currently duplicated (and buggy) across a lot of these. Also add some testing.
    
    Reviewed By: pradeep90
    
    Differential Revision: D34063010
    
    fbshipit-source-id: 15970f3e9ae5a76c4ba7273bf576d721117643ce

--- tools/upgrade/commands/command.py
+++ tools/upgrade/commands/command.py
@@ -11,6 +11,7 @@ from typing import Optional
 
 from ..configuration import Configuration
 from ..errors import Errors, PartialErrorSuppression
+from ..filesystem import add_local_mode, LocalMode
 from ..repository import Repository
 
 
@@ -145,6 +146,8 @@ class ErrorSuppressingCommand(Command):
         error_source: ErrorSource = ErrorSource.GENERATE,
         upgrade_version: bool = False,
         only_fix_error_code: Optional[int] = None,
+        fixme_threshold: Optional[int] = None,
+        fixme_threshold_fallback_mode: LocalMode = LocalMode.IGNORE,
     ) -> None:
         LOG.info("Processing %s", configuration.get_directory())
         if not configuration.is_local:
@@ -162,13 +165,25 @@ class ErrorSuppressingCommand(Command):
                 only_fix_error_code, should_clean=self._should_clean
             )
         )
-        if len(errors) > 0:
-            self._apply_suppressions(errors)
+        if len(errors) == 0:
+            return
 
-            # Lint and re-run pyre once to resolve most formatting issues
-            if self._lint:
-                if self._repository.format():
-                    errors = configuration.get_errors(
-                        only_fix_error_code, should_clean=False
+        if fixme_threshold is None:
+            self._apply_suppressions(errors)
+        else:
+            for path, path_errors in errors.paths_to_errors.items():
+                path_errors = list(path_errors)
+                if len(path_errors) > fixme_threshold:
+                    LOG.info(
+                        "%d errors found in `%s`. Adding file-level ignore.",
+                        len(path_errors),
+                        path,
                     )
-                    self._apply_suppressions(errors)
+                    add_local_mode(path, fixme_threshold_fallback_mode)
+                else:
+                    self._apply_suppressions(Errors(path_errors))
+
+        # Lint and re-run pyre once to resolve most formatting issues
+        if self._lint and self._repository.format():
+            errors = configuration.get_errors(only_fix_error_code, should_clean=False)
+            self._apply_suppressions(errors)
