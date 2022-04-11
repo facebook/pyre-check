816a80e088cd326eb74b19d41746b30326077d42
Alex Kassil <afk@fb.com>
2022-04-11 13:14:19 -0700

    Add tests showing that we catch flows through implicit sources for % and str.format
    Summary:
    While learning about implicit sinks/sources, I wanted to check if we handled flows for implicit sources with % and str.format, since we do not handle flows for implicit sinks.
    
    Turns out we do, no harm in adding these two extra tests as integration tests to ensure we keep handling it going forward.
    
    Reviewed By: arthaud
    
    Differential Revision: D35486518
    
    fbshipit-source-id: e43f107a72a84b6dd93e0c38fe70233f32faaf5b

--- source/interprocedural_analyses/taint/test/integration/implicit_sources.py
+++ source/interprocedural_analyses/taint/test/integration/implicit_sources.py
@@ -10,9 +10,19 @@ def foo():
     return "123.456.789.123"
 
 
-def bar():
+def bar_format_strings():
     user_controlled = 1
     return f"{user_controlled}:123.456.789.123"
+
+
+def bar_percent_format():
+    user_controlled = 1
+    return "%s:123.456.789.123" % (user_controlled,)
+
+
+def bar_dot_format():
+    user_controlled = 1
+    return "{}:123.456.789.123".format(user_controlled)
 
 
 def does_not_match():
