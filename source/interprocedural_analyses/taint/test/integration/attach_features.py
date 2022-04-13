fa5a81733925121bf42ccf8aa236b3dd00f5a92a
Tianhan Lu <tianhanlu@fb.com>
2022-04-13 07:24:22 -0700

    Document the improper union of user-defined models
    Summary: As titled. More details can be found in T116691133.
    
    Reviewed By: arthaud
    
    Differential Revision: D35512489
    
    fbshipit-source-id: 04072d929d9ee510338d99c862cd32fcffd6df28

--- source/interprocedural_analyses/taint/test/integration/attach_features.py
+++ source/interprocedural_analyses/taint/test/integration/attach_features.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from builtins import _test_sink
+from builtins import _test_sink, _test_source
 
 
 def source():
--- source/interprocedural_analyses/taint/test/integration/features.py
+++ source/interprocedural_analyses/taint/test/integration/features.py
@@ -103,3 +103,8 @@ def issue_via_in():
     o = _test_source()
     _test_sink("a" in o)
     _test_sink(o in "a")
+
+
+def model_missing_feature(arg):
+    # TODO(T116691133): Miss "always-via" for feature string_concat_rhs
+    return 0
