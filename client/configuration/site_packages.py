cf6631510a6c5d50565473edeceb9498c7fd50ab
Jia Chen <grievejia@fb.com>
2022-04-18 15:52:42 -0700

    Support PEP561 (3/5) -- determine site package status
    Reviewed By: stroxler
    
    Differential Revision: D35693398
    
    fbshipit-source-id: 84dc3c551cd243c062e6168fc2b9d5041ccc90f3

--- client/configuration/site_packages.py
+++ client/configuration/site_packages.py
@@ -3,10 +3,17 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import dataclasses
 import enum
-from typing import List, Optional, Sequence
+import logging
+from pathlib import Path
+from typing import List, Optional, Sequence, Union
 
 from . import search_path
+
+LOG: logging.Logger = logging.getLogger(__name__)
+MARKER_FILE: str = "py.typed"
+STUBS_SUFFIX: str = "-stubs"
 
 
 class SearchStrategy(str, enum.Enum):
@@ -23,6 +30,76 @@ class SearchStrategy(str, enum.Enum):
             return next(item for item in SearchStrategy if item.value == input)
         except StopIteration:
             return None
+
+
+class PackageStatus(enum.IntEnum):
+    UNTYPED: int = 0
+    PARTIALLY_TYPED: int = 1
+    TYPED: int = 2
+
+
+@dataclasses.dataclass(frozen=True)
+class NonStubPackage:
+    name: str
+    path: Path
+    is_typed: bool = False
+
+
+@dataclasses.dataclass(frozen=True)
+class StubPackage:
+    name: str
+    path: Path
+    is_partial: bool = False
+
+
+def is_valid_package_name(name: str) -> bool:
+    if len(name) == 0:
+        return False
+    if name == "__pycache__":
+        return False
+    if "." in name:
+        return False
+    return True
+
+
+def get_package_status(path: Path) -> PackageStatus:
+    marker_path = path / MARKER_FILE
+    try:
+        # lint-ignore: NoUnsafeFilesystemRule
+        with open(marker_path, "r") as marker_file:
+            first_line = marker_file.readline().strip()
+        if first_line == "partial":
+            return PackageStatus.PARTIALLY_TYPED
+        else:
+            return PackageStatus.TYPED
+    except FileNotFoundError:
+        return PackageStatus.UNTYPED
+    except Exception as error:
+        LOG.debug(
+            f"Exception raised while getting package status for `{path}``: {error}"
+        )
+        return PackageStatus.UNTYPED
+
+
+def create_package_from_path(path: Path) -> Union[StubPackage, NonStubPackage, None]:
+    if not path.is_dir():
+        return None
+    name = path.name
+    if not is_valid_package_name(name):
+        return None
+    status = get_package_status(path)
+    if name.endswith(STUBS_SUFFIX):
+        return StubPackage(
+            name=name[: -len(STUBS_SUFFIX)],
+            path=path,
+            is_partial=(status == PackageStatus.PARTIALLY_TYPED),
+        )
+    else:
+        return NonStubPackage(
+            name=name,
+            path=path,
+            is_typed=(status != PackageStatus.UNTYPED),
+        )
 
 
 def search_for_paths(
--- client/configuration/tests/site_packages_test.py
+++ client/configuration/tests/site_packages_test.py
@@ -3,10 +3,27 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import builtins
+import tempfile
+from pathlib import Path
+
 import testslide
 
+from ...tests.setup import (
+    ensure_files_exist,
+)
 from ..search_path import SimpleElement
-from ..site_packages import SearchStrategy, search_for_paths
+from ..site_packages import (
+    SearchStrategy,
+    search_for_paths,
+    is_valid_package_name,
+    PackageStatus,
+    get_package_status,
+    MARKER_FILE,
+    create_package_from_path,
+    NonStubPackage,
+    StubPackage,
+)
 
 
 class SitePackagesTest(testslide.TestCase):
@@ -26,3 +43,82 @@ class SitePackagesTest(testslide.TestCase):
             search_for_paths(SearchStrategy.ALL, site_roots=["/foo", "/bar"]),
             [SimpleElement("/foo"), SimpleElement("/bar")],
         )
+
+    def test_valid_package_name(self) -> None:
+        self.assertTrue(is_valid_package_name("foo"))
+        self.assertTrue(is_valid_package_name("foo_bar"))
+        self.assertTrue(is_valid_package_name("_baz"))
+
+        self.assertFalse(is_valid_package_name(""))
+        self.assertFalse(is_valid_package_name("__pycache__"))
+        self.assertFalse(is_valid_package_name(".pyre"))
+        self.assertFalse(is_valid_package_name("foo-1.2.3.dist-info"))
+        self.assertFalse(is_valid_package_name("bar-1.2.3-py3.10.egg-info"))
+
+    def test_get_package_status_untyped(self) -> None:
+        with tempfile.TemporaryDirectory() as root:
+            root_path = Path(root).resolve()
+            ensure_files_exist(root_path, ["derp.py"])
+            self.assertEqual(get_package_status(root_path), PackageStatus.UNTYPED)
+
+    def test_get_package_status_typed(self) -> None:
+        with tempfile.TemporaryDirectory() as root:
+            root_path = Path(root).resolve()
+            ensure_files_exist(root_path, ["derp.py", MARKER_FILE])
+            self.assertEqual(get_package_status(root_path), PackageStatus.TYPED)
+
+    def test_get_package_status_partial(self) -> None:
+        with tempfile.TemporaryDirectory() as root:
+            root_path = Path(root).resolve()
+            ensure_files_exist(root_path, ["derp.py"])
+            (root_path / MARKER_FILE).write_text("partial\n")
+            self.assertEqual(
+                get_package_status(root_path), PackageStatus.PARTIALLY_TYPED
+            )
+
+    def test_get_package_status_permission_error(self) -> None:
+        # A mock is needed here since there is no cross-platform API to set
+        # file permissions in pure Python
+        self.mock_callable(builtins, "open").to_raise(PermissionError)
+        with tempfile.TemporaryDirectory() as root:
+            root_path = Path(root).resolve()
+            ensure_files_exist(root_path, ["derp.py", MARKER_FILE])
+            (root_path / MARKER_FILE).write_text("partial\n")
+            self.assertEqual(get_package_status(root_path), PackageStatus.UNTYPED)
+
+    def test_create_package_from_path(self) -> None:
+        with tempfile.TemporaryDirectory() as root:
+            root_path = Path(root).resolve()
+            ensure_files_exist(
+                root_path,
+                [
+                    "foo/foo.py",
+                    "bar/bar.py",
+                    "bar/py.typed",
+                    "baz-stubs/baz.pyi",
+                    "qux-stubs/qux.pyi",
+                ],
+            )
+
+            foo_path = root_path / "foo"
+            bar_path = root_path / "bar"
+            baz_path = root_path / "baz-stubs"
+            qux_path = root_path / "qux-stubs"
+            (qux_path / MARKER_FILE).write_text("partial\n")
+
+            self.assertEqual(
+                create_package_from_path(foo_path),
+                NonStubPackage(name="foo", path=foo_path, is_typed=False),
+            )
+            self.assertEqual(
+                create_package_from_path(bar_path),
+                NonStubPackage(name="bar", path=bar_path, is_typed=True),
+            )
+            self.assertEqual(
+                create_package_from_path(baz_path),
+                StubPackage(name="baz", path=baz_path, is_partial=False),
+            )
+            self.assertEqual(
+                create_package_from_path(qux_path),
+                StubPackage(name="qux", path=qux_path, is_partial=True),
+            )
