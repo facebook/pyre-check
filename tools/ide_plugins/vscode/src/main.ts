cb8e523addb3d97a0edffe19fb1c29479fa69de8
Shannon Zhu <szhu@fb.com>
2022-04-22 14:32:27 -0700

    Fix vscode plugin indentation
    Summary: Not sure about the crashes yet, but this formatting can be fixed.
    
    Reviewed By: pradeep90
    
    Differential Revision: D35826809
    
    fbshipit-source-id: 22ff44af18bd75ce4abf1d512b402431f59121d5

--- tools/ide_plugins/vscode/src/main.ts
+++ tools/ide_plugins/vscode/src/main.ts
@@ -33,33 +33,33 @@ namespace Configuration {
 
 export async function activate(_: vscode.ExtensionContext) {
 
-    let serverOptions = {
-	command: "pyre",
-	args: ["persistent"]
-    };
-    
-    let clientOptions: LanguageClientOptions = {
-	documentSelector: [{scheme: 'file', language: 'python'}],
-	synchronize: {
-	    // Notify the server about file changes to '.clientrc files contain in the workspace
-	    fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc'),
-	}
-    };
-    
-    const languageClient = new LanguageClient(
-        'pyre',
-	'Pyre Language Client',
-	serverOptions,
-	clientOptions,
-    )
+	let serverOptions = {
+		command: "pyre",
+		args: ["persistent"]
+	};
 
-    languageClient.registerProposedFeatures();
-    languageClient.onReady().then(() => {
+	let clientOptions: LanguageClientOptions = {
+		documentSelector: [{scheme: 'file', language: 'python'}],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contain in the workspace
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc'),
+		}
+	};
+
+	const languageClient = new LanguageClient(
+		'pyre',
+		'Pyre Language Client',
+		serverOptions,
+		clientOptions,
+	)
+
+	languageClient.registerProposedFeatures();
+	languageClient.onReady().then(() => {
 		Configuration.initialize();
-    });
-    languageClient.start();
+	});
+	languageClient.start();
 }
 
 export function deactivate() {
-    Configuration.dispose();
+	Configuration.dispose();
 }
