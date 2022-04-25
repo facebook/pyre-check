f1826c0a8ce07722ac24d0a098c9cb57de6b2129
Maxime Arthaud <arthaud@fb.com>
2022-04-25 10:52:29 -0700

    Split the model parsing into smaller functions and move it under the interprocedural interface [interprocedural 2/n]
    Summary:
    This diff is mostly refactoring, no functional change.
    This cleans up the whole model parsing and initialization into smaller functions.
    
    Non exhaustive list of changes:
    * Factor out a `merge_model_maps` function, which is actually used 3 times;
    * Factor out a `join_parse_result` function, used twice;
    * Turned `let f () = ... in let result = f ()` into `let result = ... in`, making the code easier to read;
    * Remove `model_query_data` and `parse_sources_result`, use `ModelParser.parse_result` instead.
    
    Differential Revision: D35863006
    
    fbshipit-source-id: b71669e4d10d515db7f2f10b89b1b54de8e6949f

--- source/interprocedural_analyses/taint/taintAnalysis.ml
+++ source/interprocedural_analyses/taint/taintAnalysis.ml
@@ -36,235 +36,219 @@ let initialize_configuration
     ()
 
 
+(* TODO(T117715045): This should go in a generic `registry` module. *)
+let merge_model_maps left right =
+  Target.Map.merge left right ~f:(fun ~key:_ -> function
+    | `Both (left, right) -> Some (Model.join left right)
+    | `Left model
+    | `Right model ->
+        Some model)
+
+
+let join_parse_result
+    {
+      ModelParser.models = models_left;
+      queries = queries_left;
+      skip_overrides = skip_overrides_left;
+      errors = errors_left;
+    }
+    {
+      ModelParser.models = models_right;
+      queries = queries_right;
+      skip_overrides = skip_overrides_right;
+      errors = errors_right;
+    }
+  =
+  {
+    ModelParser.models = merge_model_maps models_left models_right;
+    queries = List.rev_append queries_right queries_left;
+    errors = List.rev_append errors_right errors_left;
+    skip_overrides = Set.union skip_overrides_left skip_overrides_right;
+  }
+
+
+let parse_models_and_queries_from_sources
+    ~static_analysis_configuration:{ Configuration.StaticAnalysis.rule_filter; _ }
+    ~scheduler
+    ~resolution
+    ~callables
+    ~stubs
+    ~taint_configuration
+    sources
+  =
+  (* TODO(T117715045): Do not pass all callables and stubs explicitly to map_reduce,
+   * since this will marshal-ed between processes and hence is costly. *)
+  let map state sources =
+    List.fold sources ~init:state ~f:(fun state (path, source) ->
+        ModelParser.parse
+          ~resolution
+          ~path
+          ~source
+          ~configuration:taint_configuration
+          ~callables
+          ~stubs
+          ?rule_filter
+          ()
+        |> join_parse_result state)
+  in
+  Scheduler.map_reduce
+    scheduler
+    ~policy:(Scheduler.Policy.legacy_fixed_chunk_count ())
+    ~initial:
+      {
+        ModelParser.models = Target.Map.empty;
+        queries = [];
+        skip_overrides = Ast.Reference.Set.empty;
+        errors = [];
+      }
+    ~map
+    ~reduce:join_parse_result
+    ~inputs:sources
+    ()
+
+
+let parse_models_and_queries_from_configuration
+    ~scheduler
+    ~static_analysis_configuration:
+      ({
+         Configuration.StaticAnalysis.verify_models;
+         configuration = { taint_model_paths; _ };
+         rule_filter;
+         find_missing_flows;
+         dump_model_query_results;
+         maximum_trace_length;
+         maximum_tito_depth;
+         _;
+       } as static_analysis_configuration)
+    ~environment
+    ~callables
+    ~stubs
+  =
+  let resolution =
+    Analysis.TypeCheck.resolution
+      (Analysis.TypeEnvironment.ReadOnly.global_resolution environment)
+      (* TODO(T65923817): Eliminate the need of creating a dummy context here *)
+      (module Analysis.TypeCheck.DummyContext)
+  in
+  let find_missing_flows =
+    find_missing_flows >>= TaintConfiguration.missing_flows_kind_from_string
+  in
+  let taint_configuration =
+    TaintConfiguration.create
+      ~rule_filter
+      ~find_missing_flows
+      ~dump_model_query_results_path:dump_model_query_results
+      ~maximum_trace_length
+      ~maximum_tito_depth
+      ~taint_model_paths
+    |> TaintConfiguration.exception_on_error
+  in
+  let () = TaintConfiguration.register taint_configuration in
+  let { ModelParser.models = user_models; errors; skip_overrides; queries } =
+    ModelParser.get_model_sources ~paths:taint_model_paths
+    |> parse_models_and_queries_from_sources
+         ~static_analysis_configuration
+         ~scheduler
+         ~resolution
+         ~callables
+         ~stubs
+         ~taint_configuration
+  in
+  let () = ModelVerificationError.register errors in
+  let () =
+    if not (List.is_empty errors) then
+      (* Exit or log errors, depending on whether models need to be verified. *)
+      if not verify_models then begin
+        Log.error "Found %d model verification errors!" (List.length errors);
+        List.iter errors ~f:(fun error -> Log.error "%s" (ModelVerificationError.display error))
+      end
+      else
+        raise (ModelParser.ModelVerificationError errors)
+  in
+  let class_models = ClassModels.infer ~environment ~user_models in
+  {
+    ModelParser.models = merge_model_maps user_models class_models;
+    queries;
+    skip_overrides;
+    errors;
+  }
+
+
+let generate_models_from_queries
+    ~static_analysis_configuration:
+      { Configuration.StaticAnalysis.rule_filter; find_missing_flows; _ }
+    ~scheduler
+    ~environment
+    ~callables
+    ~stubs
+    ~taint_configuration
+    ~initial_models
+    queries
+  =
+  let resolution =
+    Analysis.TypeCheck.resolution
+      (Analysis.TypeEnvironment.ReadOnly.global_resolution environment)
+      (* TODO(T65923817): Eliminate the need of creating a dummy context here *)
+      (module Analysis.TypeCheck.DummyContext)
+  in
+  let models =
+    let callables =
+      Hash_set.fold stubs ~f:(Core.Fn.flip List.cons) ~init:callables
+      |> List.filter_map ~f:(function
+             | Target.Function _ as callable -> Some callable
+             | Target.Method _ as callable -> Some callable
+             | _ -> None)
+    in
+    TaintModelQuery.ModelQuery.apply_all_rules
+      ~resolution
+      ~scheduler
+      ~configuration:taint_configuration
+      ~rule_filter
+      ~rules:queries
+      ~callables
+      ~stubs
+      ~environment
+      ~models:initial_models
+  in
+  let remove_sinks models = Target.Map.map ~f:Model.remove_sinks models in
+  let add_obscure_sinks models =
+    let add_obscure_sink models callable =
+      let model =
+        Target.Map.find models callable
+        |> Option.value ~default:Model.empty_model
+        |> Model.add_obscure_sink ~resolution ~call_target:callable
+        |> Model.remove_obscureness
+      in
+      Target.Map.set models ~key:callable ~data:model
+    in
+    stubs
+    |> Hash_set.filter ~f:(fun callable ->
+           Target.Map.find models callable >>| Model.is_obscure |> Option.value ~default:true)
+    |> Hash_set.fold ~f:add_obscure_sink ~init:models
+  in
+  let find_missing_flows =
+    find_missing_flows >>= TaintConfiguration.missing_flows_kind_from_string
+  in
+  let models =
+    match find_missing_flows with
+    | Some Obscure -> models |> remove_sinks |> add_obscure_sinks
+    | Some Type -> models |> remove_sinks
+    | None -> models
+  in
+  models
+
+
 (* Registers the Taint analysis with the interprocedural analysis framework. *)
 include Taint.Result.Register (struct
   include Taint.Result
-
-  type model_query_data = {
-    queries: ModelParser.Internal.ModelQuery.rule list;
-    taint_configuration: TaintConfiguration.t;
-  }
-
-  type parse_sources_result = {
-    initialize_result: Model.t Interprocedural.AnalysisResult.initialize_result;
-    query_data: model_query_data option;
-  }
-
-  let generate_models_from_queries
-      ~scheduler
-      ~static_analysis_configuration:
-        { Configuration.StaticAnalysis.rule_filter; find_missing_flows; _ }
-      ~environment
-      ~callables
-      ~stubs
-      ~initialize_result:{ Interprocedural.AnalysisResult.initial_models = models; skip_overrides }
-      { queries; taint_configuration }
-    =
-    let resolution =
-      Analysis.TypeCheck.resolution
-        (Analysis.TypeEnvironment.ReadOnly.global_resolution environment)
-        (* TODO(T65923817): Eliminate the need of creating a dummy context here *)
-        (module Analysis.TypeCheck.DummyContext)
-    in
-    let models =
-      let callables =
-        Hash_set.fold stubs ~f:(Core.Fn.flip List.cons) ~init:callables
-        |> List.filter_map ~f:(function
-               | Target.Function _ as callable -> Some callable
-               | Target.Method _ as callable -> Some callable
-               | _ -> None)
-      in
-      TaintModelQuery.ModelQuery.apply_all_rules
-        ~resolution
-        ~scheduler
-        ~configuration:taint_configuration
-        ~rule_filter
-        ~rules:queries
-        ~callables
-        ~stubs
-        ~environment
-        ~models
-    in
-    let remove_sinks models = Target.Map.map ~f:Model.remove_sinks models in
-    let add_obscure_sinks models =
-      let add_obscure_sink models callable =
-        let model =
-          Target.Map.find models callable
-          |> Option.value ~default:Model.empty_model
-          |> Model.add_obscure_sink ~resolution ~call_target:callable
-          |> Model.remove_obscureness
-        in
-        Target.Map.set models ~key:callable ~data:model
-      in
-      stubs
-      |> Hash_set.filter ~f:(fun callable ->
-             Target.Map.find models callable >>| Model.is_obscure |> Option.value ~default:true)
-      |> Hash_set.fold ~f:add_obscure_sink ~init:models
-    in
-    let find_missing_flows =
-      find_missing_flows >>= TaintConfiguration.missing_flows_kind_from_string
-    in
-    let models =
-      match find_missing_flows with
-      | Some Obscure -> models |> remove_sinks |> add_obscure_sinks
-      | Some Type -> models |> remove_sinks
-      | None -> models
-    in
-    { Interprocedural.AnalysisResult.initial_models = models; skip_overrides }
-
-
-  let parse_models_and_queries_from_sources
-      ~scheduler
-      ~static_analysis_configuration:
-        {
-          Configuration.StaticAnalysis.verify_models;
-          configuration = { taint_model_paths; _ };
-          rule_filter;
-          find_missing_flows;
-          dump_model_query_results;
-          maximum_trace_length;
-          maximum_tito_depth;
-          _;
-        }
-      ~environment
-      ~callables
-      ~stubs
-    =
-    let resolution =
-      Analysis.TypeCheck.resolution
-        (Analysis.TypeEnvironment.ReadOnly.global_resolution environment)
-        (* TODO(T65923817): Eliminate the need of creating a dummy context here *)
-        (module Analysis.TypeCheck.DummyContext)
-    in
-    let create_models ~taint_configuration sources =
-      let map state sources =
-        List.fold
-          sources
-          ~init:state
-          ~f:(fun (accumulated_models, errors, skip_overrides, queries) (path, source) ->
-            let {
-              ModelParser.models;
-              errors = new_errors;
-              skip_overrides = new_skip_overrides;
-              queries = new_queries;
-            }
-              =
-              ModelParser.parse
-                ~resolution
-                ~path
-                ~source
-                ~configuration:taint_configuration
-                ~callables
-                ~stubs
-                ?rule_filter
-                ()
-            in
-            let merged_models =
-              Target.Map.merge accumulated_models models ~f:(fun ~key:_ -> function
-                | `Both (left, right) -> Some (Model.join left right)
-                | `Left model
-                | `Right model ->
-                    Some model)
-            in
-            ( merged_models,
-              List.rev_append new_errors errors,
-              Set.union skip_overrides new_skip_overrides,
-              List.rev_append new_queries queries ))
-      in
-      let reduce
-          (models_left, errors_left, skip_overrides_left, queries_left)
-          (models_right, errors_right, skip_overrides_right, queries_right)
-        =
-        let merge_models ~key:_ = function
-          | `Left model
-          | `Right model ->
-              Some model
-          | `Both (left, right) -> Some (Result.join ~iteration:0 left right)
-        in
-        ( Target.Map.merge models_left models_right ~f:merge_models,
-          List.rev_append errors_left errors_right,
-          Set.union skip_overrides_left skip_overrides_right,
-          List.rev_append queries_left queries_right )
-      in
-      Scheduler.map_reduce
-        scheduler
-        ~policy:(Scheduler.Policy.legacy_fixed_chunk_count ())
-        ~initial:(Target.Map.empty, [], Ast.Reference.Set.empty, [])
-        ~map
-        ~reduce
-        ~inputs:sources
-        ()
-    in
-    let add_models_and_queries_from_sources () =
-      let find_missing_flows =
-        find_missing_flows >>= TaintConfiguration.missing_flows_kind_from_string
-      in
-      let taint_configuration =
-        TaintConfiguration.create
-          ~rule_filter
-          ~find_missing_flows
-          ~dump_model_query_results_path:dump_model_query_results
-          ~maximum_trace_length
-          ~maximum_tito_depth
-          ~taint_model_paths
-        |> TaintConfiguration.exception_on_error
-      in
-      TaintConfiguration.register taint_configuration;
-      let models, errors, skip_overrides, queries =
-        ModelParser.get_model_sources ~paths:taint_model_paths |> create_models ~taint_configuration
-      in
-      ModelVerificationError.register errors;
-      let () =
-        if not (List.is_empty errors) then
-          (* Exit or log errors, depending on whether models need to be verified. *)
-          if not verify_models then begin
-            Log.error "Found %d model verification errors!" (List.length errors);
-            List.iter errors ~f:(fun error -> Log.error "%s" (ModelVerificationError.display error))
-          end
-          else
-            raise (ModelParser.ModelVerificationError errors)
-      in
-      {
-        initialize_result =
-          { Interprocedural.AnalysisResult.initial_models = models; skip_overrides };
-        query_data = Some { queries; taint_configuration };
-      }
-    in
-    let ({ initialize_result = { initial_models = user_models; _ }; _ } as result) =
-      add_models_and_queries_from_sources ()
-    in
-    let initial_models = ClassModels.infer ~environment ~user_models in
-    match taint_model_paths with
-    | [] ->
-        {
-          initialize_result =
-            {
-              Interprocedural.AnalysisResult.initial_models;
-              skip_overrides = Ast.Reference.Set.empty;
-            };
-          query_data = None;
-        }
-    | _ ->
-        let merged_models =
-          Target.Map.merge user_models initial_models ~f:(fun ~key:_ -> function
-            | `Both (left, right) -> Some (Model.join left right)
-            | `Left model
-            | `Right model ->
-                Some model)
-        in
-        {
-          result with
-          initialize_result = { result.initialize_result with initial_models = merged_models };
-        }
-
 
   let initialize_models ~scheduler ~static_analysis_configuration ~environment ~callables ~stubs =
     let stubs = Target.HashSet.of_list stubs in
 
     Log.info "Parsing taint models...";
     let timer = Timer.start () in
-    let { initialize_result; query_data } =
-      parse_models_and_queries_from_sources
+    let { ModelParser.models; queries; skip_overrides; _ } =
+      parse_models_and_queries_from_configuration
         ~scheduler
         ~static_analysis_configuration
         ~environment
@@ -272,27 +256,32 @@ include Taint.Result.Register (struct
         ~stubs
     in
     Statistics.performance ~name:"Parsed taint models" ~phase_name:"Parsing taint models" ~timer ();
-    match query_data with
-    | Some query_data ->
-        Log.info "Generating models from model queries...";
-        let timer = Timer.start () in
-        let models =
-          generate_models_from_queries
-            ~scheduler
-            ~static_analysis_configuration
-            ~environment
-            ~callables
-            ~stubs
-            ~initialize_result
-            query_data
-        in
-        Statistics.performance
-          ~name:"Generated models from model queries"
-          ~phase_name:"Generating models from model queries"
-          ~timer
-          ();
-        models
-    | _ -> initialize_result
+
+    let models =
+      match queries with
+      | [] -> models
+      | _ ->
+          Log.info "Generating models from model queries...";
+          let timer = Timer.start () in
+          let models =
+            generate_models_from_queries
+              ~static_analysis_configuration
+              ~scheduler
+              ~environment
+              ~callables
+              ~stubs
+              ~initial_models:models
+              ~taint_configuration:(TaintConfiguration.get ())
+              queries
+          in
+          Statistics.performance
+            ~name:"Generated models from model queries"
+            ~phase_name:"Generating models from model queries"
+            ~timer
+            ();
+          models
+    in
+    { Interprocedural.AnalysisResult.initial_models = models; skip_overrides }
 
 
   let analyze ~environment ~callable ~qualifier ~define ~sanitizers ~modes existing_model =
