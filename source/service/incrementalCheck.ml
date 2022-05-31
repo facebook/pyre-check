079441e0a323c4d8b8f7a167971b7270751356b3
Steven Troxler <stroxler@fb.com>
2022-05-31 15:48:41 -0700

    Remove `Shallow` mode hook from `IncrementalCheck`
    Summary:
    It is now impossible to run a shallow-style update; the cold-start
    API is separate and is the only api that shallow mode supports.
    
    As a result, we can simplify `incrementalCheck` quite a bit by removing
    the Shallow mode hook.
    
    Reviewed By: grievejia
    
    Differential Revision: D36673247
    
    fbshipit-source-id: 6d065cc12202a88590347766cb4616e3125f1120

--- source/service/incrementalCheck.ml
+++ source/service/incrementalCheck.ml
@@ -8,18 +8,11 @@
 open Core
 open Ast
 open Analysis
-open Configuration.Analysis
 open Pyre
 
 type errors = Analysis.AnalysisError.t list [@@deriving show]
 
-let recheck
-    ~configuration:({ incremental_style; _ } as configuration)
-    ~scheduler
-    ~environment
-    ~errors
-    paths
-  =
+let recheck ~configuration ~scheduler ~environment ~errors paths =
   let timer = Timer.start () in
   let ast_environment = TypeEnvironment.ast_environment environment in
   let annotated_global_environment = TypeEnvironment.global_environment environment in
@@ -41,124 +34,78 @@ let recheck
     AnnotatedGlobalEnvironment.UpdateResult.invalidated_modules
       annotated_global_environment_update_result
   in
-  let recheck_modules, new_errors, total_rechecked_functions =
-    match incremental_style with
-    | FineGrained ->
-        let unannotated_global_environment_update_result =
-          AnnotatedGlobalEnvironment.UpdateResult.unannotated_global_environment_update_result
-            annotated_global_environment_update_result
-        in
-        let function_triggers =
-          let filter_union sofar keyset =
-            let filter registered sofar =
-              match SharedMemoryKeys.DependencyKey.get_key registered with
-              | SharedMemoryKeys.TypeCheckDefine name -> (
-                  match Reference.Map.add sofar ~key:name ~data:registered with
-                  | `Duplicate -> sofar
-                  | `Ok updated -> updated)
-              | _ -> sofar
-            in
-            SharedMemoryKeys.DependencyKey.RegisteredSet.fold filter keyset sofar
-          in
-          AnnotatedGlobalEnvironment.UpdateResult.all_triggered_dependencies
-            annotated_global_environment_update_result
-          |> List.fold ~init:Reference.Map.empty ~f:filter_union
-        in
-        let recheck_functions =
-          let register_and_add sofar trigger =
-            let register = function
-              | Some existing -> existing
-              | None ->
-                  SharedMemoryKeys.DependencyKey.Registry.register
-                    (SharedMemoryKeys.TypeCheckDefine trigger)
-            in
-            Reference.Map.update sofar trigger ~f:register
-          in
-          UnannotatedGlobalEnvironment.UpdateResult.define_additions
-            unannotated_global_environment_update_result
-          |> Set.fold ~init:function_triggers ~f:register_and_add
-        in
-        let recheck_functions_list = Map.to_alist recheck_functions in
-        let recheck_function_names = List.map recheck_functions_list ~f:fst in
-
-        (* Rerun type checking for triggered functions. *)
-        TypeEnvironment.invalidate environment recheck_function_names;
-        recheck_functions_list
-        |> List.map ~f:(fun (define, registered) -> define, Some registered)
-        |> TypeCheck.run_on_defines ~scheduler ~configuration ~environment;
-
-        (* Rerun postprocessing for triggered modules. *)
-        let recheck_modules =
-          (* For each rechecked function, its containing module needs to be included in
-             postprocessing *)
-          List.fold
-            ~init:(Reference.Set.of_list invalidated_modules)
-            (Reference.Map.keys function_triggers)
-            ~f:(fun sofar define_name ->
-              let unannotated_global_environment =
-                UnannotatedGlobalEnvironment.UpdateResult.read_only
-                  unannotated_global_environment_update_result
-              in
-              match
-                UnannotatedGlobalEnvironment.ReadOnly.get_function_definition
-                  unannotated_global_environment
-                  define_name
-              with
-              | None -> sofar
-              | Some { FunctionDefinition.qualifier; _ } -> Set.add sofar qualifier)
-          |> Set.to_list
-        in
-
-        let errors =
-          Analysis.Postprocessing.run
-            ~scheduler
-            ~configuration
-            ~environment:(Analysis.TypeEnvironment.read_only environment)
-            recheck_modules
-        in
-
-        recheck_modules, errors, Map.length recheck_functions
-    | Shallow ->
-        let total_rechecked_functions =
-          let unannotated_global_environment_update_result =
-            AnnotatedGlobalEnvironment.UpdateResult.unannotated_global_environment_update_result
-              annotated_global_environment_update_result
-          in
-          let previous_defines =
-            UnannotatedGlobalEnvironment.UpdateResult.previous_defines
-              unannotated_global_environment_update_result
-            |> Set.to_list
-          in
-          let current_defines =
-            let unannotated_global_environment =
-              UnannotatedGlobalEnvironment.UpdateResult.read_only
-                unannotated_global_environment_update_result
-            in
-            List.concat_map
-              invalidated_modules
-              ~f:
-                (UnannotatedGlobalEnvironment.ReadOnly.all_defines_in_module
-                   unannotated_global_environment)
-          in
-          TypeEnvironment.invalidate environment previous_defines;
-          TypeEnvironment.invalidate environment current_defines;
-
-          List.length current_defines
-        in
-        let errors =
-          Analysis.TypeCheck.legacy_run_on_modules
-            ~scheduler
-            ~configuration
-            ~environment
-            invalidated_modules;
-          Analysis.Postprocessing.run
-            ~scheduler
-            ~configuration
-            ~environment:(Analysis.TypeEnvironment.read_only environment)
-            invalidated_modules
-        in
-        invalidated_modules, errors, total_rechecked_functions
+  let unannotated_global_environment_update_result =
+    AnnotatedGlobalEnvironment.UpdateResult.unannotated_global_environment_update_result
+      annotated_global_environment_update_result
   in
+  let function_triggers =
+    let filter_union sofar keyset =
+      let filter registered sofar =
+        match SharedMemoryKeys.DependencyKey.get_key registered with
+        | SharedMemoryKeys.TypeCheckDefine name -> (
+            match Reference.Map.add sofar ~key:name ~data:registered with
+            | `Duplicate -> sofar
+            | `Ok updated -> updated)
+        | _ -> sofar
+      in
+      SharedMemoryKeys.DependencyKey.RegisteredSet.fold filter keyset sofar
+    in
+    AnnotatedGlobalEnvironment.UpdateResult.all_triggered_dependencies
+      annotated_global_environment_update_result
+    |> List.fold ~init:Reference.Map.empty ~f:filter_union
+  in
+  let recheck_functions =
+    let register_and_add sofar trigger =
+      let register = function
+        | Some existing -> existing
+        | None ->
+            SharedMemoryKeys.DependencyKey.Registry.register
+              (SharedMemoryKeys.TypeCheckDefine trigger)
+      in
+      Reference.Map.update sofar trigger ~f:register
+    in
+    UnannotatedGlobalEnvironment.UpdateResult.define_additions
+      unannotated_global_environment_update_result
+    |> Set.fold ~init:function_triggers ~f:register_and_add
+  in
+  let recheck_functions_list = Map.to_alist recheck_functions in
+  let recheck_function_names = List.map recheck_functions_list ~f:fst in
+
+  (* Rerun type checking for triggered functions. *)
+  TypeEnvironment.invalidate environment recheck_function_names;
+  recheck_functions_list
+  |> List.map ~f:(fun (define, registered) -> define, Some registered)
+  |> TypeCheck.run_on_defines ~scheduler ~configuration ~environment;
+
+  (* Rerun postprocessing for triggered modules. *)
+  let recheck_modules =
+    (* For each rechecked function, its containing module needs to be included in postprocessing *)
+    List.fold
+      ~init:(Reference.Set.of_list invalidated_modules)
+      (Reference.Map.keys function_triggers)
+      ~f:(fun sofar define_name ->
+        let unannotated_global_environment =
+          UnannotatedGlobalEnvironment.UpdateResult.read_only
+            unannotated_global_environment_update_result
+        in
+        match
+          UnannotatedGlobalEnvironment.ReadOnly.get_function_definition
+            unannotated_global_environment
+            define_name
+        with
+        | None -> sofar
+        | Some { FunctionDefinition.qualifier; _ } -> Set.add sofar qualifier)
+    |> Set.to_list
+  in
+
+  let new_errors =
+    Analysis.Postprocessing.run
+      ~scheduler
+      ~configuration
+      ~environment:(Analysis.TypeEnvironment.read_only environment)
+      recheck_modules
+  in
+  let rechecked_functions_count = Map.length recheck_functions in
   Statistics.event
     ~section:`Memory
     ~name:"shared memory size"
@@ -183,7 +130,7 @@ let recheck
         "number of module tracker updates", List.length module_updates;
         "number of parser updates", List.length invalidated_modules;
         "number of rechecked modules", List.length recheck_modules;
-        "number of re-checked functions", total_rechecked_functions;
+        "number of re-checked functions", rechecked_functions_count;
       ]
     ();
   recheck_modules, new_errors
