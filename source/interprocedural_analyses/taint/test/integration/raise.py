7d43fc479e468bdb0a38e030d739e861962320f7
Alex Kassil <afk@fb.com>
2022-06-11 00:56:05 -0700

    Add test showcasing unreachable state
    Summary: Unreachable state should not be reached
    
    Reviewed By: arthaud
    
    Differential Revision: D37058917
    
    fbshipit-source-id: 1c5d372d93d54108480ebe4dce8b45cf7d021947

--- source/interprocedural_analyses/taint/test/integration/raise.py
+++ source/interprocedural_analyses/taint/test/integration/raise.py
@@ -12,3 +12,29 @@ def forward():
 
 def backward(x):
     raise _test_sink(x)
+
+
+def unreachable():
+    x = _test_source()
+    raise Exception()
+    _test_sink(x)
+
+
+def unreachable_through_function_call_sink():
+    x = _test_source()
+    no_sink(x)
+
+
+def no_sink(x):
+    raise Exception()
+    _test_sink(x)
+
+
+def no_source():
+    raise Exception()
+    return _test_source()
+
+
+def unreachable_through_function_call_source():
+    x = no_source()
+    _test_sink(x)
