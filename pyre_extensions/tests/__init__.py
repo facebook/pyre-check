8d753272cbd55558a58dbb9f4afec0af1935a78f
Steven Troxler <stroxler@fb.com>
2022-06-15 14:41:11 -0700

    Test pyre_extensions in github CI (#629)
    Summary:
    Pull Request resolved: https://github.com/facebook/pyre-check/pull/629
    
    We accidentally broke 3.7 support by importing `typing.TypedDict`.
    
    The only way to catch this reliably is to test the unreleased code
    on github CI, which we currently don't do because only `client/`
    tests are run.
    
    As part of doing this, I also rename `simple_tests.py`, which is violating
    our test module naming convention, to `basic_test.py`.
    
    Reviewed By: grievejia
    
    Differential Revision: D37177511
    
    fbshipit-source-id: 68111d5c7f22a5b05c5c89eb72d7fa755156b850

--- pyre_extensions/tests/__init__.py
+++ pyre_extensions/tests/__init__.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from . import safe_json_test, simple_tests
+from . import basic_test, safe_json_test
 
 
-__all__ = ["safe_json_test", "simple_tests"]
+__all__ = ["safe_json_test", "basic_test"]
