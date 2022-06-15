805975717d2332a6046df41bddfafa507ccfab6d
Steven Troxler <stroxler@fb.com>
2022-06-15 08:21:38 -0700

    Print the actual errors if pysa test fails (#627)
    Summary: Pull Request resolved: https://github.com/facebook/pyre-check/pull/627
    
    Reviewed By: arthaud
    
    Differential Revision: D37142732
    
    fbshipit-source-id: 8f5ea3ecfebcb8a6829ecbd5e457ecbcda7df803

--- documentation/deliberately_vulnerable_flask_app/run_integration_tests.sh
+++ documentation/deliberately_vulnerable_flask_app/run_integration_tests.sh
@@ -4,6 +4,17 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+set +e
 python3 ../../tools/pysa_integration_tests/run.py \
     --skip-model-verification \
     --run-from-source
+
+exit_code=$?
+
+if [[ "$exit_code" != "0" ]]; then
+    echo "--- raw_results.json --"
+    cat raw_results.json
+    echo "---"
+fi
+
+exit $exit_code
