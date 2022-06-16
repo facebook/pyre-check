a54b3a435cb4e91838bd65d35c65a73f080f1c5f
Alex Kassil <afk@fb.com>
2022-06-16 16:47:27 -0700

    Refactor pattern match + if statement to pattern guard
    Summary: title
    
    Reviewed By: grievejia
    
    Differential Revision: D37221626
    
    fbshipit-source-id: cb6209873cdca4a3e5f576bb8029be6d412a56f3

--- source/external/specialCallResolution.ml
+++ source/external/specialCallResolution.ml
@@ -13,14 +13,12 @@ open Expression
 let recognized_callable_target_types = Type.Set.of_list [Type.Primitive "TestCallableTarget"]
 
 let redirect ~resolution { Call.callee; arguments } =
+  let is_async_task base =
+    Resolution.resolve_expression_to_type resolution base
+    |> fun annotation -> Set.exists recognized_callable_target_types ~f:(Type.equal annotation)
+  in
   match Node.value callee with
-  | Name (Name.Attribute { base; attribute = "async_delay" | "async_schedule"; _ }) ->
-      let is_async_task =
-        Resolution.resolve_expression_to_type resolution base
-        |> fun annotation -> Set.exists recognized_callable_target_types ~f:(Type.equal annotation)
-      in
-      if is_async_task then
-        Some { Call.callee = base; arguments }
-      else
-        None
+  | Name (Name.Attribute { base; attribute = "async_delay" | "async_schedule"; _ })
+    when is_async_task base ->
+      Some { Call.callee = base; arguments }
   | _ -> None
