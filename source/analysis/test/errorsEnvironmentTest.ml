e98b8b24b0f8b1f9bc5bec77a7258665ff51d596
Steven Troxler <stroxler@fb.com>
2022-06-24 17:09:05 -0700

    Add an update test for ErrorsEnvironment
    Summary:
    While we're adding some minimal tests of ErrorsEnvironment, let's
    also include a pair of very simple update tests:
    - one that makes sure we propagate cross-file dependencies
      correctly by changing an ancestor of a class
    - one that specifically checks our handling of comments in
      raw sources by flipping a file's mode
    
    Reviewed By: grievejia
    
    Differential Revision: D37430017
    
    fbshipit-source-id: c1e27936c2fb7b1d2cb9d879c4f38a8c9c12a265

--- source/analysis/test/errorsEnvironmentTest.ml
+++ source/analysis/test/errorsEnvironmentTest.ml
@@ -22,6 +22,17 @@ let instantiate_and_stringify ~lookup errors =
   |> List.map ~f:to_string
 
 
+let assert_errors ~context ~project expected =
+  let actual =
+    ScratchProject.errors_environment project
+    |> ErrorsEnvironment.ReadOnly.get_all_errors
+    |> instantiate_and_stringify
+         ~lookup:
+           (ScratchProject.ast_environment project |> AstEnvironment.ReadOnly.get_real_path_relative)
+  in
+  assert_equal ~ctxt:context ~printer:[%show: string list] expected actual
+
+
 let test_postprocessing context =
   let code header_comment =
     Format.asprintf
@@ -57,17 +68,10 @@ let test_postprocessing context =
   in
   ScratchProject.ReadWrite.errors_environment project
   |> ErrorsEnvironment.populate_all_errors ~scheduler:(Test.mock_scheduler ());
-  let errors =
-    ScratchProject.errors_environment project
-    |> ErrorsEnvironment.ReadOnly.get_all_errors
-    |> instantiate_and_stringify
-         ~lookup:
-           (ScratchProject.ast_environment project |> AstEnvironment.ReadOnly.get_real_path_relative)
-  in
   (* Note that the output is always sorted, we can depend on this in tests *)
-  assert_equal
-    ~ctxt:context
-    ~printer:[%show: string list]
+  assert_errors
+    ~context
+    ~project
     [
       "has_parse_error.py 2: Parsing failure [404]: '(' was never closed";
       "strict.py 4: Missing return annotation [3]: Return type must be annotated.";
@@ -85,8 +89,123 @@ let test_postprocessing context =
       "unsafe.py 12: Incompatible return type [7]: Expected `str` but got `int`.";
       "unsafe.py 14: Unused ignore [0]: The `pyre-ignore[7]` or `pyre-fixme[7]` comment is not \
        suppressing type errors, please remove it.";
+    ];
+  ()
+
+
+let test_update_ancestor context =
+  let project =
+    ScratchProject.setup
+      ~context
+      ~in_memory:false
+      [
+        "a.py", {|
+          class A:
+            x: float = 5
+        |};
+        ( "b.py",
+          {|
+          from a import A
+
+          class B(A):
+            y: str = "y"
+        |} );
+        ( "c.py",
+          {|
+          from b import B
+
+          def deconstruct(b: B) -> tuple[int, str]:
+            return (b.x, b.y)
+        |}
+        );
+      ]
+  in
+  ScratchProject.ReadWrite.errors_environment project
+  |> ErrorsEnvironment.populate_all_errors ~scheduler:(Test.mock_scheduler ());
+  (* validate initial state *)
+  assert_errors
+    ~context
+    ~project
+    [
+      "c.py 5: Incompatible return type [7]: Expected `Tuple[int, str]` but got `Tuple[float, str]`.";
+    ];
+  (* update and validate new errors *)
+  let update_code relative new_code =
+    ScratchProject.delete_file project ~relative;
+    ScratchProject.add_file project ~relative new_code;
+    ()
+  in
+  update_code "a.py" {|
+    class A:
+      x: int = 5
+  |};
+  let { Configuration.Analysis.local_root; _ } = ScratchProject.configuration_of project in
+  ScratchProject.update_environment
+    project
+    [Test.relative_artifact_path ~root:local_root ~relative:"a.py"]
+  |> ignore;
+  assert_errors ~context ~project [];
+  ()
+
+
+let test_update_mode context =
+  let project =
+    ScratchProject.setup
+      ~context
+      ~in_memory:false
+      [
+        ( "changes_from_unsafe_to_strict.py",
+          {|
+          # pyre-unsafe
+
+          def foo(x):
+            return x
+        |} );
+      ]
+  in
+  ScratchProject.ReadWrite.errors_environment project
+  |> ErrorsEnvironment.populate_all_errors ~scheduler:(Test.mock_scheduler ());
+  (* validate initial state *)
+  assert_errors ~context ~project [];
+  (* update and validate new errors *)
+  let update_code relative new_code =
+    ScratchProject.delete_file project ~relative;
+    ScratchProject.add_file project ~relative new_code;
+    ()
+  in
+  update_code
+    "changes_from_unsafe_to_strict.py"
+    {|
+    # pyre-strict
+
+    def foo(x):
+      return x
+  |};
+  let { Configuration.Analysis.local_root; _ } = ScratchProject.configuration_of project in
+  ScratchProject.update_environment
+    project
+    [
+      Test.relative_artifact_path ~root:local_root ~relative:"a.py";
+      Test.relative_artifact_path ~root:local_root ~relative:"changes_from_unsafe_to_strict.py";
     ]
-    errors
+  |> ignore;
+  assert_errors
+    ~context
+    ~project
+    [
+      "changes_from_unsafe_to_strict.py 4: Missing return annotation [3]: Return type must be \
+       annotated.";
+      "changes_from_unsafe_to_strict.py 4: Missing parameter annotation [2]: Parameter must be \
+       annotated.";
+    ];
+  ()
 
 
-let () = "environment" >::: ["postprocessing" >:: test_postprocessing] |> Test.run
+let () =
+  "environment"
+  >::: [
+         "postprocessing" >:: test_postprocessing;
+         "update_ancestor" >:: test_update_ancestor;
+         "update_mode" >:: test_update_mode;
+       ]
+  |> Test.run
