4c94bc36ac1928d83dbcb004c5f61bf9102d8321
Jia Chen <grievejia@fb.com>
2022-08-03 20:59:38 -0700

    Avoid using `Lwt.all` when populating linktree
    Reviewed By: stroxler
    
    Differential Revision: D38290628
    
    fbshipit-source-id: e4ac632078cb3bb1d643db83d39d05a5f0cc1a29

--- source/buck/artifacts.ml
+++ source/buck/artifacts.ml
@@ -7,17 +7,24 @@
 
 open Base
 
+exception ParentDirectoryError of string
+
 let ensure_parent_directory_exists path =
   (* The directory creation part is intentionally using a sequential API, since if we call
      `Lwt_unix.mkdir` instead it would lead to race conditions among the prefix `mkdir` invocations. *)
-  PyrePath.ensure_parent_directory_exists path |> Lwt.return
+  match PyrePath.ensure_parent_directory_exists path with
+  | Result.Ok () -> Lwt.return_unit
+  | Result.Error message -> Lwt.fail (ParentDirectoryError message)
 
 
-let catch_unix_error f =
+let catch_errors f =
   let open Lwt.Infix in
   Lwt.catch
     (fun () -> f () >>= fun () -> Lwt.return (Result.Ok ()))
     (function
+      | ParentDirectoryError message ->
+          let message = Format.sprintf "Error when ensuring parent directory exists: %s" message in
+          Lwt.return (Result.Error message)
       | Unix.Unix_error (error, function_name, _) ->
           let message =
             Format.sprintf "Error when invoking `%s`: %s" function_name (Unix.error_message error)
@@ -29,24 +36,20 @@ let catch_unix_error f =
 
 
 let create_symlink ~target link =
-  catch_unix_error (fun () -> Lwt_unix.symlink (PyrePath.absolute target) (PyrePath.absolute link))
+  Lwt_unix.symlink (PyrePath.absolute target) (PyrePath.absolute link)
 
 
 let remove_if_exists path =
-  catch_unix_error (fun () ->
-      Lwt.catch
-        (fun () -> Lwt_unix.unlink (PyrePath.absolute path))
-        (function
-          | Unix.Unix_error (Unix.ENOENT, _, _) -> Lwt.return_unit
-          | _ as exn -> Lwt.fail exn))
+  Lwt.catch
+    (fun () -> Lwt_unix.unlink (PyrePath.absolute path))
+    (function
+      | Unix.Unix_error (Unix.ENOENT, _, _) -> Lwt.return_unit
+      | _ as exn -> Lwt.fail exn)
 
 
 let create_parent_directory_and_symlink ~target link =
   let open Lwt.Infix in
-  ensure_parent_directory_exists link
-  >>= function
-  | Result.Error _ as error -> Lwt.return error
-  | Result.Ok () -> create_symlink ~target link
+  ensure_parent_directory_exists link >>= fun () -> create_symlink ~target link
 
 
 let populate ~source_root ~artifact_root build_map =
@@ -64,11 +67,7 @@ let populate ~source_root ~artifact_root build_map =
       let artifact_path = PyrePath.create_relative ~root:artifact_root ~relative:artifact in
       create_parent_directory_and_symlink ~target:source_path artifact_path
     in
-    let open Lwt.Infix in
-    BuildMap.to_alist build_map
-    |> List.map ~f:create_symlink
-    |> Lwt.all
-    >>= fun results -> Lwt.return (Result.all_unit results)
+    catch_errors (fun () -> BuildMap.to_alist build_map |> List.map ~f:create_symlink |> Lwt.join)
 
 
 let update ~source_root ~artifact_root difference =
@@ -81,21 +80,17 @@ let update ~source_root ~artifact_root difference =
     in
     Lwt.return (Result.Error message)
   else
-    let open Lwt.Infix in
     let process_update (artifact, kind) =
+      let open Lwt.Infix in
       let artifact_path = PyrePath.create_relative ~root:artifact_root ~relative:artifact in
       match kind with
       | BuildMap.Difference.Kind.Deleted -> remove_if_exists artifact_path
       | New source
-      | Changed source -> (
+      | Changed source ->
           remove_if_exists artifact_path
-          >>= function
-          | Result.Error _ as error -> Lwt.return error
-          | Result.Ok () ->
-              let source_path = PyrePath.create_relative ~root:source_root ~relative:source in
-              create_parent_directory_and_symlink ~target:source_path artifact_path)
+          >>= fun () ->
+          let source_path = PyrePath.create_relative ~root:source_root ~relative:source in
+          create_parent_directory_and_symlink ~target:source_path artifact_path
     in
-    BuildMap.Difference.to_alist difference
-    |> List.map ~f:process_update
-    |> Lwt.all
-    >>= fun results -> Lwt.return (Result.all_unit results)
+    catch_errors (fun () ->
+        BuildMap.Difference.to_alist difference |> List.map ~f:process_update |> Lwt.join)
