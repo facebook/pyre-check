591667f22e6b3b339a3a9be9f1c6eed4ea830e5b
Steven Troxler <stroxler@fb.com>
2022-08-25 14:26:33 -0700

    Make a smart constructor for StartArguments
    Summary:
    We can cut down on quite a bit of boilerplate, and keep the argument
    construction logic in top-level click entrypoints (which I think is
    mostly a good idea - the whole point of these dataclasses is to zip
    up various click-informed arg info) by using a smart constructor
    so that when we make a `StartArguments`, the fields coming straight
    from `CommandArguments` are automatically propagated.
    
    The resulting code is less repetitive and also makes it easier to
    see which arguments are *specific* to starting a server.
    
    This commit makes the smart constructor and uses it in the entrypoint
    for `persistent`, to show how it works.
    
    I'll flip all the other entrypoints in a separate diff; I want this
    one to stay small because that makes it easier to see the change and
    the effect.
    
    Reviewed By: grievejia
    
    Differential Revision: D39002774
    
    fbshipit-source-id: 676f60168f37cbfa397506957458d4ec8abd50ba

--- client/command_arguments.py
+++ client/command_arguments.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from __future__ import annotations
+
 import enum
 from dataclasses import dataclass, field
 from pathlib import Path
@@ -99,6 +101,38 @@ class StartArguments:
     wait_on_initialization: bool = False
     skip_initial_type_check: bool = False
     use_lazy_module_tracking: bool = False
+
+    @staticmethod
+    def create(
+        command_argument: CommandArguments,
+        no_watchman: bool = False,
+        store_type_check_resolution: bool = False,
+        wait_on_initialization: bool = False,
+        terminal: bool = False,
+        skip_initial_type_check: bool = False,
+        use_lazy_module_tracking: bool = False,
+    ) -> StartArguments:
+        return StartArguments(
+            changed_files_path=command_argument.changed_files_path,
+            debug=command_argument.debug,
+            enable_memory_profiling=command_argument.enable_memory_profiling,
+            enable_profiling=command_argument.enable_profiling,
+            load_initial_state_from=command_argument.load_initial_state_from,
+            log_identifier=command_argument.log_identifier,
+            logging_sections=command_argument.logging_sections,
+            no_saved_state=command_argument.no_saved_state,
+            no_watchman=no_watchman,
+            noninteractive=command_argument.noninteractive,
+            save_initial_state_to=command_argument.save_initial_state_to,
+            saved_state_project=command_argument.saved_state_project,
+            sequential=command_argument.sequential,
+            show_error_traces=command_argument.show_error_traces,
+            store_type_check_resolution=store_type_check_resolution,
+            terminal=terminal,
+            wait_on_initialization=wait_on_initialization,
+            skip_initial_type_check=skip_initial_type_check,
+            use_lazy_module_tracking=use_lazy_module_tracking,
+        )
 
 
 @dataclass(frozen=True)
