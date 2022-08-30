ae2c3490b00f8d13b03982f1ec116af32e733e18
Steven Troxler <stroxler@fb.com>
2022-08-29 19:35:48 -0700

    Remove more dead code
    Summary:
    These helper functions are not used anymore; they were made unnecessary
    when I made the `daemon_connection` refactor, but because persistent.py
    is so big and the code was spread out instead of being grouped together
    I didn't notice until now.
    
    Reviewed By: grievejia
    
    Differential Revision: D39082931
    
    fbshipit-source-id: d295a1147303a0f0acf5e27178a7f17965d9e952

--- client/commands/persistent.py
+++ client/commands/persistent.py
@@ -1124,33 +1124,6 @@ class QueryModulesOfPathResponse(json_mixins.CamlCaseAndExcludeJsonMixin):
     response: List[str]
 
 
-async def _send_request(
-    output_channel: connections.AsyncTextWriter, request: str
-) -> None:
-    LOG.debug(f"Sending `{log.truncate(request, 400)}`")
-    await output_channel.write(f"{request}\n")
-
-
-async def _receive_response(
-    input_channel: connections.AsyncTextReader,
-) -> Optional[daemon_query.Response]:
-    raw_response = await _read_server_response(input_channel)
-    LOG.info(f"Received `{log.truncate(raw_response, 400)}`")
-    try:
-        return daemon_query.Response.parse(raw_response)
-    except daemon_query.InvalidQueryResponse as exception:
-        LOG.info(f"Failed to parse json {raw_response} due to exception: {exception}")
-        return None
-
-
-async def _consume_and_drop_response(
-    input_channel: connections.AsyncTextReader,
-) -> None:
-    raw_response = await _read_server_response(input_channel)
-    LOG.info(f"Received and will drop response: `{log.truncate(raw_response, 400)}`")
-    return None
-
-
 class RequestHandler:
     def __init__(
         self,
