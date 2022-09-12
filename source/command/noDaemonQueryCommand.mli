6ea27b6847129f326a04067f7b35d75a9a3e430a
Joshua Xu <jxu425@fb.com>
2022-09-12 06:49:08 -0700

    Add documentation to noDaemonQueryCommand interface file for querying pyre
    Summary:
    **Context**: Part of a diff stack for implementing a no daemon pyre query.
    
    This diff is just for adding links to relevant documentation in a key interface file: noDaemonQueryCommand.mli.
    
    Reviewed By: stroxler
    
    Differential Revision: D39386936
    
    fbshipit-source-id: 924015cbdab7fcddb42c39536e2775aa91b2d103

--- source/command/noDaemonQueryCommand.mli
+++ source/command/noDaemonQueryCommand.mli
@@ -5,6 +5,12 @@
  * LICENSE file in the root directory of this source tree.
  *)
 
+(** The purpose of the no-daemon-query is to be able to run a query without needing to start the
+    Pyre server.
+
+    For more information about querying Pyre in general, please consult the following docs:
+    https://www.internalfb.com/intern/staticdocs/pyre/docs/querying-pyre/ **)
+
 open Core
 
 val command : Command.t
