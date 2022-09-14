d44428744097742ab1fb5a4278023f2f6fcfd84d
Joshua Xu <jxu425@fb.com>
2022-09-14 08:06:11 -0700

    Refactor: language server features into own file
    Summary:
    **Context**: Goal of this diff stack is to separate to the persistent.py behemoth into individual smaller files. See base here:  D39446836
    
    **This Diff**: Separates all language server features into their own file, since they can be considered a standalone concept.
    
    Reviewed By: stroxler
    
    Differential Revision: D39446835
    
    fbshipit-source-id: f11b1c364e9164af78573292e79cfae60c4c9ad0

---
+++ client/commands/language_server_features.py
@@ -0,0 +1,128 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+import dataclasses
+import enum
+
+from typing import Dict, Optional
+
+from .. import configuration as configuration_module
+
+from . import frontend_configuration
+
+
+class _Availability(enum.Enum):
+    ENABLED = "enabled"
+    DISABLED = "disabled"
+
+    @staticmethod
+    def from_enabled(enabled: bool) -> _Availability:
+        return _Availability.ENABLED if enabled else _Availability.DISABLED
+
+    def is_enabled(self) -> bool:
+        return self == _Availability.ENABLED
+
+    def is_disabled(self) -> bool:
+        return self == _Availability.DISABLED
+
+
+class _AvailabilityWithShadow(enum.Enum):
+    ENABLED = "enabled"
+    DISABLED = "disabled"
+    SHADOW = "shadow"
+
+    @staticmethod
+    def from_enabled(enabled: bool) -> _AvailabilityWithShadow:
+        return (
+            _AvailabilityWithShadow.ENABLED
+            if enabled
+            else _AvailabilityWithShadow.DISABLED
+        )
+
+    def is_enabled(self) -> bool:
+        return self == _AvailabilityWithShadow.ENABLED
+
+    def is_shadow(self) -> bool:
+        return self == _AvailabilityWithShadow.SHADOW
+
+    def is_disabled(self) -> bool:
+        return self == _AvailabilityWithShadow.DISABLED
+
+
+class TypeCoverageAvailability(enum.Enum):
+    DISABLED = "disabled"
+    FUNCTION_LEVEL = "function_level"
+    EXPRESSION_LEVEL = "expression_level"
+
+
+HoverAvailability = _Availability
+DefinitionAvailability = _AvailabilityWithShadow
+ReferencesAvailability = _Availability
+DocumentSymbolsAvailability = _Availability
+TypeErrorsAvailability = _Availability
+UnsavedChangesAvailability = _Availability
+
+
+@dataclasses.dataclass(frozen=True)
+class LanguageServerFeatures:
+    hover: HoverAvailability = HoverAvailability.DISABLED
+    definition: DefinitionAvailability = DefinitionAvailability.DISABLED
+    document_symbols: DocumentSymbolsAvailability = DocumentSymbolsAvailability.DISABLED
+    references: ReferencesAvailability = ReferencesAvailability.DISABLED
+    type_coverage: TypeCoverageAvailability = TypeCoverageAvailability.DISABLED
+    type_errors: TypeErrorsAvailability = TypeErrorsAvailability.ENABLED
+    unsaved_changes: UnsavedChangesAvailability = UnsavedChangesAvailability.DISABLED
+
+    @staticmethod
+    def create(
+        configuration: frontend_configuration.Base,
+        hover: Optional[HoverAvailability],
+        definition: Optional[DefinitionAvailability],
+        document_symbols: Optional[DocumentSymbolsAvailability],
+        references: Optional[ReferencesAvailability],
+        type_coverage: Optional[TypeCoverageAvailability],
+        unsaved_changes: Optional[UnsavedChangesAvailability],
+        type_errors: TypeErrorsAvailability = TypeErrorsAvailability.ENABLED,
+    ) -> LanguageServerFeatures:
+        ide_features = configuration.get_ide_features()
+        if ide_features is None:
+            ide_features = configuration_module.IdeFeatures()
+        return LanguageServerFeatures(
+            hover=hover
+            or HoverAvailability.from_enabled(ide_features.is_hover_enabled()),
+            definition=definition
+            or DefinitionAvailability.from_enabled(
+                ide_features.is_go_to_definition_enabled()
+            ),
+            document_symbols=document_symbols
+            or DocumentSymbolsAvailability.from_enabled(
+                ide_features.is_find_symbols_enabled()
+            ),
+            references=references
+            or ReferencesAvailability.from_enabled(
+                ide_features.is_find_all_references_enabled()
+            ),
+            type_coverage=type_coverage
+            or (
+                TypeCoverageAvailability.EXPRESSION_LEVEL
+                if ide_features.is_expression_level_coverage_enabled()
+                else TypeCoverageAvailability.FUNCTION_LEVEL
+            ),
+            type_errors=type_errors,
+            unsaved_changes=unsaved_changes
+            or UnsavedChangesAvailability.from_enabled(
+                ide_features.is_consume_unsaved_changes_enabled()
+            ),
+        )
+
+    def capabilities(self) -> Dict[str, bool]:
+        return {
+            "hover_provider": not self.hover.is_disabled(),
+            "definition_provider": not self.definition.is_disabled(),
+            "document_symbol_provider": not self.document_symbols.is_disabled(),
+            "references_provider": not self.references.is_disabled(),
+        }
--- client/commands/persistent.py
+++ client/commands/persistent.py
@@ -57,6 +57,16 @@ from .ide_response import (
     QueryModulesOfPathResponse,
     ReferencesResponse,
 )
+from .language_server_features import (
+    DefinitionAvailability,
+    DocumentSymbolsAvailability,
+    HoverAvailability,
+    LanguageServerFeatures,
+    ReferencesAvailability,
+    TypeCoverageAvailability,
+    TypeErrorsAvailability,
+    UnsavedChangesAvailability,
+)
 
 LOG: logging.Logger = logging.getLogger(__name__)
 
@@ -106,119 +116,6 @@ def _log_lsp_event(
 
 PyreServerOptionsReader = Callable[[], "PyreServerOptions"]
 FrontendConfigurationReader = Callable[[], frontend_configuration.Base]
-
-
-class _Availability(enum.Enum):
-    ENABLED = "enabled"
-    DISABLED = "disabled"
-
-    @staticmethod
-    def from_enabled(enabled: bool) -> _Availability:
-        return _Availability.ENABLED if enabled else _Availability.DISABLED
-
-    def is_enabled(self) -> bool:
-        return self == _Availability.ENABLED
-
-    def is_disabled(self) -> bool:
-        return self == _Availability.DISABLED
-
-
-class _AvailabilityWithShadow(enum.Enum):
-    ENABLED = "enabled"
-    DISABLED = "disabled"
-    SHADOW = "shadow"
-
-    @staticmethod
-    def from_enabled(enabled: bool) -> _AvailabilityWithShadow:
-        return (
-            _AvailabilityWithShadow.ENABLED
-            if enabled
-            else _AvailabilityWithShadow.DISABLED
-        )
-
-    def is_enabled(self) -> bool:
-        return self == _AvailabilityWithShadow.ENABLED
-
-    def is_shadow(self) -> bool:
-        return self == _AvailabilityWithShadow.SHADOW
-
-    def is_disabled(self) -> bool:
-        return self == _AvailabilityWithShadow.DISABLED
-
-
-class TypeCoverageAvailability(enum.Enum):
-    DISABLED = "disabled"
-    FUNCTION_LEVEL = "function_level"
-    EXPRESSION_LEVEL = "expression_level"
-
-
-HoverAvailability = _Availability
-DefinitionAvailability = _AvailabilityWithShadow
-ReferencesAvailability = _Availability
-DocumentSymbolsAvailability = _Availability
-TypeErrorsAvailability = _Availability
-UnsavedChangesAvailability = _Availability
-
-
-@dataclasses.dataclass(frozen=True)
-class LanguageServerFeatures:
-    hover: HoverAvailability = HoverAvailability.DISABLED
-    definition: DefinitionAvailability = DefinitionAvailability.DISABLED
-    document_symbols: DocumentSymbolsAvailability = DocumentSymbolsAvailability.DISABLED
-    references: ReferencesAvailability = ReferencesAvailability.DISABLED
-    type_coverage: TypeCoverageAvailability = TypeCoverageAvailability.DISABLED
-    type_errors: TypeErrorsAvailability = TypeErrorsAvailability.ENABLED
-    unsaved_changes: UnsavedChangesAvailability = UnsavedChangesAvailability.DISABLED
-
-    @staticmethod
-    def create(
-        configuration: frontend_configuration.Base,
-        hover: Optional[HoverAvailability],
-        definition: Optional[DefinitionAvailability],
-        document_symbols: Optional[DocumentSymbolsAvailability],
-        references: Optional[ReferencesAvailability],
-        type_coverage: Optional[TypeCoverageAvailability],
-        unsaved_changes: Optional[UnsavedChangesAvailability],
-        type_errors: TypeErrorsAvailability = TypeErrorsAvailability.ENABLED,
-    ) -> LanguageServerFeatures:
-        ide_features = configuration.get_ide_features()
-        if ide_features is None:
-            ide_features = configuration_module.IdeFeatures()
-        return LanguageServerFeatures(
-            hover=hover
-            or HoverAvailability.from_enabled(ide_features.is_hover_enabled()),
-            definition=definition
-            or DefinitionAvailability.from_enabled(
-                ide_features.is_go_to_definition_enabled()
-            ),
-            document_symbols=document_symbols
-            or DocumentSymbolsAvailability.from_enabled(
-                ide_features.is_find_symbols_enabled()
-            ),
-            references=references
-            or ReferencesAvailability.from_enabled(
-                ide_features.is_find_all_references_enabled()
-            ),
-            type_coverage=type_coverage
-            or (
-                TypeCoverageAvailability.EXPRESSION_LEVEL
-                if ide_features.is_expression_level_coverage_enabled()
-                else TypeCoverageAvailability.FUNCTION_LEVEL
-            ),
-            type_errors=type_errors,
-            unsaved_changes=unsaved_changes
-            or UnsavedChangesAvailability.from_enabled(
-                ide_features.is_consume_unsaved_changes_enabled()
-            ),
-        )
-
-    def capabilities(self) -> Dict[str, bool]:
-        return {
-            "hover_provider": not self.hover.is_disabled(),
-            "definition_provider": not self.definition.is_disabled(),
-            "document_symbol_provider": not self.document_symbols.is_disabled(),
-            "references_provider": not self.references.is_disabled(),
-        }
 
 
 @dataclasses.dataclass(frozen=True)
