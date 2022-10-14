e646c1a486ee3635a982e0989617b440e41eecc6
Maxime Arthaud <arthaud@meta.com>
2022-10-13 20:17:51 -0700

    Add integration tests showing false positives due to the representation of access paths
    Summary: We know we have a few false positives due to our access path representation. Let's document them in integration tests.
    
    Reviewed By: alexkassil
    
    Differential Revision: D40364803
    
    fbshipit-source-id: 8887a95225e7a6cc3783194439d988f767dfbab2

---
+++ source/interprocedural_analyses/taint/test/integration/access_path.py
@@ -0,0 +1,29 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from builtins import _test_sink, _test_source
+from typing import Dict, Optional
+
+
+def indexes_are_strings() -> None:
+    d = {}
+    d[1] = _test_source()
+    _test_sink(d["1"])  # False positive.
+
+
+class SpecialDict:
+    def __init__(self) -> None:
+        self.attribute: Optional[str] = None
+        self.dict: Dict[str, str] = {}
+
+    def __getitem__(self, key: str) -> str:
+        return self.dict.get(key, "")
+
+
+def indexes_and_attributes():
+    o = SpecialDict()
+    o.attribute = _test_source()
+    # False positive, attributes and indexes are mixed.
+    _test_sink(o["attribute"])
