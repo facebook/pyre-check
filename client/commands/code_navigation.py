5560fb2f7a36ea8a8fb39e4259949f7adc849a37
Sinan Cepel <sinancepel@meta.com>
2022-10-27 11:53:41 -0700

    start and subscribe to a codenav server on the client
    Summary:
    This diff handles the subscription handshake between the codenav client and server. The wait-loop doesn't quite work yet, and the daemon crashes immediately after subscribing.
    
    Next diff: Make the daemon not crash immediately after subscribing.
    
    Reviewed By: grievejia
    
    Differential Revision: D40678513
    
    fbshipit-source-id: bd650885f497fe99338e6dce9c8642787ca52e0f

--- client/commands/code_navigation.py
+++ client/commands/code_navigation.py
@@ -13,24 +13,162 @@ configurations.
 from __future__ import annotations
 
 import asyncio
+import json
 import logging
 import traceback
 
 from typing import Optional
 
-from .. import timer
-from . import backend_arguments, log_lsp_event
+from .. import timer, version
+from ..language_server import connections, features, protocol as lsp
+
+from . import (
+    backend_arguments,
+    background,
+    initialization,
+    launch_and_subscribe_handler,
+    log_lsp_event,
+    persistent,
+    pyre_language_server,
+    pyre_server_options,
+    request_handler,
+    server_state as state,
+    subscription,
+)
 
 LOG: logging.Logger = logging.getLogger(__name__)
 
+READY_MESSAGE: str = "Pyre has completed an incremental check and is currently watching on further source changes."
+READY_SHORT: str = "Pyre Ready"
+
+
+async def _read_server_response(
+    server_input_channel: connections.AsyncTextReader,
+) -> str:
+    return await server_input_channel.read_until(separator="\n")
+
+
+class PyreCodeNavigationDaemonLaunchAndSubscribeHandler(
+    launch_and_subscribe_handler.PyreDaemonLaunchAndSubscribeHandler
+):
+    def get_type_errors_availability(self) -> features.TypeErrorsAvailability:
+        return self.server_state.server_options.language_server_features.type_errors
+
+    async def handle_type_error_subscription(
+        self, type_error_subscription: subscription.TypeErrors
+    ) -> None:
+        raise NotImplementedError
+
+    async def handle_status_update_subscription(
+        self, status_update_subscription: subscription.StatusUpdate
+    ) -> None:
+        raise NotImplementedError
+
+    async def handle_error_subscription(
+        self, error_subscription: subscription.Error
+    ) -> None:
+        raise NotImplementedError
+
+    async def _subscribe(
+        self,
+        server_input_channel: connections.AsyncTextReader,
+        server_output_channel: connections.AsyncTextWriter,
+    ) -> None:
+        await server_output_channel.write('["Subscription", ["Subscribe"]]\n')
+        first_response = await _read_server_response(server_input_channel)
+        if json.loads(first_response) != ["Ok"]:
+            raise ValueError(
+                f"Unexpected server response to Subscription: {first_response!r}"
+            )
+
+
+def process_initialize_request(
+    parameters: lsp.InitializeParameters,
+    language_server_features: Optional[features.LanguageServerFeatures] = None,
+) -> lsp.InitializeResult:
+    LOG.info(
+        f"Received initialization request from {parameters.client_info} "
+        f" (pid = {parameters.process_id})"
+    )
+    if language_server_features is None:
+        language_server_features = features.LanguageServerFeatures()
+    server_info = lsp.Info(name="pyre-codenav", version=version.__version__)
+    server_capabilities = lsp.ServerCapabilities(
+        text_document_sync=lsp.TextDocumentSyncOptions(
+            open_close=True,
+            change=lsp.TextDocumentSyncKind.FULL,
+            save=lsp.SaveOptions(include_text=False),
+        ),
+        **language_server_features.capabilities(),
+    )
+    return lsp.InitializeResult(
+        capabilities=server_capabilities, server_info=server_info
+    )
+
 
 async def async_run_code_navigation_client(
+    server_options_reader: pyre_server_options.PyreServerOptionsReader,
     remote_logging: Optional[backend_arguments.RemoteLogging],
 ) -> int:
-    return 0
+    initial_server_options = launch_and_subscribe_handler.PyreDaemonLaunchAndSubscribeHandler.read_server_options(
+        server_options_reader, remote_logging=None
+    )
+    stdin, stdout = await connections.create_async_stdin_stdout()
+    initialize_result = await initialization.async_try_initialize_loop(
+        initial_server_options,
+        stdin,
+        stdout,
+        remote_logging,
+        process_initialize_request,
+    )
+    if isinstance(initialize_result, initialization.InitializationExit):
+        return 0
+    client_info = initialize_result.client_info
+    log_lsp_event._log_lsp_event(
+        remote_logging=remote_logging,
+        event=log_lsp_event.LSPEvent.INITIALIZED,
+        normals=(
+            {}
+            if client_info is None
+            else {
+                "lsp client name": client_info.name,
+                "lsp client version": client_info.version,
+            }
+        ),
+    )
+
+    client_capabilities = initialize_result.client_capabilities
+    LOG.debug(f"Client capabilities: {client_capabilities}")
+    server_state = state.ServerState(
+        client_capabilities=client_capabilities,
+        server_options=initial_server_options,
+    )
+    server = pyre_language_server.PyreLanguageServer(
+        input_channel=stdin,
+        output_channel=stdout,
+        server_state=server_state,
+        daemon_manager=background.TaskManager(
+            PyreCodeNavigationDaemonLaunchAndSubscribeHandler(
+                server_options_reader=server_options_reader,
+                remote_logging=remote_logging,
+                server_state=server_state,
+                client_status_message_handler=persistent.ClientStatusMessageHandler(
+                    stdout, server_state
+                ),
+                client_type_error_handler=persistent.ClientTypeErrorHandler(
+                    stdout, server_state, remote_logging
+                ),
+            )
+        ),
+        handler=request_handler.RequestHandler(
+            server_state=server_state,
+        ),
+    )
+    return await server.run()
 
 
 def run(
+    server_options_reader: pyre_server_options.PyreServerOptionsReader,
     remote_logging: Optional[backend_arguments.RemoteLogging],
 ) -> int:
     command_timer = timer.Timer()
@@ -38,6 +176,7 @@ def run(
     try:
         return asyncio.run(
             async_run_code_navigation_client(
+                server_options_reader,
                 remote_logging,
             )
         )
