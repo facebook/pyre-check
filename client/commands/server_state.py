3828bf0d2d7bb0c600d36c761f4c310b56437a42
Joshua Xu <jxu425@meta.com>
2022-10-27 19:31:03 -0700

    server_state.py
    Summary:
    Any module I touch will no longer contain TODO(T132414938)! Including server_state.py.
    
    {gif:3vg2rveq}
    
    Reviewed By: sinancepel
    
    Differential Revision: D40724976
    
    fbshipit-source-id: 8463a18f0b393ba3243b08074c3a6d7f4b0a5cff

--- client/commands/server_state.py
+++ client/commands/server_state.py
@@ -4,7 +4,9 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+This module stores all server state related fields as a single class, so all the mutable state
+can be accessed in one place. Note that this state is mutable and a singleton, so users of this module
+should be aware a change to this state could affect other modules that interact with this state.
 """
 
 
