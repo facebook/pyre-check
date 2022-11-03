1bc9ad63024773a16cffd2107da715f8d4a37516
Steven Troxler <stroxler@meta.com>
2022-11-03 14:06:48 -0700

    Add module dosctring for `commands/__init__.py`
    Summary: Let's keep getting high-level summaries, bit by bit :)
    
    Reviewed By: joshuaxu1995
    
    Differential Revision: D40863019
    
    fbshipit-source-id: 0fc1ab638c49240a0dbb6c837bcf11fbaf2fc38d

--- client/commands/__init__.py
+++ client/commands/__init__.py
@@ -4,7 +4,10 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+The `client.commands` namespace of Pyre contains the entrypoints
+for all of the subcommands of pyre such as `pyre check` and
+`pyre incremental`. It also contains some helper modules with shared
+library code used by several commands.
 """
 
 
