abef89e1571aa294abe7f5aaf19750f6eeed5ce7
Maxime Arthaud <arthaud@meta.com>
2022-11-03 19:08:36 -0700

    Precise access path and collapse depth for standard containers
    Summary: Now that we have introduced a proper syntax to let user define input and output path of propagations, as well as the collapse depth (see D40746937 (https://github.com/facebook/pyre-check/commit/3fe7a9f9db5621fb5992da9e8be01b3f51203693)), we can define precise models for most standard containers. This should remove a good amount of false positives. This diff also introduces proper tests for most methods.
    
    Reviewed By: alexkassil
    
    Differential Revision: D40964213
    
    fbshipit-source-id: 5417ae4c64efd3438c362b7721edde2ffe29b23c

--- stubs/integration_test/containers.py
+++ stubs/integration_test/containers.py
@@ -6,6 +6,7 @@
 # flake8: noqa
 
 from integration_test.taint import source, sink
+import collections
 
 
 def dictionary_update():
@@ -14,24 +15,168 @@ def dictionary_update():
     result.update(argument)
     sink(result)
 
+    result = {"old": {"a": source()}}
+    argument = {"new": {"b": source()}}
+    result.update(argument)
+    sink(result["old"]["a"])  # This is an issue.
+    sink(result["old"]["b"])  # This is an issue (false positive).
+    sink(result["new"]["a"])  # This is an issue (false positive).
+    sink(result["new"]["b"])  # This is an issue.
+    sink(result["unknown"])  # This is an issue (false positive).
+
+    result = {}
+    argument = [("a", {"b": source()})]
+    result.update(argument)
+    sink(result["a"])  # This is an issue.
+    sink(result["a"]["b"])  # This is an issue.
+    sink(result["a"]["c"])  # This is an issue (false positive).
+    sink(result["b"]["b"])  # This is an issue (false positive).
+
+    result = {}
+    argument = {"b": source()}
+    result.update(a=argument)
+    sink(result["a"])  # This is an issue.
+    sink(result["a"]["b"])  # This is an issue.
+    sink(result["a"]["c"])  # This is NOT an issue.
+    sink(result["b"]["b"])  # This is an issue (false positive).
+
+
+def dict_setdefault(i: int):
+    d = {0: 0}
+    d.setdefault(source(), 0)
+    sink(d[0])  # This is NOT an issue.
+    sink(d.keys())  # This is an issue.
+
+    d = {0: 0}
+    d.setdefault(i, source())
+    sink(d[i])
+
+    d = {0: 0}
+    d.setdefault(i, {"a": source()})
+    sink(d[i]["a"])  # This is an issue.
+    sink(d[i]["b"])  # This is NOT an issue.
+
+    d = {0: 0}
+    result = d.setdefault(i, {"a": source()})
+    sink(result["a"])  # This is an issue.
+    sink(result["b"])  # This is NOT an issue.
+
+    d = {0: {"a": source()}}
+    result = d.setdefault(i, 0)
+    sink(result["a"])  # This is an issue.
+    sink(result["b"])  # This is NOT an issue.
+
+
+def dict_get():
+    d = {"a": {0: source()}}
+    sink(d.get("a")[0])  # This is an issue.
+    sink(d.get("a")[1])  # This is NOT an issue.
+    sink(d.get("b")[0])  # This is currently an issue (false positive).
+    sink(d.get("b")[1])  # This is NOT an issue.
+
+
+def dict_get_default():
+    d = {}
+    sink(d.get("a", {0: source()})[0])  # This is an issue.
+    sink(d.get("a", {0: source()})[1])  # This is NOT an issue.
+
 
 def list_append():
     l = []
     l.append(source())
     sink(l[0])
 
+    l = []
+    l.append({"a": source()})
+    sink(l[0]["a"])  # This is an issue.
+    sink(l[0]["b"])  # This is NOT an issue.
 
-def list_update():
+
+def list_extend(i: int):
     l = []
     tainted_list = [source()]
     l.extend(tainted_list)
     sink(l[0])
 
+    l = []
+    tainted_list = [{"a": source()}]
+    l.extend(tainted_list)
+    sink(l[0]["a"])  # This is an issue.
+    sink(l[0]["b"])  # This is NOT an issue.
 
-def list_insert():
+    l = [{"a": source(), "c": 0}]
+    tainted_list = [{"b": source(), "c": 0}]
+    l.extend(tainted_list)
+    sink(l[i]["a"])  # This is an issue.
+    sink(l[i]["b"])  # This is an issue.
+    sink(l[i]["c"])  # This is NOT an issue.
+
+    l = []
+    l.extend({(source(), 0)})
+    sink(l[i][0])  # This is an issue.
+    sink(l[i][1])  # This is NOT an issue.
+
+
+def list_insert(i: int):
     l = [1] * 10
     l.insert(5, source())
     sink(l[5])
+
+    l = [{}] * 10
+    l.insert(5, {"a": source(), "b": 0})
+    sink(l[5]["a"])  # This is an issue.
+    sink(l[5]["b"])  # This is NOT an issue.
+
+    l = [0, source(), 0]
+    l.insert(0, 0)
+    sink(l[2])  # This is an issue.
+
+
+def list_iadd(i: int):
+    l = [0]
+    l += [source()]
+    sink(l[1])
+
+    l = [{"a": source()}]
+    l += [{"b": source()}]
+    sink(l[0]["a"])  # This is an issue.
+    sink(l[1]["b"])  # This is an issue.
+    sink(l[0]["b"])  # This is currently an issue (false positive).
+    sink(l[i]["b"])  # This is an issue.
+    sink(l[i]["c"])  # This is NOT an issue.
+
+
+def list_add(i: int):
+    l = [0] + [source()]
+    sink(l[1])
+
+    l = [{"a": source()}] + [{"b": source()}]
+    sink(l[0]["a"])  # This is an issue.
+    sink(l[1]["b"])  # This is an issue.
+    sink(l[0]["b"])  # This is currently an issue (false positive).
+    sink(l[i]["b"])  # This is an issue.
+    sink(l[i]["c"])  # This is NOT an issue.
+
+
+def list_imul():
+    l = [0, source(), 0]
+    l *= 4
+    sink(l[10])  # This is an issue.
+    sink(l[9])  # This is currently an issue (false positive).
+
+
+def list_sort(i: int):
+    l = [0, 1, 2, source(), 4, 5]
+    l.sort()
+    sink(l[0])
+    sink(l[i])
+
+
+def list_remove():
+    l = [0, source(), 0]
+    l.remove(0)
+    sink(l[0])  # This is an issue.
+    sink(l[1])  # This is currently an issue (false positive).
 
 
 def set_add():
@@ -40,6 +185,12 @@ def set_add():
     for element in s:
         sink(element)
 
+    s = set()
+    s.add((source(), 0))
+    for element in s:
+        sink(element[0])  # This is an issue.
+        sink(element[1])  # This is NOT an issue.
+
 
 def set_intersection_update():
     s = {1}
@@ -47,9 +198,51 @@ def set_intersection_update():
     for element in s:
         sink(element)
 
+    s = {(source(), 0, 0)}
+    s.intersection_update({(0, source(), 0)})
+    for element in s:
+        sink(element[0])  # This is an issue.
+        sink(element[1])  # This is an issue.
+        sink(element[2])  # This is NOT an issue.
+
 
 def set_update():
     s = {1}
     s.update({source()})
     for element in s:
         sink(element)
+
+
+def set_iand():
+    s = {(source(), 0, 0)}
+    s &= {(0, source(), 0)}
+    for element in s:
+        sink(element[0])  # This is an issue.
+        sink(element[1])  # This is an issue.
+        sink(element[2])  # This is NOT an issue.
+
+
+def deque_append(i: int):
+    d = collections.deque()
+    d.append(source())
+    sink(d[0])
+
+    d = collections.deque()
+    d.append({"a": source()})
+    d.appendleft({"b": source()})
+    sink(d[i]["a"])  # This is an issue.
+    sink(d[i]["b"])  # This is an issue.
+    sink(d[i]["c"])  # This is NOT an issue.
+
+
+def deque_extend(i: int):
+    d = collections.deque()
+    d.extend([source()])
+    sink(d[0])
+
+    d = collections.deque()
+    d.append({"a": source()})
+    d.extend([{"b": source()}])
+    sink(d[i]["a"])  # This is an issue.
+    sink(d[i]["b"])  # This is an issue.
+    sink(d[i]["c"])  # This is NOT an issue.
