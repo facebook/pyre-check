b6db3f7959c9540084e1fa40a9c8575baa6e5735
Steven Troxler <stroxler@meta.com>
2022-12-06 19:22:47 -0800

    statistics_collector.py
    Reviewed By: grievejia
    
    Differential Revision: D41756714
    
    fbshipit-source-id: c3bdca1ed2e97a5970a6927ae84c7315e464bed2

--- client/statistics_collectors.py
+++ client/statistics_collectors.py
@@ -4,7 +4,10 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+This module defines a suite of libcst visitors that discover annotation
+information and typing-related comments in code, and can thereby produce
+various code statistics (counts of annotations, annotated functions, fixmes,
+etc).
 """
 
 
