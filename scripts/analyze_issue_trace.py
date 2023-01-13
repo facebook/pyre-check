b2fc6dab5d241bcbe5ac6ecd41a01242751dd68e
Conner Nilsen <connernilsen@meta.com>
2023-01-13 15:29:05 -0800

    Create call graph representation
    Summary:
    **Background**: A representation of a call graph is needed for creating a trace of the call graph.
    
    **Context**: For static analysis, we would like the ability to trace an issue (on the shortest path) back to its entrypoint(s). Previous diff creates the file and adds CLI flags.
    
    Reviewed By: sinancepel
    
    Differential Revision: D42377319
    
    fbshipit-source-id: fe70c9c877c2d9bf30f78fa05e96fed4e6e0c966

--- scripts/analyze_issue_trace.py
+++ scripts/analyze_issue_trace.py
@@ -3,9 +3,51 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Optional, TextIO
+from collections import defaultdict
+from typing import cast, Dict, List, Optional, Set, TextIO
 
 import click
+
+
+class CallGraph:
+    call_graph: Dict[str, Set[str]]
+
+    def __init__(self, call_graph: object) -> None:
+        self.validate_call_graph(call_graph)
+        self.call_graph = self.json_to_call_graph(
+            cast(Dict[str, List[str]], call_graph)
+        )
+
+    @staticmethod
+    def json_to_call_graph(call_graph: Dict[str, List[str]]) -> Dict[str, Set[str]]:
+        nodes = defaultdict(lambda: set())
+
+        for caller, callees in call_graph.items():
+            nodes[caller] = set(callees)
+
+            # skip self calls, since they're irrelevant for trace purposes
+            nodes[caller] -= {caller}
+
+        return nodes
+
+    @staticmethod
+    def validate_call_graph(call_graph: object) -> None:
+        if not isinstance(call_graph, Dict):
+            raise ValueError(
+                f"Call graph structure in call graph file is not a JSON dict: {type(call_graph)}"
+            )
+
+        for caller, callees in call_graph.items():
+            if not isinstance(callees, list):
+                raise ValueError(
+                    f"Value for {caller} in top-level dictionary is not a list of callees: {type(callees)} -> {str(callees)}"
+                )
+
+            for callee in callees:
+                if not isinstance(callee, str):
+                    raise ValueError(
+                        f"Callee in callee list for {caller} is not a str: {type(callee)} -> {str(callee)}"
+                    )
 
 
 @click.command()
---
+++ scripts/tests/analyze_issue_trace_test.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env fbpython
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+from ..analyze_issue_trace import CallGraph
+
+
+CALL_GRAPH = {
+    "test.nested_run.do_the_thing": ["Obj{test.glob}", "Ovr{list::append}"],
+    "test.$toplevel": [
+        "Obj{test.glob}",
+        "object.__init__",
+        "object.__new__",
+        "test.main",
+        "str.__eq__",
+        "str.__ne__",
+    ],
+    "test.call_multi_path4": ["test.multi_path2", "test.multi_path4"],
+    "test.cycle_call1": ["test.cycle_call2"],
+    "test.cycle_call2": ["test.cycle_call3"],
+    "test.cycle_call3": ["test.cycle_call1"],
+    "test.get_these": [
+        "test.call_multi_path4",
+        "test.cycle_call1",
+        "test.immediate_examples",
+        "test.leak_globals_by_passing_in",
+        "test.leak_globals_by_transitive_call",
+        "test.long_chain1",
+        "test.multi_path1",
+        "test.nested_run",
+        "test.self_call",
+        "test.transitive_call_accessing_globals",
+    ],
+    "test.immediate_examples": ["Obj{test.glob}", "Ovr{list::append}"],
+    "test.leak_globals_by_passing_in": [
+        "Obj{test.glob}",
+        "test.transitive_call_with_globals_passed_in",
+    ],
+    "test.leak_globals_by_transitive_call": ["test.transitive_call_accessing_globals"],
+    "test.long_chain1": ["test.long_chain2"],
+    "test.long_chain2": ["test.long_chain3"],
+    "test.long_chain3": ["test.long_chain4"],
+    "test.long_chain4": ["print"],
+    "test.main": [
+        "Ovr{test.MyClass::this_one_too}",
+        "test.get_these",
+        "test.this_one_shouldnt_be_found",
+    ],
+    "test.multi_path1": ["test.multi_path2", "test.multi_path3"],
+    "test.multi_path2": ["test.multi_path3", "test.multi_path4"],
+    "test.multi_path3": ["test.multi_path4"],
+    "test.multi_path4": ["print"],
+    "test.nested_run": ["test.nested_run.do_the_thing"],
+    "test.self_call": ["test.self_call"],
+    "test.this_one_shouldnt_be_found": ["Obj{test.glob}", "Ovr{list::append}"],
+    "test.transitive_call_accessing_globals": ["Obj{test.glob}", "Ovr{list::append}"],
+    "test.transitive_call_with_globals_passed_in": ["Ovr{list::append}"],
+    "test.MySubclass.this_one_too": ["Obj{test.glob}", "Ovr{list::append}"],
+}
+
+
+class AnalyzeIssueTraceTest(unittest.TestCase):
+    def test_load_call_graph_happy_path(self) -> None:
+        call_graph = {
+            "my_module.my_function": [
+                "something_that.my_function_calls",
+                "builtins.print",
+                "my_module.my_function",
+            ],
+            "something_that.my_function_calls": ["int.__str__"],
+        }
+
+        CallGraph.validate_call_graph(call_graph)
+        result = CallGraph.json_to_call_graph(call_graph)
+
+        self.assertEqual(len(result), 2)
+        self.assertSetEqual(result["something_that.my_function_calls"], {"int.__str__"})
+        self.assertSetEqual(
+            result["my_module.my_function"],
+            {"something_that.my_function_calls", "builtins.print"},
+        )
+
+    def test_load_call_graph_bad_root(self) -> None:
+        call_graph = ["1234"]
+
+        try:
+            CallGraph.validate_call_graph(call_graph)
+            self.fail("should have thrown")
+        except ValueError:
+            pass
+
+    def test_load_call_graph_bad_callers(self) -> None:
+        call_graph = {"caller": 1234}
+
+        try:
+            CallGraph.validate_call_graph(call_graph)
+            self.fail("should have thrown")
+        except ValueError:
+            pass
+
+    def test_load_call_graph_bad_callees(self) -> None:
+        call_graph = {"caller": [1, 2, 3]}
+
+        try:
+            CallGraph.validate_call_graph(call_graph)
+            self.fail("should have thrown")
+        except ValueError:
+            pass
