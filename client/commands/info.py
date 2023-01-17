b5ba59fa56a71377eec7e74674316bc307a1982e
Steven Troxler <stroxler@meta.com>
2023-01-17 08:38:50 -0800

    Docstring for info.py
    Reviewed By: grievejia
    
    Differential Revision: D42489939
    
    fbshipit-source-id: 32bac407d9646d04a761b1d69d67568f2fe1661c

--- client/commands/info.py
+++ client/commands/info.py
@@ -4,7 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+Logic for the `pyre info` command, which prints information about
+the current pyre environment (e.g. socket path, log locations).
 """
 
 
