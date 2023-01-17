13fb4123ec41b7d667b31b73962b2df7b7926118
Steven Troxler <stroxler@meta.com>
2023-01-17 08:38:50 -0800

    Docstring for initialize.py
    Reviewed By: grievejia
    
    Differential Revision: D42490075
    
    fbshipit-source-id: 4947c6307cb2295c30328e134068318122974d16

--- client/commands/initialize.py
+++ client/commands/initialize.py
@@ -4,7 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+This module contains logic for `pyre init`, which interactively
+helps a user create a new pyre configuration.
 """
 
 
