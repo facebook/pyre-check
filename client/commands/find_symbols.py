88da6f141f02dd7d10c650461e25d44b6a34393f
Steven Troxler <stroxler@meta.com>
2023-01-19 14:02:53 -0800

    Docstring for find_symbols.py
    Reviewed By: pradeep90
    
    Differential Revision: D42608956
    
    fbshipit-source-id: b7dfdf75a5a5de5d85a15a2b96b2786edfb2cf76

--- client/commands/find_symbols.py
+++ client/commands/find_symbols.py
@@ -4,7 +4,9 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+This module contains logic for a Python-based implementation of
+gathering document symbols for a language server. We traverse the
+output of `ast.parse` to do this.
 """
 
 
