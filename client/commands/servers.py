2b9f3d81bf9b326ca3ee776a153cc69d0fbdd183
Steven Troxler <stroxler@meta.com>
2023-01-19 14:02:53 -0800

    Docstring for servers.py
    Reviewed By: kinto0
    
    Differential Revision: D42608958
    
    fbshipit-source-id: 42eec996cd0cf2bbfe24ed25abe6ce484564db76

--- client/commands/servers.py
+++ client/commands/servers.py
@@ -4,7 +4,9 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+This module provides the logic for the `pyre servers` command, which lists
+running servers (i.e. daemons - background backend processes available for
+incremental checks and queries, including from language servers).
 """
 
 
