9ea6929566389762f1a7177a9e225f2cc9e47595
Steven Troxler <stroxler@meta.com>
2023-01-19 14:02:53 -0800

    Docstring for commands.py
    Reviewed By: kinto0
    
    Differential Revision: D42608954
    
    fbshipit-source-id: d59a75966dd389b51d3c632652be09e95554dd0c

--- client/commands/commands.py
+++ client/commands/commands.py
@@ -4,7 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+This module provides some utility code for handling exit status and
+uncaught exceptions in Pyre commands.
 """
 
 
