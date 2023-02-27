4884d25006e1e380ce078bc37731dbdc5de842b0
Steven Troxler <stroxler@meta.com>
2023-02-27 10:18:36 -0800

    Docstring for `tests/setup.py`
    Summary:
    When we placed TODO markers we skipped unit tests, because they
    generally shadow production code and therefore don't really
    need a docstring. But this module actually isn't a unit test, it
    has testing helper logic and deserves a docstring.
    
    Reviewed By: grievejia
    
    Differential Revision: D43563908
    
    fbshipit-source-id: 9ed75dd74a534796bbb9b76affa9da311dfab3ed

--- client/tests/setup.py
+++ client/tests/setup.py
@@ -4,7 +4,10 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-TODO(T132414938) Add a module-level docstring
+This module provides various helper functions that unit tests
+can use to create test fixtures - for example, making sure
+files and directories exist and controlling the working directory
+from which tests are run.
 """
 
 
