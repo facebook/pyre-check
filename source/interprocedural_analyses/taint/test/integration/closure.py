0c083943ec5769ea8ba5727cb332c3e36773b9e5
Alex Kassil <afk@meta.com>
2023-03-22 17:32:26 -0700

    Demonstrate known false negatives for closures and nonlocal
    Summary: Closures can be treated and analyzed the same was as globals and class attributes
    
    Reviewed By: arthaud
    
    Differential Revision: D44276516
    
    fbshipit-source-id: 896e91871caeecc3733b5e62b60e02144edd51c3

---
+++ source/interprocedural_analyses/taint/test/integration/closure.py
@@ -0,0 +1,61 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from builtins import _test_sink, _test_source
+
+
+class Object:
+    pass
+
+
+def closure():
+    obj = Object()
+
+    def source():
+        obj.x = _test_source()
+
+    def sink():
+        _test_sink(obj.x)
+
+    return source, sink
+
+
+def closure_flow():
+    # TODO(T145247918): False Negative
+    source, sink = closure()
+    source()
+    sink()
+
+
+def closure_no_flow():
+    source, sink = closure()
+    sink()
+    source()
+
+
+def nonlocal_closure():
+    obj = ""
+
+    def source():
+        nonlocal obj
+        obj = _test_source()
+
+    def sink():
+        _test_sink(obj)
+
+    return source, sink
+
+
+def nonlocal_flow():
+    # TODO(T145247918): False Negative
+    source, sink = nonlocal_closure()
+    source()
+    sink()
+
+
+def nonlocal_no_flow():
+    source, sink = nonlocal_closure()
+    sink()
+    source()
