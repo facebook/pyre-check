7b3fe70d7f0d98dd8be20292935d4317a3c4e98b
Marat Turaev <maratturaev@meta.com>
2023-04-27 08:53:38 -0700

    compact OCaml heap before initial models
    Summary: a low effort to recollect memory before starting an intense phase of initial models
    
    Reviewed By: arthaud
    
    Differential Revision: D45303706
    
    fbshipit-source-id: 91c3ffbb8eb81cd89d1ee31c7aa150e5a44ecf9e

--- source/interprocedural_analyses/taint/taintAnalysis.ml
+++ source/interprocedural_analyses/taint/taintAnalysis.ml
@@ -404,6 +404,8 @@ let run_taint_analysis
   (* Save the cache here, in case there is a model verification error. *)
   let () = Cache.save cache in
 
+  compact_ocaml_heap ~name:"before initial models";
+
   let { ModelParseResult.models = initial_models; errors = model_verification_errors; _ } =
     initialize_models
       ~scheduler
