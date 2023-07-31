fa773e1fb86481c279b4c61ece26a1d3a768d1c3
Justin Ngai <justinngai@meta.com>
2023-07-31 08:00:00 -0700

    Fix imports in `server_event_test` based on pyre style guide
    Reviewed By: kinto0
    
    Differential Revision: D47888052
    
    fbshipit-source-id: 7911aa25836dcc10b46096524f875239f6d75dab

--- client/commands/tests/server_event_test.py
+++ client/commands/tests/server_event_test.py
@@ -9,92 +9,112 @@ from typing import Type
 
 import testslide
 
-from ...language_server.connections import create_memory_text_reader
-
+from ...language_server import connections
 from ...tests import setup
-from ..server_event import (
-    create_from_string,
-    ErrorKind,
-    EventParsingException,
-    ServerException,
-    ServerInitialized,
-    ServerStartException,
-    SocketCreated,
-    Waiter,
-)
+
+from .. import server_event
 
 
 class ServerEventTest(testslide.TestCase):
     def test_error_kind(self) -> None:
-        self.assertEqual(ErrorKind.from_string("Watchman"), ErrorKind.WATCHMAN)
-        self.assertEqual(ErrorKind.from_string("BuckInternal"), ErrorKind.BUCK_INTERNAL)
-        self.assertEqual(ErrorKind.from_string("BuckUser"), ErrorKind.BUCK_USER)
-        self.assertEqual(ErrorKind.from_string("Pyre"), ErrorKind.PYRE)
-        self.assertEqual(ErrorKind.from_string("Unknown"), ErrorKind.UNKNOWN)
-        self.assertEqual(ErrorKind.from_string("derp"), ErrorKind.UNKNOWN)
+        self.assertEqual(
+            server_event.ErrorKind.from_string("Watchman"),
+            server_event.ErrorKind.WATCHMAN,
+        )
+        self.assertEqual(
+            server_event.ErrorKind.from_string("BuckInternal"),
+            server_event.ErrorKind.BUCK_INTERNAL,
+        )
+        self.assertEqual(
+            server_event.ErrorKind.from_string("BuckUser"),
+            server_event.ErrorKind.BUCK_USER,
+        )
+        self.assertEqual(
+            server_event.ErrorKind.from_string("Pyre"), server_event.ErrorKind.PYRE
+        )
+        self.assertEqual(
+            server_event.ErrorKind.from_string("Unknown"),
+            server_event.ErrorKind.UNKNOWN,
+        )
+        self.assertEqual(
+            server_event.ErrorKind.from_string("derp"), server_event.ErrorKind.UNKNOWN
+        )
 
     def test_create(self) -> None:
-        self.assertIsNone(create_from_string("derp"))
-        self.assertIsNone(create_from_string("[]"))
+        self.assertIsNone(server_event.create_from_string("derp"))
+        self.assertIsNone(server_event.create_from_string("[]"))
         self.assertEqual(
-            create_from_string('["SocketCreated", "/foo/bar"]'),
-            SocketCreated(Path("/foo/bar")),
+            server_event.create_from_string('["SocketCreated", "/foo/bar"]'),
+            server_event.SocketCreated(Path("/foo/bar")),
         )
-        self.assertIsNone(create_from_string('["SocketCreated"]'))
+        self.assertIsNone(server_event.create_from_string('["SocketCreated"]'))
         self.assertEqual(
-            create_from_string('["ServerInitialized"]'), ServerInitialized()
-        )
-        self.assertEqual(
-            create_from_string('["Exception", "Burn baby burn!"]'),
-            ServerException("Burn baby burn!"),
+            server_event.create_from_string('["ServerInitialized"]'),
+            server_event.ServerInitialized(),
         )
         self.assertEqual(
-            create_from_string('["Exception", "Burn baby burn!", ["BuckUser"]]'),
-            ServerException(message="Burn baby burn!", kind=ErrorKind.BUCK_USER),
+            server_event.create_from_string('["Exception", "Burn baby burn!"]'),
+            server_event.ServerException("Burn baby burn!"),
         )
         self.assertEqual(
-            create_from_string('["Exception", "Burn baby burn!", "derp"]'),
-            ServerException(message="Burn baby burn!", kind=ErrorKind.UNKNOWN),
+            server_event.create_from_string(
+                '["Exception", "Burn baby burn!", ["BuckUser"]]'
+            ),
+            server_event.ServerException(
+                message="Burn baby burn!", kind=server_event.ErrorKind.BUCK_USER
+            ),
         )
-        self.assertIsNone(create_from_string('["Exception"]'))
-        self.assertIsNone(create_from_string('["Exception", 42]'))
-        self.assertIsNone(create_from_string('["UNRECOGNIZABLE", "message"]'))
+        self.assertEqual(
+            server_event.create_from_string('["Exception", "Burn baby burn!", "derp"]'),
+            server_event.ServerException(
+                message="Burn baby burn!", kind=server_event.ErrorKind.UNKNOWN
+            ),
+        )
+        self.assertIsNone(server_event.create_from_string('["Exception"]'))
+        self.assertIsNone(server_event.create_from_string('["Exception", 42]'))
+        self.assertIsNone(
+            server_event.create_from_string('["UNRECOGNIZABLE", "message"]')
+        )
 
     def test_waiter(self) -> None:
         def assert_ok(event_output: str, wait_on_initialization: bool) -> None:
-            Waiter(wait_on_initialization=wait_on_initialization).wait_on(
+            server_event.Waiter(wait_on_initialization=wait_on_initialization).wait_on(
                 io.StringIO(event_output)
             )
 
         def assert_raises(
             event_output: str,
             wait_on_initialization: bool,
-            exception: Type[Exception] = EventParsingException,
+            exception: Type[Exception] = server_event.EventParsingException,
         ) -> None:
             with self.assertRaises(exception):
-                Waiter(wait_on_initialization=wait_on_initialization).wait_on(
-                    io.StringIO(event_output)
-                )
+                server_event.Waiter(
+                    wait_on_initialization=wait_on_initialization
+                ).wait_on(io.StringIO(event_output))
 
         assert_raises("garbage", wait_on_initialization=False)
         assert_raises("[]", wait_on_initialization=False)
-        assert_ok('["SocketCreated", "/path/to/socket"]', wait_on_initialization=False)
+        assert_ok(
+            '["SocketCreated", "/path/to/socket"]',
+            wait_on_initialization=False,
+        )
         assert_raises('["ServerInitialized"]', wait_on_initialization=False)
         assert_raises(
             '["Exception", "message"]',
             wait_on_initialization=False,
-            exception=ServerStartException,
+            exception=server_event.ServerStartException,
         )
 
         assert_raises("garbage", wait_on_initialization=True)
         assert_raises("[]", wait_on_initialization=True)
         assert_raises(
-            '["SocketCreated", "/path/to/socket"]', wait_on_initialization=True
+            '["SocketCreated", "/path/to/socket"]',
+            wait_on_initialization=True,
         )
         assert_raises(
             '["Exception", "message"]',
             wait_on_initialization=True,
-            exception=ServerStartException,
+            exception=server_event.ServerStartException,
         )
         assert_raises(
             '["SocketCreated", "/path/to/socket"]\n' + '["ServerException", "message"]',
@@ -113,41 +133,43 @@ class ServerEventTest(testslide.TestCase):
     @setup.async_test
     async def test_async_waiter(self) -> None:
         async def assert_ok(event_output: str, wait_on_initialization: bool) -> None:
-            await Waiter(wait_on_initialization=wait_on_initialization).async_wait_on(
-                create_memory_text_reader(event_output)
-            )
+            await server_event.Waiter(
+                wait_on_initialization=wait_on_initialization
+            ).async_wait_on(connections.create_memory_text_reader(event_output))
 
         async def assert_raises(
             event_output: str,
             wait_on_initialization: bool,
-            exception: Type[Exception] = EventParsingException,
+            exception: Type[Exception] = server_event.EventParsingException,
         ) -> None:
             with self.assertRaises(exception):
-                await Waiter(
+                await server_event.Waiter(
                     wait_on_initialization=wait_on_initialization
-                ).async_wait_on(create_memory_text_reader(event_output))
+                ).async_wait_on(connections.create_memory_text_reader(event_output))
 
         await assert_raises("garbage", wait_on_initialization=False)
         await assert_raises("[]", wait_on_initialization=False)
         await assert_ok(
-            '["SocketCreated", "/path/to/socket"]', wait_on_initialization=False
+            '["SocketCreated", "/path/to/socket"]',
+            wait_on_initialization=False,
         )
         await assert_raises('["ServerInitialized"]', wait_on_initialization=False)
         await assert_raises(
             '["Exception", "message"]',
             wait_on_initialization=False,
-            exception=ServerStartException,
+            exception=server_event.ServerStartException,
         )
 
         await assert_raises("garbage", wait_on_initialization=True)
         await assert_raises("[]", wait_on_initialization=True)
         await assert_raises(
-            '["SocketCreated", "/path/to/socket"]', wait_on_initialization=True
+            '["SocketCreated", "/path/to/socket"]',
+            wait_on_initialization=True,
         )
         await assert_raises(
             '["Exception", "message"]',
             wait_on_initialization=True,
-            exception=ServerStartException,
+            exception=server_event.ServerStartException,
         )
         await assert_raises(
             '["SocketCreated", "/path/to/socket"]\n' + '["ServerException", "message"]',
