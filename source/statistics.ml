1820fb9106fc89ba8b3cf5e7530bb6479277879f
Jason Tatton <jat@meta.com>
2023-08-29 12:44:15 -0700

    Add Module docstrings - statistics.ml
    Summary: Add Module docstrings for BE day
    
    Reviewed By: MaggieMoss
    
    Differential Revision: D48417070
    
    fbshipit-source-id: ede034a6d626a2b26579871eea0675d4bc02ac3d

--- source/statistics.ml
+++ source/statistics.ml
@@ -5,7 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  *)
 
-(* TODO(T132410158) Add a module-level doc comment. *)
+(* Loggers of performance and other notworthy events which we wish to persist to scuba via
+   perfpipe. *)
 
 open Core
 open Pyre
