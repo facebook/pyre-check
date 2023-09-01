b65a82c26415bbc085e4527ce0d740b5f2047ea9
Alex Kassil <afk@meta.com>
2023-08-31 21:20:02 -0700

    Demonstrate False Negative with Bound TypeVar
    Summary: Simple false negative
    
    Reviewed By: arthaud
    
    Differential Revision: D48850889
    
    fbshipit-source-id: a8abda77d99421177a2f512f718a5d8fdb10bbf1

---
+++ source/interprocedural_analyses/taint/test/integration/typevar.py
@@ -0,0 +1,18 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from builtins import _test_sink
+from typing import TypeVar
+
+
+T = TypeVar("T", bound="Foo")
+
+class Foo:
+    def __init__(self, tainted: str) -> None:
+        self.tainted: str = tainted
+
+def issue(foo: T) -> T:
+    _test_sink(foo.tainted) # TODO(T162475681): False Negative for Bound TypeVar
+    return foo
