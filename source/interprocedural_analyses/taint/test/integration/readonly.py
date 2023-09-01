549001949fda47939a2af3a2ce356f15e0f69d64
Alex Kassil <afk@meta.com>
2023-08-31 21:20:02 -0700

    Demonstrate false negative with `ReadOnly` on a class
    Summary:
    The issue is our model
    
    ```
    readonly.Foo.tainted: TaintSource[Test] = ...
    ```
    Will not match the type of `ReadOnly[Foo]` due to the ReadOnly wrapper on the type level.
    ```
    2023-08-30 10:49:06 DUMP Building call graph of statement: `_test_sink($parameter$foo.tainted)`
    2023-08-30 10:49:06 DUMP Checking if `tainted` is an attribute, property or global variable. Resolved type for base `$parameter$foo` is `pyre_extensions.ReadOnly[readonly.Foo]`
    2023-08-30 10:49:06 DUMP Resolved callees for expression `$parameter$foo.tainted`:
    { call = None;
      attribute_access =
      (Some { property_targets = []; global_targets = []; is_attribute = true });
      identifier = None; string_format = None }
    2023-08-30 10:49:06 DUMP Resolving function call `_test_sink($parameter$foo.tainted)`
    2023-08-30 10:49:06 DUMP Checking if `$parameter$foo.tainted` is a callable, resolved type is `pyre_extensions.ReadOnly[str]`
    ```
    
    One solution is to just always ignore `ReadOnly`
    
    Reviewed By: arthaud
    
    Differential Revision: D48836232
    
    fbshipit-source-id: 371d60d8ce45043b4740cb0fc6934ea4e7d597c1

--- source/interprocedural_analyses/taint/test/integration/readonly.py
+++ source/interprocedural_analyses/taint/test/integration/readonly.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from builtins import _test_sink
+from builtins import _test_source, _test_sink
 from typing import Any, MutableMapping
 from typing_extensions import Self
 from pyre_extensions import ReadOnly
@@ -34,3 +34,34 @@ class A:
         self
     ) -> None:
         _test_sink(self.mapping.get(""))
+
+    def readonly_tito(self, x: ReadOnly[str]):
+        return x
+
+
+def readonly_tito():
+    a = A()
+    x = a.readonly_tito(_test_source())
+    _test_sink(x)
+
+
+class Foo:
+    tainted: str = ""
+    not_tainted: str = ""
+
+
+def readonly_foo_tainted(foo: ReadOnly[Foo]) -> None:
+    # TODO(T162446777): ReadOnly False Negative
+    _test_sink(foo.tainted)
+
+
+def readonly_foo_not_tainted(foo: ReadOnly[Foo]) -> None:
+    _test_sink(foo.not_tainted)
+
+
+def regular_foo_tainted(foo: Foo) -> None:
+    _test_sink(foo.tainted)
+
+
+def regular_foo_not_tainted(foo: Foo) -> None:
+    _test_sink(foo.not_tainted)
