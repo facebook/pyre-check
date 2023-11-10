6da1d41bf5529059787110ca532ab34636836ce1
Steven Troxler <stroxler@meta.com>
2023-11-10 10:48:21 -0800

    Refactor AddTransform logic
    Summary:
    In order to reuse all of the logic for tracking name and finding
    the parent class or module - which we will want to do for
    "delete" and "replace" actions just as for "add" - this commit
    extracts that boilerplate into the `PatchTransform` base
    transform class.
    
    This is a simple refactor based on inheritance and a smallish
    code change; a better solution would actually be to use dependency
    injection for transforming the parent body which is what I'll
    do in the next diff.
    
    Reviewed By: grievejia
    
    Differential Revision: D51045161
    
    fbshipit-source-id: 3c443317f5851d79e30cb0412cc49e0e066fbd2a

--- tools/typeshed_patcher/transforms.py
+++ tools/typeshed_patcher/transforms.py
@@ -31,14 +31,6 @@ class PatchTransform(libcst.codemod.ContextAwareTransformer):
 
     CONTEXT_KEY = "PatchTransform"
 
-    def __init__(self) -> None:
-        super().__init__(libcst.codemod.CodemodContext())
-
-
-class AddTransform(PatchTransform):
-
-    statements_to_add: Sequence[libcst.BaseStatement]
-    add_position: patch.AddPosition
     parent: str
     current_names: list[str]
     found_parent: bool
@@ -46,23 +38,54 @@ class AddTransform(PatchTransform):
     def __init__(
         self,
         parent: patch.QualifiedName,
-        content: str,
-        add_position: patch.AddPosition,
     ) -> None:
-        super().__init__()
-        # Data determining what to transform
-        self.statements_to_add = statements_from_content(content)
-        self.add_position = add_position
+        super().__init__(libcst.codemod.CodemodContext())
+        # All of the actions take a parent
         self.parent = parent.to_string()
         # Tracking visitor state
-        self.current_names: list[str] = []
-        self.found_parent = self.parent == ""
+        self.current_names = []
+        self.found_parent = False
 
     def visit_ClassDef(
         self,
         node: libcst.ClassDef,
     ) -> None:
         self.current_names.append(node.name.value)
+
+    def get_current_name(self) -> str:
+        return ".".join(self.current_names)
+
+    def pop_current_name(self) -> str:
+        this_name = self.get_current_name()
+        self.current_names.pop()
+        return this_name
+
+    def is_parent(self, current_name: str) -> bool:
+        if current_name == self.parent:
+            if self.found_parent:
+                raise ValueError(f"Encountered two classes with name {self.parent}")
+            else:
+                self.found_parent = True
+            return True
+        else:
+            return False
+
+
+class AddTransform(PatchTransform):
+
+    statements_to_add: Sequence[libcst.BaseStatement]
+    add_position: patch.AddPosition
+
+    def __init__(
+        self,
+        parent: patch.QualifiedName,
+        content: str,
+        add_position: patch.AddPosition,
+    ) -> None:
+        super().__init__(parent=parent)
+        # Data determining what to transform
+        self.statements_to_add = statements_from_content(content)
+        self.add_position = add_position
 
     def extend_body(
         self, body: Sequence[libcst.BaseStatement]
@@ -80,65 +103,65 @@ class AddTransform(PatchTransform):
         else:
             raise RuntimeError(f"Unexpected add_position value {self.add_position}")
 
-    def get_current_name_and_pop(self) -> str:
-        this_name = ".".join(self.current_names)
-        self.current_names.pop()
-        return this_name
+    def transform_parent_class(
+        self,
+        node: libcst.ClassDef,
+    ) -> libcst.ClassDef:
+        """
+        Add statements to a class body.
 
-    def should_modify_class(self, current_name: str) -> bool:
-        if current_name == self.parent:
-            if self.found_parent:
-                raise ValueError(f"Encountered two classes with name {self.parent}")
-            else:
-                self.found_parent = True
-            return True
+        This update code has to cope with LibCST's need to to distinguish
+        between indented vs non-indented bodies. As a result, there are two
+        layers of bodies to deal with. Moreover, if the outer body is not
+        indented then we will need to coerce the body to be indented
+        before adding statements.
+        """
+        outer_body = node.body
+        if isinstance(outer_body, libcst.IndentedBlock):
+            new_outer_body = outer_body.with_changes(
+                body=self.extend_body(outer_body.body)
+            )
         else:
-            return False
+            inner_body_as_base_statements = [
+                statement
+                if isinstance(statement, libcst.BaseStatement)
+                else libcst.SimpleStatementLine(body=[statement])
+                for statement in outer_body.body
+            ]
+            new_outer_body = libcst.IndentedBlock(
+                body=self.extend_body(inner_body_as_base_statements)
+            )
+        return node.with_changes(body=new_outer_body)
 
     def leave_ClassDef(
         self,
         original_node: libcst.ClassDef,
         updated_node: libcst.ClassDef,
     ) -> libcst.ClassDef:
-        current_name = self.get_current_name_and_pop()
-        if self.should_modify_class(current_name):
-            # This update code has to cope with LibCST's need to to distinguish
-            # between indented vs non-indented bodies. As a result, there are
-            # two layers of bodies to deal with, and if the outer body is not
-            # indented then we will need to coerce the body to be indented
-            # before adding statements.
-            outer_body = updated_node.body
-            if isinstance(outer_body, libcst.IndentedBlock):
-                new_outer_body = outer_body.with_changes(
-                    body=self.extend_body(outer_body.body)
-                )
-            else:
-                inner_body_as_base_statements = [
-                    statement
-                    if isinstance(statement, libcst.BaseStatement)
-                    else libcst.SimpleStatementLine(body=[statement])
-                    for statement in outer_body.body
-                ]
-                new_outer_body = libcst.IndentedBlock(
-                    body=self.extend_body(inner_body_as_base_statements)
-                )
-            return updated_node.with_changes(body=new_outer_body)
+        current_name = self.pop_current_name()
+        if self.is_parent(current_name):
+            return self.transform_parent_class(updated_node)
         else:
             return updated_node
+
+    def transform_parent_module(
+        self,
+        node: libcst.Module,
+    ) -> libcst.Module:
+        return node.with_changes(body=self.extend_body(node.body))
 
     def leave_Module(
         self,
         original_node: libcst.Module,
         updated_node: libcst.Module,
     ) -> libcst.Module:
-        if self.parent == "":
-            return updated_node.with_changes(body=self.extend_body(updated_node.body))
+        if self.is_parent(self.get_current_name()):
+            out = self.transform_parent_module(updated_node)
         else:
-            if not self.found_parent:
-                raise ValueError(
-                    "Could not find class with name {}".format(self.parent)
-                )
-            return updated_node
+            out = updated_node
+        if not self.found_parent:
+            raise ValueError(f"Did not find any classes matching {self.parent}")
+        return out
 
 
 def run_transform(code: str, transform: PatchTransform) -> str:
