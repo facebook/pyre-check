8f86fcba9eb9d6bd9f96272aef2355d86fc080d0
Alex Kassil <afk@meta.com>
2023-11-14 04:17:41 -0800

    Closure test more tests and task cleanup
    Summary: Similar to the suite of closure read tests, let's have some better write tests
    
    Reviewed By: arthaud
    
    Differential Revision: D50925924
    
    fbshipit-source-id: ebf58a85f02d26f29b91e7662b315f49a111cbc4

--- source/interprocedural_analyses/taint/test/integration/closure.py
+++ source/interprocedural_analyses/taint/test/integration/closure.py
@@ -6,7 +6,7 @@
 from builtins import _test_sink, _test_source
 
 
-def side_effect_reduction_closure():
+def nonlocal_closure_read_reduction():
     x = _test_source()
     z = _test_source()
 
@@ -65,6 +65,26 @@ def parameter_order_swap_different_variable_names(x, y, z):
     inner()
 
 
+def nonlocal_closure_multiple_reads():
+    x = _test_source()
+
+    def conditional_read(conditional):
+        if conditional:
+            _test_sink(x)
+        else:
+            _test_sink(0)
+    conditional_read()
+
+    def overread():
+        y = x
+        y = 0
+        _test_sink(y)
+    overread()
+
+    x = 0
+    conditional_read()
+
+
 class Object:
     pass
 
@@ -82,7 +102,7 @@ def closure():
 
 
 def closure_flow():
-    # TODO(T145247918): False Negative
+    # TODO(T168869049): False Negative
     source, sink = closure()
     source()
     sink()
@@ -94,7 +114,103 @@ def closure_no_flow():
     source()
 
 
-def nonlocal_closure():
+def nonlocal_closure_write_reduction():
+    # TODO(T168868911): FN Nonlocal closure write
+    x = ""
+    z = ""
+
+    def source():
+        nonlocal x
+        x = _test_source()
+        return _test_source()
+
+    z = source()
+    _test_sink(x)
+    _test_sink(z)
+
+
+def nonlocal_closure_reduction():
+    # TODO(T168868911): FN Nonlocal closure write
+    obj1 = ""
+
+    def source():
+        nonlocal obj1
+        obj1 = _test_source()
+        obj2 = _test_source()
+        return obj2
+
+    def sink(obj2):
+        _test_sink(obj1)
+        _test_sink(obj2)
+
+    obj2 = source()
+    sink(obj2)
+
+
+def nonlocal_closure_multiple_writes():
+    x = 1
+
+    def conditional_write(condition):
+        nonlocal x
+        if condition:
+            x = _test_source()
+        else:
+            x = 0
+    # Note: The truthiness of the conditional isn't actually used
+    conditional_write(True)
+    # TODO(T168868911): FN Nonlocal closure write
+    _test_sink(x)
+
+    def overwrite():
+        nonlocal x
+        x = _test_source()
+        x = 0
+    x = 1
+    overwrite()
+    _test_sink(x)
+
+
+def nonlocal_closure_flow():
+    # TODO(T168868911): FN Nonlocal closure write
+    obj = ""
+
+    def source():
+        nonlocal obj
+        obj = _test_source()
+
+    def sink():
+        _test_sink(obj)
+
+    source()
+    sink()
+
+
+def nonlocal_closure_no_flow():
+    obj = ""
+
+    def source():
+        nonlocal obj
+        obj = _test_source()
+
+    def sink():
+        _test_sink(obj)
+
+    sink()
+    source()
+
+
+def nonlocal_closure_inner_flow():
+    obj = ""
+
+    def flow():
+        nonlocal obj
+        obj = _test_source()
+        _test_sink(obj)
+
+    flow()
+
+
+def nonlocal_closure_obscure():
     obj = ""
 
     def source():
@@ -107,14 +223,15 @@ def nonlocal_closure():
     return source, sink
 
 
-def nonlocal_flow():
-    # TODO(T145247918): False Negative
-    source, sink = nonlocal_closure()
+def nonlocal_obscure_flow():
+    # TODO(T168868830): FN due to not knowing the returned functions match
+    # the models of functions defined in nonlocal_closure_obscure
+    source, sink = nonlocal_closure_obscure()
     source()
     sink()
 
 
-def nonlocal_no_flow():
-    source, sink = nonlocal_closure()
+def nonlocal_obscure_no_flow():
+    source, sink = nonlocal_closure_obscure()
     sink()
     source()
