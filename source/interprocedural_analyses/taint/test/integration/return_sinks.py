57b4d0ae0221cbe21f7c3f98827aff78b11f3822
Tianhan Lu <tianhanlu@meta.com>
2023-11-16 11:15:41 -0800

    Demonstrate not propagating sink that is generated from a function call's return
    Reviewed By: arthaud
    
    Differential Revision: D51363838
    
    fbshipit-source-id: 718ea33e6fef20d1fd2bba0235604ad9abf28bd3

---
+++ source/interprocedural_analyses/taint/test/integration/return_sinks.py
@@ -0,0 +1,30 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from builtins import _test_sink
+
+
+def sink_on_return():
+    # Mark the expression used in a return statement as a sink
+    return 0
+
+
+def sink_on_return_not_propagated_to_arg(arg):
+    # We do not expect the sink to be propagated
+    arg = sink_on_return()
+
+
+def sink_on_return_not_propagated_to_return():
+    # We do not expect the sink to be propagated
+    return sink_on_return()
+
+
+def sink_on_return_and_tito(arg):
+    return arg
+
+
+def propagate_returned_sink_to_arg(arg):
+    # Sink is first generated and then propagated to arg
+    sink_on_return_and_tito(arg)
