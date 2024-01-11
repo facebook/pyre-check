d3e7669341290fa453bd930024593615606f3f07
Steven Troxler <stroxler@meta.com>
2024-01-11 08:30:39 -0800

    Fix a bad copy-pasted name in `SourcePath.ml`
    Summary:
    The SourcePath.t and ArtifactPath.t types currently
    are exactly the same representation (they are different
    in *meaning*, but the code is the same).
    
    We actually created them by defining it and copy-pasting
    the implementation.
    
    This is mostly not a problem, but I forgot to rename
    the data constructor which is very confusing!!
    
    Fixing that :)
    
    Also fixing license formatting; a codemod to add warning
    suppressions is making the linter angry.
    
    Reviewed By: inseokhwang
    
    Differential Revision: D52672026
    
    fbshipit-source-id: f86ded5b258273e497115d930e519b417a26a982

--- source/sourcePath.ml
+++ source/sourcePath.ml
@@ -4,16 +4,17 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *)
+
 (* TODO(T170743593) new warning with ppx_conv_sexp.v0.16.X *)
 [@@@warning "-name-out-of-scope"]
 
 (* TODO(T132410158) Add a module-level doc comment. *)
 
-type t = ArtifactPath of PyrePath.t [@@deriving sexp, compare, hash, show]
+type t = SourcePath of PyrePath.t [@@deriving sexp, compare, hash, show]
 
-let create raw = ArtifactPath raw
+let create raw = SourcePath raw
 
-let raw (ArtifactPath raw) = raw
+let raw (SourcePath raw) = raw
 
 module Event = struct
   module Kind = struct
