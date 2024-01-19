dec83d9c4ea73e100ac85b23b11c6963f3dc503e
Tianhan Lu <tianhanlu@meta.com>
2024-01-19 12:08:56 -0800

    Add an integration test for unresolved calls to @abstractclassmethod
    Summary:
    The changes in *.models files are mainly due to no longer using the hardcoded
    models.  Instead, the models are provided by the newly added *.pysa file.
    
    Reviewed By: alexkassil
    
    Differential Revision: D52812715
    
    fbshipit-source-id: 65523e94dcaa2522889e4c69bf7c0a27aef3e893

--- source/interprocedural_analyses/taint/test/integration/class_methods.py
+++ source/interprocedural_analyses/taint/test/integration/class_methods.py
@@ -4,6 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 
 from builtins import _test_sink, _test_source
+from abc import abstractclassmethod
+from typing import TypeVar, Generic
 
 
 class Test:
@@ -14,3 +16,31 @@ class Test:
 
 def bar():
     Test.foo(_test_source())
+
+
+TInput = TypeVar("TInput")
+
+
+class C(Generic[TInput]):
+    @abstractclassmethod
+    def abstract_class_method(cls, arg):
+        _test_sink(arg)
+
+    @classmethod
+    def one_hop_abstract_class_method(cls, arg):
+        # Expect sink on `arg`
+        cls.abstract_class_method(arg)
+
+    @classmethod
+    def class_method(cls, arg):
+        _test_sink(arg)
+
+    @classmethod
+    def one_hop_class_method(cls, arg):
+        # Expect sink on `arg`
+        cls.class_method(arg)
+
+
+def issue_with_abstract_class_method():
+    C.one_hop_abstract_class_method(_test_source())  # Expect an issue
+    C.one_hop_class_method(_test_source())  # Expect an issue
