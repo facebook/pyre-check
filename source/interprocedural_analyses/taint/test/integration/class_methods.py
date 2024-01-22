89789b3a72f4174d6dae42ef7642fd33c07fcb56
Tianhan Lu <tianhanlu@meta.com>
2024-01-22 12:50:24 -0800

    Add more tests related with @property and @abstractmethod
    Reviewed By: arthaud
    
    Differential Revision: D52920201
    
    fbshipit-source-id: a2e0aa2884f7e63d8c8cb13dcb80f3339f826942

--- source/interprocedural_analyses/taint/test/integration/class_methods.py
+++ source/interprocedural_analyses/taint/test/integration/class_methods.py
@@ -65,6 +65,16 @@ class C(Generic[TInput]):
         # Currently false negative
         cls.static_abstractmethod(arg)  # Expect sink on `arg`
 
+    @property
+    @abstractmethod
+    def my_abstract_property(self):
+        pass
+
+    @my_abstract_property.setter
+    @abstractmethod
+    def my_abstract_property(self, my_abstract_property):
+        pass
+
 
 class D(C[str]):
     @classmethod
@@ -78,10 +88,26 @@ class D(C[str]):
     def class_abstractmethod(cls, arg):
         _test_sink(arg)
 
+    @property
+    def my_abstract_property(self):
+        return self._my_abstract_property
+
+    @my_abstract_property.setter
+    def my_abstract_property(self, my_abstract_property):
+        self._my_abstract_property = my_abstract_property
+
 
 def issue_with_abstract_class_method():
     D.one_hop_abstract_class_method(_test_source())  # Expect an issue
     D.one_hop_class_method(_test_source())  # Expect an issue
-    D.one_hop_abstract_method(_test_source())  # Expect an issue. Currently false negative.
+    D.one_hop_abstract_method(
+        _test_source()
+    )  # Expect an issue. Currently false negative.
     D.one_hop_class_abstractmethod(_test_source())  # Expect an issue
     D.one_hop_static_abstractmethod(_test_source())  # Expect an issue.
+
+
+def issue_with_abstract_property():
+    d = D()
+    d.my_abstract_property = _test_source()
+    _test_sink(d.my_abstract_property)  # Expect an issue
