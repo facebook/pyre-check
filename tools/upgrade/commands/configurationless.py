ddb8b07087115006f719c39e4935f48211adcae9
Conner Nilsen <connernilsen@meta.com>
2024-02-01 16:53:58 -0800

    Create default Pyre Upgrade command for Configurationless
    Summary:
    Creates a basic `Command` with the CLI args we'll need (at least for the first version of the script). This file follows the (super basic) outline that all `Command`s follow so far.
    
    Upcoming diffs will build out the remaining functionality needed to push Pyre modes down into files (when necessary).
    
    **This Diff Stack** implements functionality to begin migrating to a configurationless setup required for the [Per-Target architecture changes](https://fb.workplace.com/groups/295311271085134/permalink/1383305655619018/) that will be coming soon and the [Global Lazy IDE Type Checker](https://fb.workplace.com/groups/657557118131740/permalink/1396168097603968/) that we're in the process of rolling out (slowly) today. The goal of this diff is to begin pushing Pyre modes (`# pyre-strict`, `# pyre-unsafe`, and `# pyre-ignore-all-errors`) into files so that we can deprecate those options from .pyre_configuration.local files.
    
    Reviewed By: grievejia
    
    Differential Revision: D53288762
    
    fbshipit-source-id: b1f2d0052cc8374adf03776120bc3014aa55ce32

---
+++ tools/upgrade/commands/configurationless.py
@@ -0,0 +1,58 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import logging
+from pathlib import Path
+from typing import List
+
+from ..repository import Repository
+from .command import Command
+
+LOG: logging.Logger = logging.getLogger(__name__)
+
+
+class Configurationless(Command):
+    def __init__(
+        self, *, repository: Repository, path: Path, includes: List[str], commit: bool
+    ) -> None:
+        super().__init__(repository)
+        self._path: Path = path
+        self._includes: List[str] = includes
+        self._commit: bool = commit
+
+    @staticmethod
+    def from_arguments(
+        arguments: argparse.Namespace, repository: Repository
+    ) -> "Configurationless":
+        return Configurationless(
+            repository=repository,
+            path=arguments.path,
+            includes=arguments.include_file_suffixes,
+            commit=(not arguments.no_commit),
+        )
+
+    @classmethod
+    def add_arguments(cls, parser: argparse.ArgumentParser) -> None:
+        super(Configurationless, cls).add_arguments(parser)
+        parser.set_defaults(command=cls.from_arguments)
+        parser.add_argument(
+            "path",
+            help="Path to project root with local configuration.",
+            type=Path,
+        )
+        parser.add_argument(
+            "--no-commit",
+            help="Do not commit changes after completing codemod.",
+            action="store_true",
+        )
+        parser.add_argument(
+            "--include-file-suffixes",
+            action="extend",
+            nargs="+",
+            type=str,
+            default=["**.py"],
+            help="The suffixes to search for and include in the codemod. Default is '**.py'.",
+        )
