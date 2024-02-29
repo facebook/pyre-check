033161d5cefaa4d2ac9a1c76cf1332ca91bb1a35
Alex Kassil <afk@meta.com>
2024-02-29 09:39:52 -0800

    Tests with models and literal features
    Summary: More tests demonstrating adding on features through flows.
    
    Reviewed By: gbleaney
    
    Differential Revision: D54261096
    
    fbshipit-source-id: 9852254deb40e917d190d410ef95f9ccdd82be5b

--- source/interprocedural_analyses/taint/test/integration/closure_models.py
+++ source/interprocedural_analyses/taint/test/integration/closure_models.py
@@ -8,6 +8,69 @@ from builtins import _test_sink, _test_source
 
 def inner_sink_wrapper():
     def inner_sink(x):
-        ...
+        pass
     # TODO(T179041949): Find flow through inner function model
     inner_sink(_test_source())
+
+
+class Reclassification:
+    def __init__(self, inner, feature):
+        self.inner = inner
+        self.feature = feature
+
+    def reclassify(self, feature):
+        self.feature = feature
+        return self.inner()
+
+
+def reclassify(inner, feature) -> Reclassification:
+    pass
+
+
+def outer():
+    pass
+
+
+def use_source(x):
+    pass
+
+
+def test() -> None:
+    value = "benign"
+
+    def return_taint():
+        return _test_source()
+
+    def return_model_taint():
+        return value
+
+    # TODO(T180807435): Model flow from source to return of `return_model_taint_tito`
+    source = _test_source()
+    def return_model_taint_tito():
+        use_source(source)
+
+    value = reclassify(
+        inner=return_taint,
+        feature="breadcrumb1",
+    )
+
+    value.reclassify(feature="breadcrumb2")
+
+    value = reclassify(
+        inner=return_model_taint,
+        feature="breadcrumb1",
+    )
+    value.reclassify(feature="breadcrumb2")
+
+    value = reclassify(
+        inner=outer,
+        feature="breadcrumb1",
+    )
+    value.reclassify(feature="breadcrumb2")
+
+    # TODO(T180807435): Model flow from source to return of `return_model_taint_tito`
+    value = reclassify(
+        inner=return_model_taint_tito,
+        feature="breadcrumb1",
+    )
+    value.reclassify(feature="breadcrumb2")
