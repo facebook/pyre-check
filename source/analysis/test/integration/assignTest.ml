0546c2135e192b9ea65a32c0b12a168c4b393666
Steven Troxler <stroxler@meta.com>
2024-04-25 11:10:44 -0700

    Add test case documenting assignment bug
    Summary:
    In the previous diff I wrote a detailed domment describing
    `forward_assign`, and I also noticed a bug in multiple assignments.
    
    The bug has to be fixed as part of introducing subscripts to Pyre's
    AST, so I want to have a test case exercising the bug before I get
    started.
    
    Reviewed By: grievejia
    
    Differential Revision: D56519571
    
    fbshipit-source-id: afa9609649116589679315699fdb5336388d60f6

--- source/analysis/test/integration/assignTest.ml
+++ source/analysis/test/integration/assignTest.ml
@@ -575,6 +575,16 @@ let test_check_assign =
              "Incompatible variable type [9]: y is declared to have type `Never` but is used as \
               type `int`.";
            ];
+      (* TODO(T146934909) This test case documents a bug: Pyre fails to check subscripted
+         assignments inside of multiple-target left-hand-sides. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
+              x: dict[str, int] = ...
+
+              x["key"], y = "whoops this should be an int", 42
+            |}
+           [];
     ]
 
 
