b35a1fa12ec28fae50a6cff1214a0c7c642e32ce
Steven Troxler <stroxler@meta.com>
2024-04-25 11:10:44 -0700

    Add a doc comment to pyreMenhirParser
    Summary:
    It's nice to put some context here, in particular making it
    clear that this is not usually the parser we want to use.
    
    Reviewed By: yangdanny97
    
    Differential Revision: D56530257
    
    fbshipit-source-id: 3eb18bf3ccf7ad90c99b126af2e86b3043040df5

--- source/menhir_parser/parser.ml
+++ source/menhir_parser/parser.ml
@@ -5,7 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  *)
 
-(* TODO(T132410158) Add a module-level doc comment. *)
+(* This module provides the top-level interface to Pyre's original Menhir parser. Pyre mostly uses
+   other parsers (either an ocaml wrapper around the CPython parser, or the error-recovering Errpy
+   parser), but Pysa models still use this parser and for the moment there are still a few places
+   where Pyre uses this parser to parse individual expressions *)
 
 open Core
 open Pyre
