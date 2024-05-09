1e8e59fe8c57a48e3f23202aa6dcfdf05e773b2e
Sam Goldman <samgoldman@meta.com>
2024-05-09 09:43:53 -0700

    Remove unused sysinfo C stubs
    Summary:
    None of this is used. It's kind of a grab-bag of stuff, but we don't care about
    any of it.
    
    Reviewed By: grievejia
    
    Differential Revision: D57138066
    
    fbshipit-source-id: 16d2b5e1f0349329c75b3f72addbb63f8a0aac94

--- source/hack_parallel/hack_parallel/utils/sys_utils.ml
+++ source/hack_parallel/hack_parallel/utils/sys_utils.ml
@@ -11,18 +11,11 @@ let setsid =
   (* Not implemented on Windows. Let's just return the pid *)
   if Sys.win32 then Unix.getpid else Unix.setsid
 
-external get_total_ram : unit -> int = "hh_sysinfo_totalram"
-external uptime : unit -> int = "hh_sysinfo_uptime"
 external nproc: unit -> int = "nproc"
 
-let total_ram = get_total_ram ()
 let nbr_procs = nproc ()
 
 external set_priorities : cpu_priority:int -> io_priority:int -> unit =
   "hh_set_priorities"
-
-external pid_of_handle: int -> int = "pid_of_handle"
-external handle_of_pid_for_termination: int -> int =
-  "handle_of_pid_for_termination"
 
 let terminate_process pid = Unix.kill pid Sys.sigkill
