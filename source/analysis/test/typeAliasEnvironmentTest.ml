16fca49908e3216fbeafca8406f112dade18743f
Steven Troxler <stroxler@meta.com>
2024-05-11 07:48:29 -0700

    Split test cases (where it's easy) in typeAliasEnvironmentTest
    Summary:
    This diff is motivated by the need to debug alias environment issues that
    popped up during flipping Pyre to use Subscript nodes, but it's a good
    BE fix in general - easier test isolation means much better iteration
    cycles.
    
    The update tests are serial and share a project, so those remain one long
    chain of assertions and likely need to stay this way.
    
    Reviewed By: yangdanny97
    
    Differential Revision: D57189746
    
    fbshipit-source-id: 524255fb450291f44f9781ffb77724c2794cc905

--- source/analysis/test/typeAliasEnvironmentTest.ml
+++ source/analysis/test/typeAliasEnvironmentTest.ml
@@ -12,8 +12,8 @@ open Analysis
 open Pyre
 open Test
 
-let test_simple_registration context =
-  let assert_registers source name expected =
+let test_simple_registration =
+  let assert_registers source name expected context =
     let project = ScratchProject.setup ["test.py", source] ~include_typeshed_stubs:false ~context in
     let read_only =
       ScratchProject.errors_environment project
@@ -23,40 +23,56 @@ let test_simple_registration context =
     let printer v = v >>| Type.show_alias |> Option.value ~default:"none" in
     assert_equal ~printer expected (TypeAliasEnvironment.ReadOnly.get_type_alias read_only name)
   in
-  assert_registers {|
-    class C:
-      pass
-    X = C
-  |} "test.X" (Some "test.C");
-  assert_registers {|
-    class D:
-      pass
-    X = D
-    Y = X
-  |} "test.Y" (Some "test.D");
-  assert_registers
-    {|
-    class E:
-      pass
-    X = E
-    Y = X
-    Z = Y
-  |}
-    "test.Z"
-    (Some "test.E");
-  assert_registers {|
-    X = Z
-    Y = X
-    Z = Y
-  |} "test.Z" None;
-  assert_registers {|
-    x = None
-  |} "test.x" None;
-  ()
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+          class C:
+            pass
+          X = C
+        |}
+           "test.X"
+           (Some "test.C");
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+          class D:
+            pass
+          X = D
+          Y = X
+        |}
+           "test.Y"
+           (Some "test.D");
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+          class E:
+            pass
+          X = E
+          Y = X
+          Z = Y
+        |}
+           "test.Z"
+           (Some "test.E");
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+          X = Z
+          Y = X
+          Z = Y
+        |}
+           "test.Z"
+           None;
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers {|
+          x = None
+        |} "test.x" None;
+    ]
 
 
-let test_harder_registrations context =
-  let assert_registers ?external_sources ~expected_alias source name =
+let test_harder_registrations =
+  let assert_registers ?external_sources ~expected_alias source name context =
     let project = ScratchProject.setup ?external_sources ["test.py", source] ~context in
     let read_only =
       ScratchProject.errors_environment project
@@ -81,214 +97,251 @@ let test_harder_registrations context =
     let expected_alias = expected >>| fun alias -> Type.TypeAlias alias in
     assert_registers ~expected_alias source name
   in
-  parsed_assert_registers {|
-    X = int
-  |} "test.X" (Some "int");
-  parsed_assert_registers
-    {|
-    from typing import Tuple
-    X = int
-    Y = Tuple[X, X]
-  |}
-    "test.Y"
-    (Some "typing.Tuple[int, int]");
-  parsed_assert_registers
-    {|
-    from typing import Tuple, List
-    B = int
-    A = List[B]
-    Z = Tuple[A, B]
-  |}
-    "test.Z"
-    (Some "typing.Tuple[typing.List[int], int]");
-  unparsed_assert_registers
-    {|
-    from mypy_extensions import TypedDict
-    X = int
-    class Q(TypedDict):
-      a: X
-  |}
-    "test.Q"
-    (* TypedDicts are treated as proper classes, not aliases. *)
-    None;
-  parsed_assert_registers {|
-    class Foo: ...
-    X = Foo[unknown.get("key")]
-  |} "test.X" None;
-  (* Don't treat a global string assignment as an alias unless it is marked as `TypeAlias`. *)
-  assert_registers {|
-    X = int
-    Y = "X"
-  |} "test.Y" ~expected_alias:None;
-  assert_registers
-    {|
-    import typing
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ parsed_assert_registers {|
+          X = int
+        |} "test.X" (Some "int");
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ parsed_assert_registers
+           {|
+          from typing import Tuple
+          X = int
+          Y = Tuple[X, X]
+        |}
+           "test.Y"
+           (Some "typing.Tuple[int, int]");
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ parsed_assert_registers
+           {|
+          from typing import Tuple, List
+          B = int
+          A = List[B]
+          Z = Tuple[A, B]
+        |}
+           "test.Z"
+           (Some "typing.Tuple[typing.List[int], int]");
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ unparsed_assert_registers
+           {|
+          from mypy_extensions import TypedDict
+          X = int
+          class Q(TypedDict):
+            a: X
+        |}
+           "test.Q"
+           (* TypedDicts are treated as proper classes, not aliases. *)
+           None;
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ parsed_assert_registers
+           {|
+          class Foo: ...
+          X = Foo[unknown.get("key")]
+        |}
+           "test.X"
+           None;
+      (* Don't treat a global string assignment as an alias unless it is marked as `TypeAlias`. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+          X = int
+          Y = "X"
+        |}
+           "test.Y"
+           ~expected_alias:None;
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+          import typing
 
-    X = int
-    Y: typing_extensions.TypeAlias = "X"
-  |}
-    "test.Y"
-    ~expected_alias:(Some (Type.TypeAlias Type.integer));
+          X = int
+          Y: typing_extensions.TypeAlias = "X"
+        |}
+           "test.Y"
+           ~expected_alias:(Some (Type.TypeAlias Type.integer));
+      (* Recursive alias. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+            from typing import Tuple, Union
 
-  (* Recursive alias. *)
-  assert_registers
-    {|
-      from typing import Tuple, Union
-
-      Tree = Union[int, Tuple["Tree", "Tree"]]
-    |}
-    "test.Tree"
-    ~expected_alias:
-      (Some
-         (Type.TypeAlias
-            (Type.RecursiveType.create
-               ~name:"test.Tree"
-               ~body:
-                 (Type.union
-                    [
-                      Type.integer;
-                      Type.tuple [Type.Primitive "test.Tree"; Type.Primitive "test.Tree"];
-                    ]))));
-  (* Forbid directly-recursive aliases. *)
-  assert_registers {|
-      Tree = "Tree"
-    |} "test.Tree" ~expected_alias:None;
-  assert_registers
-    {|
-      from typing import Union
-
-      X = Union[int, "X"]
-    |}
-    "test.X"
-    ~expected_alias:None;
-  assert_registers
-    {|
-      from typing import Annotated
-
-      X = Annotated["X", int]
-    |}
-    "test.X"
-    ~expected_alias:None;
-  assert_registers
-    {|
-      from typing import Tuple, TypeVar, Union
-
-      T = TypeVar("T")
-      GenericTree = Union[T, Tuple["GenericTree[T]", "GenericTree[T]"]]
-    |}
-    "test.GenericTree"
-    ~expected_alias:None;
-  (* Aliases referring to recursive aliases. *)
-  assert_registers
-    {|
-      from typing import List, Union
-
-      X = List["X"]
-      Y = List[X]
-    |}
-    "test.Y"
-    ~expected_alias:
-      (Some
-         (Type.TypeAlias
-            (Type.list
-               (Type.RecursiveType.create
-                  ~name:"test.X"
-                  ~body:(Type.list (Type.Primitive "test.X"))))));
-  assert_registers
-    {|
-      from typing import List, Sequence, Union
-
-      X = Union[
-          Sequence["X"],
-          List["X"]
-      ]
-      Y = Union[int, X]
-    |}
-    "test.Y"
-    ~expected_alias:
-      (Some
-         (Type.TypeAlias
-            (Type.union
-               [
-                 Type.integer;
-                 Type.RecursiveType.create
-                   ~name:"test.X"
-                   ~body:
-                     (Type.union
-                        [
-                          Type.list (Type.Primitive "test.X");
-                          Type.parametric "typing.Sequence" [Single (Type.Primitive "test.X")];
-                        ]);
-               ])));
-  assert_registers
-    {|
-      from typing import List, Sequence, Union
-
-      X = Union[
-          Sequence["X"],
-          List["X"]
-      ]
-      Y = Union[int, X]
-      Z = List[Y]
-    |}
-    "test.Z"
-    ~expected_alias:
-      (Some
-         (Type.TypeAlias
-            (Type.list
-               (Type.union
-                  [
-                    Type.integer;
-                    Type.RecursiveType.create
-                      ~name:"test.X"
+            Tree = Union[int, Tuple["Tree", "Tree"]]
+          |}
+           "test.Tree"
+           ~expected_alias:
+             (Some
+                (Type.TypeAlias
+                   (Type.RecursiveType.create
+                      ~name:"test.Tree"
                       ~body:
                         (Type.union
                            [
-                             Type.list (Type.Primitive "test.X");
-                             Type.parametric "typing.Sequence" [Single (Type.Primitive "test.X")];
-                           ]);
-                  ]))));
-  assert_registers
-    {|
-    from typing import Generic, TypeVar
-    from pyre_extensions import TypeVarTuple, Unpack
-    from typing_extensions import Literal as L
+                             Type.integer;
+                             Type.tuple [Type.Primitive "test.Tree"; Type.Primitive "test.Tree"];
+                           ]))));
+      (* Forbid directly-recursive aliases. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers {|
+            Tree = "Tree"
+          |} "test.Tree" ~expected_alias:None;
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+            from typing import Union
 
-    T = TypeVar("T")
-    Ts = TypeVarTuple("Ts")
+            X = Union[int, "X"]
+          |}
+           "test.X"
+           ~expected_alias:None;
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+            from typing import Annotated
 
-    class Tensor(Generic[T, Unpack[Ts]]): ...
+            X = Annotated["X", int]
+          |}
+           "test.X"
+           ~expected_alias:None;
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+            from typing import Tuple, TypeVar, Union
 
-    FloatTensor = Tensor[float, Unpack[Ts]]
-  |}
-    "test.FloatTensor"
-    ~expected_alias:
-      (Some
-         (Type.TypeAlias
-            (Type.parametric
-               "test.Tensor"
-               [
-                 Single Type.float;
-                 Unpacked
-                   (Type.OrderedTypes.Concatenation.create_unpackable
-                      (Type.Variable.Variadic.Tuple.create "test.Ts"));
-               ])));
-  (* An alias containing "..." should not mistake the "..." for some unknown alias. *)
-  assert_registers
-    ~external_sources:["reexports_callable.py", {|
-    from typing import Callable as Callable
-  |}]
-    {|
-    from reexports_callable import Callable
-    F = Callable[..., int]
-  |}
-    "test.F"
-    ~expected_alias:(Some (Type.TypeAlias (Type.Callable.create ~annotation:Type.integer ())));
-  (* Allow the union syntax in type aliases. *)
-  parsed_assert_registers {|
-    X = int | str
-  |} "test.X" (Some "typing.Union[int, str]");
-  ()
+            T = TypeVar("T")
+            GenericTree = Union[T, Tuple["GenericTree[T]", "GenericTree[T]"]]
+          |}
+           "test.GenericTree"
+           ~expected_alias:None;
+      (* Aliases referring to recursive aliases. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+            from typing import List, Union
+
+            X = List["X"]
+            Y = List[X]
+          |}
+           "test.Y"
+           ~expected_alias:
+             (Some
+                (Type.TypeAlias
+                   (Type.list
+                      (Type.RecursiveType.create
+                         ~name:"test.X"
+                         ~body:(Type.list (Type.Primitive "test.X"))))));
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+            from typing import List, Sequence, Union
+
+            X = Union[
+                Sequence["X"],
+                List["X"]
+            ]
+            Y = Union[int, X]
+          |}
+           "test.Y"
+           ~expected_alias:
+             (Some
+                (Type.TypeAlias
+                   (Type.union
+                      [
+                        Type.integer;
+                        Type.RecursiveType.create
+                          ~name:"test.X"
+                          ~body:
+                            (Type.union
+                               [
+                                 Type.list (Type.Primitive "test.X");
+                                 Type.parametric
+                                   "typing.Sequence"
+                                   [Single (Type.Primitive "test.X")];
+                               ]);
+                      ])));
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+            from typing import List, Sequence, Union
+
+            X = Union[
+                Sequence["X"],
+                List["X"]
+            ]
+            Y = Union[int, X]
+            Z = List[Y]
+          |}
+           "test.Z"
+           ~expected_alias:
+             (Some
+                (Type.TypeAlias
+                   (Type.list
+                      (Type.union
+                         [
+                           Type.integer;
+                           Type.RecursiveType.create
+                             ~name:"test.X"
+                             ~body:
+                               (Type.union
+                                  [
+                                    Type.list (Type.Primitive "test.X");
+                                    Type.parametric
+                                      "typing.Sequence"
+                                      [Single (Type.Primitive "test.X")];
+                                  ]);
+                         ]))));
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           {|
+          from typing import Generic, TypeVar
+          from pyre_extensions import TypeVarTuple, Unpack
+          from typing_extensions import Literal as L
+
+          T = TypeVar("T")
+          Ts = TypeVarTuple("Ts")
+
+          class Tensor(Generic[T, Unpack[Ts]]): ...
+
+          FloatTensor = Tensor[float, Unpack[Ts]]
+        |}
+           "test.FloatTensor"
+           ~expected_alias:
+             (Some
+                (Type.TypeAlias
+                   (Type.parametric
+                      "test.Tensor"
+                      [
+                        Single Type.float;
+                        Unpacked
+                          (Type.OrderedTypes.Concatenation.create_unpackable
+                             (Type.Variable.Variadic.Tuple.create "test.Ts"));
+                      ])));
+      (* An alias containing "..." should not mistake the "..." for some unknown alias. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_registers
+           ~external_sources:
+             [
+               ( "reexports_callable.py",
+                 {|
+          from typing import Callable as Callable
+        |} );
+             ]
+           {|
+          from reexports_callable import Callable
+          F = Callable[..., int]
+        |}
+           "test.F"
+           ~expected_alias:
+             (Some (Type.TypeAlias (Type.Callable.create ~annotation:Type.integer ())));
+      (* Allow the union syntax in type aliases. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ parsed_assert_registers
+           {|
+          X = int | str
+        |}
+           "test.X"
+           (Some "typing.Union[int, str]");
+    ]
 
 
 let test_updates context =
@@ -450,9 +503,5 @@ let test_updates context =
 
 let () =
   "environment"
-  >::: [
-         "simple_registration" >:: test_simple_registration;
-         "compounds" >:: test_harder_registrations;
-         "updates" >:: test_updates;
-       ]
+  >::: [test_simple_registration; test_harder_registrations; "updates" >:: test_updates]
   |> Test.run
