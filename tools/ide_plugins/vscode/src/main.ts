8b9d60a75e356a36aee636b40412881f67cff67f
Vasilis Themelis <vdthemelis@gmail.com>
2024-06-03 14:38:45 -0700

    Use the python extension venv (#820)
    Summary:
    Fixes https://github.com/facebook/pyre-check/issues/6
    Fixes https://github.com/facebook/pyre-check/issues/645
    Fixes https://github.com/facebook/pyre-check/issues/183
    
    If [the VsCode Python extension](https://marketplace.visualstudio.com/items?itemName=ms-python.python) is installed, use the Python interpreter that is selected in the extension to start the pyre language server. Also, subscribe to environment switches and restart the pyre language server with the new environments.
    
    If the extension is not present, we retain the original behaviour of the extension.
    
    This is very useful for people that may have multiple environments installed but is more useful for people that use the VsCode SSH extension and cannot benefit from [this workaround](https://github.com/facebook/pyre-check/issues/6#issuecomment-388575825).
    
    Pull Request resolved: https://github.com/facebook/pyre-check/pull/820
    
    Test Plan: I ran the extension locally and it seems to be picking up the interpreter path.
    
    Reviewed By: stroxler, inseokhwang
    
    Differential Revision: D56205532
    
    Pulled By: kinto0
    
    fbshipit-source-id: dc0c00f5a41d37c5cb7d9f85a60ac0ba9230456a

--- tools/ide_plugins/vscode/src/main.ts
+++ tools/ide_plugins/vscode/src/main.ts
@@ -11,34 +11,60 @@
 
 import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, DidChangeConfigurationNotification } from 'vscode-languageclient';
+import { EnvironmentPath, PVSC_EXTENSION_ID, PythonExtension } from '@vscode/python-extension'
+import { dirname, join } from 'path';
+import { existsSync, statSync } from 'fs';
+import which from 'which';
 
-let languageClient: LanguageClient;
-
-namespace Configuration {
-
-	let configurationListener: vscode.Disposable;
-
-	export function initialize() {
-		configurationListener = vscode.workspace.onDidChangeConfiguration(() => {
-			languageClient.sendNotification(DidChangeConfigurationNotification.type, { settings: null });
-		});
-	}
-
-	export function dispose() {
-		if (configurationListener) {
-			configurationListener.dispose();
-		}
-	}
+type LanguageClientState = {
+	languageClient: LanguageClient,
+	configListener: Promise<vscode.Disposable>
 }
+
+// Extension state
+let state : LanguageClientState | undefined;
+let envListener: vscode.Disposable | undefined;
+
+let outputChannel = vscode.window.createOutputChannel("pyre");
 
 export async function activate(_: vscode.ExtensionContext) {
 
-	let serverOptions = {
-		command: "pyre",
+	const pythonExtension = vscode.extensions.getExtension<PythonExtension>(PVSC_EXTENSION_ID);
+
+	if (!pythonExtension) {
+		outputChannel.appendLine("Python extension not found. Will use the default console environment.");
+		state = createLanguageClient('pyre');
+		outputChannel.appendLine("Done");
+		return;
+	}
+
+	const activatedEnvPath = pythonExtension.exports.environments.getActiveEnvironmentPath();
+	const pyrePath = await findPyreCommand(activatedEnvPath);
+
+	if (pyrePath) {
+		state = createLanguageClient(pyrePath);
+	}
+
+	envListener = pythonExtension.exports.environments.onDidChangeActiveEnvironmentPath(async (e) => {
+		state?.languageClient?.stop();
+		state?.configListener.then((listener) => listener.dispose());
+		state = undefined;
+
+		const pyrePath = await findPyreCommand(e);
+		if (pyrePath) {
+			state = createLanguageClient(pyrePath);
+		}
+	});
+}
+
+function createLanguageClient(pyrePath: string) : LanguageClientState {
+
+	const serverOptions = {
+		command: pyrePath,
 		args: ["persistent"]
 	};
 
-	let clientOptions: LanguageClientOptions = {
+	const clientOptions: LanguageClientOptions = {
 		documentSelector: [{scheme: 'file', language: 'python'}],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contain in the workspace
@@ -54,12 +80,51 @@ export async function activate(_: vscode.ExtensionContext) {
 	)
 
 	languageClient.registerProposedFeatures();
-	languageClient.onReady().then(() => {
-		Configuration.initialize();
+
+	const configListener = languageClient.onReady().then(() => {
+		return vscode.workspace.onDidChangeConfiguration(() => {
+			languageClient.sendNotification(DidChangeConfigurationNotification.type, { settings: null });
+		});
 	});
+
 	languageClient.start();
+
+	return {languageClient, configListener};
+}
+
+async function findPyreCommand(envPath: EnvironmentPath) : Promise<string | undefined> {
+
+	if (envPath.id === 'DEFAULT_PYTHON') {
+		outputChannel.appendLine(`Using the default python environment`);
+		return 'pyre';
+	}
+
+	const path = envPath.path;
+	const stat = statSync(path)
+
+	const pyrePath = stat.isFile()
+		? join(dirname(envPath.path), 'pyre')
+		: stat.isDirectory()
+			? join(path, 'bin', 'pyre')
+			: undefined;
+
+	if (pyrePath && existsSync(pyrePath) && statSync(pyrePath).isFile()) {
+		outputChannel.appendLine(`Using pyre path: ${pyrePath} from python environment: ${envPath.id} at ${envPath.path}`);
+		return pyrePath;
+	}
+
+	const pyreFromPathEnvVariable = await which('pyre', { nothrow: true });
+	if (pyreFromPathEnvVariable != null) {
+		outputChannel.appendLine(`Using pyre path: ${pyreFromPathEnvVariable} from PATH`);
+		return pyreFromPathEnvVariable;
+	}
+
+	outputChannel.appendLine(`Could not find pyre path from python environment: ${envPath.id} at ${envPath.path}`);
+	return undefined;
 }
 
 export function deactivate() {
-	Configuration.dispose();
+	state?.languageClient.stop();
+	state?.configListener.then((listener) => listener.dispose());
+	envListener?.dispose();
 }
