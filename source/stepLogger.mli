90f64968c750e1380267182a8175ee87ce5642e4
esohel30 <eric.sohel@stonybrook.edu>
2024-06-19 02:47:07 -0400

    removed the unused files

--- source/stepLogger.mli
+++
@@ -1,8 +0,0 @@
-module StepLogger : sig
-    type t
-  
-    val start : start_message:string -> end_message:string -> t
-  
-    val finish : step_logger:t -> section:Log.section -> integers:(string * int) list -> unit -> unit
-  end
-  
