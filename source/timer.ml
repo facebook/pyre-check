68311d2c8cdf32b7c558207f42a40c1d03797998
Olivier Nicole <olivier@chnik.fr>
2024-06-28 19:41:10 +0200

    mtime fixes

--- source/timer.ml
+++ source/timer.ml
@@ -15,8 +15,13 @@ let start () = Mtime_clock.counter ()
 
 let stop counter = Mtime_clock.count counter
 
-let stop_in_sec counter = stop counter |> Mtime.Span.to_s
+let stop_in_sec counter =
+  Int64.(Mtime.Span.to_uint64_ns (stop counter) / 1_000_000_000L)
+  |> Int64.to_float
 
-let stop_in_ms counter = stop counter |> Mtime.Span.to_ms |> Int.of_float
+let stop_in_ms counter =
+  Int64.(Mtime.Span.to_uint64_ns (stop counter) / 1_000_000L)
+  |> Int.of_int64_exn
 
-let stop_in_us counter = stop counter |> Mtime.Span.to_us |> Int.of_float
+let stop_in_us counter =
+  Int64.(Mtime.Span.to_uint64_ns (stop counter) / 1_000L) |> Int.of_int64_exn
