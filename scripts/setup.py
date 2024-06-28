02595f3a8bfd6f2fc85a476175e16d66b3face32
Olivier Nicole <olivier@chnik.fr>
2024-06-28 10:26:52 +0200

    setup script: don't install packages nor create the switch

--- scripts/setup.py
+++ scripts/setup.py
@@ -30,6 +30,7 @@ LOG: logging.Logger = logging.getLogger(__name__)
 
 COMPILER_VERSION = "5.2.0"
 DEPENDENCIES = [
+    # If you change this, also update the package_list file!
     "base64.3.5.1",
     "cmdliner.1.1.1",
     "core.v0.16.2",
@@ -137,7 +138,7 @@ def _run_command(
 
 
 def _switch_name(release: bool) -> str:
-    return f"{COMPILER_VERSION}+flambda" if release else COMPILER_VERSION
+    return f"{COMPILER_VERSION}+flambda" if release else f"{COMPILER_VERSION}+tsan"
 
 
 def _compiler_specification(release: bool) -> str:
@@ -148,7 +149,12 @@ def _compiler_specification(release: bool) -> str:
     https://discuss.ocaml.org/t/experimental-new-layout-for-the-ocaml-variants-packages-in-opam-repository/6779
     """
     if not release:
-        return COMPILER_VERSION
+        return ",".join(
+            [
+                f"--packages=ocaml-variants.{COMPILER_VERSION}+options",
+                "ocaml-option-tsan",
+            ]
+        )
     else:
         return ",".join(
             [
@@ -347,39 +353,39 @@ def full_setup(
     add_environment_variables: Optional[Mapping[str, str]] = None,
     rust_path: Optional[Path] = None,
 ) -> None:
-    opam_environment_variables: Mapping[
-        str, str
-    ] = set_opam_switch_and_install_dependencies(
-        opam_root,
-        opam_version,
-        release=release,
-        add_environment_variables=add_environment_variables,
-        rust_path=rust_path,
-    )
+    #opam_environment_variables: Mapping[
+    #    str, str
+    #] = set_opam_switch_and_install_dependencies(
+    #    opam_root,
+    #    opam_version,
+    #    release=release,
+    #    add_environment_variables=add_environment_variables,
+    #    rust_path=rust_path,
+    #)
 
-    def run_in_opam_environment(command: List[str]) -> None:
-        _run_command(
-            command,
-            current_working_directory=pyre_directory / "source",
-            add_environment_variables=opam_environment_variables,
-        )
+    #def run_in_opam_environment(command: List[str]) -> None:
+    #    _run_command(
+    #        command,
+    #        current_working_directory=pyre_directory / "source",
+    #        add_environment_variables=opam_environment_variables,
+    #    )
 
     produce_dune_file(pyre_directory, build_type)
-    if run_clean:
-        # Note: we do not run `make clean` because we want the result of the
-        # explicit `produce_dune_file` to remain.
-        # Dune 3.7 runs into `rmdir` failure when cleaning the `_build` directory
-        # for some reason. Manually clean the dir to work around the issue.
-        run_in_opam_environment(["rm", "-rf", "_build"])
-    if release:
-        LOG.info("Running a release build. This may take a while.")
-        run_in_opam_environment(["make", "release"])
-        if run_tests:
-            run_in_opam_environment(["make", "release_test"])
-    else:
-        run_in_opam_environment(["make", "dev"])
-        if run_tests:
-            run_in_opam_environment(["make", "test"])
+    #if run_clean:
+    #    # Note: we do not run `make clean` because we want the result of the
+    #    # explicit `produce_dune_file` to remain.
+    #    # Dune 3.7 runs into `rmdir` failure when cleaning the `_build` directory
+    #    # for some reason. Manually clean the dir to work around the issue.
+    #    run_in_opam_environment(["rm", "-rf", "_build"])
+    #if release:
+    #    LOG.info("Running a release build. This may take a while.")
+    #    run_in_opam_environment(["make", "release"])
+    #    if run_tests:
+    #        run_in_opam_environment(["make", "release_test"])
+    #else:
+    #    run_in_opam_environment(["make", "dev"])
+    #    if run_tests:
+    #        run_in_opam_environment(["make", "test"])
 
 
 def _make_opam_root(local: bool) -> Path:
@@ -433,10 +439,13 @@ def setup(
         produce_dune_file(pyre_directory, build_type)
     else:
         if not _opam_already_initialized(opam_root):
+            LOG.info("opam not detected as initialized. Initializing.")
+            exit(1)
             initialize_opam_switch(
                 opam_root, opam_version, release, add_environment_variables
             )
         else:
+            LOG.info("opam already initialized.")
             opam_update(opam_root, opam_version, add_environment_variables)
         full_setup(
             opam_root,
