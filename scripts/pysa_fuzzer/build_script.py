35d1f8ffb698aadd0f12c4033b5d6d023f9c2724
esohel30 <eric.sohel@stonybrook.edu>
2024-07-04 01:15:05 -0400

    added undetected file logic within the main script

--- scripts/pysa_fuzzer/build_script.py
+++ scripts/pysa_fuzzer/build_script.py
@@ -53,8 +53,32 @@ def configure_and_analyze():
 
     run_command('. tutorial/bin/activate && cd generated_files && pyre analyze > analysis_output.tmp')
 
-def run_report():
-    run_command('. tutorial/bin/activate && python find_undetected_files.py')
+def find_undetected_files():
+    # Load the analysis output from the file
+    with open('generated_files/analysis_output.tmp', 'r') as file:
+        analysis_output = json.load(file)
+
+    # Extract the list of files where the flow has been detected
+    detected_files = set()
+    for entry in analysis_output:
+        detected_files.add(entry['path'])
+
+    # Generate the full list of files (from 1.py to 100.py)
+    all_files = {f"{i}.py" for i in range(1, 101)}
+
+    # Find the files where the flow has not been detected
+    undetected_files = all_files - detected_files
+
+    # Output the list of undetected files
+    print("Files where the flow has not been detected:")
+    for file in sorted(undetected_files, key=lambda x: int(x.split('.')[0])):
+        print(file)
+
+    # Calculate and print the percentage of undetected files
+    total_files = len(all_files)
+    undetected_count = len(undetected_files)
+    undetected_percentage = (undetected_count / total_files) * 100
+    print(f"Flow has not been detected in {undetected_percentage:.2f}% of the files")
 
 def clean_up():
     run_command('rm -rf tutorial')
@@ -70,7 +94,7 @@ def main():
         generate_python_files()
         setup_virtual_environment()
         configure_and_analyze()
-        run_report()
+        find_undetected_files()
     elif args.action == 'clean':
         clean_up()
 
