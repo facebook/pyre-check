865ae20d3cebf988cbd72288ed75f7432158ad22
Steven Troxler <stroxler@meta.com>
2024-07-08 02:54:03 -0700

    Split test cases in classTest.ml
    Summary: The usual split, which lets us isolate failures for easier debugging.
    
    Reviewed By: arthaud
    
    Differential Revision: D59427924
    
    fbshipit-source-id: c70bb905d85ff103295407ef10e48509deed15b7

--- source/analysis/test/integration/classTest.ml
+++ source/analysis/test/integration/classTest.ml
@@ -8,78 +8,80 @@
 open OUnit2
 open IntegrationTest
 
-let test_inconsistent_mro context =
-  assert_type_errors
-    {|
-        class A(B): pass
-        class B(A): pass
-    |}
+let test_inconsistent_mro =
+  test_list
     [
-      "Inconsistent method resolution order [64]: Class `A` does not have a consistent method \
-       resolution order";
-      "Inconsistent method resolution order [64]: Class `B` does not have a consistent method \
-       resolution order";
-    ]
-    context;
-  (* Inconsistent MRO would shadow other checks like override validity *)
-  assert_type_errors
-    {|
-        class A(B):
-          def foo(self) -> int: ...
-        class B(A):
-          def foo(self) -> str: ...
-    |}
-    [
-      "Inconsistent method resolution order [64]: Class `A` does not have a consistent method \
-       resolution order";
-      "Inconsistent method resolution order [64]: Class `B` does not have a consistent method \
-       resolution order";
-    ]
-    context;
-  assert_type_errors
-    {|
-        class A: pass
-        class B(A): pass
-        class C(A, B): pass
-    |}
-    [
-      "Inconsistent method resolution order [64]: Class `C` does not have a consistent method \
-       resolution order";
-    ]
-    context;
-  (* No MRO error due to special-handling of GenericMeta.__mro_entries__ *)
-  assert_type_errors
-    {|
-        from typing import Generic, TypeVar
-        T1 = TypeVar("T1")
-        T2 = TypeVar("T2")
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
+                class A(B): pass
+                class B(A): pass
+            |}
+           [
+             "Inconsistent method resolution order [64]: Class `A` does not have a consistent \
+              method resolution order";
+             "Inconsistent method resolution order [64]: Class `B` does not have a consistent \
+              method resolution order";
+           ];
+      (* Inconsistent MRO would shadow other checks like override validity *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
+                class A(B):
+                  def foo(self) -> int: ...
+                class B(A):
+                  def foo(self) -> str: ...
+            |}
+           [
+             "Inconsistent method resolution order [64]: Class `A` does not have a consistent \
+              method resolution order";
+             "Inconsistent method resolution order [64]: Class `B` does not have a consistent \
+              method resolution order";
+           ];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
+                class A: pass
+                class B(A): pass
+                class C(A, B): pass
+            |}
+           [
+             "Inconsistent method resolution order [64]: Class `C` does not have a consistent \
+              method resolution order";
+           ];
+      (* No MRO error due to special-handling of GenericMeta.__mro_entries__ *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
+                from typing import Generic, TypeVar
+                T1 = TypeVar("T1")
+                T2 = TypeVar("T2")
 
-        class Foo1(Generic[T1]): pass
-        class Foo2(Generic[T1]): pass
-        class Bar1(Generic[T1, T2], Foo1[T1], Foo2[T2]): pass
-        class Bar2(Generic[T1, T2], Foo1, Foo2[T2]): pass
-    |}
-    ["Invalid type parameters [24]: Generic type `Foo1` expects 1 type parameter."]
-    context;
-  (* MRO error even in the presence of GenericMeta.__mro_entries__ *)
-  assert_type_errors
-    {|
-        from typing import Generic, TypeVar
-        T1 = TypeVar("T1")
-        T2 = TypeVar("T2")
+                class Foo1(Generic[T1]): pass
+                class Foo2(Generic[T1]): pass
+                class Bar1(Generic[T1, T2], Foo1[T1], Foo2[T2]): pass
+                class Bar2(Generic[T1, T2], Foo1, Foo2[T2]): pass
+            |}
+           ["Invalid type parameters [24]: Generic type `Foo1` expects 1 type parameter."];
+      (* MRO error even in the presence of GenericMeta.__mro_entries__ *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
+                from typing import Generic, TypeVar
+                T1 = TypeVar("T1")
+                T2 = TypeVar("T2")
 
-        class Foo1(Generic[T1]): pass
-        class Foo2(Generic[T1]): pass
-        class Bar3(Generic[T1, T2], Foo1, Foo2): pass
-    |}
-    [
-      "Inconsistent method resolution order [64]: Class `Bar3` does not have a consistent method \
-       resolution order";
-      "Invalid type parameters [24]: Generic type `Foo1` expects 1 type parameter.";
-      "Invalid type parameters [24]: Generic type `Foo2` expects 1 type parameter.";
+                class Foo1(Generic[T1]): pass
+                class Foo2(Generic[T1]): pass
+                class Bar3(Generic[T1, T2], Foo1, Foo2): pass
+            |}
+           [
+             "Inconsistent method resolution order [64]: Class `Bar3` does not have a consistent \
+              method resolution order";
+             "Invalid type parameters [24]: Generic type `Foo1` expects 1 type parameter.";
+             "Invalid type parameters [24]: Generic type `Foo2` expects 1 type parameter.";
+           ];
     ]
-    context;
-  ()
 
 
-let () = "class" >::: ["inconsistent_mro" >:: test_inconsistent_mro] |> Test.run
+let () = "class" >::: [test_inconsistent_mro] |> Test.run
