b623408a697457e7fce9170afb6ab6bfa4f13473
esohel30 <eric.sohel@stonybrook.edu>
2024-07-14 22:08:23 -0400

    working on advanced code generation

---
+++ scripts/pysa_fuzzer/example.py
@@ -0,0 +1,27 @@
+
+# initial state 
+
+input() -> print() 
+
+# one mutation 
+
+input() -> [?] -> print() 
+
+# two mutations 
+
+input() -> [?] -> [?] -> print() 
+
+# three mutations 
+
+input() -> [?] -> [?] -> [?] -> print() 
+
+
+# example of a one variable mutation 
+
+input() -> [A] -> print() 
+
+# example of a two variable mutation 
+
+input() -> [A] -> [B] -> print() 
+
+# this is still code generation in a simple linear pattern 
