0d25f082d2425dac22d5e896f0037f2c45a10962
esohel30 <eric.sohel@stonybrook.edu>
2024-07-14 23:48:17 -0400

    added description

--- source/interprocedural_analyses/taint/test/integration/node_patterns.py
+++ source/interprocedural_analyses/taint/test/integration/node_patterns.py
@@ -15,3 +15,7 @@ def linked_list_pattern(x):
     e = d.next.next.val 
 
     _test_sink(e) # false negative 
+
+    # Pysa treats memory as flat and doesn't understand aliases.
+    # It fails to recognize that `d.next` -> `c` and `c.next` -> `b`, 
+    # so it misses that `e` (i.e., `d.next.next.val`) is derived from `x`.
