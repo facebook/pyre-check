780b01d63b44908fcaf1ae650a9e83c26cc70144
esohel30 <eric.sohel@stonybrook.edu>
2024-07-17 16:42:18 -0400

    improved code generation by introducing varaible logic

--- scripts/pysa_fuzzer/code_generator2.py
+++ scripts/pysa_fuzzer/code_generator2.py
@@ -5,10 +5,10 @@ import string
 class CodeGenerator:
     def __init__(self) -> None:
         self.variables = self.generate_variable_names()
-        self.current_var = 0
+        self.current_var = 1
         self.current_function_number = 1
-        self.source_statements = []
-        self.last_source = "input()"
+        self.source_statements = ['a = input()']
+        self.last_source = "a"
         self.sink_statements = []
         self.last_sink = "print"
 
@@ -20,6 +20,11 @@ class CodeGenerator:
         reserved_keywords = {'as', 'in', 'if', 'is', 'or'}
         names = [name for name in names if name not in reserved_keywords]
         return names
+
+    def generate_new_variable(self) -> str: 
+        variable_name = f"{self.variables[self.current_var]}"
+        self.current_var += 1
+        return variable_name
 
     def generate_new_function(self) -> str:
         function_name = f"f{self.current_function_number}"
@@ -35,17 +40,23 @@ class CodeGenerator:
         current_function_source = self.generate_new_function()
         temp_source = self.last_source
         self.last_source = current_function_source + "()"
+        new_var = self.generate_new_variable()
+
         current_function_sink = self.generate_new_function()
         temp_sink = self.last_sink
         self.last_sink = current_function_sink
 
-        self.source_statements.append(f"def {current_function_source}():\n{indent_space}random_number = random.randint(1,3)\n{indent_space}if random_number == 1:\n{indent_space*2}return {temp_source}\n{indent_space}else:\n{indent_space*2}return {current_function_source}()")
+        self.source_statements.append(f"def {current_function_source}():\n{indent_space}random_number = random.randint(1,3)\n{indent_space}{new_var} = {temp_source}\n{indent_space}if random_number == 1:\n{indent_space*2}return {new_var}\n{indent_space}else:\n{indent_space*2}return {current_function_source}()")
         self.sink_statements.append(f"def {current_function_sink}(x):\n{indent_space}random_number = random.randint(1,3)\n{indent_space}if random_number == 1:\n{indent_space*2}return {temp_sink}(x)\n{indent_space}else:\n{indent_space*2}return {current_function_sink}(x)")
+
+
 
     def generate(self) -> str:
         code_lines = self.source_statements + self.sink_statements
         code_lines.append(self.generate_sink())
         return '\n'.join(code_lines)
+    
+
 
 # Example usage
 generator = CodeGenerator()
@@ -55,18 +66,18 @@ print(generator.generate())
 # example generation
 import random
 
+a = input()
 def f1():
-    random_number = random.randint(1,5)
+    random_number = random.randint(1,3)
+    b = a
     if random_number == 1:
-        return input()
+        return b
     else:
         return f1()
 def f2(x):
-    random_number = random.randint(1,5)
+    random_number = random.randint(1,3)
     if random_number == 1:
         return print(x)
     else:
         return f2(x)
 f2(f1())
-
-
