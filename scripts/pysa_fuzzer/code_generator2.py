24bc39a31e5a5b0680d2890bafacb9eb37de2642
esohel30 <eric.sohel@stonybrook.edu>
2024-07-16 23:55:21 -0400

    fixed add varaible to get rid of global variable problem

--- scripts/pysa_fuzzer/code_generator2.py
+++ scripts/pysa_fuzzer/code_generator2.py
@@ -11,6 +11,7 @@ class CodeGenerator:
         self.last_source = "input()"
         self.sink_statements = []
         self.last_sink = "print"
+        self.variable_assignments = []  # Initialize the variable_assignments attribute
 
     def generate_variable_names(self) -> List[str]:
         single_letter_names = list(string.ascii_lowercase)
@@ -28,20 +29,35 @@ class CodeGenerator:
 
     def add_function(self) -> None:
         indent_space = ' ' * 4
-
         current_function_source = self.generate_new_function()
-        temp_source = self.last_source
-        self.last_source = current_function_source + "()"
+        # Adjust to include variables directly in function
+        if self.variable_assignments:
+            # Include the variable assignment in the last function
+            function_body = f"{self.variable_assignments[-1]}\n{indent_space}return {self.last_source}"
+            self.variable_assignments.pop()  # Remove the last variable assignment as it's now included
+        else:
+            function_body = f"return {self.last_source}"
 
+        self.source_statements.append(f"def {current_function_source}():\n{indent_space}{function_body}")
+        self.last_source = current_function_source + "()"
+        
         current_function_sink = self.generate_new_function()
         temp_sink = self.last_sink
         self.last_sink = current_function_sink
-
-        self.source_statements.append(f"def {current_function_source}():\n{indent_space}return {temp_source}")
         self.sink_statements.append(f"def {current_function_sink}(x):\n{indent_space}return {temp_sink}(x)")
 
     def generate_sink(self) -> str:
         return f"{self.last_sink}({self.last_source})"
+
+    def add_variable(self) -> None:
+        current_var = self.generate_new_variable()
+        self.variable_assignments.append(f"{current_var} = {self.last_source}")
+        self.last_source = current_var
+
+    def generate_new_variable(self) -> str:
+        index = self.current_var
+        self.current_var += 1
+        return self.variables[index]
 
     def generate(self) -> str:
         code_lines = self.source_statements + self.sink_statements
@@ -51,7 +67,19 @@ class CodeGenerator:
 # Example usage
 generator = CodeGenerator()
 generator.add_function()
+
+generator.add_variable()  # Variable assignments are now within the last function added
 generator.add_function()
+generator.add_function()
+generator.add_variable()  # Variable assignments are now within the last function added
+
+generator.add_function()
+generator.add_variable()  # Variable assignments are now within the last function added
+
+generator.add_function()
+generator.add_function()
+generator.add_variable()  # Variable assignments are now within the last function added
+
 generator.add_function()
 
 print(generator.generate())
@@ -60,7 +88,8 @@ print(generator.generate())
 def f1():
     return input()
 def f3():
-    return f1()
+    a = f1()
+    return a
 def f5():
     return f3()
 def f2(x):
