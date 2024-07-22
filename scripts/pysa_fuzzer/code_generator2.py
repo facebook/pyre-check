45a5743a3fef1322500bce17347ccfcbf0744cca
esohel30 <eric.sohel@stonybrook.edu>
2024-07-22 03:26:22 -0400

    added another hashmap pattern

--- scripts/pysa_fuzzer/code_generator2.py
+++ scripts/pysa_fuzzer/code_generator2.py
@@ -223,6 +223,34 @@ def {current_function_sink}(x):
 {indent_space*2}return {current_function_sink}(x + 'z')
     """)
 
+    def add_function_9(self) -> None:
+        indent_space = ' ' * 4
+        # source stuff
+        current_function_source = self.generate_new_function()
+        temp_source = self.last_source
+        self.last_source = f"{current_function_source}({temp_source})"
+        # sink stuff
+        current_function_sink = self.generate_new_function()
+        temp_sink = self.last_sink
+        self.last_sink = current_function_sink
+        # adding statements
+        self.source_statements.append(f"""
+def {current_function_source}(x):
+{indent_space}hashmap = {{char: x.count(char) for char in set(x)}}
+{indent_space}if (count := hashmap.get('a', 0)) > 2:
+{indent_space*2}return 'b' * count + x
+{indent_space}else:
+{indent_space*2}return {current_function_source}(x + 'a')
+    """)
+        self.sink_statements.append(f"""
+def {current_function_sink}(x):
+{indent_space}hashmap = {{char: x.count(char) for char in set(x)}}
+{indent_space}if (count := hashmap.get('z', 0)) > 1:
+{indent_space*2}return {temp_sink}('y' * count + x)
+{indent_space}else:
+{indent_space*2}return {current_function_sink}(x + 'z')
+    """)
+
    
 
     
@@ -238,12 +266,17 @@ generator = CodeGenerator()
 
 
 
-
 generator.add_function_1()
-generator.add_function_6()
-generator.add_function_8()
-generator.add_function_8()
+generator.add_function_2()
+generator.add_function_3()
 
+generator.add_function_4()
+generator.add_function_6()
+generator.add_function_7()
+
+generator.add_function_8()
+generator.add_function_9()
+generator.add_function_1()
 
 
 
@@ -251,8 +284,6 @@ print(generator.generate())
 
 
 
-
-import random 
 def f0():
     if random.randint(1, 3) == 1:
         return input()
@@ -261,27 +292,63 @@ def f0():
         
 
 def f2(x):
-    if (y := random.randint(1, 3)) == 1:
-        return x + str(y)
+    if random.randint(1, 3) == 1:
+        return x
     else:
         return f2(x)
-    
+        
 
-def f4(x):
-    hashmap = {char: ord(char) for char in x}
-    if (value := hashmap.get('a')) is not None:
-        return chr(value + 1) + x
+def f4(x, y):
+    if random.randint(1, 3) == 1:
+        return x + y
     else:
-        return f4(x + 'a')
+        return f4(x, y)
     
 
 def f6(x):
+    y = x * 2
+    if random.randint(1, 3) == 1:
+        return y
+    else:
+        return f6(y)
+    
+
+def f8(x):
+    if (y := random.randint(1, 3)) == 1:
+        return x + str(y)
+    else:
+        return f8(x)
+    
+
+def f10(x):
+    if (z := x.count('a')) > 2:
+        return x.replace('a', 'arep')
+    else:
+        return f10(x + 'a')
+    
+
+def f12(x):
     hashmap = {char: ord(char) for char in x}
     if (value := hashmap.get('a')) is not None:
         return chr(value + 1) + x
     else:
-        return f6(x + 'a')
+        return f12(x + 'a')
     
+
+def f14(x):
+    hashmap = {char: x.count(char) for char in set(x)}
+    if (count := hashmap.get('a', 0)) > 2:
+        return 'b' * count + x
+    else:
+        return f14(x + 'a')
+    
+
+def f16():
+    if random.randint(1, 3) == 1:
+        return f14(f12(f10(f8(f6(f4(f2(f0()), 'temp'))))))
+    else:
+        return f16()
+        
 
 def f1(x):
     if random.randint(1, 3) == 1:
@@ -291,25 +358,60 @@ def f1(x):
         
 
 def f3(x):
-    if (y := random.randint(1, 3)) == 1:
-        return f1(x + str(y))
+    if random.randint(1, 3) == 1:
+        return f1(x)
     else:
         return f3(x)
-    
+        
 
 def f5(x):
-    hashmap = {char: ord(char) for char in x}
-    if (value := hashmap.get('z')) is not None:
-        return f3(chr(value - 1) + x)
+    if random.randint(1, 3) == 1:
+        return f3(x)
     else:
-        return f5(x + 'z')
+        return f5(x)
     
 
 def f7(x):
+    if random.randint(1, 3) == 1:
+        return f5(x)
+    else:
+        return f7(x)
+    
+
+def f9(x):
+    if (y := random.randint(1, 3)) == 1:
+        return f7(x + str(y))
+    else:
+        return f9(x)
+    
+
+def f11(x):
+    if (z := x.count('z')) > 1:
+        return f9(x.replace('a', 'arep'))
+    else:
+        return f11(x + 'z')
+    
+
+def f13(x):
     hashmap = {char: ord(char) for char in x}
     if (value := hashmap.get('z')) is not None:
-        return f5(chr(value - 1) + x)
+        return f11(chr(value - 1) + x)
     else:
-        return f7(x + 'z')
+        return f13(x + 'z')
     
-f7(f6(f4(f2(f0()))))
+
+def f15(x):
+    hashmap = {char: x.count(char) for char in set(x)}
+    if (count := hashmap.get('z', 0)) > 1:
+        return f13('y' * count + x)
+    else:
+        return f15(x + 'z')
+    
+
+def f17(x):
+    if random.randint(1, 3) == 1:
+        return f15(x)
+    else:
+        return f17(x)
+        
+f17(f16())
