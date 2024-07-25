0317a903390b6d969a058c11557cf67ca20d2cbf
Steven Troxler <stroxler@meta.com>
2024-07-25 09:35:26 -0700

    Remove poorly-chosen names in moduleTrackerTest
    Summary:
    It is not a good idea to put spaces in an OUnit test name, especially
    at the end of the name, because the space is almost unprintable which
    means the user won't easily see the test name when trying to rerun just
    one test.
    
    This commit strips all trailing spaces from test names.
    
    Reviewed By: yangdanny97
    
    Differential Revision: D60208401
    
    fbshipit-source-id: 946cc24d11d0a2ae0dc58c220da753767066a5a2

--- source/analysis/test/moduleTrackerTest.ml
+++ source/analysis/test/moduleTrackerTest.ml
@@ -2112,20 +2112,20 @@ let () =
   "environment"
   >::: [
          "module_path_create" >:: test_module_path_create;
-         "module_path_search_path_subdirectory " >:: test_module_path_search_path_subdirectory;
-         "module_path_exclude " >:: test_module_path_exclude;
-         "module_path_directory_filter " >:: test_module_path_directory_filter;
-         "module_path_directory_filter2 " >:: test_module_path_directory_filter2;
-         "module_path_directory_filter3 " >:: test_module_path_directory_filter3;
-         "module_path_overlapping " >:: test_module_path_overlapping;
-         "module_path_overlapping2 " >:: test_module_path_overlapping2;
+         "module_path_search_path_subdirectory" >:: test_module_path_search_path_subdirectory;
+         "module_path_exclude" >:: test_module_path_exclude;
+         "module_path_directory_filter" >:: test_module_path_directory_filter;
+         "module_path_directory_filter2" >:: test_module_path_directory_filter2;
+         "module_path_directory_filter3" >:: test_module_path_directory_filter3;
+         "module_path_overlapping" >:: test_module_path_overlapping;
+         "module_path_overlapping2" >:: test_module_path_overlapping2;
          "initialization" >:: test_initialization;
-         "priority " >:: test_priority;
-         "priority_multi_source_paths " >:: test_priority_multi_source_paths;
-         "root_independence " >:: test_root_independence;
-         "hidden_files " >:: test_hidden_files;
-         "hidden_files2 " >:: test_hidden_files2;
-         "namespace_modules " >:: test_namespace_modules;
+         "priority" >:: test_priority;
+         "priority_multi_source_paths" >:: test_priority_multi_source_paths;
+         "root_independence" >:: test_root_independence;
+         "hidden_files" >:: test_hidden_files;
+         "hidden_files2" >:: test_hidden_files2;
+         "namespace_modules" >:: test_namespace_modules;
          "stub_package_priority" >:: test_stub_package_priority;
          test_update_new_files;
          test_update_remove_files;
