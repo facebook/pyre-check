42067b619cedae63b4c913775efde72d37abadc3
esohel30 <eric.sohel@stonybrook.edu>
2024-07-26 03:09:25 -0400

    added the example

--- scripts/pysa_fuzzer/example.py
+++ scripts/pysa_fuzzer/example.py
@@ -1,2 +1,188 @@
-# pattern 1 (one time, then two times)
+def f0(x):
+    return x
+        
 
+a = f0(input())
+        
+
+b = random.randint(1, 5)
+if b < 5 and b > 5:
+    print("hello world")
+else: 
+    c = a
+        
+
+if False: 
+    def f1(x): 
+        return x
+else: 
+    def f2(x): 
+        return x
+        
+
+def f3(x):
+    return x
+hashmap = {"choice_1": f3}
+        
+
+d = random.choice([True, False]) and not True
+if d:
+    def f4(x):
+        return x
+else:
+    def f5(x):
+        return x
+    
+
+e = 'a' 
+f = 'b'  
+if e == 'a':
+    def f6(x):
+        return x
+else:
+    def f7(x):
+        return x
+if f == 'b':
+        def f8(y):
+            return y
+    
+
+class Class9:
+    def f9(self, x):
+        return x
+
+g = Class9()
+    
+
+h = lambda x: x
+i = [h(char) for char in g.f9(f8(f6(f5(hashmap["choice_1"](f2(c))))))]
+    
+
+def f10(func):
+    def wrapper(x):
+        return func(x)
+    return wrapper
+
+@f10
+def f11(x):
+    return x
+
+f12 = f11
+    
+
+def f13():
+    def f14(x):
+        return x
+    return f14
+
+j = f13()
+    
+
+def f15(x):
+    return x
+
+def f16(x):
+    return x
+
+k = {
+    'key1': f15,
+    'key2': f16
+}
+
+l = 'key1' if 1 == 1 else 'key2'
+    
+
+m = list(k[l](j(f12(''.join(i)))))
+n = m[::1]
+    
+
+class Class17:
+    @staticmethod
+    def f17(x):
+        return x
+
+    
+
+o = (Class17.f17(''.join(n)),)
+p, = o
+q = p
+    
+
+r = "{}".format(q)
+    
+
+def f18(x):
+    yield x
+
+s = next(f18(r))
+    
+
+t = list(map(lambda x: x, s))
+    
+
+def f19(x):
+    return x
+
+def f20(x):
+    return x
+
+def f21(x):
+    return f19(f20(x))
+    
+
+DynamicClass22 = type('DynamicClass22', (object,), {
+    'f22': lambda self, x: x
+})
+
+u = DynamicClass22()
+    
+
+def f23(func):
+    def wrapper(x):
+        result = func(x)
+        # Additional layer of complexity
+        return result
+    return wrapper
+
+@f23
+def f24(x):
+    return x
+
+f25 = f24
+    
+
+class OuterClass26:
+    class InnerClass26:
+        def f26(self, x):
+            return x
+
+v = OuterClass26.InnerClass26()
+    
+
+def f27(x, y):
+    return y
+
+f28 = functools.partial(f27, y=v.f26(f25(u.f22(f21(''.join(t))))))
+w = f28(None)
+    
+
+class IteratorClass29:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.data):
+            result = self.data[self.index]
+            self.index += 1
+            return result
+        else:
+            raise StopIteration
+
+x = IteratorClass29(w)
+y = ''.join([char for char in x])
+    
+print(y)
