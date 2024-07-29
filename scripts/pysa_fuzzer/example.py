ae3acc519e94565da80610f14d34b17f0c21a851
esohel30 <eric.sohel@stonybrook.edu>
2024-07-28 21:07:09 -0400

    added the exmaple

--- scripts/pysa_fuzzer/example.py
+++ scripts/pysa_fuzzer/example.py
@@ -185,4 +185,145 @@ class IteratorClass29:
 x = IteratorClass29(w)
 y = ''.join([char for char in x])
     
-print(y)
+
+def f29(x):
+    print(x)
+        
+
+if False:
+    def f30(x):
+        f29(x)
+else:
+    def f31(x):
+        f29(x)
+        
+
+def f33(func):
+    def wrapper(x):
+        f31(x)
+        return func(x)
+    return wrapper
+
+@f33
+def f32(x):
+    return x
+    
+
+class Class35:
+    def __init__(self, sink_function):
+        self.sink_function = sink_function
+
+    def f34(self, x):
+        self.sink_function(x)
+
+z = Class35(f32)
+    
+
+def f35(x):
+    aa = list(str(x))
+    ab = map(lambda char: (z.f34(char), char)[1], aa)
+    return ''.join(ab)
+    
+
+def f36(x):
+    ac = (f35(x), x)
+    return ac[1]
+    
+
+def f37(x):
+    ad = f36(x)
+    with open('sink_output.txt', 'w') as file:
+        file.write(str(ad))
+    return x
+    
+
+def f38(x):
+    ae = f37(x)
+    return x
+    
+
+def f39(x):
+    ag = [str(f38(item)) for item in str(x)]
+    result = ''.join(ag)
+    return result
+    
+
+def f40(x):
+    ah = str(f39(x))
+    return f"{len(ah)}: {x}"
+    
+
+def f41(x):
+    ai = set(str(f40(x)))
+    return ''.join(sorted(ai))
+    
+
+def f42(x):
+    aj = [char.upper() if char.islower() else char.lower() for char in str(f41(x))]
+    return ''.join(aj)
+    
+
+def f43(x):
+    ak = [ord(char) for char in str(f42(x))]
+    return sum(ak)
+    
+
+def f44(x):
+    al = str(f43(x))[::-1]
+    return al
+    
+
+def f45(x):
+    am = str(f44(x)).split()
+    an = '_'.join(am)
+    return an
+    
+
+def f46(x):
+    ap = str(f45(x))
+    aq = str(x)
+    ao = ''.join(a + b for a, b in zip(ap, aq))
+    return ao
+    
+
+def f47(x):
+    ar = str(f46(x))
+    return '-'.join(ar[i:i+2] for i in range(0, len(ar), 2))
+    
+
+def f48(x):
+    at = str(f47(x))
+    au = len(at) // 2
+    return at[:au] + '-' + at[au:]
+    
+
+def f49(x):
+    av = str(f48(x)).encode('utf-8').hex()
+    return av
+    
+
+def f50(x):
+    aw = ''.join(reversed(str(f49(x))))
+    return ''.join(c if i % 2 == 0 else '*' for i, c in enumerate(aw))
+    
+
+def f51(x):
+    import random
+    ax = list(str(f50(x)))
+    random.shuffle(ax)
+    ay = ''.join(ax)
+    return ay
+    
+
+def f52(x):
+    az = [str(f51(x))[i:i+3] for i in range(0, len(str(f51(x))), 3)]
+    ba = '-'.join(az)
+    return ba
+    
+
+def f53(x):
+    bb = str(f52(x))
+    bc = ''.join(c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(bb))
+    return bc
+    
+f53(y)
