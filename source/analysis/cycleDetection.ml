08f24a039b2d766c084ab0c2d9cd1d9cc8ea7e5e
Steven Troxler <stroxler@meta.com>
2024-08-14 21:44:07 -0700

    Add a module comment to `cycleDetection.ml`
    Summary:
    This module (which a previous diff renamed from `assumptions.ml`) has
    long confused me; I finally dug through how each cycle short-circuiting
    works so that we could have a module description here.
    
    Reviewed By: grievejia
    
    Differential Revision: D61287997
    
    fbshipit-source-id: 4beb8834a39cb74457eef5309f8cd9f5c65bb8e6

--- source/analysis/cycleDetection.ml
+++ source/analysis/cycleDetection.ml
@@ -5,7 +5,45 @@
  * LICENSE file in the root directory of this source tree.
  *)
 
-(* TODO(T132410158) Add a module-level doc comment. *)
+(* The CycleDetection.t data structure is a place where code involved in resolving attributes and
+   constraint solving (all of which is mutually recursive logic in some cases) can attach notions of
+   "visited" computations in order to break cycles that would lead to infinite recursion. The
+   different cycle detection components are unrelated to one another other than all three of them
+   break some kind of postential cycle.
+
+   The `decorators_being_resolved` is used to record any time we attempt to resolve some name as a
+   decorator; if in any later recursive call we encounter the same decorator we know to skip it and
+   record an error saying the decorator cannot be resolved; this can be triggered by cyclic use of
+   decorators (which would typically produce a runtime error); the simplest case is a decorator
+   trying to decorate itself.
+
+   The `assumed_protocol_instantiations` is used to track any combination of a candidate `Type.t`
+   and a protocol (as a bare name) that we might want to subtype; the process of subtyping involves
+   solving for the type arguments on the protocol if it is generic, and we can hit recursive cases:
+   for example a protocol with a method that returns itself. We break the cycle by basically
+   assuming the candidate satisfies the protocol in any recursive call, then bubbling back out to
+   the topmost call and checking for contradictions. This logic can be triggered both by generic
+   protocols and by generic recursive type aliases (which share some type checking logic with
+   protocols because both are structural types that are potentially recursive).
+
+   The `assumed_callable_types` seems to get hit mainly when understanding the `__call__` method of
+   a callable type as a `BoundMethod`. This particular cycle may just be an implementation detail of
+   Pyre; the issue happens when we try to understand a `BoundMethod` as a callable; we end up
+   resolving the `__call__` attribute through a special case that eventually calls back into an
+   identical `resolve_callable_protocol` call, and there is probably perf to gain by handling it
+   better. It *may* also be possible to directly hit a cycle if people are annotating a `__call__`
+   attribute directly as a (mutually) recursive type; ideally we would just reject this and ignore
+   it which would rule out cycles.
+
+   All three of these cycle detectors are "simple cycle breaking" in the sense that the only place
+   which *sets* some cycle detection is the same block of code that *uses* it; none of them are used
+   to "magically" communicate between different parts of the code:
+
+   - decorators_being_resolved is used when handling a decorator to resolve a define
+
+   - assumed_callable_types is used when resolving a type "as a callable protocol"
+
+   - assumed_protocol_instantiations is used when instantiating protocol / recursive alias params *)
 
 open Core
 open Ast
