3d41785d7a5124c969cd23789436931dce918c89
Conner Nilsen <connernilsen@meta.com>
2024-08-16 08:23:05 -0700

    Add --output-package-location flag to dump JSON of location
    Summary:
    Our PyPI publishing script no longer works due to (probably) a new bug in Eden. This fixes that issue by using the build in /tmp rather than moving the result into Eden-backed directories.
    
    This diff dumps the dataclass when the `--output-artifact-location` flag is passed in. We don't use this internally (as far as I can tell), so it doesn't matter much how we return this information.
    
    Reviewed By: grievejia
    
    Differential Revision: D61341113
    
    fbshipit-source-id: 159ccab3e6f7e7d17d8efd5f3b45bfef3cee04dc

--- scripts/pypi/__main__.py
+++ scripts/pypi/__main__.py
@@ -10,6 +10,7 @@ TODO(T132414938) Add a module-level docstring
 
 import argparse
 from pathlib import Path
+import dataclasses
 
 from pypi.build_pypi_package import build_pypi_package
 
@@ -19,11 +20,15 @@ if __name__ == "__main__":
     parser.add_argument("--typeshed-path", type=Path, required=True)
     parser.add_argument("--version", type=str, required=True)
     parser.add_argument("--nightly", action="store_true")
+    parser.add_argument("--output-artifact-location", action="store_true")
 
     arguments = parser.parse_args()
-    build_pypi_package(
+    artifacts = build_pypi_package(
         pyre_directory=Path(__file__).resolve().parent.parent.parent,
         typeshed_path=arguments.typeshed_path,
         version=arguments.version,
         nightly=arguments.nightly,
     )
+
+    if arguments.output_artifact_location:
+        print(dataclasses.as_dict(artifacts))
