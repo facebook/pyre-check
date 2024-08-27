5f73c8b276340b6219623b519c4b637288616373
Danny Yang <yangdanny@meta.com>
2024-08-27 14:12:43 -0700

    split assertTest
    Summary: make it easier to debug
    
    Reviewed By: stroxler
    
    Differential Revision: D61851895
    
    fbshipit-source-id: b865791bcc0cf525fb0b7f70a00d36b732c4b575

--- source/analysis/test/integration/assertTest.ml
+++ source/analysis/test/integration/assertTest.ml
@@ -8,37 +8,42 @@
 open OUnit2
 open IntegrationTest
 
-let test_check_assert context =
-  let assert_type_errors source errors = assert_type_errors source errors context in
-  assert_type_errors
-    {|
+let test_check_assert =
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(optional: typing.Optional[str]) -> None:
         if optional or len(optional) > 0:
           pass
     |}
-    [
-      "Incompatible parameter type [6]: In call `len`, for 1st positional argument, expected \
-       `Sized` but got `Optional[str]`.";
-    ];
-  assert_type_errors
-    {|
+           [
+             "Incompatible parameter type [6]: In call `len`, for 1st positional argument, \
+              expected `Sized` but got `Optional[str]`.";
+           ];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(optional: typing.Optional[str]) -> None:
         if optional is None or len(optional) > 0:
           pass
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(optional: typing.Optional[str]) -> None:
         if optional and len(optional) > 0:
           pass
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import int_to_int
       def foo() -> int:
         if 1 > 2:
@@ -47,9 +52,10 @@ let test_check_assert context =
           assert False
         return int_to_int(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import int_to_int
       def foo() -> int:
         if 1 > 2:
@@ -58,10 +64,11 @@ let test_check_assert context =
           assert False, "unreachable, surely"
         return int_to_int(x)
     |}
-    [];
-  (* TODO(T93984519): Uninitialized local should not throw an error in these. *)
-  assert_type_errors
-    {|
+           [];
+      (* TODO(T93984519): Uninitialized local should not throw an error in these. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import int_to_int
       def foo() -> int:
         if 1 > 2:
@@ -70,9 +77,10 @@ let test_check_assert context =
           assert not True
         return int_to_int(x)
     |}
-    ["Uninitialized local [61]: Local variable `x` is undefined, or not always defined."];
-  assert_type_errors
-    {|
+           ["Uninitialized local [61]: Local variable `x` is undefined, or not always defined."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import int_to_int
       def foo() -> int:
         if True:
@@ -80,9 +88,10 @@ let test_check_assert context =
         else:
           return int_to_int("monkey news")
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import expect_int
       import typing
       def foo(x: typing.Optional[int]) -> None:
@@ -92,9 +101,10 @@ let test_check_assert context =
           expect_int(y)
           expect_int(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import expect_int
       import typing
       def foo(x: typing.Optional[int]) -> None:
@@ -104,9 +114,10 @@ let test_check_assert context =
           expect_int(y)
           expect_int(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import expect_int
       import typing
       def foo(x: typing.Optional[int]) -> None:
@@ -119,9 +130,10 @@ let test_check_assert context =
           expect_int(y)
           expect_int(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import expect_int
       import typing
       def foo(x: typing.Optional[int]) -> None:
@@ -134,9 +146,10 @@ let test_check_assert context =
           expect_int(y)
           expect_int(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import expect_int
       import typing
       def foo(x: typing.Optional[int]) -> None:
@@ -148,9 +161,10 @@ let test_check_assert context =
           expect_int(y)
           expect_int(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from builtins import expect_int
       import typing
       def foo(x: typing.Optional[int]) -> None:
@@ -162,9 +176,10 @@ let test_check_assert context =
           expect_int(y)
           expect_int(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from typing import Tuple, Any
 
       def foo() -> None:
@@ -175,120 +190,130 @@ let test_check_assert context =
         if isinstance(y, stringish_types):
           pass
    |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       def foo() -> None:
         assert True, 5 + "f"
    |}
-    ["Unsupported operand [58]: `+` is not supported for operand types `int` and `str`."];
-  ()
-
-
-let test_check_assert_functions context =
-  assert_default_type_errors
-    {|
-      import typing
-      class One:
-          a: int = 1
-
-      # The actual content of this function does not really matter.
-      def pyretestassert(x: typing.Any) -> None:
-          pass
-
-      def f(o: typing.Optional[One]) -> int:
-          assert o
-          return o.a
-
-      def f2(o: typing.Optional[One]) -> int:
-          pyretestassert(o)
-          return o.a
-    |}
-    ["Undefined attribute [16]: Optional type has no attribute `a`."]
-    context;
-  assert_default_type_errors
-    ~handle:"foo.py"
-    {|
-      import typing
-      class One:
-          a: int = 1
-
-      # The actual content of this function does not really matter.
-      def pyretestassert(x: typing.Any) -> None:
-          pass
-
-      def f(o: typing.Optional[One]) -> int:
-          assert o
-          return o.a
-
-      def f2(o: typing.Optional[One]) -> int:
-          pyretestassert(o)
-          return o.a
-    |}
-    []
-    context;
-  assert_type_errors
-    {|
-      import typing
-      class One:
-          a: int = 1
-
-      def f(o: typing.Optional[One]) -> int:
-          assert o
-          return o.a
-
-      def f2(o: typing.Optional[One]) -> int:
-          pyretestassert(o)
-          return o.a
-    |}
-    [
-      "Unbound name [10]: Name `pyretestassert` is used but not defined in the current scope.";
-      "Incompatible return type [7]: Expected `int` but got `unknown`.";
-      "Undefined attribute [16]: Optional type has no attribute `a`.";
+           ["Unsupported operand [58]: `+` is not supported for operand types `int` and `str`."];
     ]
-    context;
-  ()
 
 
-let test_check_all context =
-  let assert_type_errors source errors = assert_type_errors source errors context in
-  assert_type_errors
-    {|
+let test_check_assert_functions =
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_default_type_errors
+           {|
+      import typing
+      class One:
+          a: int = 1
+
+      # The actual content of this function does not really matter.
+      def pyretestassert(x: typing.Any) -> None:
+          pass
+
+      def f(o: typing.Optional[One]) -> int:
+          assert o
+          return o.a
+
+      def f2(o: typing.Optional[One]) -> int:
+          pyretestassert(o)
+          return o.a
+    |}
+           ["Undefined attribute [16]: Optional type has no attribute `a`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_default_type_errors
+           ~handle:"foo.py"
+           {|
+      import typing
+      class One:
+          a: int = 1
+
+      # The actual content of this function does not really matter.
+      def pyretestassert(x: typing.Any) -> None:
+          pass
+
+      def f(o: typing.Optional[One]) -> int:
+          assert o
+          return o.a
+
+      def f2(o: typing.Optional[One]) -> int:
+          pyretestassert(o)
+          return o.a
+    |}
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
+      import typing
+      class One:
+          a: int = 1
+
+      def f(o: typing.Optional[One]) -> int:
+          assert o
+          return o.a
+
+      def f2(o: typing.Optional[One]) -> int:
+          pyretestassert(o)
+          return o.a
+    |}
+           [
+             "Unbound name [10]: Name `pyretestassert` is used but not defined in the current scope.";
+             "Incompatible return type [7]: Expected `int` but got `unknown`.";
+             "Undefined attribute [16]: Optional type has no attribute `a`.";
+           ];
+    ]
+
+
+let test_check_all =
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(x: typing.List[typing.Optional[str]]) -> typing.Optional[str]:
         if all(x):
           return ','.join(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(x: typing.Iterable[typing.Optional[str]]) -> typing.Optional[str]:
         if all( x):
           return ','.join(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(x: typing.Iterable[typing.Optional[str]]) -> typing.Optional[str]:
         if not all(x):
           return ','.join(x)
     |}
-    [
-      "Incompatible parameter type [6]: In call `str.join`, for 1st positional argument, expected \
-       `Iterable[typing_extensions.LiteralString]` but got `Iterable[Optional[str]]`.";
-    ];
-  assert_type_errors
-    {|
+           [
+             "Incompatible parameter type [6]: In call `str.join`, for 1st positional argument, \
+              expected `Iterable[typing_extensions.LiteralString]` but got \
+              `Iterable[Optional[str]]`.";
+           ];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(x: typing.Iterable[typing.Union[str, None]]) -> typing.Optional[str]:
         if all(x):
           return ','.join(x)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(x: typing.Iterable[typing.Union[str, int, None]]) -> \
           typing.Iterable[typing.Union[str, int]]:
@@ -296,9 +321,10 @@ let test_check_all context =
           return x
         return []
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       _T = typing.TypeVar('_T')
       def foo(x: typing.Dict[typing.Optional[int], _T]) -> typing.Dict[int, _T]:
@@ -306,88 +332,98 @@ let test_check_all context =
           return x
         return {}
     |}
-    [
-      "Incompatible return type [7]: Expected `Dict[int, Variable[_T]]` but got \
-       `Dict[Optional[int], Variable[_T]]`.";
+           [
+             "Incompatible return type [7]: Expected `Dict[int, Variable[_T]]` but got \
+              `Dict[Optional[int], Variable[_T]]`.";
+           ];
     ]
 
 
-let test_check_impossible_assert context =
-  let assert_type_errors source errors = assert_type_errors source errors context in
-  let assert_default_type_errors source errors = assert_default_type_errors source errors context in
-  assert_type_errors {|
+let test_check_impossible_assert =
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors {|
       def foo() -> None:
         x = None
         assert x
     |} [];
-  assert_type_errors {|
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors {|
       def foo(x: None) -> None:
         assert x
     |} [];
-  assert_default_type_errors
-    {|
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_default_type_errors
+           {|
       from typing import Optional, Any
       def foo(x: Optional[Any] = None) -> None:
         assert x
     |}
-    (* We should not treat `x` as having type `None` here *)
-    [];
-  assert_type_errors
-    {|
+           (* We should not treat `x` as having type `None` here *)
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       def foo(x: Derp) -> None:
         assert x
     |}
-    ["Unbound name [10]: Name `Derp` is used but not defined in the current scope."];
-  assert_default_type_errors
-    {|
+           ["Unbound name [10]: Name `Derp` is used but not defined in the current scope."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_default_type_errors
+           {|
       import typing
       def foo(x: typing.Any) -> None:
         assert x
     |}
-    [];
-
-  assert_default_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_default_type_errors
+           {|
       class Derp: ...
       def derp(x: Derp) -> None:
         assert not isinstance(x, Herp)
     |}
-    ["Unbound name [10]: Name `Herp` is used but not defined in the current scope."];
-  assert_default_type_errors
-    {|
+           ["Unbound name [10]: Name `Herp` is used but not defined in the current scope."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_default_type_errors
+           {|
       import typing
       class Derp: ...
       def derp(x: Derp, y: typing.Type[typing.Any]) -> None:
         assert not isinstance(x, y)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       class Derp: ...
       def derp(x: Derp) -> None:
         assert not isinstance(x, int)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       class Derp: ...
       def derp() -> None:
         assert not isinstance(42, Derp)
     |}
-    [];
-
-  (* Fabricated asserts won't get the type error surfaced. *)
-  assert_type_errors
-    {|
+           [];
+      (* Fabricated asserts won't get the type error surfaced. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
      from typing import Union, Dict, Any
 
      def foo(x: Dict[str, Any]) -> None:
        if isinstance(x, dict):
          pass
    |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
      from typing import Union, Dict, Any
 
      def foo() -> None:
@@ -395,9 +431,10 @@ let test_check_impossible_assert context =
        if x is None:
          pass
    |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       def f() -> None:
         a = None
         b = None
@@ -413,17 +450,19 @@ let test_check_impossible_assert context =
         if a >= 0:
           pass
     |}
+           [
+             "Revealed type [-1]: Revealed type for `a` is `typing.Optional[int]`.";
+             "Revealed type [-1]: Revealed type for `a` is `int`.";
+           ];
+    ]
+
+
+let test_if_statement =
+  test_list
     [
-      "Revealed type [-1]: Revealed type for `a` is `typing.Optional[int]`.";
-      "Revealed type [-1]: Revealed type for `a` is `int`.";
-    ];
-  ()
-
-
-let test_if_statement context =
-  let assert_type_errors source errors = assert_type_errors source errors context in
-  assert_type_errors
-    {|
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from typing import Optional
 
       def foo(some_int: int) -> None:
@@ -440,13 +479,14 @@ let test_if_statement context =
         reveal_type(x)
         reveal_type(y)
     |}
-    [
-      "Revealed type [-1]: Revealed type for `x` is `Optional[int]` (inferred: `int`).";
-      "Revealed type [-1]: Revealed type for `y` is `Optional[int]` (inferred: `int`).";
-    ];
-  (* No revealed types because that code is unreachable. *)
-  assert_type_errors
-    {|
+           [
+             "Revealed type [-1]: Revealed type for `x` is `Optional[int]` (inferred: `int`).";
+             "Revealed type [-1]: Revealed type for `y` is `Optional[int]` (inferred: `int`).";
+           ];
+      (* No revealed types because that code is unreachable. *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       from typing import Optional
 
       def foo(some_int: int) -> None:
@@ -459,17 +499,17 @@ let test_if_statement context =
         reveal_type(x)
         reveal_type(y)
     |}
-    [];
-  ()
+           [];
+    ]
 
 
 let () =
   "assert"
   >::: [
-         "check_assert" >:: test_check_assert;
-         "check_assert_functions" >:: test_check_assert_functions;
-         "check_all" >:: test_check_all;
-         "check_impossible_assert" >:: test_check_impossible_assert;
-         "if_statement" >:: test_if_statement;
+         test_check_assert;
+         test_check_assert_functions;
+         test_check_all;
+         test_check_impossible_assert;
+         test_if_statement;
        ]
   |> Test.run
