d09ce86d166fa3de4bef5fd5f957226c7e40d81f
Danny Yang <yangdanny@meta.com>
2024-08-27 14:12:43 -0700

    split lambdaTest
    Summary: make it easier to debug
    
    Reviewed By: stroxler
    
    Differential Revision: D61852048
    
    fbshipit-source-id: dc5b4bc1807b460df3172d9d26886cc1f15379c2

--- source/analysis/test/integration/lambdaTest.ml
+++ source/analysis/test/integration/lambdaTest.ml
@@ -8,221 +8,238 @@
 open OUnit2
 open IntegrationTest
 
-let test_check_map_lambda context =
-  let assert_type_errors source errors = assert_type_errors source errors context in
-
-  assert_type_errors
-    {|
+let test_check_map_lambda =
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.Iterator[int]) -> typing.Iterator[int]:
         return map(lambda x: str(x), l)
     |}
-    ["Incompatible return type [7]: Expected `Iterator[int]` but got `map[str]`."];
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `Iterator[int]` but got `map[str]`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.Iterator[int]) -> typing.Iterator[str]:
         return map(lambda x: str(x), l)
     |}
-    [];
-
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[int]) -> typing.List[int]:
         return map(lambda x: str(x), l)
     |}
-    ["Incompatible return type [7]: Expected `List[int]` but got `map[str]`."];
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `List[int]` but got `map[str]`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[int]) -> typing.List[str]:
         return map(lambda x: str(x), l)
     |}
-    ["Incompatible return type [7]: Expected `List[str]` but got `map[str]`."];
-
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `List[str]` but got `map[str]`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.Iterator[int]) -> typing.Iterator[int]:
         return list(map(lambda x: str(x), l))
     |}
-    ["Incompatible return type [7]: Expected `Iterator[int]` but got `List[str]`."];
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `Iterator[int]` but got `List[str]`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.Iterator[int]) -> typing.Iterator[str]:
         return list(map(lambda x: str(x), l))
     |}
-    ["Incompatible return type [7]: Expected `Iterator[str]` but got `List[str]`."];
-
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `Iterator[str]` but got `List[str]`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[int]) -> typing.List[int]:
         return list(map(lambda x: x, l))
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[int]) -> typing.List[str]:
         return list(map(lambda x: x, l))
     |}
-    ["Incompatible return type [7]: Expected `List[str]` but got `List[int]`."];
-
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `List[str]` but got `List[int]`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[int]) -> typing.List[str]:
         return list(map(lambda x: str(x) if isinstance(x, int) else None, l))
     |}
-    ["Incompatible return type [7]: Expected `List[str]` but got `List[Optional[str]]`."]
+           ["Incompatible return type [7]: Expected `List[str]` but got `List[Optional[str]]`."];
+    ]
 
 
-let test_check_filter_lambda context =
-  let assert_type_errors source errors = assert_type_errors source errors context in
-
-  assert_type_errors
-    {|
+let test_check_filter_lambda =
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.Iterator[int]) -> typing.Iterator[int]:
         return filter(lambda x: x * x == 10, l)
     |}
-    [];
-  (* False positive *)
-  assert_type_errors
-    {|
+           [];
+      (* False positive *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.Iterator[str]) -> typing.Iterator[str]:
         return filter(lambda x: x * x == 10, l)
     |}
-    [];
-
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[int]) -> typing.Iterator[int]:
         return filter(lambda x: x * x == 10, l)
     |}
-    [];
-  (* False positive *)
-  assert_type_errors
-    {|
+           [];
+      (* False positive *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[str]) -> typing.Iterator[str]:
         return filter(lambda x: x * x == 10, l)
     |}
-    [];
-
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[int]) -> typing.List[int]:
         return filter(lambda x: x * x == 10, l)
     |}
-    ["Incompatible return type [7]: Expected `List[int]` but got `filter[typing.Any]`."];
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `List[int]` but got `filter[typing.Any]`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.List[str]) -> typing.List[str]:
         return filter(lambda x: x * x == 10, l)
     |}
-    ["Incompatible return type [7]: Expected `List[str]` but got `filter[typing.Any]`."];
-
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `List[str]` but got `filter[typing.Any]`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.Iterator[str]) -> typing.Iterator[str]:
         return filter(lambda x: x[:4] == "http", l)
     |}
-    [];
-  (* False positive *)
-  assert_type_errors
-    {|
+           [];
+      (* False positive *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       def foo(l: typing.Iterator[int]) -> typing.Iterator[int]:
         return filter(lambda x: x[:4] == "http", l)
     |}
-    []
+           [];
+    ]
 
 
-let test_check_reduce_lambda context =
-  let assert_type_errors source errors = assert_type_errors source errors context in
-
-  (* False negative *)
-  assert_type_errors
-    {|
+let test_check_reduce_lambda =
+  test_list
+    [
+      (* False negative *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       import functools
       def foo(l: typing.Iterator[int]) -> int:
         return functools.reduce(lambda x, y: x + y, l)
     |}
-    [];
-  (* False negative *)
-  assert_type_errors
-    {|
+           [];
+      (* False negative *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       import functools
       def foo(l: typing.Iterator[int]) -> str:
         return functools.reduce(lambda x, y: x + y, l)
     |}
-    [];
-
-  (* False positive and false negative *)
-  assert_type_errors
-    {|
+           [];
+      (* False positive and false negative *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       import functools
       def foo(l: typing.Iterator[str]) -> str:
         return functools.reduce(lambda x, y: x * y, l)
     |}
-    [];
-  (* False positive and false negative *)
-  assert_type_errors
-    {|
+           [];
+      (* False positive and false negative *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       import functools
       def foo(l: typing.Iterator[str]) -> int:
         return functools.reduce(lambda x, y: x * y, l)
     |}
-    [];
-
-  (* False negative *)
-  assert_type_errors
-    {|
+           [];
+      (* False negative *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       import functools
       def foo(l: typing.Iterator[int]) -> int:
         return functools.reduce(lambda x, y: x + y, l, 1)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       import functools
       def foo(l: typing.Iterator[int]) -> str:
         return functools.reduce(lambda x, y: x + y, l, 1)
     |}
-    ["Incompatible return type [7]: Expected `str` but got `int`."];
-
-  (* False positive and false negative *)
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `str` but got `int`."];
+      (* False positive and false negative *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       import functools
       def foo(l: typing.Iterator[str]) -> str:
         return functools.reduce(lambda x, y: x * y, l, "a")
     |}
-    [];
-  ()
+           [];
+    ]
 
 
-let test_check_apply_lambda context =
-  let assert_type_errors source errors = assert_type_errors source errors context in
-
-  assert_type_errors
-    {|
+let test_check_apply_lambda =
+  test_list
+    [
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       _T = typing.TypeVar("_T")
       _S = typing.TypeVar("_S")
@@ -233,9 +250,10 @@ let test_check_apply_lambda context =
       def foo(y: int) -> int:
         return apply(lambda x: x, y)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       _T = typing.TypeVar("_T")
       _S = typing.TypeVar("_S")
@@ -246,10 +264,10 @@ let test_check_apply_lambda context =
       def foo(y: int) -> str:
         return apply(lambda x: x, y)
     |}
-    ["Incompatible return type [7]: Expected `str` but got `int`."];
-
-  assert_type_errors
-    {|
+           ["Incompatible return type [7]: Expected `str` but got `int`."];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       _T = typing.TypeVar("_T")
       _S = typing.TypeVar("_S")
@@ -260,9 +278,10 @@ let test_check_apply_lambda context =
       def foo(y: int) -> str:
         return apply(lambda x: str(x), y)
     |}
-    [];
-  assert_type_errors
-    {|
+           [];
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
       import typing
       _T = typing.TypeVar("_T")
       _S = typing.TypeVar("_S")
@@ -273,15 +292,16 @@ let test_check_apply_lambda context =
       def foo(y: int) -> int:
         return apply(lambda x: str(x), y)
     |}
-    ["Incompatible return type [7]: Expected `int` but got `str`."]
+           ["Incompatible return type [7]: Expected `int` but got `str`."];
+    ]
 
 
 let () =
   "lambda"
   >::: [
-         "check_map_lambda" >:: test_check_map_lambda;
-         "check_filter_lambda" >:: test_check_filter_lambda;
-         "check_reduce_lambda" >:: test_check_reduce_lambda;
-         "check_apply_lambda" >:: test_check_apply_lambda;
+         test_check_map_lambda;
+         test_check_filter_lambda;
+         test_check_reduce_lambda;
+         test_check_apply_lambda;
        ]
   |> Test.run
