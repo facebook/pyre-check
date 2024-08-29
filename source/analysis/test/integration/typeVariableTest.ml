31091a484c0c830fce5c2f1a2b225d46be8452c7
Zeina Migeed <migeedz@meta.com>
2024-08-29 15:47:09 -0700

    Add two unit tests for handling generic methods.
    Summary:
    In this diff stack, we add support for handling generic functions. We add two unit test for handling generic methods.
    
    One works already and the other one doesn't.
    
    Explanation:
    
    We resolve types correctly in this example:
    
              T = TypeVar("T")
                class A(Generic[T]):
                    def func(self, x: T) -> T:
                        ...
    
                    def func2[U](self, x:U) -> U:
                        ...
    
    The same applies to this example:
    
                class A:
                    def func2[U](self, x:U) -> U:
                        …
    
    However, if we switch to the new syntax for generic classes, then we do not resolve the types for func2
    
                class A[T]:
                    def func(self, x: T) -> T:
                        ...
    
                    def func2[U](self, x:U) -> U:
                        …
    
    In that example, we resolve instantiations a of A[int] for example, as unknown and then a.func2 as
    any vs what we would want, which is to resolve both of these types correctly.
    
    So we believe that it is a prerequisite to support generic classes with the new syntax in order to get support for generic methods inside classes that use this new syntax.
    
    Reviewed By: rchen152
    
    Differential Revision: D61890621
    
    fbshipit-source-id: 37b936bb15d8d052a19ecf4935d300340554d89d

--- source/analysis/test/integration/typeVariableTest.ml
+++ source/analysis/test/integration/typeVariableTest.ml
@@ -25,24 +25,56 @@ let test_type_variable_scoping =
              "Mutually recursive type variables [36]: Solving type variables for call `f1` led to \
               infinite recursion.";
            ];
-      (* TODO T194670955: reveal_type(a.func(42)) should return int once we update
-         attributeResolution *)
+      (* TODO T194670955: reveal_type(a.func(42)) should return int and eveal_type(a.func2("42"))
+         should return str. This will work once we support generic classes in PEP695 syntax *)
       labeled_test_case __FUNCTION__ __LINE__
       @@ assert_type_errors
            {|
             class A[T]:
                 def func(self, x: T) -> T:
                     ...
+
+                def func2[U](self, x:U) -> U:
+                    ...
+
             a = A[int]()
             reveal_type(a.func(42))
+            reveal_type(a.func2("42"))
             |}
            [
              "Parsing failure [404]: PEP 695 type params are unsupported";
              "Unbound name [10]: Name `T` is used but not defined in the current scope.";
+             "Parsing failure [404]: PEP 695 type params are unsupported";
+             "Unbound name [10]: Name `U` is used but not defined in the current scope.";
              "Missing global annotation [5]: Globally accessible variable `a` has no type \
               specified.";
              "Undefined attribute [16]: `A` has no attribute `__getitem__`.";
              "Revealed type [-1]: Revealed type for `a.func(42)` is `typing.Any`.";
+             "Revealed type [-1]: Revealed type for `a.func2(\"42\")` is `typing.Any`.";
+           ];
+      (* PEP695 generic methods from non-generic classes *)
+      labeled_test_case __FUNCTION__ __LINE__
+      @@ assert_type_errors
+           {|
+            class A:
+
+                def func2[U](self, x:U) -> U:
+                    ...
+
+            a = A()
+            reveal_type(a)
+            reveal_type(a.func2)
+            reveal_type(a.func2("42"))
+            |}
+           [
+             "Parsing failure [404]: PEP 695 type params are unsupported";
+             "Unbound name [10]: Name `U` is used but not defined in the current scope.";
+             "Revealed type [-1]: Revealed type for `a` is `A`.";
+             "Revealed type [-1]: Revealed type for `a.func2` is \
+              `BoundMethod[typing.Callable(A.func2)[[Named(self, A), Named(x, Variable[U])], \
+              Variable[U]], A]`.";
+             "Revealed type [-1]: Revealed type for `a.func2(\"42\")` is \
+              `typing_extensions.Literal['42']`.";
            ];
       labeled_test_case __FUNCTION__ __LINE__
       @@ assert_type_errors
