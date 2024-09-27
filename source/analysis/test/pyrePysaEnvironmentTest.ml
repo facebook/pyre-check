7887e31f6ae59ddab1e6095a3385869c82344acc
Steven Troxler <stroxler@meta.com>
2024-09-27 09:09:45 -0700

    Cleanup: move tests of resolve_qualified_name_to_global
    Summary:
    I didn't want to muck up the previous diff - which was more substantial
    than most of my Pyre/Pysa refactors - with unimportant test changes.
    
    But the fact is that this test was entirely of the function we moved into
    pyrePysaEnvironment, so the test ought to live there going forward.
    
    Reviewed By: arthaud
    
    Differential Revision: D63498405
    
    fbshipit-source-id: bd4e388d8c0191296e6719c779e63d8cf0ac0a29

--- source/analysis/test/pyrePysaEnvironmentTest.ml
+++ source/analysis/test/pyrePysaEnvironmentTest.ml
@@ -6,10 +6,13 @@
  *)
 
 open Core
+open Pyre
 open OUnit2
 open Analysis
 open Ast
 open Test
+module PyrePysaEnvironment = Analysis.PyrePysaEnvironment
+module Global = PyrePysaEnvironment.ModelQueries.Global
 
 let test_source_is_unit_test context =
   let assert_is_unit_test ?(expected = true) ?(extra_sources = []) source =
@@ -46,4 +49,391 @@ let test_source_is_unit_test context =
   ()
 
 
-let () = "pyrePysaApi" >::: ["source_is_unit_test" >:: test_source_is_unit_test] |> Test.run
+let test_resolve_qualified_name_to_global context =
+  let assert_resolve ~context sources name ~expect =
+    PyrePysaEnvironment.ModelQueries.invalidate_cache ();
+    let pyre_api =
+      ScratchProject.setup ~context sources |> ScratchProject.pyre_pysa_read_only_api
+    in
+    let actual =
+      PyrePysaEnvironment.ModelQueries.resolve_qualified_name_to_global
+        pyre_api
+        (Ast.Reference.create name)
+    in
+    let printer = function
+      | None -> "None"
+      | Some global -> Global.show global
+    in
+    assert_equal ~printer expect actual
+  in
+  let create_parameter ?(annotation = Type.Any) ?(default = false) name =
+    Type.Callable.CallableParamType.Named { name = "$parameter$" ^ name; annotation; default }
+  in
+  let create_callable ?name ?(overloads = []) ?(parameters = []) ?(annotation = Type.Any) () =
+    Type.Callable.create
+      ?name:(name >>| Reference.create)
+      ~overloads
+      ~parameters:(Type.Callable.Defined parameters)
+      ~annotation
+      ()
+  in
+  (* Most common cases. *)
+  assert_resolve
+    ~context
+    ["test.py", {|
+      def foo():
+        return
+    |}]
+    "test.foo"
+    ~expect:(Some (Global.Attribute (create_callable ~name:"test.foo" ())));
+  assert_resolve
+    ~context
+    ["test.py", {|
+      class Foo:
+        def bar(self):
+          return
+    |}]
+    "test.Foo.bar"
+    ~expect:
+      (Some
+         (Global.Attribute
+            (create_callable
+               ~parameters:[create_parameter ~annotation:(Type.Primitive "test.Foo") "self"]
+               ())));
+  assert_resolve
+    ~context
+    [
+      ( "test.py",
+        {|
+          from typing import Callable
+          foo: Callable[[], None] = lambda: None
+        |}
+      );
+    ]
+    "test.foo"
+    ~expect:(Some (Global.Attribute (create_callable ~annotation:Type.NoneType ())));
+  assert_resolve
+    ~context
+    [
+      ( "test.py",
+        {|
+          from typing import Callable
+          class Foo:
+            bar: Callable[[], None] = lambda: None
+        |}
+      );
+    ]
+    "test.Foo.bar"
+    ~expect:(Some (Global.Attribute (create_callable ~annotation:Type.NoneType ())));
+  assert_resolve
+    ~context
+    ["test.py", {|
+      class Foo:
+        pass
+    |}]
+    "test.Foo"
+    ~expect:(Some Global.Class);
+  assert_resolve
+    ~context
+    ["test.py", {|
+      class Foo:
+        class Bar:
+          pass
+    |}]
+    "test.Foo.Bar"
+    ~expect:(Some Global.Class);
+  assert_resolve
+    ~context
+    ["test.py", {|
+      def foo():
+        return None
+    |}]
+    "test"
+    ~expect:(Some Global.Module);
+  assert_resolve
+    ~context
+    ["test.py", {|
+      class Foo:
+        x: int = 1
+    |}]
+    "test.Foo.x"
+    ~expect:(Some (Global.Attribute Type.integer));
+  assert_resolve
+    ~context
+    ["test.py", {|
+      x: int = 1
+    |}]
+    "test.x"
+    ~expect:(Some (Global.Attribute Type.integer));
+  assert_resolve
+    ~context
+    ["test.py", {|
+      from typing import Any
+      x: Any = 1
+    |}]
+    "test.x"
+    ~expect:(Some (Global.Attribute Type.Any));
+  assert_resolve
+    ~context
+    [
+      ( "test.py",
+        {|
+          from typing import Type
+          class Foo:
+            pass
+          x: Type[Foo] = Foo
+        |}
+      );
+    ]
+    "test.x"
+    ~expect:
+      (Some
+         (Global.Attribute
+            (Type.Parametric { name = "type"; arguments = [Single (Type.Primitive "test.Foo")] })));
+
+  (* Symbol is not found. *)
+  assert_resolve
+    ~context
+    ["test.py", {|
+      def foo():
+        return
+    |}]
+    "test.bar"
+    ~expect:None;
+  assert_resolve
+    ~context
+    ["test.py", {|
+      class Foo:
+        def bar():
+          return
+    |}]
+    "test.Foo.baz"
+    ~expect:None;
+  assert_resolve
+    ~context
+    ["test.py", {|
+      class Foo:
+        pass
+    |}]
+    "test.Bar"
+    ~expect:None;
+  assert_resolve ~context ["foo.py", "x: int = 1"] "bar" ~expect:None;
+
+  (* Decorators. *)
+  assert_resolve
+    ~context
+    [
+      ( "test.py",
+        {|
+          class Memoize:
+            def __init__(self, f):
+              self.f = f
+            def __call__(self, *args, **kwargs):
+              pass
+
+          def memoize(f) -> Memoize:
+            return Memoize(f)
+
+          @memoize
+          def foo(x: int) -> int:
+            return x
+        |}
+      );
+    ]
+    "test.foo"
+    ~expect:
+      (Some
+         (Global.Attribute
+            (create_callable
+               ~annotation:Type.integer
+               ~name:"test.foo"
+               ~parameters:[create_parameter ~annotation:Type.integer "x"]
+               ())));
+  assert_resolve
+    ~context
+    [
+      ( "test.py",
+        {|
+          class Memoize:
+            def __init__(self, f):
+              self.f = f
+            def __call__(self, *args, **kwargs):
+              pass
+
+          def memoize(f) -> Memoize:
+            return Memoize(f)
+
+          class Foo:
+            @memoize
+            def bar(self, x: int) -> int:
+              return x
+        |}
+      );
+    ]
+    "test.Foo.bar"
+    ~expect:
+      (Some
+         (Global.Attribute
+            (create_callable
+               ~annotation:Type.integer
+               ~parameters:
+                 [
+                   create_parameter ~annotation:(Type.Primitive "test.Foo") "self";
+                   create_parameter ~annotation:Type.integer "x";
+                 ]
+               ())));
+
+  (* Overloads *)
+  assert_resolve
+    ~context
+    [
+      ( "test.py",
+        {|
+          from typing import overload
+          class Foo:
+            @overload
+            def bar(self, x: int) -> str: ...
+            @overload
+            def bar(self, x: str) -> int: ...
+        |}
+      );
+    ]
+    "test.Foo.bar"
+    ~expect:
+      (Some
+         (Global.Attribute
+            (Type.Callable.create
+               ~parameters:
+                 (Type.Callable.Defined
+                    [
+                      create_parameter ~annotation:(Type.Primitive "test.Foo") "self";
+                      create_parameter ~annotation:Type.integer "x";
+                    ])
+               ~annotation:Type.string
+               ~overloads:
+                 [
+                   {
+                     parameters =
+                       Defined
+                         [
+                           create_parameter ~annotation:(Type.Primitive "test.Foo") "self";
+                           create_parameter ~annotation:Type.integer "x";
+                         ];
+                     annotation = Type.string;
+                   };
+                   {
+                     parameters =
+                       Defined
+                         [
+                           create_parameter ~annotation:(Type.Primitive "test.Foo") "self";
+                           create_parameter ~annotation:Type.string "x";
+                         ];
+                     annotation = Type.integer;
+                   };
+                 ]
+               ())));
+
+  (* Top. *)
+  assert_resolve
+    ~context
+    [
+      ( "test.py",
+        {|
+          class Foo:
+            def bar(self):
+              pass
+            baz = bar
+        |}
+      );
+    ]
+    "test.Foo.baz"
+    ~expect:(Some (Global.Attribute Type.Top));
+
+  (* Definition in type stub. *)
+  assert_resolve
+    ~context
+    ["test.pyi", {|
+      def foo() -> None: ...
+    |}]
+    "test.foo"
+    ~expect:
+      (Some (Global.Attribute (create_callable ~annotation:Type.NoneType ~name:"test.foo" ())));
+  assert_resolve
+    ~context
+    ["test.pyi", {|
+      class Foo:
+        def bar(self) -> None: ...
+    |}]
+    "test.Foo.bar"
+    ~expect:
+      (Some
+         (Global.Attribute
+            (create_callable
+               ~annotation:Type.NoneType
+               ~parameters:[create_parameter ~annotation:(Type.Primitive "test.Foo") "self"]
+               ())));
+  assert_resolve
+    ~context
+    [
+      ( "test.pyi",
+        {|
+          from typing import Callable
+          foo: Callable[[], None]
+        |} );
+    ]
+    "test.foo"
+    ~expect:(Some (Global.Attribute (create_callable ~annotation:Type.NoneType ())));
+  assert_resolve
+    ~context
+    ["test.pyi", {|
+      x: int = 1
+    |}]
+    "test.x"
+    ~expect:(Some (Global.Attribute Type.integer));
+
+  (* Deeply nested code, where outer packages are not importable *)
+  assert_resolve
+    ~context
+    [
+      "outer/middle/inner/a.py", {|
+      def foo() -> None: ...
+    |};
+      "outer/middle/inner/b.py", {|
+      from .a import foo
+    |};
+    ]
+    "outer.middle.inner.b.foo"
+    ~expect:
+      (Some
+         (Global.Attribute
+            (create_callable ~annotation:Type.NoneType ~name:"outer.middle.inner.a.foo" ())));
+  assert_resolve
+    ~context
+    [
+      "outer/middle/inner/a.py", {|
+      class Foo:
+        def bar(self): ...
+    |};
+      "outer/middle/inner/b.py", {|
+      from .a import Foo
+    |};
+    ]
+    "outer.middle.inner.b.Foo.bar"
+    ~expect:
+      (Some
+         (Global.Attribute
+            (create_callable
+               ~name:"outer.middle.inner.a.Foo.bar"
+               ~parameters:
+                 [create_parameter ~annotation:(Type.Primitive "outer.middle.inner.a.Foo") "self"]
+               ())));
+  ()
+
+
+let () =
+  "pyrePysaApi"
+  >::: [
+         "source_is_unit_test" >:: test_source_is_unit_test;
+         "resolve_qualified_name_to_global" >:: test_resolve_qualified_name_to_global;
+       ]
+  |> Test.run
