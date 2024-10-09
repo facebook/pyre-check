15e2108e439cc60fd7da2fe0a974a72519dd1a3c
Maxime Arthaud <arthaud@meta.com>
2024-10-09 13:54:37 -0700

    Add annotations for integration tests
    Summary:
    See context in https://docs.google.com/document/d/1SwOWt_1rO9i7paF8jVtTVJsw1W1p9G5ROEP7b-B63Xw/edit
    
    This diff defines the decorators used to annotate our integration tests.
    
    Reviewed By: r0rshark
    
    Differential Revision: D64109129
    
    fbshipit-source-id: 84d6d1b46276d5d82ffcf2c993bcf05b9cdc10b2

---
+++ tools/pysa_integration_tests/annotations.py
@@ -0,0 +1,45 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+"""
+This module contains annotations used in integration tests to
+define where we expect to find issues.
+"""
+
+from typing import Optional, TypeVar
+
+T = TypeVar("T")
+
+
+class ExpectIssue:
+    def __new__(
+        cls,
+        *,
+        code: int,
+        line: Optional[int] = None,
+        task: Optional[str] = None,
+        currently_found: bool = True,
+    ) -> "ExpectIssue":
+        return super().__new__(cls)
+
+    def __call__(self, f: T) -> T:
+        return f
+
+
+class ExpectNoIssue:
+    def __new__(
+        cls,
+        *,
+        code: int,
+        line: Optional[int] = None,
+        task: Optional[str] = None,
+        currently_found: bool = False,
+    ) -> "ExpectNoIssue":
+        return super().__new__(cls)
+
+    def __call__(self, f: T) -> T:
+        return f
