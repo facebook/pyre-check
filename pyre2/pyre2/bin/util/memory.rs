4026003366ec2a855e0feba5164eb8c1fa4bdf87
Neil Mitchell <ndmitchell@meta.com>
2024-12-01 01:54:58 -0800

    Include jemalloc stats
    Summary: Now one large example shows: `Memory usage: physical 85.7 GiB, allocated 93.2 GiB, active 93.3 GiB`
    
    Reviewed By: samwgoldman
    
    Differential Revision: D66580053
    
    fbshipit-source-id: f2b26ecdaca37e7f48a2a00df70e8c7f54b39361

--- pyre2/pyre2/bin/util/memory.rs
+++ pyre2/pyre2/bin/util/memory.rs
@@ -12,10 +12,12 @@ use dupe::Dupe;
 use human_bytes::human_bytes;
 use memory_stats::memory_stats;
 
+use crate::util::display::commas_iter;
+
 #[derive(
     Debug, Clone, Copy, Dupe, Default, PartialEq, Eq, PartialOrd, Ord, Hash
 )]
-pub struct Bytes(usize);
+pub struct Bytes(u64);
 
 impl Display for Bytes {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -26,22 +28,74 @@ impl Display for Bytes {
 #[derive(Debug, Clone, Default)]
 pub struct MemoryUsage {
     pub physical: Option<Bytes>,
+    pub allocated: Option<Bytes>,
+    pub active: Option<Bytes>,
 }
 
 impl Display for MemoryUsage {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if let Some(physical) = self.physical {
-            write!(f, "physical {physical}")
-        } else {
+        let fields = [
+            ("physical", self.physical),
+            ("allocated", self.allocated),
+            ("active", self.active),
+        ];
+        if fields.iter().all(|(_, v)| v.is_none()) {
             write!(f, "unknown")
+        } else {
+            write!(
+                f,
+                "{}",
+                commas_iter(|| fields
+                    .iter()
+                    .filter_map(|(name, v)| v.map(|v| format!("{name} {v}"))))
+            )
         }
     }
 }
 
 impl MemoryUsage {
     pub fn new() -> Self {
+        let jemalloc = get_jemalloc_stats();
         Self {
-            physical: memory_stats().map(|x| Bytes(x.physical_mem)),
+            physical: memory_stats().map(|x| Bytes(x.physical_mem as u64)),
+            allocated: jemalloc.allocated,
+            active: jemalloc.active,
         }
+    }
+}
+
+pub struct JemallocStats {
+    pub active: Option<Bytes>,
+    pub allocated: Option<Bytes>,
+}
+
+#[cfg(not(fbcode_build))]
+fn get_jemalloc_stats() -> JemallocStats {
+    JemallocStats {
+        active: None,
+        allocated: None,
+    }
+}
+
+#[cfg(fbcode_build)]
+fn get_jemalloc_stats() -> JemallocStats {
+    fn get_all() -> Option<serde_json::Value> {
+        // This options configuration flag string is passed to `malloc_stats_print()`.
+        // Explanation: (<https://jemalloc.net/jemalloc.3.html>)
+        // The default configuration prints minimal output, formatted as JSON.
+        let alloc_stats_options = "Jmdablxg";
+        serde_json::from_str(&allocator_stats::malloc_stats(alloc_stats_options).ok()?).ok()
+    }
+
+    fn get_field(stats: &serde_json::Value, val: &str) -> Option<Bytes> {
+        Some(Bytes(
+            stats.get("jemalloc")?.get("stats")?.get(val)?.as_u64()?,
+        ))
+    }
+
+    let v = get_all().unwrap_or_default();
+    JemallocStats {
+        active: get_field(&v, "active"),
+        allocated: get_field(&v, "allocated"),
     }
 }
