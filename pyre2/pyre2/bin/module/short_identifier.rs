7f485957c6aff90c5211f91178887f58c962bcf1
Neil Mitchell <ndmitchell@meta.com>
2024-12-02 12:38:42 -0800

    Add a test that ShortIdentifier Display works
    Reviewed By: yangdanny97
    
    Differential Revision: D66637415
    
    fbshipit-source-id: 336516033b1d113ac35380eaf5568b15c6067e27

--- pyre2/pyre2/bin/module/short_identifier.rs
+++ pyre2/pyre2/bin/module/short_identifier.rs
@@ -18,6 +18,7 @@ use crate::util::display::DisplayWith;
 /// An identifier, where we can drop the `Name` part because it came from a `ModuleInfo`.
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct ShortIdentifier(TextRange);
+
 impl ShortIdentifier {
     pub fn new(name: &Identifier) -> Self {
         Self(name.range)
@@ -38,5 +39,49 @@ impl Ranged for ShortIdentifier {
 impl DisplayWith<ModuleInfo> for ShortIdentifier {
     fn fmt(&self, f: &mut fmt::Formatter<'_>, ctx: &ModuleInfo) -> fmt::Result {
         write!(f, "{}", ctx.code_at(self.0))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use ruff_python_ast::Expr;
+    use ruff_python_ast::Stmt;
+    use ruff_python_ast::StmtAssign;
+
+    use super::*;
+    use crate::error::collector::ErrorCollector;
+    use crate::module::module_name::ModuleName;
+
+    fn from_expr(x: &Expr) -> ShortIdentifier {
+        match x {
+            Expr::Name(x) => ShortIdentifier::expr_name(x),
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn test_display_short_identifier() {
+        let module_info = ModuleInfo::new(
+            ModuleName::from_str("foo"),
+            Path::new("foo.py").to_owned(),
+            "hello_world = Baz123.attribute".to_owned(),
+            false,
+        );
+        let module = module_info.parse(&ErrorCollector::new());
+        let show = |x: &ShortIdentifier| module_info.display(x).to_string();
+        if let Stmt::Assign(StmtAssign {
+            targets: x1,
+            value: box Expr::Attribute(x23),
+            ..
+        }) = &module.body[0]
+        {
+            assert_eq!(show(&from_expr(&x1[0])), "hello_world");
+            assert_eq!(show(&from_expr(&x23.value)), "Baz123");
+            assert_eq!(show(&ShortIdentifier::new(&x23.attr)), "attribute");
+            return;
+        }
+        unreachable!();
     }
 }
