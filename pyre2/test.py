4dc5495656bcb4bc27352aeb6dfef1c0e41f0af4
Neil Mitchell <ndmitchell@meta.com>
2024-12-02 07:03:06 -0800

    Fix the license header on test.py
    Reviewed By: yangdanny97
    
    Differential Revision: D66646716
    
    fbshipit-source-id: 28397b1699c59a6921f5e3ae5b59a933f4a97c3d

--- pyre2/test.py
+++ pyre2/test.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 # pyre-unsafe
 
