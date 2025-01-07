1d958a1b829d8593f4f33d3bc1853ed1643667ca
Neil Mitchell <ndmitchell@meta.com>
2025-01-07 14:22:08 -0800

    No longer postprocess the backtrace
    Summary: Someone fixed the Meta VS Code.
    
    Reviewed By: rchen152
    
    Differential Revision: D67912305
    
    fbshipit-source-id: 4a68319550ecb5702c39c6035ac1915a94056cbc

--- pyre2/pyre2/bin/util/panic.rs
+++ pyre2/pyre2/bin/util/panic.rs
@@ -10,17 +10,7 @@ use std::backtrace::Backtrace;
 pub fn exit_on_panic() {
     std::panic::set_hook(Box::new(move |info| {
         eprintln!("Thread panicked, shutting down: {}", info);
-        // The Meta VS Code can't find files that look like
-        // `./foo/bar.rs` in backtraces, so we replace them with `foo/bar.rs`.
-        // Ideally we'd fix VS Code to understand `./` in paths, or fix
-        // backtraces to not include the `./`.
-        // But this improves our efficiency in the short term.
-        eprintln!(
-            "Backtrace:\n{}",
-            Backtrace::force_capture()
-                .to_string()
-                .replace("at ./", "at ")
-        );
+        eprintln!("Backtrace:\n{}", Backtrace::force_capture());
         std::process::exit(1);
     }));
 }
