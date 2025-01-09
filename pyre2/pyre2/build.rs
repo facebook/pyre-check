875040c00afd1f434ec5f230c5fc049d1864c891
Jia Chen <grievejia@meta.com>
2025-01-09 14:17:33 -0800

    Bundle & embed typeshed sources into Pyre2 binary using `build.rs`
    Summary:
    build_succeeded
    
    With D67927524 having all typeshed source files checked in, this diff attempts to bundle & embed all of them into the main Pyre2 binary.
    
    This is not that hard to do if there's only 1 way to build Pyre. But given that we now needs to maintain both the internal Buck build and the oss Cargo build, having both of them work at the same time while minimizing code duplication becomes a challenge.
    
    The approach adopted in this diff is to rely on [`build.rs`](https://doc.rust-lang.org/cargo/reference/build-scripts.html) in the Cargo build, while having the same build script running as a genrule in the Buck build. To make sure that both builds are running the same logic, I had to make this `build.rs` script runnable under both build systems. Problem is, Buck and Cargo have different ideas of where to locate the input/output of the build script. To bridge the gap I need to add some funny branches to `build.rs`, to make sure both build systems get accommodated.
    
    The build script itself is rather straightforward: it takes the typeshed root dir as input, and put all files into a single tar archive. Zstd is used for compression to reduce the archive size (and hence the size bloat in Pyre2 binary).
    
    This diff only puts the tar archive into the binary without using it anywhere. In next diff we'll start to add logic to read from the embedded archive.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D67954102
    
    fbshipit-source-id: 0cd1ae203ecf86deb2cc3da22cb507b7b24b7634

---
+++ pyre2/pyre2/build.rs
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::env;
+use std::fs::File;
+use std::path::Path;
+use std::path::PathBuf;
+
+fn get_input_path() -> PathBuf {
+    match env::var_os("TYPESHED_ROOT") {
+        Some(root) => {
+            // When building with Buck, typeshed filegroup dir is passed in using TYPESHED_ROOT
+            Path::new(&root).join("typeshed")
+        }
+        None => {
+            // When building with Cargo, we could locate typeshed directly using relative dir
+            PathBuf::from("stubs/typeshed")
+        }
+    }
+}
+
+fn get_output_path() -> Result<PathBuf, std::env::VarError> {
+    // When building with Buck, output artifact path is specified directly using this env var
+    match env::var_os("OUT") {
+        Some(path) => Ok(PathBuf::from(path)),
+        None => {
+            // When building with Cargo, this env var is the containing directory of the artifact
+            let out_dir = env::var("OUT_DIR")?;
+            Ok(Path::new(&out_dir).join("typeshed.tar.zst"))
+        }
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
+    // Only watch for metadata changes to avoid having Cargo repeatedly crawling for
+    // changes in the entire typeshed dir.
+    println!("cargo::rerun-if-changed=stubs/typeshed_metadata.json");
+
+    let input_path = get_input_path();
+    let output_path = get_output_path().unwrap();
+    let output_file = File::create(output_path)?;
+    let encoder = zstd::stream::write::Encoder::new(output_file, 0)?;
+    let mut tar = tar::Builder::new(encoder);
+    tar.append_dir_all("", input_path)?;
+    let encoder = tar.into_inner()?;
+    encoder.finish()?;
+    Ok(())
+}
