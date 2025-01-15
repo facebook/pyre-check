2e27fe3ced400f5ed3637336da327db65c4c76e8
Steven Troxler <stroxler@meta.com>
2025-01-15 14:21:44 -0800

    Verify decorator constraint solving
    Summary:
    To get started I used a trivial decorator that spits out a primitive type, but
    let's add tests verifying constraint solving (which most real decorators require)
    for signature-preserving and signature-modifying decorators.
    
    These tests work, although I encountered a bug in `assert_type` which causes
    the assert to fail even though the type appears to be right; we will need to
    investigate.
    
    Reviewed By: rchen152
    
    Differential Revision: D68218913
    
    fbshipit-source-id: e4aabb9938a65877ab2f6b1098e332051243c223

--- pyre2/pyre2/bin/test/decorators.rs
+++ pyre2/pyre2/bin/test/decorators.rs
@@ -6,6 +6,7 @@
  */
 
 use crate::testcase;
+use crate::testcase_with_bug;
 
 testcase!(
     test_simple_function_decorator,
@@ -19,5 +20,36 @@ def decorated(x: int) -> int:
    return x
 
 assert_type(decorated, int)
+    "#,
+);
+
+testcase_with_bug!(
+    "The logic tested here passes, but assert_type is running into a problem we need to debug.",
+    test_identity_function_decorator,
+    r#"
+from typing import assert_type, Callable, Any
+
+def decorator[T](f: T) -> T: ...
+
+@decorator
+def decorated(x: int) -> str:
+   return f"{x}"
+
+assert_type(decorated, Callable[[int], str])  # E: assert_type(Callable[[int], str], Callable[[int], str])
+    "#,
+);
+
+testcase!(
+    test_signature_modifying_function_decorator,
+    r#"
+from typing import assert_type, Callable, Any
+
+def decorator[T, R](f: Callable[[T], R]) -> Callable[[T, T], R]: ...
+
+@decorator
+def decorated(x: int) -> str:
+   return f"{x}"
+
+assert_type(decorated, Callable[[int, int], str])
     "#,
 );
