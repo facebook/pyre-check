cb1fbb24befb0017996cfa02a0055fd6e6dbeb5a
Rebecca Chen (Python) <rechen@meta.com>
2025-01-21 15:10:31 -0800

    Add a few dataclass tests
    Summary:
    * Adds a couple already passing tests for attribute lookup, to make sure I don't break this.
    * Adds a failing test for constructing a dataclass.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D68462022
    
    fbshipit-source-id: 9468f7472e5c71343ff32ede2ba8c7b41d0481c1

--- pyre2/pyre2/bin/test/dataclasses.rs
+++ pyre2/pyre2/bin/test/dataclasses.rs
@@ -6,6 +6,7 @@
  */
 
 use crate::testcase;
+use crate::testcase_with_bug;
 
 testcase!(
     test_def,
@@ -17,5 +18,47 @@ class Data:
     x: int
     y: str
 assert_type(Data, type[Data])
+    "#,
+);
+
+testcase!(
+    test_fields,
+    r#"
+from typing import assert_type
+import dataclasses
+@dataclasses.dataclass
+class Data:
+    x: int
+    y: str
+def f(d: Data):
+    assert_type(d.x, int)
+    assert_type(d.y, str)
+    "#,
+);
+
+testcase!(
+    test_generic,
+    r#"
+from typing import assert_type
+import dataclasses
+@dataclasses.dataclass
+class Data[T]:
+    x: T
+def f(d: Data[int]):
+    assert_type(d.x, int)
+    "#,
+);
+
+testcase_with_bug!(
+    "Need to support dataclass-generated __init__",
+    test_construction,
+    r#"
+import dataclasses
+@dataclasses.dataclass
+class Data:
+    x: int
+    y: str
+Data(0, "1")  # Should be ok  # E: Expected 0 positional arguments
+Data(0, 1)  # Should be an arg type mismatch  # E: Expected 0 positional arguments
     "#,
 );
