dc776974c5fe971da378f65740adbb9d7da57e24
Neil Mitchell <ndmitchell@meta.com>
2025-01-30 03:36:49 -0800

    Add a lot of ParamSpec tests
    Summary: All the ones copied from https://typing.readthedocs.io/en/latest/spec/generics.html#paramspec, we fail most, so aiming to incrementally fix these over the next day or so.
    
    Reviewed By: stroxler
    
    Differential Revision: D68831198
    
    fbshipit-source-id: 0dc6666e6a1621f6d9bc1d4bb6994ffc5a0f6f9f

--- pyre2/pyre2/bin/test/paramspec.rs
+++ pyre2/pyre2/bin/test/paramspec.rs
@@ -5,7 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+//! Many of these tests come from <https://typing.readthedocs.io/en/latest/spec/generics.html#paramspec>.
+
 use crate::testcase;
+use crate::testcase_with_bug;
 
 testcase!(
     test_callable_param_spec,
@@ -47,5 +50,253 @@ f("A", True)
 f(a="A", b=True)
 f("A", "A")  # E: Literal['A'] <: bool
 assert_type(f("A", True), str)
+"#,
+);
+
+testcase_with_bug!(
+    "All these cases should be rejected, but they are not",
+    test_paramspec_in_right_place,
+    r#"
+from typing import Callable, Concatenate, ParamSpec
+
+P = ParamSpec("P")
+
+def foo(x: P) -> P: ...                           # Rejected
+def foo(x: Concatenate[int, P]) -> int: ...       # Rejected
+def foo(x: list[P]) -> None: ...                  # Rejected
+def foo(x: Callable[[int, str], P]) -> None: ...  # Rejected
+"#,
+);
+
+testcase_with_bug!(
+    "Reject some things that should be accepted, accept some things that should be rejected",
+    test_paramspec_generic,
+    r#"
+from typing import Callable, Concatenate, ParamSpec, Generic, TypeVar
+
+T = TypeVar("T")
+P = ParamSpec("P")
+P_2 = ParamSpec("P_2")
+
+class X(Generic[T, P]):
+  f: Callable[P, int]
+  x: T
+
+def f1(x: X[int, P_2]) -> str: ...                    # Accepted
+def f2(x: X[int, Concatenate[int, P_2]]) -> str: ...  # Accepted
+def f3(x: X[int, [int, bool]]) -> str: ...            # Accepted # E: untype, got list[type[bool] | type[int]]
+def f4(x: X[int, ...]) -> str: ...                    # Accepted
+def f5(x: X[int, int]) -> str: ...                    # Rejected
+
+class X2[T, **P]:
+  f: Callable[P, int]
+  x: T
+
+def f6(x: X2[int, [int, bool]]) -> str: ...           # Accepted # E: untype, got list[type[bool] | type[int]]
+"#,
+);
+
+testcase_with_bug!(
+    "Can't yet deal with ParamSpec brackets properly",
+    test_paramspec_omit_brackets,
+    r#"
+from typing import Callable, Generic, ParamSpec
+
+P = ParamSpec("P")
+
+class Z(Generic[P]):
+  f: Callable[P, int]
+
+def f(x: Z[[int, str, bool]]) -> str: ...   # Accepted # E: untype, got list[type[bool] | type[int] | type[str]]
+def f(x: Z[int, str, bool]) -> str: ...     # Equivalent  # E: Expected 1 type argument for class `Z`, got 3.
+"#,
+);
+
+testcase_with_bug!(
+    "Can't merge too callables to make the reveal_type, otherwise mostly correct",
+    test_paramspec_repeated,
+    r#"
+from typing import Callable, ParamSpec, reveal_type
+
+P = ParamSpec("P")
+
+def foo(x: Callable[P, int], y: Callable[P, int]) -> Callable[P, bool]: ...
+
+def x_y(x: int, y: str) -> int: ...
+def y_x(y: int, x: str) -> int: ...
+
+reveal_type(foo(x_y, x_y)) # E: (x: int, y: str) -> int <: (x: int, y: str) -> int  # E: revealed type: (x: int, y: str) -> bool
+               # Should return (x: int, y: str) -> bool
+               # (a callable with two positional-or-keyword parameters)
+
+foo(x_y, y_x) # E: EXPECTED (y: int, x: str) -> int <: (x: int, y: str) -> int
+               # Could return (a: int, b: str, /) -> bool
+               # (a callable with two positional-only parameters)
+               # This works because both callables have types that are
+               # behavioral subtypes of Callable[[int, str], int]
+
+
+def keyword_only_x(*, x: int) -> int: ...
+def keyword_only_y(*, y: int) -> int: ...
+foo(keyword_only_x, keyword_only_y) # Rejected # E: EXPECTED (*, y: int) -> int <: (*, x: int) -> int
+"#,
+);
+
+testcase_with_bug!(
+    "The penultimate type should have brackets around (q: int)",
+    test_paramspec_constructor,
+    r#"
+from typing import TypeVar, Generic, Callable, ParamSpec, reveal_type
+
+P = ParamSpec("P")
+U = TypeVar("U")
+
+class Y(Generic[U, P]):
+  f: Callable[P, str]
+  prop: U
+
+  def __init__(self, f: Callable[P, str], prop: U) -> None:
+    self.f = f
+    self.prop = prop
+
+def a(q: int) -> str: ...
+
+reveal_type(Y(a, 1))   # E: revealed type: Y[int, q: int]
+reveal_type(Y(a, 1).f) # E: revealed type: (q: int) -> str
+"#,
+);
+
+testcase_with_bug!(
+    "Types are right, but slightly different formatting. Thoughts?",
+    test_simple_concatenate,
+    r#"
+from typing import Callable, Concatenate, ParamSpec, reveal_type
+
+P = ParamSpec("P")
+
+def bar(x: int, *args: bool) -> int: ...
+
+def add(x: Callable[P, int]) -> Callable[Concatenate[str, P], bool]: ...
+
+reveal_type(add(bar))       # Should return (a: str, /, x: int, *args: bool) -> bool # E: revealed type: (str, x: int, *bool) -> bool
+
+def remove(x: Callable[Concatenate[int, P], int]) -> Callable[P, bool]: ...
+
+reveal_type(remove(bar))    # Should return (*args: bool) -> bool # E: revealed type: (*bool) -> bool
+
+def transform(
+  x: Callable[Concatenate[int, P], int]
+) -> Callable[Concatenate[str, P], bool]: ...
+
+reveal_type(transform(bar)) # Should return (a: str, /, *args: bool) -> bool # E: revealed type: (str, *bool) -> bool
+"#,
+);
+
+testcase_with_bug!(
+    "None of the rejected ones are actually rejected",
+    test_paramspec_component_usage,
+    r#"
+from typing import Callable, ParamSpec
+
+P = ParamSpec("P")
+
+def puts_p_into_scope(f: Callable[P, int]) -> None:
+
+  def inner(*args: P.args, **kwargs: P.kwargs) -> None:      # Accepted
+    pass
+
+  def mixed_up(*args: P.kwargs, **kwargs: P.args) -> None:   # Rejected
+    pass
+
+  def misplaced(x: P.args) -> None:                          # Rejected
+    pass
+
+def out_of_scope(*args: P.args, **kwargs: P.kwargs) -> None: # Rejected
+  pass
+"#,
+);
+
+testcase_with_bug!(
+    "None of these are rejected, but they should be",
+    test_paramspec_together,
+    r#"
+from typing import Callable, ParamSpec
+
+P = ParamSpec("P")
+
+def puts_p_into_scope(f: Callable[P, int]) -> None:
+
+  stored_args: P.args                           # Rejected
+
+  stored_kwargs: P.kwargs                       # Rejected
+
+  def just_args(*args: P.args) -> None:         # Rejected
+    pass
+
+  def just_kwargs(**kwargs: P.kwargs) -> None:  # Rejected
+    pass
+"#,
+);
+
+testcase!(
+    test_paramspec_decorator,
+    r#"
+from typing import Callable, ParamSpec, assert_type
+
+P = ParamSpec("P")
+
+def decorator(f: Callable[P, int]) -> Callable[P, None]:
+
+  def foo(*args: P.args, **kwargs: P.kwargs) -> None:
+
+    assert_type(f(*args, **kwargs), int)    # Accepted, should resolve to int
+
+    f(*kwargs, **args)    # Rejected # E: Expected a `*args` and `**kwargs` for `ParamSpec`
+
+    f(1, *args, **kwargs) # Rejected # E: Expected 0 positional arguments, got 1
+
+  return foo              # Accepted
+"#,
+);
+
+testcase_with_bug!(
+    "One accepted is rejected, rejected has too many error messages",
+    test_paramspec_named_arguments_concatenate,
+    r#"
+from typing import Callable, ParamSpec
+
+P = ParamSpec("P")
+
+def outer(f: Callable[P, None]) -> Callable[P, None]:
+  def foo(x: int, *args: P.args, **kwargs: P.kwargs) -> None:
+    f(*args, **kwargs)
+
+  def bar(*args: P.args, **kwargs: P.kwargs) -> None:
+    foo(1, *args, **kwargs)   # Accepted  # E: `Args[?_]` is not iterable # E: Expected argument after ** to be a mapping, got: Kwargs[?_]
+    foo(x=1, *args, **kwargs) # Rejected # E: `Args[?_]` is not iterable # E: Multiple values for argument 'x' # E: Expected argument after ** to be a mapping, got: Kwargs[?_]
+
+  return bar
+"#,
+);
+
+testcase_with_bug!(
+    "Rejects everything",
+    test_paramspec_twice,
+    r#"
+from typing import Callable, ParamSpec
+
+P = ParamSpec("P")
+
+def twice(f: Callable[P, int], *args: P.args, **kwargs: P.kwargs) -> int:
+  return f(*args, **kwargs) + f(*args, **kwargs)
+
+def a_int_b_str(a: int, b: str) -> int:
+  return a
+
+twice(a_int_b_str, 1, "A")       # Accepted # E: EXPECTED Literal[1] <: Args[?_] # E: EXPECTED Literal['A'] <: Args[?_]
+
+twice(a_int_b_str, b="A", a=1)   # Accepted # E: EXPECTED Literal['A'] <: Kwargs[?_] # E: EXPECTED Literal[1] <: Kwargs[?_]
+
+twice(a_int_b_str, "A", 1)       # Rejected # E: EXPECTED Literal['A'] <: Args[?_] # E: EXPECTED Literal[1] <: Args[?_]
 "#,
 );
