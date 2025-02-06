7a104c7ad51de96afaa32acbdbbe72a05a732986
Jia Chen <grievejia@meta.com>
2025-02-06 10:47:19 -0800

    Add some basic tests for module finder
    Summary: I noticed that we don't have any tests for the finder. Try to add some basic ones.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D69224142
    
    fbshipit-source-id: a47c836befd0396cc6d1a6e587db14c839dc84d8

--- pyre2/pyre2/bin/module/finder.rs
+++ pyre2/pyre2/bin/module/finder.rs
@@ -31,3 +31,116 @@ pub fn find_module(module: ModuleName, include: &[PathBuf]) -> Option<PathBuf> {
     }
     None
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::*;
+
+    // Utility structure to facilitate setting up filesystem structure under test directories.
+    enum TestPathKind {
+        File,
+        Directory(Vec<TestPath>),
+    }
+    struct TestPath {
+        name: String,
+        kind: TestPathKind,
+    }
+
+    impl TestPath {
+        fn file(name: &str) -> Self {
+            Self {
+                name: name.to_owned(),
+                kind: TestPathKind::File,
+            }
+        }
+        fn dir(name: &str, children: Vec<TestPath>) -> Self {
+            Self {
+                name: name.to_owned(),
+                kind: TestPathKind::Directory(children),
+            }
+        }
+    }
+
+    fn setup_test_directory(root: &Path, paths: Vec<TestPath>) {
+        for path in paths {
+            match path.kind {
+                TestPathKind::File => {
+                    std::fs::File::create(root.join(path.name)).unwrap();
+                }
+                TestPathKind::Directory(children) => {
+                    let dir = root.join(path.name);
+                    std::fs::create_dir(&dir).unwrap();
+                    setup_test_directory(&dir, children);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_find_module_simple() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let root = tempdir.path();
+        setup_test_directory(
+            root,
+            vec![TestPath::dir(
+                "foo",
+                vec![TestPath::file("bar.py"), TestPath::file("baz.pyi")],
+            )],
+        );
+        assert_eq!(
+            find_module(ModuleName::from_str("foo.bar"), &[root.to_path_buf()]),
+            Some(root.join("foo/bar.py"))
+        );
+        assert_eq!(
+            find_module(ModuleName::from_str("foo.baz"), &[root.to_path_buf()]),
+            Some(root.join("foo/baz.pyi"))
+        );
+        assert_eq!(
+            find_module(ModuleName::from_str("foo.qux"), &[root.to_path_buf()]),
+            None,
+        );
+    }
+
+    #[test]
+    fn test_find_module_init() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let root = tempdir.path();
+        setup_test_directory(
+            root,
+            vec![TestPath::dir(
+                "foo",
+                vec![
+                    TestPath::dir("bar", vec![TestPath::file("__init__.py")]),
+                    TestPath::dir("baz", vec![TestPath::file("__init__.pyi")]),
+                ],
+            )],
+        );
+        assert_eq!(
+            find_module(ModuleName::from_str("foo.bar"), &[root.to_path_buf()]),
+            Some(root.join("foo/bar/__init__.py"))
+        );
+        assert_eq!(
+            find_module(ModuleName::from_str("foo.baz"), &[root.to_path_buf()]),
+            Some(root.join("foo/baz/__init__.pyi"))
+        );
+    }
+
+    #[test]
+    fn test_find_pyi_takes_precedence() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let root = tempdir.path();
+        setup_test_directory(
+            root,
+            vec![TestPath::dir(
+                "foo",
+                vec![TestPath::file("bar.pyi"), TestPath::file("bar.py")],
+            )],
+        );
+        assert_eq!(
+            find_module(ModuleName::from_str("foo.bar"), &[root.to_path_buf()]),
+            Some(root.join("foo/bar.pyi"))
+        );
+    }
+}
