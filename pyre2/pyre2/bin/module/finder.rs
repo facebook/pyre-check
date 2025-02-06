fef500c6561b7a8d24f52fb9d390f9d173a7f580
Jia Chen <grievejia@meta.com>
2025-02-06 15:13:04 -0800

    Search modules recursively in finder
    Summary:
    _recursion
    
    Say if we have two search roots `[r0, r1]` and we want to find a module named `a.b` under them. Previously, Pyre2 simply attempts to check for the existence of these paths directly in order and return the first hit:
    ```
    r0/a/b/__init__.pyi
    r0/a/b/__init__.py
    r0/a/b.pyi
    r0/a/b.py
    r1/a/b/__init__.pyi
    r1/a/b/__init__.py
    r1/a/b.pyi
    r1/a/b.py
    ```
    This is a good approximation for how the runtime searches for modules, but not quite the same. What [CPython would do at runtime](https://fb.workplace.com/groups/657557118131740/permalink/1361339517753493/) is to search in a "layer-by-layer" manner. For module `a.b`, it'll first try to locate where `a` is by checking these files:
    ```
    r0/a/__init__.pyi
    r0/a/__init__.py
    r1/a/__init__.pyi
    r1/a/__init__.py
    ```
    If any one of those check hits, it'll use that as the "root" for the next phase of the search where it'll try to locate where `b` is under `a`. For example, let's say that `r0/a/__init__.py` exists, then we'll start from `r0/a/` and check the following paths for `a.b`:
    ```
    r0/a/b/__init__.pyi
    r0/a/b/__init__.py
    r0/a/b.pyi
    r0/a/b.py
    ```
    Note that in this second phase of the search, `r1/a/` is not looked at all -- we've already "committed" to `r0/a/` when looking for `a`. The current algorithm used by Pyre2 finder wasn't able to emulate this subtlety today.
    
    This diff attempts to replace what we have today with the "layer-by-layer" CPython algorithm described above. Not only is the new algorithm better aligned with CPython runtime, it's also very straightforward to extend it to support namespace packages (D69256720).
    
    Reviewed By: ndmitchell
    
    Differential Revision: D69224139
    
    fbshipit-source-id: 3b508bb8a23a9cf2c2c79fbd055ff18441e1c3f5

--- pyre2/pyre2/bin/module/finder.rs
+++ pyre2/pyre2/bin/module/finder.rs
@@ -7,29 +7,62 @@
 
 use std::path::PathBuf;
 
-use tracing::debug;
+use ruff_python_ast::name::Name;
 
 use crate::module::module_name::ModuleName;
 
-pub fn find_module(module: ModuleName, include: &[PathBuf]) -> Option<PathBuf> {
-    let parts = module.components();
-    let possibilities = vec![
-        parts.join("/") + "/__init__.pyi",
-        parts.join("/") + "/__init__.py",
-        parts.join("/") + ".pyi",
-        parts.join("/") + ".py",
-    ];
+enum FindResult {
+    /// Found a single-file module. The path must not point to an __init__ file.
+    SingleFileModule(PathBuf),
+    /// Found a regular package. First path must point to an __init__ file.
+    /// Second path indicates where to continue search next. It should always point to the parent of the __init__ file.
+    RegularPackage(PathBuf, PathBuf),
+}
 
-    for include in include {
-        for suffix in &possibilities {
-            let path = include.join(suffix);
-            if path.exists() {
-                debug!("Found {module} at {}", path.display());
-                return Some(path);
+fn find_one_part(name: &Name, roots: &[PathBuf]) -> Option<FindResult> {
+    for root in roots {
+        for candidate_init_suffix in ["__init__.pyi", "__init__.py"] {
+            let candidate_dir = root.join(name.as_str());
+            let init_path = candidate_dir.join(candidate_init_suffix);
+            if init_path.exists() {
+                return Some(FindResult::RegularPackage(init_path, candidate_dir));
+            }
+        }
+        for candidate_file_suffix in ["pyi", "py"] {
+            let candidate_path = root.join(format!("{name}.{candidate_file_suffix}"));
+            if candidate_path.exists() {
+                return Some(FindResult::SingleFileModule(candidate_path));
             }
         }
     }
     None
+}
+
+pub fn find_module(module: ModuleName, include: &[PathBuf]) -> Option<PathBuf> {
+    let parts = module.components();
+    if parts.is_empty() {
+        return None;
+    }
+    let mut current_result = find_one_part(&parts[0], include);
+    for part in parts.iter().skip(1) {
+        match current_result {
+            None => {
+                // Nothing has been found in the previous round. No point keep looking.
+                break;
+            }
+            Some(FindResult::SingleFileModule(_)) => {
+                // We've already reached leaf nodes. Cannot keep searching
+                current_result = None;
+                break;
+            }
+            Some(FindResult::RegularPackage(_, next_root)) => {
+                current_result = find_one_part(part, &[next_root]);
+            }
+        }
+    }
+    current_result.map(|x| match x {
+        FindResult::SingleFileModule(path) | FindResult::RegularPackage(path, _) => path,
+    })
 }
 
 #[cfg(test)]
@@ -171,6 +204,41 @@ mod tests {
         assert_eq!(
             find_module(ModuleName::from_str("foo.bar"), &[root.to_path_buf()]),
             Some(root.join("foo/bar/__init__.py"))
+        );
+    }
+
+    #[test]
+    fn test_find_regular_package_early_return() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let root = tempdir.path();
+        setup_test_directory(
+            root,
+            vec![
+                TestPath::dir(
+                    "search_root0",
+                    vec![TestPath::dir(
+                        "a",
+                        vec![TestPath::file("__init__.py"), TestPath::file("b.py")],
+                    )],
+                ),
+                TestPath::dir(
+                    "search_root1",
+                    vec![TestPath::dir(
+                        "a",
+                        vec![TestPath::file("__init__.py"), TestPath::file("c.py")],
+                    )],
+                ),
+            ],
+        );
+        assert_eq!(
+            find_module(
+                ModuleName::from_str("a.c"),
+                &[root.join("search_root0"), root.join("search_root1")]
+            ),
+            // We won't find `a.c` because when searching for package `a`, we've already
+            // committed to `search_root0/a/` as the path to search next for `c`. And there's
+            // no `c.py` in `search_root0/a/`.
+            None
         );
     }
 }
