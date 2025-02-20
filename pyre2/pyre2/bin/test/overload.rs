c1f6994e185496f9652b354421f19a63b676f5bf
Sam Goldman <samgoldman@meta.com>
2025-02-20 11:46:29 -0800

    Add failing test case for overload chaining in static branches
    Summary:
    While adding an error on overloads with only one signature (2+ are required), I encountered this issue in the standard library, in EnumMeta.__call__
    
    I reduced the problematic code to this test case. Fixed in the next diff.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D69907201
    
    fbshipit-source-id: c459f9c31eabd08bb4cbbce33e79f8475e7f1783

--- pyre2/pyre2/bin/test/overload.rs
+++ pyre2/pyre2/bin/test/overload.rs
@@ -127,3 +127,31 @@ assert_type(f(0), int)
 assert_type(f(""), str)
 "#,
 );
+
+testcase_with_bug!(
+    "Env merging creates Binding::Forward which defeats predecessor traversal",
+    test_overload_static_config,
+    r#"
+from typing import overload, assert_type
+import sys
+
+@overload
+def f(x: int) -> int: ...
+
+if sys.version_info >= (3, 11):
+    @overload
+    def f(x: str) -> str: ...
+else:
+    @overload
+    def f(x: int, int) -> bool: ...
+
+if sys.version_info >= (3, 12):
+    @overload
+    def f() -> None: ...  # E: Overloaded function must have an implementation
+
+assert_type(f(0), int)  # E: assert_type # E: No matching overload found
+assert_type(f(""), str) # E: assert_type # E: No matching overload found
+assert_type(f(), None)
+f(0, 0) # E: No matching overload found
+"#,
+);
