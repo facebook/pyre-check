796a843416dfc07d4f63f7312fe5c6b3bc204315
Zeina Migeed <migeedz@meta.com>
2025-02-20 16:12:05 -0800

    Test that overrides work with overloads
    Summary: Extend a unittest involving overloads to make sure it works with overrides.
    
    Reviewed By: rchen152
    
    Differential Revision: D69940264
    
    fbshipit-source-id: cf1a1a24d5ab73236217446cb07a2ea864f22f35

--- pyre2/pyre2/bin/test/class_overrides.rs
+++ pyre2/pyre2/bin/test/class_overrides.rs
@@ -62,6 +62,7 @@ testcase!(
 from typing import overload
 
 class A:
+
     @overload
     def method(self, x: int) -> int:
         ...
@@ -71,7 +72,21 @@ class A:
         ...
 
     def method(self, x: int | str) -> int | str:
-        return 0        
+        return 0
+
+
+class B(A):
+
+    @overload
+    def method(self, x: int) -> int:
+        ...
+
+    @overload
+    def method(self, x: str) -> str:
+        ...
+
+    def method(self, x: int | str) -> int | str:
+        return 0
  "#,
 );
 
