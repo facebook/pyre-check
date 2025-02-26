28599cd4236848260e776433ec6b233d9cf35dd0
Steven Troxler <stroxler@meta.com>
2025-02-26 10:33:20 -0800

    Move failing tests to `attributes.rs`
    Summary:
    These tests are related to gaps in `ClassField` handling, they
    don't belong in `simple.rs`
    
    Reviewed By: ndmitchell
    
    Differential Revision: D70256142
    
    fbshipit-source-id: e68f174d49b21b8f36175b88f92057d3a78b9455

--- pyre2/pyre2/lib/test/simple.rs
+++ pyre2/pyre2/lib/test/simple.rs
@@ -410,35 +410,6 @@ y = "bar" # TODO: y can not be assigned
 "#,
 );
 
-testcase_with_bug!(
-    "TODO(steven): We are failing to promote literals that have an annotation.",
-    test_literal_attr_with_annotation,
-    r#"
-from typing import ClassVar, assert_type
-class C:
-    x0 = 0
-    x1: ClassVar = 0
-assert_type(C.x0, int)
-assert_type(C.x1, int)  # E: assert_type(Literal[0], int)
-"#,
-);
-
-testcase_with_bug!(
-    "TODO(zeina): We are not enforcing `Final` override bans when the type matches",
-    test_final_annotated_override,
-    r#"
-from typing import Final
-
-def f() -> int: ...
-
-class Base:
-    p: Final = f()
-    
-class Derived(Base):
-    p = f()  # Oops, this should be an error
-"#,
-);
-
 testcase!(
     test_solver_variables,
     r#"
