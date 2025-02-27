66db2e16a1aa6035b4320b6d284f200819ad6d36
Rebecca Chen (Python) <rechen@meta.com>
2025-02-26 18:55:50 -0800

    Add more super() tests
    Reviewed By: yangdanny97
    
    Differential Revision: D70295112
    
    fbshipit-source-id: f35b9e2034153904fd8486d3ddb466a278e7dbd8

--- pyre2/pyre2/lib/test/class_super.rs
+++ pyre2/pyre2/lib/test/class_super.rs
@@ -9,7 +9,7 @@ use crate::testcase_with_bug;
 
 testcase_with_bug!(
     "We don't understand super()",
-    test_class_super,
+    test_class_super_no_args,
     r#"
 class A:
     def m(self) -> int:
@@ -19,4 +19,48 @@ class B(A):
     def m(self) -> int:
         return super().m() # E: Object of class `super` has no attribute `m`
 "#,
+);
+
+testcase_with_bug!(
+    "We don't understand super()",
+    test_class_super_with_args,
+    r#"
+from typing import assert_type
+
+class A:
+    def f(self) -> int:
+        return 0
+
+class B:
+    def f(self) -> bool:
+        return True
+
+class C(B, A):
+    def g(self):
+        assert_type(super(C, self).f(), bool)  # E: assert_type  # E: no attribute `f`
+        assert_type(super(B, self).f(), int)  # E: assert_type  # E: no attribute `f`
+    "#,
+);
+
+testcase_with_bug!(
+    "We don't understand super()",
+    test_inherit_method_with_super,
+    r#"
+from typing import assert_type
+
+class A:
+    def f(self) -> int:
+        return 0
+class B(A):
+    def f(self):
+        return super().f()  # E: Object of class `super` has no attribute `f`
+class C:
+    def f(self) -> bool:
+        return True
+class D(B, C, A):
+    pass
+
+# The super() call in B.f should be evaluated with D as the starting class, so that C.f is called
+assert_type(D().f(), bool)  # E: assert_type
+    "#,
 );
