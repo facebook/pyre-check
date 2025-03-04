1d29af9daf61728352e70e5e40107e19f6dd2ab7
Neil Mitchell <ndmitchell@meta.com>
2025-03-04 02:48:10 -0800

    Add an AtomicTextRange
    Summary: Will need this in the next diff. Add it and test it.
    
    Reviewed By: Solumin
    
    Differential Revision: D70515005
    
    fbshipit-source-id: 1082f517167b9b1495286a947a6b523f30c6460d

--- pyre2/pyre2/lib/ast.rs
+++ pyre2/pyre2/lib/ast.rs
@@ -6,6 +6,8 @@
  */
 
 use std::slice;
+use std::sync::atomic::AtomicU64;
+use std::sync::atomic::Ordering;
 
 use ruff_python_ast::DictItem;
 use ruff_python_ast::Expr;
@@ -269,5 +271,50 @@ impl Ast {
                 value: x.value == Singleton::True,
             }),
         }
+    }
+}
+
+#[allow(dead_code)]
+pub struct AtomicTextRange(AtomicU64);
+
+#[allow(dead_code)]
+impl AtomicTextRange {
+    pub fn new(range: TextRange) -> Self {
+        Self(AtomicU64::new(Self::to_u64(range)))
+    }
+
+    pub fn get(&self) -> TextRange {
+        Self::from_u64(self.0.load(Ordering::Relaxed))
+    }
+
+    pub fn set(&self, range: TextRange) {
+        self.0.store(Self::to_u64(range), Ordering::Relaxed);
+    }
+
+    fn to_u64(range: TextRange) -> u64 {
+        let low = range.start().to_u32();
+        let high = range.end().to_u32();
+        ((high as u64) << 32) | (low as u64)
+    }
+
+    fn from_u64(packed: u64) -> TextRange {
+        let high = (packed >> 32) as u32;
+        let low = packed as u32;
+        TextRange::new(TextSize::new(low), TextSize::new(high))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_atomic_text_range() {
+        let range1 = TextRange::new(TextSize::new(0), TextSize::new(1));
+        let range2 = TextRange::new(TextSize::new(100), TextSize::new(800));
+        let atomic = AtomicTextRange::new(range1);
+        assert_eq!(range1, atomic.get());
+        atomic.set(range2);
+        assert_eq!(range2, atomic.get());
     }
 }
