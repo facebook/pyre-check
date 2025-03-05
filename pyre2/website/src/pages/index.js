0aa18b96f1db137efb28a4f511e6ea4244cf6134
Maggie Moss <maggiemoss@meta.com>
2025-03-05 15:05:06 -0800

    Landing - simplify layout and styling in preparation for larger changes
    Summary: This is just a prep diff: see D70653624 for end result
    
    Reviewed By: yangdanny97
    
    Differential Revision: D70641548
    
    fbshipit-source-id: 8653330e3e47c0bbf8e9568f7b7fdbfe04cbfb59

--- pyre2/website/src/pages/index.js
+++ pyre2/website/src/pages/index.js
@@ -33,237 +33,27 @@ export default component Home() {
             static type <br className={styles.hiddenLargerUp} /> checker for{' '}
             <br className={styles.hiddenLargerUp} /> <span>Python.</span>
           </p>
-          <Link
-            className={styles.featureButton}
-            to={useBaseUrl('en/docs/getting-started')}>
-            Get Started
-          </Link>
-          <Link
-            className={styles.featureButton}
-            to={useBaseUrl('en/docs/install')}>
-            Install Pyrefly
-          </Link>
-          <iframe
-            className="gh-btn"
-            src="https://ghbtns.com/github-btn.html?user=facebook&repo=pyrefly&type=star&count=false&size=large"
-            frameBorder="0"
-            scrolling="0"
-            width="160px"
-            height="30px"
-            title="Star"></iframe>
-          <div className={clsx(styles.featureHeroText)}>
-            Current version:{' '}
-            <strong className="navbar-text">
-              {/* TODO: add release */}
-              <a
-                href={`https://github.com/facebook/pyrefly/releases/tag/${siteConfig.customFields.PyreflyVersion}`}
-                className={styles.releaseVersion}>
-                {siteConfig.customFields.PyreflyVersion}
-              </a>
-            </strong>
-          </div>
+          <section>
+            <Link
+              className={styles.featureButton}
+              to={useBaseUrl('en/docs/getting-started')}>
+              Get Started
+            </Link>
+            <Link
+              className={styles.featureButton}
+              to={useBaseUrl('en/docs/install')}>
+              Install Pyrefly
+            </Link>
+            {/* Add font awesome github logo */}
+            <Link
+              className={styles.featureButton}
+              to="https://ghbtns.com/github-btn.html?user=facebook&repo=pyrefly&type=star&count=false&size=large">
+              Github
+            </Link>
+          </section>
         </div>
       </header>
-      <main>
-        <section className={clsx(styles.feature, styles.featureLight)}>
-          <div
-            className={clsx(
-              styles.featureDecoration,
-              styles.featureDecorationDrop,
-            )}
-          />
-          <div className="container">
-            <div className="row">
-              <div className="col col--7">
-                <h2
-                  className={clsx(
-                    styles.featureHeading,
-                    styles.featureHeadingCenter,
-                  )}>
-                  Code Faster.
-                </h2>
-                <p className={styles.featureText}>
-                  Tired of having to run your code to find bugs? Pyrefly
-                  identifies problems as you code. Stop wasting your time
-                  guessing and checking.
-                </p>
-              </div>
-              <div className="col col--5">
-                <img
-                  className={clsx(styles.imgFluid, 'rounded')}
-                  width={890}
-                  height={564}
-                  src={useBaseUrl('img/featurette-faster.gif')}
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className={clsx(styles.feature, styles.featureGray)}>
-          <div className="container">
-            <div className="row">
-              <div className="col col--5">
-                <img
-                  className={clsx(styles.imgFluid, 'rounded')}
-                  width={890}
-                  height={564}
-                  src={useBaseUrl('img/featurette-smarter.gif')}
-                />
-              </div>
-              <div className="col col--7">
-                <h2
-                  className={clsx(
-                    styles.featureHeading,
-                    styles.featureHeadingCenter,
-                  )}>
-                  Code Smarter.
-                </h2>
-                <p className={styles.featureText}>
-                  It's hard to build smart tools for dynamic languages like
-                  Python. Pyrefly understands your code and makes its knowledge
-                  available, enabling other smart tools to be built on top of
-                  Pyrefly.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className={clsx(styles.feature, styles.featureDark)}>
-          <div
-            className={clsx(
-              styles.featureDecoration,
-              styles.featureDecorationDrop,
-            )}
-          />
-          <div
-            className={clsx(
-              styles.featureDecoration,
-              styles.featureDecorationRise,
-            )}
-          />
-          <div className="container">
-            <div className="row">
-              <div className="col col--7">
-                <h2
-                  className={clsx(
-                    styles.featureHeading,
-                    styles.featureHeadingCenter,
-                  )}>
-                  Code Confidently.
-                </h2>
-                <p className={styles.featureText}>
-                  Making major changes to large codebases can be scary. Pyrefly
-                  helps you refactor safely, so you can focus on the changes you
-                  want to make, and stop worrying about what you might break.
-                </p>
-              </div>
-              <div className="col col--5">
-                <img
-                  className={clsx(styles.imgFluid, 'rounded')}
-                  width={890}
-                  height={564}
-                  src={useBaseUrl('img/featurette-confidently.gif')}
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className={clsx(styles.feature, styles.featureLight)}>
-          <div className="container">
-            <div className="row">
-              <div className="col col--5">
-                <img
-                  className={clsx(styles.imgFluid, 'rounded')}
-                  width={890}
-                  height={564}
-                  src={useBaseUrl('img/featurette-bigger.gif')}
-                />
-              </div>
-              <div className="col col--7">
-                <h2
-                  className={clsx(
-                    styles.featureHeading,
-                    styles.featureHeadingCenter,
-                  )}>
-                  Code Bigger.
-                </h2>
-                <p className={styles.featureText}>
-                  Working in a codebase with lots of developers can make it
-                  difficult to keep your master branch working. Pyrefly can help
-                  prevent bad rebases. Pyrefly can help protect your carefully
-                  designed library from misuse and misinterpretation. And
-                  Pyrefly can help you understand the code you wrote six months
-                  ago.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section
-          className={clsx(
-            styles.feature,
-            styles.featureYellow,
-            styles.featureSmall,
-          )}>
-          <div
-            className={clsx(
-              styles.featureDecoration,
-              styles.featureDecorationRise,
-            )}
-          />
-          <div
-            className={clsx(
-              styles.featureDecoration,
-              styles.featureDecorationDrop,
-            )}
-          />
-          <div className="container">
-            <h2 className={styles.featureHeading}>Ready to get going?</h2>
-            <div>
-              <Link
-                className={styles.featureButton}
-                to={useBaseUrl('en/docs/getting-started')}>
-                Get Started
-              </Link>
-              <Link
-                className={styles.featureButton}
-                to={useBaseUrl('en/docs/install')}>
-                Install Pyrefly
-              </Link>
-            </div>
-          </div>
-        </section>
-
-        <section
-          className={clsx(
-            styles.feature,
-            styles.featureLight,
-            styles.featureSmall,
-          )}>
-          <div className="container">
-            <div className="row">
-              <div className={clsx('col col--6', styles.featurette)}>
-                <h2 className={styles.featuretteHeading}>Realtime Feedback</h2>
-                <p className={styles.featuretteText}>
-                  Pyrefly gives you fast feedback while you code by
-                  incrementally rechecking your code as you make changes.
-                </p>
-              </div>
-              <div className={clsx('col col--6', styles.featurette)}>
-                <h2 className={styles.featuretteHeading}>Easy Integration</h2>
-                <p className={styles.featuretteText}>
-                  Pyrefly integrates well with many tools, making it easy to
-                  insert into your existing workflow and toolchain.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
+      <main></main>
     </Layout>
   );
 }
