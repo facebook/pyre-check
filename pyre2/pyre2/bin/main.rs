72707c533b95e56d7f66285698d849e31179d61d
Jia Chen <grievejia@meta.com>
2025-03-05 12:28:16 -0800

    Allow OSS and Meta entry point to support different CLI subcommands
    Summary:
    scooby_lets_split_up
    
    Duplicating the subcommand structure for both OSS and Meta entry point, so what subcommands are available in what entry point could diverge (see D70580851)
    
    Reviewed By: ndmitchell
    
    Differential Revision: D70580853
    
    fbshipit-source-id: 861bab5c91e5c3f0c70d4b7d1d53277aa0d7291f

--- pyre2/pyre2/bin/main.rs
+++ pyre2/pyre2/bin/main.rs
@@ -12,12 +12,15 @@ use std::path::PathBuf;
 use std::process::ExitCode;
 
 use clap::Parser;
+use clap::Subcommand;
 use pyre2::clap_env;
 use pyre2::get_args_expanded;
 use pyre2::init_rayon;
 use pyre2::init_tracing;
-use pyre2::run::Command;
+use pyre2::run::BuckCheckArgs;
+use pyre2::run::CheckArgs;
 use pyre2::run::CommandExitStatus;
+use pyre2::run::LspArgs;
 use pyre2::ConfigFile;
 use pyre2::Globs;
 use pyre2::NotifyWatcher;
@@ -42,6 +45,37 @@ struct Args {
 
     #[command(subcommand)]
     command: Command,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Command {
+    /// Full type checking on a file or a project
+    Check {
+        /// Files to check (glob supported).
+        /// If no file is specified, switch to project-checking mode where the files to
+        /// check are determined from the closest configuration file.
+        files: Vec<String>,
+        /// Watch for file changes and re-check them.
+        #[clap(long, env = clap_env("WATCH"))]
+        watch: bool,
+
+        /// Explicitly set the Pyre configuration to use when type checking or starting a language server.
+        /// It is an error to pass this flag in "single-file checking mode".
+        /// When not set, Pyre will perform an upward-filesystem-walk approach to find the nearest
+        /// pyre.toml or 'pyproject.toml with `tool.pyre` section'. If no config is found, Pyre exits with error.
+        /// If both a pyre.toml and valid pyproject.toml are found, pyre.toml takes precedence.
+        #[clap(long = "config-file", env = clap_env("CONFIG_FILE"))]
+        config_file: Option<std::path::PathBuf>,
+
+        #[clap(flatten)]
+        args: CheckArgs,
+    },
+
+    /// Entry point for Buck integration
+    BuckCheck(BuckCheckArgs),
+
+    /// Start an LSP server
+    Lsp(LspArgs),
 }
 
 fn exit_on_panic() {
--- pyre2/pyre2/lib/commands/run.rs
+++ pyre2/pyre2/lib/commands/run.rs
@@ -5,43 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use clap::Subcommand;
-
-use crate::clap_env;
 pub use crate::commands::buck_check::Args as BuckCheckArgs;
 pub use crate::commands::check::Args as CheckArgs;
 pub use crate::commands::lsp::Args as LspArgs;
-
-#[derive(Debug, Clone, Subcommand)]
-pub enum Command {
-    /// Full type checking on a file or a project
-    Check {
-        /// Files to check (glob supported).
-        /// If no file is specified, switch to project-checking mode where the files to
-        /// check are determined from the closest configuration file.
-        files: Vec<String>,
-        /// Watch for file changes and re-check them.
-        #[clap(long, env = clap_env("WATCH"))]
-        watch: bool,
-
-        /// Explicitly set the Pyre configuration to use when type checking or starting a language server.
-        /// It is an error to pass this flag in "single-file checking mode".
-        /// When not set, Pyre will perform an upward-filesystem-walk approach to find the nearest
-        /// pyre.toml or 'pyproject.toml with `tool.pyre` section'. If no config is found, Pyre exits with error.
-        /// If both a pyre.toml and valid pyproject.toml are found, pyre.toml takes precedence.
-        #[clap(long = "config-file", env = clap_env("CONFIG_FILE"))]
-        config_file: Option<std::path::PathBuf>,
-
-        #[clap(flatten)]
-        args: CheckArgs,
-    },
-
-    /// Entry point for Buck integration
-    BuckCheck(BuckCheckArgs),
-
-    /// Start an LSP server
-    Lsp(LspArgs),
-}
 
 /// Exit status of a command, if the run is completed.
 pub enum CommandExitStatus {
