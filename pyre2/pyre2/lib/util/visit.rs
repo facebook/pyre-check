4acbf8ef0ed52734f01691e22ea3ef8321b8fb8c
Neil Mitchell <ndmitchell@meta.com>
2025-03-15 03:55:18 -0700

    Add tests for Visit
    Summary: Also add a bunch more instances.
    
    Reviewed By: stroxler
    
    Differential Revision: D71212154
    
    fbshipit-source-id: bbc5399ed340ede3c05fe9af0b741a28a6b73048

--- pyre2/pyre2/lib/util/visit.rs
+++ pyre2/pyre2/lib/util/visit.rs
@@ -42,6 +42,33 @@ pub trait VisitMut<To: 'static = Self>: 'static + Sized {
     }
 }
 
+macro_rules! no_children {
+    ($t:ty) => {
+        impl<To: 'static> Visit<To> for $t {
+            fn visit<'a>(&'a self, _: &mut dyn FnMut(&'a To)) {}
+        }
+
+        impl<To: 'static> VisitMut<To> for $t {
+            fn visit_mut(&mut self, _: &mut dyn FnMut(&mut To)) {}
+        }
+    };
+}
+
+no_children!(bool);
+no_children!(u8);
+no_children!(u16);
+no_children!(u32);
+no_children!(u64);
+no_children!(u128);
+no_children!(usize);
+no_children!(i8);
+no_children!(i16);
+no_children!(i32);
+no_children!(i64);
+no_children!(i128);
+no_children!(isize);
+no_children!(());
+
 impl<To: 'static, T: Visit<To>> Visit<To> for Vec<T> {
     fn visit<'a>(&'a self, f: &mut dyn FnMut(&'a To)) {
         for item in self {
@@ -99,5 +126,77 @@ impl<To: 'static, T: Visit<To>> Visit<To> for Box<T> {
 impl<To: 'static, T: VisitMut<To>> VisitMut<To> for Box<T> {
     fn visit_mut(&mut self, f: &mut dyn FnMut(&mut To)) {
         (**self).visit0_mut(f)
+    }
+}
+
+impl<To: 'static, T1: Visit<To>, T2: Visit<To>> Visit<To> for (T1, T2) {
+    fn visit<'a>(&'a self, f: &mut dyn FnMut(&'a To)) {
+        self.0.visit0(f);
+        self.1.visit0(f);
+    }
+}
+
+impl<To: 'static, T1: VisitMut<To>, T2: VisitMut<To>> VisitMut<To> for (T1, T2) {
+    fn visit_mut(&mut self, f: &mut dyn FnMut(&mut To)) {
+        self.0.visit0_mut(f);
+        self.1.visit0_mut(f);
+    }
+}
+
+impl<To: 'static, T1: Visit<To>, T2: Visit<To>, T3: Visit<To>> Visit<To> for (T1, T2, T3) {
+    fn visit<'a>(&'a self, f: &mut dyn FnMut(&'a To)) {
+        self.0.visit0(f);
+        self.1.visit0(f);
+        self.2.visit0(f);
+    }
+}
+
+impl<To: 'static, T1: VisitMut<To>, T2: VisitMut<To>, T3: VisitMut<To>> VisitMut<To>
+    for (T1, T2, T3)
+{
+    fn visit_mut(&mut self, f: &mut dyn FnMut(&mut To)) {
+        self.0.visit0_mut(f);
+        self.1.visit0_mut(f);
+        self.2.visit0_mut(f);
+    }
+}
+
+impl<To: 'static, T1: Visit<To>, T2: Visit<To>, T3: Visit<To>, T4: Visit<To>> Visit<To>
+    for (T1, T2, T3, T4)
+{
+    fn visit<'a>(&'a self, f: &mut dyn FnMut(&'a To)) {
+        self.0.visit0(f);
+        self.1.visit0(f);
+        self.2.visit0(f);
+        self.3.visit0(f);
+    }
+}
+
+impl<To: 'static, T1: VisitMut<To>, T2: VisitMut<To>, T3: VisitMut<To>, T4: VisitMut<To>>
+    VisitMut<To> for (T1, T2, T3, T4)
+{
+    fn visit_mut(&mut self, f: &mut dyn FnMut(&mut To)) {
+        self.0.visit0_mut(f);
+        self.1.visit0_mut(f);
+        self.2.visit0_mut(f);
+        self.3.visit0_mut(f);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_visit() {
+        let mut info = (vec![1, 2, 3], Some(4i32), vec![Some(5i32)]);
+        let mut collect = Vec::new();
+        info.visit(&mut |x: &i32| collect.push(*x));
+        assert_eq!(&collect, &[1i32, 2, 3, 4, 5]);
+
+        info.visit_mut(&mut |x: &mut i32| *x *= 2);
+        collect.clear();
+        info.visit(&mut |x: &i32| collect.push(*x));
+        assert_eq!(&collect, &[2i32, 4, 6, 8, 10]);
     }
 }
