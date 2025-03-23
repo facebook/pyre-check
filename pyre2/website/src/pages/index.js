03186dfc85c67124765c21016a80f3e9fb38f37d
David Luo <dluo@meta.com>
2025-03-22 20:49:10 -0700

    Move styles.module.css to inline css
    Summary: moving css inline into `index.js`. reusing this in `newLandingPage.js`
    
    Reviewed By: yangdanny97
    
    Differential Revision: D71565283
    
    fbshipit-source-id: 32c08c18d35e14ace56ec9a29c850849f68c30b8

--- pyre2/website/src/pages/index.js
+++ pyre2/website/src/pages/index.js
@@ -8,13 +8,13 @@
  */
 
 import * as React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import * as stylex from '@stylexjs/stylex';
 import NewLandingPage from './newLandingPage';
+import cssStyles from './styles.module.css';
 
 // This import serves no runtime purposes, but we import it to force webpack to run babel on it,
 // so we can test whether babel can handle newer syntax.
@@ -31,30 +31,121 @@ export default component Home() {
     <Layout
       title="Pyrefly: A Static Type Checker for Python"
       description={siteConfig.description}>
-      <header className={clsx(styles.feature, styles.featureHero)}>
+      <header {...stylex.props(styles.featureHero)}>
         <div className="container">
-          <h1 className={styles.title}>
+          <h1 {...stylex.props(styles.title)}>
             pyrefly<span>.</span>
           </h1>
-          <p className={clsx(styles.frontText, styles.subtitle)}>
+          <p {...stylex.props(styles.subtitle)}>
             <span>
               {' '}
               <a
                 href="https://github.com/facebook/pyrefly/milestone/1"
-                className={styles.yellowLink}>
+                {...stylex.props(styles.yellowLink)}>
                 Coming soon
               </a>
               : A faster Python type checker written in Rust
             </span>
           </p>
           <section>
-            <li className={styles.firefly}></li>
-            <li className={styles.firefly}></li>
-            <li className={styles.firefly}></li>
-            <li className={styles.firefly}></li>
+            <li className={cssStyles.firefly}></li>
+            <li className={cssStyles.firefly}></li>
+            <li className={cssStyles.firefly}></li>
+            <li className={cssStyles.firefly}></li>
           </section>
         </div>
       </header>
     </Layout>
   );
 }
+
+const styles: any = stylex.create({
+  root: {
+    '--title-font-size': '7rem',
+    '--subtitle-font-size': '3.5rem',
+  },
+  featureHero: {
+    padding: '7rem 0',
+    width: '100%',
+    alignItems: 'center',
+    position: 'relative',
+    overflow: 'hidden',
+    background: 'var(--flow-gray-darker)',
+    color: 'var(--off-white)',
+    WebkitFontSmoothing: 'antialiased',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    lineHeight: 1.1,
+    flex: 1,
+  },
+  title: {
+    fontFamily: 'Source Serif Pro',
+    textAlign: 'center',
+    marginBottom: '4rem',
+    marginTop: '3rem',
+    fontSize: '7rem',
+    '@media screen and (max-width: 346px)': {
+      fontSize: '3.5rem',
+    },
+    '@media screen and (max-width: 404px)': {
+      fontSize: '3.8rem',
+    },
+    '@media screen and (max-width: 460px)': {
+      fontSize: '4rem',
+    },
+    '@media screen and (max-width: 518px)': {
+      fontSize: '4.5rem',
+    },
+    '@media screen and (max-width: 548px)': {
+      fontSize: '5.5rem',
+    },
+    '@media screen and (max-width: 576px)': {
+      fontSize: 'calc(6rem)',
+      maxWidth: '100%',
+    },
+    '@media screen and (max-width: 768px)': {
+      fontSize: 'var(--title-font-size)',
+    },
+  },
+  subtitle: {
+    fontFamily: 'Source Serif Pro',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: '1rem',
+    fontSize: '3.5rem',
+    '@media screen and (max-width: 346px)': {
+      fontSize: '1.5rem',
+    },
+    '@media screen and (max-width: 404px)': {
+      fontSize: '1.8rem',
+    },
+    '@media screen and (max-width: 460px)': {
+      fontSize: '1.9rem',
+    },
+    '@media screen and (max-width: 518px)': {
+      fontSize: '2rem',
+    },
+    '@media screen and (max-width: 548px)': {
+      fontSize: '2.5rem',
+    },
+    '@media screen and (max-width: 576px)': {
+      fontSize: 'calc(3rem)',
+      maxWidth: '100%',
+    },
+    '@media screen and (max-width: 768px)': {
+      fontSize: 'var(--subtitle-font-size)',
+    },
+  },
+  yellowLink: {
+    color: 'var(--flow-yellow)',
+    textDecoration: 'underline',
+    transition:
+      'color var(--ifm-transition-fast) var(--ifm-transition-timing-default)',
+    ':hover': {
+      color: '#BA8E23',
+      textDecoration: 'var(--ifm-link-decoration)',
+    },
+  },
+});
+
+export {styles as landingPageStyles};
--- pyre2/website/src/pages/newLandingPage.js
+++ pyre2/website/src/pages/newLandingPage.js
@@ -13,7 +13,9 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import * as stylex from '@stylexjs/stylex';
+import {landingPageStyles} from './index';
+import cssStyles from './styles.module.css';
 
 // This import serves no runtime purposes, but we import it to force webpack to run babel on it,
 // so we can test whether babel can handle newer syntax.
@@ -29,27 +31,27 @@ export default component NewLandingPage() {
     <Layout
       title="Pyrefly: A Static Type Checker for Python"
       description={siteConfig.description}>
-      <header className={clsx(styles.feature, styles.featureHero)}>
+      <header {...stylex.props(landingPageStyles.featureHero)}>
         <div className="container">
-          <h1 className={styles.title}>
+          <h1 {...stylex.props(landingPageStyles.title)}>
             pyrefly<span>.</span>
           </h1>
-          <p className={clsx(styles.frontText, styles.subtitle)}>
+          <p {...stylex.props(landingPageStyles.subtitle)}>
             <span>
               {' '}
               <a
                 href="https://github.com/facebook/pyrefly/milestone/1"
-                className={styles.yellowLink}>
+                {...stylex.props(landingPageStyles.yellowLink)}>
                 Coming soon
               </a>
               : A faster Python type checker written in Rust
             </span>
           </p>
           <section>
-            <li className={styles.firefly}></li>
-            <li className={styles.firefly}></li>
-            <li className={styles.firefly}></li>
-            <li className={styles.firefly}></li>
+            <li className={cssStyles.firefly}></li>
+            <li className={cssStyles.firefly}></li>
+            <li className={cssStyles.firefly}></li>
+            <li className={cssStyles.firefly}></li>
           </section>
         </div>
       </header>
