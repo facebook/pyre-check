4a8ace5c35c2b14c9e515c1b99d914e0d8d19566
Jia Chen <grievejia@meta.com>
2025-03-25 09:00:18 -0700

    Add tests for `fs_upward_search::first_match()`
    Summary:
    motests
    
    These are the tests I promised to add in D71767103
    
    Reviewed By: stroxler
    
    Differential Revision: D71782832
    
    fbshipit-source-id: d0e8448cae57909bb139498e4ef72a836ae4aed2

--- pyre2/pyre2/lib/util/fs_upward_search.rs
+++ pyre2/pyre2/lib/util/fs_upward_search.rs
@@ -19,12 +19,11 @@ impl SearchResult {
     }
 }
 
-/// Search upward from the given directory `start_dir` for a file that matches any of the given `names`.
-/// Return the first match found, or None if no match is found.
-/// Elements in `names` are ordered: names that come earlier in the list are prioritized over names that come later.
-/// It's the caller's responsibility to ensure `start_dir` is meaningful. This function simply assumes it is, without
-/// checking e.g. whether `start_dir` is a directory or if it's an absolute path.
-pub fn first_match(start_dir: &Path, names: &[impl AsRef<Path>]) -> Option<SearchResult> {
+fn first_match_inner(
+    start_dir: &Path,
+    names: &[impl AsRef<Path>],
+    stop_after: Option<&Path>,
+) -> Option<SearchResult> {
     let mut current_dir = start_dir;
 
     loop {
@@ -38,10 +37,87 @@ pub fn first_match(start_dir: &Path, names: &[impl AsRef<Path>]) -> Option<Searc
             }
         }
 
+        if let Some(stop_after) = stop_after
+            && current_dir == stop_after
+        {
+            return None;
+        }
+
         match current_dir.parent() {
             Some(parent) => current_dir = parent,
             None => break,
         }
     }
     None
+}
+
+/// Search upward from the given directory `start_dir` for a file that matches any of the given `names`.
+/// Return the first match found, or None if no match is found.
+/// Elements in `names` are ordered: names that come earlier in the list are prioritized over names that come later.
+/// It's the caller's responsibility to ensure `start_dir` is meaningful. This function simply assumes it is, without
+/// checking e.g. whether `start_dir` is a directory or if it's an absolute path.
+pub fn first_match(start_dir: &Path, names: &[impl AsRef<Path>]) -> Option<SearchResult> {
+    first_match_inner(start_dir, names, None)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use tempfile::TempDir;
+
+    use super::*;
+
+    fn assert_first_match(files: &[&str], from: &str, targets: &[&str], expected: Option<&str>) {
+        let root = TempDir::new().unwrap();
+        for file in files {
+            let file_path = root.path().join(file);
+            if let Some(parent) = file_path.parent() {
+                fs::create_dir_all(parent).unwrap();
+            }
+            fs::write(&file_path, b"").unwrap();
+        }
+        let start_path = root.path().join(from);
+        let expected = expected.map(PathBuf::from);
+        let actual = first_match_inner(&start_path, targets, Some(root.path())).map(|result| {
+            result
+                .to_path_buf()
+                .strip_prefix(root.path())
+                .unwrap()
+                .to_path_buf()
+        });
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_first_match() {
+        assert_first_match(&[], "", &["a"], None);
+
+        assert_first_match(&["a"], "", &["a"], Some("a"));
+        assert_first_match(&["a"], "", &["b"], None);
+        assert_first_match(&["a", "b"], "", &["a", "b"], Some("a"));
+        assert_first_match(&["b"], "", &["a", "b"], Some("b"));
+
+        assert_first_match(&["a", "b/c"], "b", &["a"], Some("a"));
+        assert_first_match(&["a", "b/c"], "b", &["c"], Some("b/c"));
+        assert_first_match(&["a", "b/c"], "b/c", &["c"], Some("b/c"));
+        assert_first_match(&["a", "b/c"], "b", &["a", "c"], Some("b/c"));
+        assert_first_match(&["a", "b/c"], "b", &["d"], None);
+
+        assert_first_match(&["a/d", "a/b/d", "a/b/c/d"], "", &["d"], None);
+        assert_first_match(&["a/d", "a/b/d", "a/b/c/d"], "a", &["d"], Some("a/d"));
+        assert_first_match(&["a/d", "a/b/d", "a/b/c/d"], "a/b", &["d"], Some("a/b/d"));
+        assert_first_match(
+            &["a/d", "a/b/d", "a/b/c/d"],
+            "a/b/c",
+            &["d"],
+            Some("a/b/c/d"),
+        );
+
+        assert_first_match(&["a/d", "a/b/e", "a/b/c/f"], "", &["d"], None);
+        assert_first_match(&["a/d", "a/b/e", "a/b/c/f"], "a", &["d"], Some("a/d"));
+        assert_first_match(&["a/d", "a/b/e", "a/b/c/f"], "a/b", &["d"], Some("a/d"));
+        assert_first_match(&["a/d", "a/b/e", "a/b/c/f"], "a/b/c", &["d"], Some("a/d"));
+        assert_first_match(&["a/d", "a/b/e", "a/b/c/f"], "a/b/e", &["d"], Some("a/d"));
+    }
 }
