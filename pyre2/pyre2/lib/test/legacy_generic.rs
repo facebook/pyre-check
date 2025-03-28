6fc7511358b335130ddc3ce4ecf6042c910426e5
Neil Mitchell <ndmitchell@meta.com>
2025-03-28 07:30:24 -0700

    Add a failing test case
    Summary: From https://github.com/facebook/pyrefly/issues/37
    
    Reviewed By: yangdanny97
    
    Differential Revision: D72046141
    
    fbshipit-source-id: e18cfba9b29e2ebd3d8c8fc95909cd1458481d8d

--- pyre2/pyre2/lib/test/legacy_generic.rs
+++ pyre2/pyre2/lib/test/legacy_generic.rs
@@ -478,3 +478,17 @@ T: int = 0
 T = TypeVar('T')  # E: `type[TypeVar[T]]` is not assignable to variable `T` with type `int`
     "#,
 );
+
+testcase_with_bug!(
+    "Should use the bound to check it is safe",
+    test_typevar_bound,
+    r#"
+from typing import Callable, TypeVar
+
+T = TypeVar('T', bound=Callable[[int], int])
+
+def func(a: T, b: int) -> T:
+    print(a(b))  # E: Expected a callable
+    return a
+"#,
+);
