ea8c33405ac09e31beaaca288f09c1ca88e2315e
Maggie Moss <maggiemoss@meta.com>
2025-03-31 17:21:39 -0700

    Add media query for grid items (2/n)
    Summary: Prevents the grid items from being hidden on smaller screens
    
    Reviewed By: dluo
    
    Differential Revision: D72183352
    
    fbshipit-source-id: a2f317944c9dbb1a57350229d27a8a5e9440a5df

--- pyre2/website/src/components/landing-page/whyPyrefly.tsx
+++ pyre2/website/src/components/landing-page/whyPyrefly.tsx
@@ -12,49 +12,55 @@ import * as stylex from '@stylexjs/stylex';
 import WhyPyreflyGridItem from './whyPyreflyGridItem';
 
 export default function WhyPyrefly(): React.ReactElement {
-    return (
-        <div {...stylex.props(styles.whyPyreflyGrid)}>
-            {/* Short-term benefits */}
-            <WhyPyreflyGridItem
-                title="Scale with Confidence"
-                content="Lightning fast, Pyrefly can process 1.8 million lines of code in a second!"
-            />
-            <WhyPyreflyGridItem
-                title="Developer Delight"
-                content=" Write better code, faster, and with confidence using your favorite
+  return (
+    <div {...stylex.props(styles.whyPyreflyGrid)}>
+      {/* Short-term benefits */}
+      <WhyPyreflyGridItem
+        title="Scale with Confidence"
+        content="Lightning fast, Pyrefly can process 1.8 million lines of code in a second!"
+      />
+      <WhyPyreflyGridItem
+        title="Developer Delight"
+        content=" Write better code, faster, and with confidence using your favorite
           editor supported by the Pyrefly IDE."
-            />
-            <WhyPyreflyGridItem
-                title="Easy Onboarding"
-                content=" Seamless transition from existing typechecker configurations to
+      />
+      <WhyPyreflyGridItem
+        title="Easy Onboarding"
+        content=" Seamless transition from existing typechecker configurations to
           Pyrefly to get started!"
-            />
+      />
 
-            {/* Long-term benefits */}
-            <WhyPyreflyGridItem
-                title="Codebase clarity"
-                content="Improve code readability and maintainability with precise type
+      {/* Long-term benefits */}
+      <WhyPyreflyGridItem
+        title="Codebase clarity"
+        content="Improve code readability and maintainability with precise type
           annotations."
-            />
-            <WhyPyreflyGridItem
-                title="Tooling for Improving Types"
-                content="Use the tools packaged with Pyrefly to elevate your typing coverage
+      />
+      <WhyPyreflyGridItem
+        title="Tooling for Improving Types"
+        content="Use the tools packaged with Pyrefly to elevate your typing coverage
           from 0 to 100."
-            />
-            <WhyPyreflyGridItem
-                title="Fully featured"
-                content="Follows the typing standards introduced in PEPs 484, 526, 612, along
+      />
+      <WhyPyreflyGridItem
+        title="Fully featured"
+        content="Follows the typing standards introduced in PEPs 484, 526, 612, along
           with being a typechecker that is being actively developed on and
           constantly improving."
-            />
-        </div>
-    );
+      />
+    </div>
+  );
 }
 
 const styles = stylex.create({
-    whyPyreflyGrid: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        gap: '2rem',
+  whyPyreflyGrid: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gap: '2rem',
+    '@media (max-width: 800px)': {
+      gridTemplateColumns: 'repeat(2, 1fr)',
     },
+    '@media (max-width: 550px)': {
+      gridTemplateColumns: 'repeat(1, 1fr)',
+    },
+  },
 });
