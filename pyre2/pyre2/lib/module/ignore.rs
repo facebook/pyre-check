5fdc32631aec1db171b3e2f068e75c639179b8b7
Maggie Moss <maggiemoss@meta.com>
2025-04-02 09:31:43 -0700

    Add basic support for tracking the kind of suppressions
    Summary:
    Goal:
    To track different kinds of ignores so can eventually write a script that only removes pyrefly or pyre specific error suppressions.
    
    Reviewed By: stroxler
    
    Differential Revision: D72193094
    
    fbshipit-source-id: f9919e050d31ed9db28ee48d3dcdb537c7156759

--- pyre2/pyre2/lib/module/ignore.rs
+++ pyre2/pyre2/lib/module/ignore.rs
@@ -5,36 +5,48 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use dupe::Dupe;
 use ruff_source_file::OneIndexed;
-use starlark_map::small_set::SmallSet;
+use starlark_map::small_map::SmallMap;
 
 use crate::module::module_info::SourceRange;
+
+#[derive(PartialEq, Debug, Clone, Hash, Eq, Dupe, Copy)]
+enum SuppressionKind {
+    Ignore,
+    Pyre,
+    Pyrefly,
+}
 
 /// Record the position of `# type: ignore[valid-type]` statements.
 /// For now we don't record the content of the ignore, but we could.
 #[derive(Debug, Clone, Default)]
 pub struct Ignore {
-    ignores: SmallSet<OneIndexed>,
+    ignores: SmallMap<OneIndexed, Vec<SuppressionKind>>,
 }
 
 impl Ignore {
     pub fn new(code: &str) -> Self {
-        let mut ignores = SmallSet::new();
+        let mut ignores = SmallMap::new();
         for (line, line_str) in code.lines().enumerate() {
-            if Self::is_ignore_directive(line_str) {
-                ignores.insert(OneIndexed::from_zero_indexed(line));
+            if let Some(kind) = Self::get_suppression_kind(line_str) {
+                ignores.insert(OneIndexed::from_zero_indexed(line), [kind].to_vec());
             }
         }
         Self { ignores }
     }
 
-    fn is_ignore_directive(line: &str) -> bool {
-        line.split("# ").skip(1).any(|part| {
-            part.starts_with("type: ignore")
-                || part.starts_with("pyrefly: ignore")
-                || part.starts_with("pyre-ignore")
-                || part.starts_with("pyre-fixme")
-        })
+    fn get_suppression_kind(line: &str) -> Option<SuppressionKind> {
+        for l in line.split("# ").skip(1) {
+            if l.starts_with("type: ignore") {
+                return Some(SuppressionKind::Ignore);
+            } else if l.starts_with("pyrefly: ignore") {
+                return Some(SuppressionKind::Pyrefly);
+            } else if l.starts_with("pyre-ignore") || l.starts_with("pyre-fixme") {
+                return Some(SuppressionKind::Pyre);
+            }
+        }
+        None
     }
 
     pub fn is_ignored(&self, range: &SourceRange, msg: &str) -> bool {
@@ -43,7 +55,7 @@ impl Ignore {
         // We allow an ignore the line before the range, or on any line within the range.
         // We convert to/from zero-indexed because OneIndexed does not implement Step.
         (range.start.row.to_zero_indexed().saturating_sub(1)..=range.end.row.to_zero_indexed())
-            .any(|x| self.ignores.contains(&OneIndexed::from_zero_indexed(x)))
+            .any(|x| self.ignores.contains_key(&OneIndexed::from_zero_indexed(x)))
     }
 }
 
@@ -52,15 +64,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_is_ignore_directive() {
-        assert!(Ignore::is_ignore_directive(
-            "stuff # type: ignore # and then stuff"
-        ));
-        assert!(Ignore::is_ignore_directive(
-            "more # stuff # type: ignore[valid-type]"
-        ));
-        assert!(!Ignore::is_ignore_directive("# ignore: pyrefly"));
-        assert!(!Ignore::is_ignore_directive(" pyrefly: ignore"));
-        assert!(!Ignore::is_ignore_directive("normal line"));
+    fn test_get_suppression_kind() {
+        assert!(Ignore::get_suppression_kind("stuff # type: ignore # and then stuff").is_some());
+        assert!(Ignore::get_suppression_kind("more # stuff # type: ignore[valid-type]").is_some());
+        assert!(Ignore::get_suppression_kind("# ignore: pyrefly").is_none());
+        assert!(Ignore::get_suppression_kind(" pyrefly: ignore").is_none());
+        assert!(Ignore::get_suppression_kind("normal line").is_none());
     }
 }
