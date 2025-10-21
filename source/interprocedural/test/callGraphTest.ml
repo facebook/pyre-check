03806e9d6e6b7f691e5310aca027b5f8bf646175
Tianhan Lu <tianhanlu@meta.com>
2025-10-21 08:54:43 -0700

    Handle `SelfType` in `receiver_class_from_type`
    Reviewed By: arthaud
    
    Differential Revision: D85085937
    
    fbshipit-source-id: 908a92ec563f0ad257b2f12dedf986d174a58ead

--- source/interprocedural/test/callGraphTest.ml
+++ source/interprocedural/test/callGraphTest.ml
@@ -981,6 +981,7 @@ let test_call_graph_of_define =
            ();
       labeled_test_case __FUNCTION__ __LINE__
       @@ assert_call_graph_of_define
+           ~_is_migrated:true
            ~source:
              {|
       from abc import abstractmethod
