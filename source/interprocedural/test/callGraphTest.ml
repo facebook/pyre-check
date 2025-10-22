cbb565446d0912dceef819f3c2872aabd8e6c7af
Tianhan Lu <tianhanlu@meta.com>
2025-10-22 11:37:52 -0700

    Handle calls to `repr`
    Summary:
    **Alternative approach.** I thought about using `as_call_target` in `call.rs`
    but that does not seem promosing, because it needs a callee type, which in this
    case points to `builtins.repr`, which we want to avoid when `__repr__` is
    overriden.
    
    Reviewed By: arthaud
    
    Differential Revision: D85088342
    
    fbshipit-source-id: f337a8351da2431d8e2954e2c24eeeb9f64e4260

--- source/interprocedural/test/callGraphTest.ml
+++ source/interprocedural/test/callGraphTest.ml
@@ -1040,7 +1040,7 @@ let test_call_graph_of_define =
            ();
       labeled_test_case __FUNCTION__ __LINE__
       @@ assert_call_graph_of_define
-           ~_is_migrated:false
+           ~_is_migrated:true
            ~source:
              {|
       class C:
