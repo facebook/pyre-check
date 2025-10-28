bf85b40ab9acb31449355a92b159140275dd17d3
Tianhan Lu <tianhanlu@meta.com>
2025-10-28 07:59:23 -0700

    Migrate more call graph unit tests
    Summary: This is already correctly handled.
    
    Reviewed By: arthaud
    
    Differential Revision: D85591464
    
    fbshipit-source-id: 7d5bdd66703fe732e7d2594dc0c8b07bfde2ae7d

--- source/interprocedural/test/callGraphTest.ml
+++ source/interprocedural/test/callGraphTest.ml
@@ -1715,7 +1715,7 @@ let test_call_graph_of_define =
            ();
       labeled_test_case __FUNCTION__ __LINE__
       @@ assert_call_graph_of_define
-           ~_migrated_to_pyrefly:false
+           ~_migrated_to_pyrefly:true
            ~source:
              {|
         class Builder:
@@ -2111,7 +2111,7 @@ let test_call_graph_of_define =
            ();
       labeled_test_case __FUNCTION__ __LINE__
       @@ assert_call_graph_of_define
-           ~_migrated_to_pyrefly:false
+           ~_migrated_to_pyrefly:true
            ~source:
              {|
         from typing import Protocol
