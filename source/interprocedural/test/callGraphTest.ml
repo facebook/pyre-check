515a63e0a198f0ee73c6d78fbc8fdfb8ba932217
Tianhan Lu <tianhanlu@meta.com>
2025-10-29 08:35:50 -0700

    Fix infinite recursion in `as_call_target_impl` in `call.rs`
    Summary:
    The newly added test case (which is migrated from Pysa) demonstrates the
    infinite recursion.
    
    Reviewed By: arthaud
    
    Differential Revision: D85595619
    
    fbshipit-source-id: 429e7af72e6dad598728b02c2de709aee72481ca

--- source/interprocedural/test/callGraphTest.ml
+++ source/interprocedural/test/callGraphTest.ml
@@ -2004,7 +2004,7 @@ let test_call_graph_of_define =
       (* Ensure we don't infinite loop when resolving callable classes. *)
       labeled_test_case __FUNCTION__ __LINE__
       @@ assert_call_graph_of_define
-           ~_migrated_to_pyrefly:false
+           ~_migrated_to_pyrefly:true
            ~source:
              {|
     from typing import Any, Target
