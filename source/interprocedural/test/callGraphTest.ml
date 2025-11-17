a0b7058cbc8965873510ea2bfda80936c781df37
Tianhan Lu <tianhanlu@meta.com>
2025-11-17 11:15:18 -0800

    Register callees for `ExprListComp`
    Reviewed By: arthaud
    
    Differential Revision: D87096524
    
    fbshipit-source-id: 8684c4d43e3d80b4fb3d307e62b533118a5c22d1

--- source/interprocedural/test/callGraphTest.ml
+++ source/interprocedural/test/callGraphTest.ml
@@ -2102,7 +2102,7 @@ let test_call_graph_of_define =
            ();
       labeled_test_case __FUNCTION__ __LINE__
       @@ assert_call_graph_of_define
-           ~_migrated_to_pyrefly:false
+           ~_migrated_to_pyrefly:true
            ~source:
              {|
     class C:
